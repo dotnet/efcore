220c7fe5f15bf999193a0cafe7c27b93dba49af8
AndrewPeters <anpete@microsoft.com>
2014-01-23 10:56:15 -0800

    Added inital solution structure, early metadata support, and utilities.

---
+++ src/Microsoft.Data.Core/Metadata/Property.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace Microsoft.Data.Core.Metadata
+{
+    using System.Reflection;
+    using JetBrains.Annotations;
+    using Microsoft.Data.Core.Utilities;
+
+    public class Property : MetadataBase
+    {
+        private readonly PropertyInfo _propertyInfo;
+
+        public Property([NotNull] PropertyInfo propertyInfo)
+            : base(Check.NotNull(propertyInfo, "propertyInfo", p => p.Name))
+        {
+            _propertyInfo = propertyInfo;
+        }
+
+        public virtual PropertyInfo PropertyInfo
+        {
+            get { return _propertyInfo; }
+        }
+    }
+}
---
+++ test/Microsoft.Data.Core.UnitTest/Metadata/AnnotationFacts.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace Microsoft.Data.Core.Metadata
+{
+    using Xunit;
+
+    public class AnnotationFacts
+    {
+        [Fact]
+        public void Can_create_annotation()
+        {
+            var annotation = new Annotation("Foo", "Bar");
+
+            Assert.Equal("Foo", annotation.Name);
+            Assert.Equal("Bar", annotation.Value);
+        }
+    }
+}
