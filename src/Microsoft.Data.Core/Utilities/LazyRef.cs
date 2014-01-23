9ae8ec3ecd5762ce3445edf8b7ac6b04a2168191
AndrewPeters <anpete@microsoft.com>
2014-01-23 10:56:15 -0800

    Added inital solution structure, early metadata support, and utilities.

---
+++ src/Microsoft.Data.Core/Utilities/LazyRef.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace Microsoft.Data.Core.Utilities
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using JetBrains.Annotations;
+
+    [DebuggerStepThrough]
+    public sealed class LazyRef<T>
+        where T : class
+    {
+        private Func<T> _initializer;
+        private object _syncLock;
+
+        private T _value;
+
+        internal LazyRef()
+        {
+        }
+
+        public LazyRef([NotNull] Func<T> initializer)
+        {
+            Check.NotNull(initializer, "initializer");
+
+            _initializer = initializer;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (_value == null)
+                {
+                    var syncLock = new object();
+
+                    syncLock
+                        = Interlocked.CompareExchange(ref _syncLock, syncLock, null)
+                          ?? syncLock;
+
+                    lock (syncLock)
+                    {
+                        if (_value == null)
+                        {
+                            _value = _initializer();
+
+                            _syncLock = null;
+                            _initializer = null;
+                        }
+                    }
+                }
+
+                return _value;
+            }
+        }
+
+        public void ExchangeValue([NotNull] Func<T, T> newValueCreator)
+        {
+            Check.NotNull(newValueCreator, "newValueCreator");
+
+            T originalValue, newValue;
+
+            do
+            {
+                originalValue = Value;
+                newValue = newValueCreator(originalValue);
+
+                if (ReferenceEquals(newValue, originalValue))
+                {
+                    return;
+                }
+            }
+            while (Interlocked.CompareExchange(ref _value, newValue, originalValue) != originalValue);
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+    }
+}
