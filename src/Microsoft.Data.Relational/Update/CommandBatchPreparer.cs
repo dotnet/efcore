38221462b7050753d69732cbfb333e1cd1571979
moozzyk <pawelka@microsoft.com>
2014-03-14 14:05:04 -0700

    Changing ModificationCommand to use StateEntries

--- src/Microsoft.Data.Relational/Update/CommandBatchPreparer.cs
+++ src/Microsoft.Data.Relational/Update/CommandBatchPreparer.cs
@@ -1,11 +1,8 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.ChangeTracking;
-using Microsoft.Data.Entity.Metadata;
 
 namespace Microsoft.Data.Relational.Update
 {
@@ -13,47 +10,9 @@ namespace Microsoft.Data.Relational.Update
     {
         public IEnumerable<ModificationCommandBatch> BatchCommands(IEnumerable<StateEntry> stateEntries)
         {
-            foreach (var stateEntry in stateEntries)
-            {
-                var tableName = stateEntry.EntityType.StorageName;
-
-                switch (stateEntry.EntityState)
-                {
-                    case EntityState.Added:
-                        yield return new ModificationCommandBatch(
-                            new[] { new ModificationCommand(tableName, GetColumnValues(stateEntry, true), null) });
-                        break;
-                    case EntityState.Modified:
-                        yield return new ModificationCommandBatch(
-                            new[] { new ModificationCommand(tableName, GetColumnValues(stateEntry, false), GetWhereClauses(stateEntry)) });
-                        break;
-                    case EntityState.Deleted:
-                        yield return new ModificationCommandBatch(
-                            new[] { new ModificationCommand(tableName, null, GetWhereClauses(stateEntry)) });
-                        break;
-                }
-            }
-        }
-
-        private static IEnumerable<KeyValuePair<string, Object>> GetColumnValues(StateEntry stateEntry, bool includeKeys)
-        {
-            var entityType = stateEntry.EntityType;
-
-            return entityType
-                .Properties
-                .Where(p =>
-                    p.ValueGenerationStrategy != ValueGenerationStrategy.StoreComputed &&
-                    p.ValueGenerationStrategy != ValueGenerationStrategy.StoreIdentity &&
-                    (includeKeys || !entityType.GetKey().Properties.Contains(p)))
-                .Select(p => new KeyValuePair<string, object>(p.StorageName, stateEntry.GetPropertyValue(p)));
-        }
-
-        private static IEnumerable<KeyValuePair<string, object>> GetWhereClauses(StateEntry stateEntry)
-        {
             return
-                stateEntry
-                    .EntityType.GetKey().Properties
-                        .Select(k => new KeyValuePair<string, object>(k.Name, stateEntry.GetPropertyValue(k)));
+                stateEntries.Select(
+                    e => new ModificationCommandBatch(new[] { new ModificationCommand(e) }));
         }
     }
 }
--- src/Microsoft.Data.Relational/Update/ModificationCommand.cs
+++ src/Microsoft.Data.Relational/Update/ModificationCommand.cs
@@ -1,53 +1,101 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.ChangeTracking;
+using Microsoft.Data.Entity.Metadata;
 
 namespace Microsoft.Data.Relational.Update
 {
     internal class ModificationCommand
     {
-        private readonly string _tableName;
+        private readonly StateEntry _stateEntry;
         private readonly KeyValuePair<string, object>[] _columnValues;
         private readonly KeyValuePair<string, object>[] _whereClauses;
+        private readonly ModificationOperation _operation;
 
-        public ModificationCommand(string tableName,
-            IEnumerable<KeyValuePair<string, object>> columnValues, IEnumerable<KeyValuePair<string, object>> whereClauses)
+        /// <summary>
+        ///     This constructor is intended only for use when creating test doubles that will override members
+        ///     with mocked or faked behavior. Use of this constructor for other purposes may result in unexpected
+        ///     behavior including but not limited to throwing <see cref="NullReferenceException" />.
+        /// </summary>
+        protected ModificationCommand()
         {
-            Contract.Assert(columnValues != null || whereClauses != null, "both columnValues and whereClauses are null");
-
-            _tableName = tableName;
-            _columnValues = columnValues != null ? columnValues.ToArray() : null;
-            _whereClauses = whereClauses != null ? whereClauses.ToArray() : null;
         }
 
-        public ModificationOperation Operation
+        public ModificationCommand([NotNull] StateEntry stateEntry)
         {
-            get
-            {
-                return
-                    _columnValues != null && _whereClauses != null
+            Contract.Assert(
+                stateEntry.EntityState == EntityState.Added || stateEntry.EntityState == EntityState.Modified || 
+                stateEntry.EntityState == EntityState.Deleted, "Unexpected entity state");
+
+            _stateEntry = stateEntry;
+            _operation =
+                stateEntry.EntityState == EntityState.Added
+                    ? ModificationOperation.Insert
+                    : stateEntry.EntityState == EntityState.Modified
                         ? ModificationOperation.Update
-                        : _columnValues != null
-                            ? ModificationOperation.Insert
-                            : ModificationOperation.Delete;
+                        : ModificationOperation.Delete;
+
+            if (_operation == ModificationOperation.Insert)
+            {
+                _columnValues = GetColumnValues(stateEntry, true).ToArray();
+            }
+            else
+            {
+                if (_operation == ModificationOperation.Update)
+                {
+                    _columnValues = GetColumnValues(stateEntry, false).ToArray();
+                }
+
+                _whereClauses = GetWhereClauses(stateEntry).ToArray();
             }
         }
 
-        public string TableName
+        public virtual ModificationOperation Operation
         {
-            get { return _tableName; }
+            get { return _operation; }
         }
 
-        public KeyValuePair<string, object>[] ColumnValues
+        public virtual string TableName
+        {
+            get { return _stateEntry.EntityType.StorageName; }
+        }
+
+        public virtual KeyValuePair<string, object>[] ColumnValues
         {
             get { return _columnValues; }
         }
 
-        public KeyValuePair<string, object>[] WhereClauses
+        public virtual KeyValuePair<string, object>[] WhereClauses
         {
             get { return _whereClauses; }
+        }
+
+        private static IEnumerable<KeyValuePair<string, object>> GetColumnValues(StateEntry stateEntry, bool includeKeys)
+        {
+            var entityType = stateEntry.EntityType;
+
+            return entityType
+                .Properties
+                .Where(p =>
+                    p.ValueGenerationStrategy != ValueGenerationStrategy.StoreComputed &&
+                    p.ValueGenerationStrategy != ValueGenerationStrategy.StoreIdentity &&
+                    (includeKeys || !entityType.GetKey().Properties.Contains(p)))
+                .Select(p => new KeyValuePair<string, object>(p.StorageName, stateEntry.GetPropertyValue(p)));
+        }
+
+        private static IEnumerable<KeyValuePair<string, object>> GetWhereClauses(StateEntry stateEntry)
+        {
+            return
+                stateEntry
+                    .EntityType
+                    .GetKey().Properties
+                    .Select(k => new KeyValuePair<string, object>(k.Name, stateEntry.GetPropertyValue(k)));
         }
     }
 }
