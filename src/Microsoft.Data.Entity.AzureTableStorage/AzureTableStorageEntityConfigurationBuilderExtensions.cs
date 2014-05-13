07857ef058eb1a8d0957f6c3d6ff862b7c89f400
Nate McMaster <nate.mcmaster@outlook.com>
2014-05-12 17:47:04 -0700

    Add csproj to sln and add skeleton code rowanmiller

---
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageEntityConfigurationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.AzureTableStorage;
+
+namespace Microsoft.Data.Entity
+{
+    public static class AzureTableStorageEntityConfigurationBuilderExtensions
+    {
+        public static DbContextOptions UseAzureTableStorge(this DbContextOptions builder, string connectionString)
+        {
+            builder.AddBuildAction(c => c.AddOrUpdateExtension<AzureTableStorageConfigurationExtension>(
+                e => e.ConnectionString = connectionString));
+
+            return builder;
+        }
+    }
+}
