e8af64abbae8f5fd028cef706a79e0dd69f38449
Nate McMaster <nate.mcmaster@outlook.com>
2014-05-12 17:47:04 -0700

    Add csproj to sln and add skeleton code rowanmiller

---
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageConnection.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Storage;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Linq;
+
+namespace Microsoft.Data.Entity.AzureTableStorage
+{
+    public class AzureTableStorageConnection : DataStoreConnection
+    {
+        private readonly string _connectionString;
+        private readonly CloudStorageAccount _account;
+
+        public AzureTableStorageConnection(DbContextConfiguration configuration)
+        {
+            var storeConfig = configuration
+                .ContextOptions
+                .Extensions
+                .OfType<AzureTableStorageConfigurationExtension>()
+                .Single();
+
+            _connectionString = storeConfig.ConnectionString;
+
+            _account = CloudStorageAccount.Parse(_connectionString);
+        }
+
+        public CloudStorageAccount Account
+        {
+            get { return _account; }
+        }
+
+        public CloudTable GetTableReference(string tableName)
+        {
+            return _account.CreateCloudTableClient().GetTableReference(tableName);
+        }
+    }
+}
---
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageDataStoreCreator.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Data.Entity.AzureTableStorage
+{
+    class AzureTableStorageDataStoreCreator : DataStoreCreator
+    {
+        private readonly AzureTableStorageConnection _connection;
+
+        public AzureTableStorageDataStoreCreator(AzureTableStorageConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public override void Create(Microsoft.Data.Entity.Metadata.IModel model)
+        {
+            foreach (var type in model.EntityTypes)
+            {
+                var table = _connection.GetTableReference(type.StorageName);
+                table.CreateIfNotExists();
+            }
+        }
+
+        public override async Task CreateAsync(Microsoft.Data.Entity.Metadata.IModel model, System.Threading.CancellationToken cancellationToken)
+        {
+            foreach (var type in model.EntityTypes)
+            {
+                var table = _connection.GetTableReference(type.StorageName);
+                await table.CreateIfNotExistsAsync();
+            }
+        }
+
+        public override void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task DeleteAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Exists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> ExistsAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
