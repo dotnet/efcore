28a67ddfea843c0c67770ac16da042e28d3e07a9
Nate McMaster <nate.mcmaster@outlook.com>
2014-05-12 18:19:29 -0700

    Beginnings of a provider for Azure Table Storage.
    Add null checks on public interfaces.
    Batching is now configured per context.
    Wrap Azure SDK in more testable interfaces and wrappers. Fix async issues on fetching
    Update datastore to match new API.
    Minor tweaks. Change batching setup to use configuration.
    Whitespace fixes (R# code cleanup)
    Implement HasTables. Add strings resource and check utilities. Wrap the CloudtTableClient in a testable interface
    Add TODOs and more whitespace corrections
    Culture invariant dates and other minor tweaks.
    Add functional and unit testing to ATS requests. Enhance error handling.

--- src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageDataStoreCreator.cs
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageDataStoreCreator.cs
@@ -1,56 +1,91 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Data.Entity.Storage;
 using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.AzureTableStorage.Utilities;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Storage;
 
 namespace Microsoft.Data.Entity.AzureTableStorage
 {
-    class AzureTableStorageDataStoreCreator : DataStoreCreator
+    public class AzureTableStorageDataStoreCreator : DataStoreCreator
     {
         private readonly AzureTableStorageConnection _connection;
 
-        public AzureTableStorageDataStoreCreator(AzureTableStorageConnection connection)
+        public AzureTableStorageDataStoreCreator([NotNull] AzureTableStorageConnection connection)
         {
+            Check.NotNull(connection, "connection");
             _connection = connection;
         }
 
-        public override void Create(Microsoft.Data.Entity.Metadata.IModel model)
+        public override void CreateTables([NotNull] IModel model)
         {
+            Check.NotNull(model, "model");
             foreach (var type in model.EntityTypes)
             {
                 var table = _connection.GetTableReference(type.StorageName);
-                table.CreateIfNotExists();
+                if (table != null)
+                {
+                    table.CreateIfNotExists();
+                }
             }
         }
 
-        public override async Task CreateAsync(Microsoft.Data.Entity.Metadata.IModel model, System.Threading.CancellationToken cancellationToken)
+        public override async Task CreateTablesAsync([NotNull] IModel model, CancellationToken cancellationToken = default(CancellationToken))
         {
             foreach (var type in model.EntityTypes)
             {
                 var table = _connection.GetTableReference(type.StorageName);
-                await table.CreateIfNotExistsAsync();
+                if (table != null)
+                {
+                    await table.CreateIfNotExistsAsync(cancellationToken);
+                }
             }
+        }
+
+        public override bool HasTables()
+        {
+            return _connection.Account.CreateCloudTableClient().ListTables().Any();
+        }
+
+        public override Task<bool> HasTablesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return Task.Factory.StartNew(()=>HasTables(), cancellationToken);
+        }
+
+        public override void Create()
+        {
+            throw new AzureAccountException(Strings.CannotModifyAccount);
+        }
+
+        public override Task CreateAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new AzureAccountException(Strings.CannotModifyAccount);
         }
 
         public override void Delete()
         {
-            throw new NotImplementedException();
+            throw new AzureAccountException(Strings.CannotModifyAccount);
         }
 
-        public override Task DeleteAsync(System.Threading.CancellationToken cancellationToken)
+        public override Task DeleteAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            throw new AzureAccountException(Strings.CannotModifyAccount);
         }
 
         public override bool Exists()
         {
+            //TODO implement check if account credentials are correct && server is available
             throw new NotImplementedException();
         }
 
-        public override Task<bool> ExistsAsync(System.Threading.CancellationToken cancellationToken)
+        public override Task<bool> ExistsAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
+            // TODO implement as async
             throw new NotImplementedException();
         }
     }
