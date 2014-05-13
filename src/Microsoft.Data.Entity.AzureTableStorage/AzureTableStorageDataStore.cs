a3a39320e2aa4c57fd10221f4a26b90d0e0daffc
Nate McMaster <nate.mcmaster@outlook.com>
2014-05-12 17:47:04 -0700

    Add csproj to sln and add skeleton code rowanmiller

---
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageDataStore.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.ChangeTracking;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Remotion.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Data.Entity.AzureTableStorage
+{
+    public class AzureStorageDataStore : DataStore
+    {
+        private readonly AzureTableStorageConnection _connection;
+
+        public AzureStorageDataStore(DbContextConfiguration configuration, AzureTableStorageConnection connection)
+            : base(configuration)
+        {
+            _connection = connection;
+        }
+
+        public override IEnumerable<TResult> Query<TResult>(QueryModel queryModel, StateManager stateManager)
+        {
+            var queryExecutor = new AzureTableStorageQueryModelVisitor().CreateQueryExecutor<TResult>(queryModel);
+            var queryContext = new AzureTableStorageQueryContext(Model, Logger, stateManager, _connection);
+
+            return queryExecutor(queryContext);
+        }
+
+        public override IAsyncEnumerable<TResult> AsyncQuery<TResult>(QueryModel queryModel, StateManager stateManager)
+        {
+            // TODO This should happen properly async
+            return Query<TResult>(queryModel, stateManager).ToAsyncEnumerable();
+        }
+
+        public override Task<int> SaveChangesAsync(IEnumerable<StateEntry> stateEntries, CancellationToken cancellationToken)
+        {
+            var typeGroups = stateEntries.GroupBy(e => e.EntityType);
+
+            foreach (var typeGroup in typeGroups)
+            {
+                var table = _connection.GetTableReference(typeGroup.Key.StorageName);
+                // TODO Use batches - was hanging for some reason
+                var batch = new TableBatchOperation();
+
+                // TODO Break up into batches of 100 (tried batching but it just seemed to hang)
+                foreach (var entry in typeGroup)
+                {
+                    switch (entry.EntityState)
+                    {
+                        case EntityState.Added:
+                            table.Execute(TableOperation.Insert((ITableEntity)entry.Entity));
+                            break;
+
+                        case EntityState.Deleted:
+                            table.Execute(TableOperation.Delete((ITableEntity)entry.Entity));
+                            break;
+
+                        case EntityState.Modified:
+                            table.Execute(TableOperation.Replace((ITableEntity)entry.Entity));
+                            break;
+
+                        default:
+                            break;
+                    }   
+                }
+            }
+
+            // TODO Return affected rows 
+            // TODO Maybe need to check result of operations? (not sure if you can fail without an exception)
+            return Task.FromResult(stateEntries.Count());
+        }
+    }
+}
---
+++ src/Microsoft.Data.Entity.AzureTableStorage/AzureTableStorageQueryContext.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.ChangeTracking;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Query;
+using Microsoft.Framework.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Data.Entity.AzureTableStorage
+{
+    public class AzureTableStorageQueryContext : QueryContext
+    {
+        private readonly AzureTableStorageConnection _database;
+
+        public AzureTableStorageQueryContext(
+            IModel model,
+            ILogger logger,
+           StateManager stateManager,
+            AzureTableStorageConnection database)
+            : base(model, logger, stateManager)
+        {
+            _database = database;
+        }
+
+        public virtual AzureTableStorageConnection Database
+        {
+            get { return _database; }
+        }
+    }
+
+}
