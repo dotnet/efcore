e3dc6d070f12bf33607c7612cca8a6b7ead03dd8
Emil Cicos <emil.cico@hotmail.com>
2014-05-15 22:12:24 -0700

    Added APIs to ModelBuilder to be able to construct a model without requiring CLR types. Updated the CSharpModelCodeGenerator to generated code that uses the new APIs. Added and updated unit tests.

--- test/Microsoft.Data.Entity.Tests/Metadata/ModelBuilderTest.cs
+++ test/Microsoft.Data.Entity.Tests/Metadata/ModelBuilderTest.cs
@@ -43,12 +43,75 @@ namespace Microsoft.Data.Entity.Tests.Metadata
         }
 
         [Fact]
-        public void Can_set_entity_key_from_clr_properties()
+        public void Can_get_entity_builder_for_entity_type_name()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            var entityBuilder = modelBuilder.Entity("Customer");
+
+            Assert.NotNull(entityBuilder);
+            Assert.NotNull(model.TryGetEntityType("Customer"));
+        }
+
+        [Fact]
+        public void Can_set_entity_key_from_clr_property()
         {
             var model = new Model();
             var modelBuilder = new ModelBuilder(model);
 
             modelBuilder.Entity<Customer>().Key(e => e.Id);
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(1, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+        }
+
+        [Fact]
+        public void Can_set_entity_key_from_property_name()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(ps => ps.Property(e => e.Id))
+                .Key("Id");
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(1, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+        }
+
+        [Fact]
+        public void Can_set_entity_key_from_property_name_when_no_clr_property()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(ps => ps.Property<int>("Id"))
+                .Key("Id");
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(1, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+        }
+
+        [Fact]
+        public void Can_set_entity_key_from_property_name_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(ps => ps.Property<int>("Id"))
+                .Key("Id");
 
             var entity = model.GetEntityType(typeof(Customer));
 
@@ -74,6 +137,75 @@ namespace Microsoft.Data.Entity.Tests.Metadata
         }
 
         [Fact]
+        public void Can_set_composite_entity_key_from_property_names()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(
+                    ps =>
+                        {
+                            ps.Property(e => e.Id);
+                            ps.Property(e => e.Name);
+                        })
+                .Key("Id", "Name");
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(2, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+            Assert.Equal("Name", entity.GetKey().Properties.Last().Name);
+        }
+
+        [Fact]
+        public void Can_set_composite_entity_key_from_property_names_when_mixed_properties()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(
+                    ps =>
+                    {
+                        ps.Property(e => e.Id);
+                        ps.Property<string>("Name");
+                    })
+                .Key("Id", "Name");
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(2, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+            Assert.Equal("Name", entity.GetKey().Properties.Last().Name);
+        }
+
+        [Fact]
+        public void Can_set_composite_entity_key_from_property_names_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(
+                    ps =>
+                    {
+                        ps.Property<int>("Id");
+                        ps.Property<string>("Name");
+                    })
+                .Key("Id", "Name");
+
+            var entity = model.GetEntityType(typeof(Customer));
+
+            Assert.Equal(2, entity.GetKey().Properties.Count());
+            Assert.Equal("Id", entity.GetKey().Properties.First().Name);
+            Assert.Equal("Name", entity.GetKey().Properties.Last().Name);
+        }
+
+        [Fact]
         public void Can_set_entity_annotation()
         {
             var model = new Model();
@@ -81,6 +213,19 @@ namespace Microsoft.Data.Entity.Tests.Metadata
 
             modelBuilder
                 .Entity<Customer>()
+                .Annotation("foo", "bar");
+
+            Assert.Equal("bar", model.GetEntityType(typeof(Customer))["foo"]);
+        }
+
+        [Fact]
+        public void Can_set_entity_annotation_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
                 .Annotation("foo", "bar");
 
             Assert.Equal("bar", model.GetEntityType(typeof(Customer))["foo"]);
@@ -100,6 +245,19 @@ namespace Microsoft.Data.Entity.Tests.Metadata
         }
 
         [Fact]
+        public void Can_set_entity_storage_name_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .StorageName("foo");
+
+            Assert.Equal("foo", model.GetEntityType(typeof(Customer)).StorageName);
+        }
+
+        [Fact]
         public void Can_set_property_annotation()
         {
             var model = new Model();
@@ -113,6 +271,32 @@ namespace Microsoft.Data.Entity.Tests.Metadata
         }
 
         [Fact]
+        public void Can_set_property_annotation_when_no_clr_property()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(ps => ps.Property<string>("Name").Annotation("foo", "bar"));
+
+            Assert.Equal("bar", model.GetEntityType(typeof(Customer)).GetProperty("Name")["foo"]);
+        }
+
+        [Fact]
+        public void Can_set_property_annotation_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(ps => ps.Property<string>("Name").Annotation("foo", "bar"));
+
+            Assert.Equal("bar", model.GetEntityType(typeof(Customer)).GetProperty("Name")["foo"]);
+        }
+
+        [Fact]
         public void Can_set_property_storage_name()
         {
             var model = new Model();
@@ -121,6 +305,32 @@ namespace Microsoft.Data.Entity.Tests.Metadata
             modelBuilder
                 .Entity<Customer>()
                 .Properties(ps => ps.Property(c => c.Name).StorageName("foo"));
+
+            Assert.Equal("foo", model.GetEntityType(typeof(Customer)).GetProperty("Name").StorageName);
+        }
+
+        [Fact]
+        public void Can_set_property_storage_name_when_no_clr_property()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(ps => ps.Property<string>("Name").StorageName("foo"));
+
+            Assert.Equal("foo", model.GetEntityType(typeof(Customer)).GetProperty("Name").StorageName);
+        }
+
+        [Fact]
+        public void Can_set_property_storage_name_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(ps => ps.Property<string>("Name").StorageName("foo"));
 
             Assert.Equal("foo", model.GetEntityType(typeof(Customer)).GetProperty("Name").StorageName);
         }
@@ -144,6 +354,42 @@ namespace Microsoft.Data.Entity.Tests.Metadata
         }
 
         [Fact]
+        public void Can_add_multiple_mixed_properties()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Properties(
+                    ps =>
+                    {
+                        ps.Property(c => c.Id);
+                        ps.Property<string>("Name").Annotation("foo", "bar");
+                    });
+
+            Assert.Equal(2, model.GetEntityType(typeof(Customer)).Properties.Count());
+        }
+
+        [Fact]
+        public void Can_add_multiple_properties_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(
+                    ps =>
+                    {
+                        ps.Property<int>("Id");
+                        ps.Property<string>("Name").Annotation("foo", "bar");
+                    });
+
+            Assert.Equal(2, model.GetEntityType(typeof(Customer)).Properties.Count());
+        }
+
+        [Fact]
         public void Can_add_foreign_key()
         {
             var model = new Model();
@@ -152,6 +398,61 @@ namespace Microsoft.Data.Entity.Tests.Metadata
             modelBuilder
                 .Entity<Order>()
                 .ForeignKeys(fks => fks.ForeignKey<Customer>(c => c.CustomerId));
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(1, entityType.ForeignKeys.Count());
+        }
+
+        [Fact]
+        public void Can_add_foreign_key_when_no_clr_property()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+            modelBuilder
+                .Entity<Customer>()
+                .Key(c => c.Id);
+            modelBuilder
+                .Entity<Order>()
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(fks => fks.ForeignKey("Customer", new [] { "CustomerId" }));
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(1, entityType.ForeignKeys.Count());
+        }
+
+        [Fact]
+        public void Can_add_foreign_key_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+            modelBuilder
+                .Entity<Customer>()
+                .Key(c => c.Id);
+            modelBuilder
+                .Entity("Order")
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(fks => fks.ForeignKey("Customer", "CustomerId"));
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(1, entityType.ForeignKeys.Count());
+        }
+
+        [Fact]
+        public void Can_add_foreign_key_when_no_clr_type_on_both_ends()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+            modelBuilder
+                .Entity("Customer")
+                .Properties(ps => ps.Property<int>("Id"))
+                .Key(new [] { "Id" });
+            modelBuilder
+                .Entity("Order")
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(fks => fks.ForeignKey("Customer", "CustomerId"));
 
             var entityType = model.GetEntityType(typeof(Order));
 
@@ -174,6 +475,81 @@ namespace Microsoft.Data.Entity.Tests.Metadata
                             fks.ForeignKey<Customer>(c => c.CustomerId);
                             fks.ForeignKey<Customer>(c => c.CustomerId).IsUnique();
                         });
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(2, entityType.ForeignKeys.Count());
+            Assert.True(entityType.ForeignKeys.Last().IsUnique);
+        }
+
+        [Fact]
+        public void Can_add_multiple_foreign_keys_when_mixed_properties()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder.Entity<Customer>().Key(c => c.Id);
+
+            modelBuilder
+                .Entity<Order>()
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(
+                    fks =>
+                    {
+                        fks.ForeignKey<Customer>(c => c.CustomerId);
+                        fks.ForeignKey("Customer", "CustomerId").IsUnique();
+                    });
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(2, entityType.ForeignKeys.Count());
+            Assert.True(entityType.ForeignKeys.Last().IsUnique);
+        }
+
+        [Fact]
+        public void Can_add_multiple_foreign_keys_when_no_clr_type()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder.Entity<Customer>().Key(c => c.Id);
+
+            modelBuilder
+                .Entity("Order")
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(
+                    fks =>
+                    {
+                        fks.ForeignKey("Customer", "CustomerId");
+                        fks.ForeignKey("Customer", "CustomerId").IsUnique();
+                    });
+
+            var entityType = model.GetEntityType(typeof(Order));
+
+            Assert.Equal(2, entityType.ForeignKeys.Count());
+            Assert.True(entityType.ForeignKeys.Last().IsUnique);
+        }
+
+        [Fact]
+        public void Can_add_multiple_foreign_keys_when_no_clr_type_on_both_ends()
+        {
+            var model = new Model();
+            var modelBuilder = new ModelBuilder(model);
+
+            modelBuilder
+                .Entity("Customer")
+                .Properties(ps => ps.Property<int>("Id"))
+                .Key("Id");
+
+            modelBuilder
+                .Entity("Order")
+                .Properties(ps => ps.Property<int>("CustomerId"))
+                .ForeignKeys(
+                    fks =>
+                    {
+                        fks.ForeignKey("Customer", "CustomerId");
+                        fks.ForeignKey("Customer", "CustomerId").IsUnique();
+                    });
 
             var entityType = model.GetEntityType(typeof(Order));
 
