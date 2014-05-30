6fee0af038773e0665462e7d76643b1380819431
Nate McMaster <nate.mcmaster@outlook.com>
2014-05-29 18:56:17 -0700

    POCO support for AzureTableStorage

--- test/Microsoft.Data.Entity.AzureTableStorage.FunctionalTests/BatchTests.cs
+++ test/Microsoft.Data.Entity.AzureTableStorage.FunctionalTests/BatchTests.cs
@@ -4,24 +4,22 @@
 using System;
 using System.Configuration;
 using Microsoft.Data.Entity.AzureTableStorage.FunctionalTests.Helpers;
-using Microsoft.Data.Entity.Metadata;
-using Microsoft.WindowsAzure.Storage.Table;
 using Xunit;
 
 namespace Microsoft.Data.Entity.AzureTableStorage.FunctionalTests
 {
     public class BatchTests : IClassFixture<CloudTableFixture>
     {
-        private readonly TestContext _context;
+        private readonly PurchaseContext _context;
         private readonly string _testParition;
 
         public BatchTests(CloudTableFixture fixture)
         {
             var connectionString = ConfigurationManager.AppSettings["TestConnectionString"];
-            _context = new TestContext();
-            fixture.GetOrCreateTable("AzureStorageBatchEmulatorEntity", connectionString);
-            //fixture.DeleteOnDispose = true;
-            _testParition = "BatchTests-" + DateTime.UtcNow.ToString("O");
+            var tableName = "BatchTestsTable" + DateTime.UtcNow.ToBinary();
+            fixture.GetOrCreateTable(tableName, connectionString);
+            _context = new PurchaseContext(tableName,true);
+            _testParition = "BatchTests-" + DateTime.UtcNow.ToString("R");
         }
 
         [Theory]
@@ -34,8 +32,8 @@ namespace Microsoft.Data.Entity.AzureTableStorage.FunctionalTests
         {
             for (var i = 0; i < count; i++)
             {
-                var item = new AzureStorageBatchEmulatorEntity { Count = i, PartitionKey = _testParition, RowKey = (count + i).ToString() };
-                _context.Items.Add(item);
+                var item = new Purchase { Count = i, PartitionKey = _testParition, RowKey = (count + i).ToString() };
+                _context.Purchases.Add(item);
             }
             var changes = _context.SaveChanges();
             Assert.Equal(count, changes);
@@ -44,48 +42,11 @@ namespace Microsoft.Data.Entity.AzureTableStorage.FunctionalTests
         [Fact]
         public void It_separates_by_partition_key()
         {
-            var partition1 = new AzureStorageBatchEmulatorEntity { PartitionKey = _testParition + "A", RowKey = "0" };
-            var partition2 = new AzureStorageBatchEmulatorEntity { PartitionKey = _testParition + "B", RowKey = "0" };
-            _context.Items.AddRange(new[] { partition1, partition2 });
+            var partition1 = new Purchase { PartitionKey = _testParition + "A", RowKey = "0" };
+            var partition2 = new Purchase { PartitionKey = _testParition + "B", RowKey = "0" };
+            _context.Purchases.AddRange(new[] { partition1, partition2 });
             var changes = _context.SaveChanges();
             Assert.Equal(2, changes);
-        }
-
-        //TODO see https://github.com/aspnet/EntityFramework/issues/251
-        // [Fact]
-        public void It_handles_many_changes()
-        {
-            var item = new AzureStorageBatchEmulatorEntity { PartitionKey = _testParition, RowKey = "z" };
-            _context.Items.Add(item);
-            item.Count = 12435;
-            _context.Items.Remove(item);
-            var changes = _context.SaveChanges();
-            Assert.Equal(2, changes);
-        }
-
-        private class TestContext : DbContext
-        {
-            public DbSet<AzureStorageBatchEmulatorEntity> Items { get; set; }
-
-            protected override void OnModelCreating(ModelBuilder builder)
-            {
-                builder.Entity<AzureStorageBatchEmulatorEntity>().Key(s => s.Key);
-            }
-
-            protected override void OnConfiguring(DbContextOptions builder)
-            {
-                builder.UseAzureTableStorge(ConfigurationManager.AppSettings["TestConnectionString"], true);
-            }
-        }
-
-        private class AzureStorageBatchEmulatorEntity : TableEntity
-        {
-            public string Key
-            {
-                get { return PartitionKey + RowKey; }
-            }
-
-            public int Count { get; set; }
         }
     }
 }
