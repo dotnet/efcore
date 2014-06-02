948088d550e5f85fe9e8214f9b4685c6ed3b915b
Nate McMaster <nate.mcmaster@outlook.com>
2014-06-02 16:40:09 -0700

    Use factory to resolve adapter types and other improvements to POCO support for ATS

--- test/Microsoft.Data.Entity.AzureTableStorage.Tests/BatchedDataStoreTests.cs
+++ test/Microsoft.Data.Entity.AzureTableStorage.Tests/BatchedDataStoreTests.cs
@@ -20,8 +20,10 @@ namespace Microsoft.Data.Entity.AzureTableStorage.Tests
     {
         private readonly FakeConnection _fakeConnection;
 
+        private readonly ITableEntityFactory _entityFactory = new PocoTableEntityAdapterFactory();
+
         public BatchedDataStoreTests(FakeConnection connection)
-            : base(connection)
+            : base(connection,new PocoTableEntityAdapterFactory())
         {
             _fakeConnection = connection;
             _fakeConnection.ClearQueue();
@@ -94,7 +96,7 @@ namespace Microsoft.Data.Entity.AzureTableStorage.Tests
         public void It_maps_entity_state_to_table_operations(EntityState entityState, TableOperationType operationType)
         {
             var entry = TestStateEntry.Mock().WithState(entityState);
-            var operation = GetOperation(entry, PocoTableEntityAdapter.FromObject(entry.Entity));
+            var operation = GetOperation(entry, _entityFactory.MakeFromObject(entry.Entity));
 
             if (operation == null)
             {
