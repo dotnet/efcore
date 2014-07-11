a94dc799a53f455b576586e5a576896bc7e5042f
Andrew Peters <anpete@microsoft.com>
2014-07-11 10:53:55 -0700

    Adding support for equals method

---
+++ src/EntityFramework.Relational/Query/Methods/EqualsTranslator.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq.Expressions;
+
+namespace Microsoft.Data.Entity.Relational.Query.Methods
+{
+    public class EqualsTranslator : IMethodCallTranslator
+    {
+        public virtual Expression Translate(MethodCallExpression expression)
+        {
+            return expression.Method.Name == "Equals"
+                   && expression.Arguments.Count == 1
+                ? Expression.Equal(expression.Object, expression.Arguments[0])
+                : null;
+        }
+    }
+}
