b600accb9fa180f65eb8c48dc75da8048c42b0dd
lajones <lajones@microsoft.com>
2014-07-28 13:12:46 -0700

    Added ability to respond to synch queries - both materialized and non-materialized queries working TODO: add same ability for asynch queries

---
+++ src/EntityFramework.Redis/Query/RedisQueryCompilationContext.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Query;
+
+namespace Microsoft.Data.Entity.Redis.Query
+{
+    public class RedisQueryCompilationContext : QueryCompilationContext
+    {
+        public RedisQueryCompilationContext(
+            [NotNull] IModel model,
+            [NotNull] ILinqOperatorProvider linqOperatorProvider,
+            [NotNull] IResultOperatorHandler resultOperatorHandler)
+            : base(model, linqOperatorProvider, resultOperatorHandler)
+        {
+        }
+
+        public override EntityQueryModelVisitor CreateQueryModelVisitor(
+            EntityQueryModelVisitor parentEntityQueryModelVisitor)
+        {
+            return new RedisQueryModelVisitor(this);
+        }
+    }
+}
