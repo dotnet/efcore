0472eb515929a902b6c13ba7764f6fe41901b53d
lajones <lajones@microsoft.com>
2014-09-12 16:58:22 -0700

    Issue 653 - added support for DateTimeOffset properties in Redis. Also added tests for all built-in data types for all providers except Azure - which needs special treatment (PartitionKeys etc). As noted several of the data types are not yet supported on all providers - so these have been moved to a Supplemental set of tests which are   implemented only on the providers that support them. As SQL Server and   SQLite start supporting these we should move those data types into the   BuiltInDataTypes* classes and remove them from the Supplemental   ones.

---
+++ test/EntityFramework.FunctionalTests/BuiltInDataTypesFixtureBase.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Data.Entity.Metadata;
+
+namespace Microsoft.Data.Entity.FunctionalTests
+{
+    /// <summary>
+    /// See also <see cref="SupplementalBuiltInDataTypesFixtureBase" />.
+    /// Not all built-in data types are supported on all providers yet.
+    /// At the same time, not all conventions (e.g. Ignore) are available yet.
+    /// So this class provides a base fixture for those data types which are
+    /// supported on all current providers.
+    /// Over time, the aim is to transfer as many data types as possible into
+    /// this class and ultimately to delete <see cref="SupplementalBuiltInDataTypesFixtureBase" />.
+    /// </summary>
+    public abstract class BuiltInDataTypesFixtureBase
+    {
+        public abstract DbContext CreateContext();
+
+        public virtual IModel CreateModel()
+        {
+            var model = new Model();
+            var builder = new BasicModelBuilder(model);
+            builder.Entity<BuiltInNonNullableDataTypes>(b =>
+                {
+                    b.Key(dt => dt.Id);
+                    b.Property(dt => dt.Id);
+                    b.Property(dt => dt.TestInt32);
+                    b.Property(dt => dt.TestInt64);
+                    b.Property(dt => dt.TestDouble);
+                    b.Property(dt => dt.TestDecimal);
+                    b.Property(dt => dt.TestDateTime);
+                    b.Property(dt => dt.TestDateTimeOffset);
+                    b.Property(dt => dt.TestSingle);
+                    b.Property(dt => dt.TestBoolean);
+                    b.Property(dt => dt.TestByte);
+                    b.Property(dt => dt.TestInt16);
+                });
+
+            builder.Entity<BuiltInNullableDataTypes>(b =>
+            {
+                b.Key(dt => dt.Id);
+                b.Property(dt => dt.Id);
+                b.Property(dt => dt.TestNullableInt32);
+                b.Property(dt => dt.TestString);
+                b.Property(dt => dt.TestNullableInt64);
+                b.Property(dt => dt.TestNullableDouble);
+                b.Property(dt => dt.TestNullableDecimal);
+                b.Property(dt => dt.TestNullableDateTime);
+                b.Property(dt => dt.TestNullableDateTimeOffset);
+                b.Property(dt => dt.TestNullableSingle);
+                b.Property(dt => dt.TestNullableBoolean);
+                b.Property(dt => dt.TestNullableByte);
+                b.Property(dt => dt.TestNullableInt16);
+            });
+
+            return model;
+        }
+    }
+
+    public class BuiltInNonNullableDataTypes
+    {
+        public int Id { get; set; }
+        public int TestInt32 { get; set; }
+        public long TestInt64 { get; set; }
+        public double TestDouble { get; set; }
+        public decimal TestDecimal { get; set; }
+        public DateTime TestDateTime { get; set; }
+        public DateTimeOffset TestDateTimeOffset { get; set; }
+        public float TestSingle { get; set; }
+        public bool TestBoolean { get; set; }
+        public byte TestByte { get; set; }
+        public uint TestUnsignedInt32 { get; set; }
+        public ulong TestUnsignedInt64 { get; set; }
+        public short TestInt16 { get; set; }
+    }
+
+    public class BuiltInNullableDataTypes
+    {
+        public int Id { get; set; }
+        public int? TestNullableInt32 { get; set; }
+        public string TestString { get; set; }
+        public long? TestNullableInt64 { get; set; }
+        public double? TestNullableDouble { get; set; }
+        public decimal? TestNullableDecimal { get; set; }
+        public DateTime? TestNullableDateTime { get; set; }
+        public DateTimeOffset? TestNullableDateTimeOffset { get; set; }
+        public float? TestNullableSingle { get; set; }
+        public bool? TestNullableBoolean { get; set; }
+        public byte? TestNullableByte { get; set; }
+        public short? TestNullableInt16 { get; set; }
+    }
+}
