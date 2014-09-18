2e5e276c9a526985842c0cea1383c3210e92b50d
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2014-09-18 14:19:09 -0700

    Adding WithCurrentCulture extension method
    Part of #374

---
+++ src/EntityFramework/Extensions/TaskExtensions.cs
@@ -0,0 +1,267 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Utilities;
+
+// ReSharper disable once CheckNamespace
+
+namespace System.Threading.Tasks
+{
+    /// <summary>
+    /// Contains extension methods for the <see cref="Task" /> class.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Configures an awaiter used to await this <see cref="Task{TResult}" /> to avoid
+        /// marshalling the continuation back to the original context, but preserve the
+        /// current culture and UI culture.
+        /// </summary>
+        /// <remarks> Calling this has no effect on platforms that don't use <see cref="SynchronizationContext"/>. </remarks>
+        /// <typeparam name="T">
+        /// The type of the result produced by the associated <see cref="Task{TResult}"/>.
+        /// </typeparam>
+        /// <param name="task">The task to be awaited on.</param>
+        /// <returns>An object used to await this task.</returns>
+#if NET45
+        public static CultureAwaiter<T> WithCurrentCulture<T>([NotNull] this Task<T> task)
+        {
+            Check.NotNull(task, "task");
+
+            return new CultureAwaiter<T>(task);
+        }
+#else
+        public static Task<T> WithCurrentCulture<T>([NotNull] this Task<T> task)
+        {
+            return task;
+        }
+#endif
+
+        /// <summary>
+        /// Configures an awaiter used to await this <see cref="Task" /> to avoid
+        /// marshalling the continuation back to the original context, but preserve the
+        /// current culture and UI culture.
+        /// </summary>
+        /// <remarks> Calling this has no effect on platforms that don't use <see cref="SynchronizationContext"/>. </remarks>
+        /// <param name="task">The task to be awaited on.</param>
+        /// <returns>An object used to await this task.</returns>
+#if NET45
+        public static CultureAwaiter WithCurrentCulture([NotNull] this Task task)
+        {
+            Check.NotNull(task, "task");
+
+            return new CultureAwaiter(task);
+        }
+#else
+        public static Task WithCurrentCulture([NotNull] this Task task)
+        {
+            return task;
+        }
+#endif
+
+        
+#if NET45
+        /// <summary>
+        /// Provides an awaitable object that allows for awaits on <see cref="Task{TResult}" /> that
+        /// preserve the culture.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the result produced by the associated <see cref="Task{TResult}"/>.
+        /// </typeparam>
+        /// <remarks>This type is intended for compiler use only.</remarks>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Awaiter")]
+        [SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+        [SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes")]
+        public struct CultureAwaiter<T> : ICriticalNotifyCompletion
+        {
+            private readonly Task<T> _task;
+
+            /// <summary>
+            /// Constructs a new instance of the <see cref="CultureAwaiter{T}" /> class.
+            /// </summary>
+            /// <param name="task">The task to be awaited on.</param>
+            public CultureAwaiter(Task<T> task)
+            {
+                _task = task;
+            }
+
+            /// <summary>Gets an awaiter used to await this <see cref="Task{TResult}" />.</summary>
+            /// <returns>An awaiter instance.</returns>
+            /// <remarks>This method is intended for compiler user rather than use directly in code.</remarks>
+            [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Awaiter")]
+            [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+            public CultureAwaiter<T> GetAwaiter()
+            {
+                return this;
+            }
+
+            /// <summary>
+            /// Gets whether this <see cref="Task">Task</see> has completed.
+            /// </summary>
+            /// <remarks>
+            /// <see cref="IsCompleted" /> will return true when the Task is in one of the three
+            /// final states: <see cref="TaskStatus.RanToCompletion">RanToCompletion</see>,
+            /// <see cref="TaskStatus.Faulted">Faulted</see>, or
+            /// <see cref="TaskStatus.Canceled">Canceled</see>.
+            /// </remarks>
+            public bool IsCompleted
+            {
+                get { return _task.IsCompleted; }
+            }
+
+            /// <summary>Ends the await on the completed <see cref="Task{TResult}" />.</summary>
+            /// <returns>The result of the completed <see cref="Task{TResult}" />.</returns>
+            /// <exception cref="System.NullReferenceException">The awaiter was not properly initialized.</exception>
+            /// <exception cref="TaskCanceledException">The task was canceled.</exception>
+            /// <exception cref="System.Exception">The task completed in a Faulted state.</exception>
+            [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+            public T GetResult()
+            {
+                return _task.GetAwaiter().GetResult();
+            }
+
+            /// <summary>This method is not implemented and should not be called.</summary>
+            /// <param name="continuation">The action to invoke when the await operation completes.</param>
+            public void OnCompleted(Action continuation)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Schedules the continuation onto the <see cref="Task{TResult}" /> associated with this
+            /// <see cref="TaskAwaiter{TResult}" />.
+            /// </summary>
+            /// <param name="continuation">The action to invoke when the await operation completes.</param>
+            /// <exception cref="System.ArgumentNullException">
+            /// The <paramref name="continuation" /> argument is null
+            /// (Nothing in Visual Basic).
+            /// </exception>
+            /// <exception cref="System.InvalidOperationException">The awaiter was not properly initialized.</exception>
+            /// <remarks>This method is intended for compiler user rather than use directly in code.</remarks>
+            public void UnsafeOnCompleted(Action continuation)
+            {
+                var currentCulture = Thread.CurrentThread.CurrentCulture;
+                var currentUICulture = Thread.CurrentThread.CurrentUICulture;
+                _task.ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().UnsafeOnCompleted(
+                    () =>
+                    {
+                        var originalCulture = Thread.CurrentThread.CurrentCulture;
+                        var originalUICulture = Thread.CurrentThread.CurrentUICulture;
+                        Thread.CurrentThread.CurrentCulture = currentCulture;
+                        Thread.CurrentThread.CurrentUICulture = currentUICulture;
+                        try
+                        {
+                            continuation();
+                        }
+                        finally
+                        {
+                            Thread.CurrentThread.CurrentCulture = originalCulture;
+                            Thread.CurrentThread.CurrentUICulture = originalUICulture;
+                        }
+                    });
+            }
+        }
+
+        /// <summary>
+        /// Provides an awaitable object that allows for awaits on <see cref="Task" /> that
+        /// preserve the culture.
+        /// </summary>
+        /// <remarks>This type is intended for compiler use only.</remarks>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Awaiter")]
+        [SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+        [SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes")]
+        public struct CultureAwaiter : ICriticalNotifyCompletion
+        {
+            private readonly Task _task;
+
+            /// <summary>
+            /// Constructs a new instance of the <see cref="CultureAwaiter" /> class.
+            /// </summary>
+            /// <param name="task">The task to be awaited on.</param>
+            public CultureAwaiter(Task task)
+            {
+                _task = task;
+            }
+
+            /// <summary>Gets an awaiter used to await this <see cref="Task" />.</summary>
+            /// <returns>An awaiter instance.</returns>
+            /// <remarks>This method is intended for compiler user rather than use directly in code.</remarks>
+            [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Awaiter")]
+            [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+            public CultureAwaiter GetAwaiter()
+            {
+                return this;
+            }
+
+            /// <summary>
+            /// Gets whether this <see cref="Task">Task</see> has completed.
+            /// </summary>
+            /// <remarks>
+            /// <see cref="IsCompleted" /> will return true when the Task is in one of the three
+            /// final states: <see cref="TaskStatus.RanToCompletion">RanToCompletion</see>,
+            /// <see cref="TaskStatus.Faulted">Faulted</see>, or
+            /// <see cref="TaskStatus.Canceled">Canceled</see>.
+            /// </remarks>
+            public bool IsCompleted
+            {
+                get { return _task.IsCompleted; }
+            }
+
+            /// <summary>Ends the await on the completed <see cref="Task" />.</summary>
+            /// <exception cref="System.NullReferenceException">The awaiter was not properly initialized.</exception>
+            /// <exception cref="TaskCanceledException">The task was canceled.</exception>
+            /// <exception cref="System.Exception">The task completed in a Faulted state.</exception>
+            public void GetResult()
+            {
+                _task.GetAwaiter().GetResult();
+            }
+
+            /// <summary>This method is not implemented and should not be called.</summary>
+            /// <param name="continuation">The action to invoke when the await operation completes.</param>
+            public void OnCompleted(Action continuation)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Schedules the continuation onto the <see cref="Task" /> associated with this
+            /// <see cref="TaskAwaiter" />.
+            /// </summary>
+            /// <param name="continuation">The action to invoke when the await operation completes.</param>
+            /// <exception cref="System.ArgumentNullException">
+            /// The <paramref name="continuation" /> argument is null
+            /// (Nothing in Visual Basic).
+            /// </exception>
+            /// <exception cref="System.InvalidOperationException">The awaiter was not properly initialized.</exception>
+            /// <remarks>This method is intended for compiler user rather than use directly in code.</remarks>
+            public void UnsafeOnCompleted(Action continuation)
+            {
+                var currentCulture = Thread.CurrentThread.CurrentCulture;
+                var currentUICulture = Thread.CurrentThread.CurrentUICulture;
+                _task.ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().UnsafeOnCompleted(
+                    () =>
+                    {
+                        var originalCulture = Thread.CurrentThread.CurrentCulture;
+                        var originalUICulture = Thread.CurrentThread.CurrentUICulture;
+                        Thread.CurrentThread.CurrentCulture = currentCulture;
+                        Thread.CurrentThread.CurrentUICulture = currentUICulture;
+                        try
+                        {
+                            continuation();
+                        }
+                        finally
+                        {
+                            Thread.CurrentThread.CurrentCulture = originalCulture;
+                            Thread.CurrentThread.CurrentUICulture = originalUICulture;
+                        }
+                    });
+            }
+        }        
+#endif
+    }
+}
---
+++ test/EntityFramework.Tests/Extensions/TaskExtensionsTest.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace System.Threading.Tasks.Tests
+{
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class TaskExtensionsTest
+    {
+#if NET45
+        [Fact]
+        public void NonGeneric_WithCurrentCulture_preserves_culture()
+        {
+            var originalCulture = Thread.CurrentThread.CurrentCulture;
+            var expectedCulture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = expectedCulture;
+
+            try
+            {
+                var culture = GetCurrentCultureAsync().Result;
+
+                Assert.Equal(expectedCulture, culture);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = originalCulture;
+            }
+        }
+
+        [Fact]
+        public void Generic_WithCurrentCulture_preserves_culture()
+        {
+            var originalCulture = Thread.CurrentThread.CurrentCulture;
+            var expectedCulture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = expectedCulture;
+
+            try
+            {
+                var culture = GetCurrentCultureAsync<object>().Result;
+
+                Assert.Equal(expectedCulture, culture);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = originalCulture;
+            }
+        }
+
+        [Fact]
+        public void NonGeneric_WithCurrentCulture_preserves_ui_culture()
+        {
+            var originalCulture = Thread.CurrentThread.CurrentUICulture;
+            var expectedCulture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentUICulture = expectedCulture;
+
+            try
+            {
+                var culture = GetCurrentUICultureAsync().Result;
+
+                Assert.Equal(expectedCulture, culture);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = originalCulture;
+            }
+        }
+
+        [Fact]
+        public void Generic_WithCurrentCulture_preserves_ui_culture()
+        {
+            var originalCulture = Thread.CurrentThread.CurrentUICulture;
+            var expectedCulture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentUICulture = expectedCulture;
+
+            try
+            {
+                var culture = GetCurrentUICultureAsync<object>().Result;
+
+                Assert.Equal(expectedCulture, culture);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = originalCulture;
+            }
+        }
+
+        private async Task<CultureInfo> GetCurrentCultureAsync()
+        {
+            await ConfigurableYield().WithCurrentCulture();
+            return Thread.CurrentThread.CurrentCulture;
+        }
+
+        private async Task<CultureInfo> GetCurrentCultureAsync<T>()
+        {
+            await ConfigurableYield<T>().WithCurrentCulture();
+            return Thread.CurrentThread.CurrentCulture;
+        }
+
+        private async Task<CultureInfo> GetCurrentUICultureAsync()
+        {
+            await ConfigurableYield().WithCurrentCulture();
+            return Thread.CurrentThread.CurrentUICulture;
+        }
+
+        private async Task<CultureInfo> GetCurrentUICultureAsync<T>()
+        {
+            await ConfigurableYield<T>().WithCurrentCulture();
+            return Thread.CurrentThread.CurrentUICulture;
+        }
+
+        private async Task ConfigurableYield()
+        {
+            await Task.Yield();
+        }
+
+        private async Task<T> ConfigurableYield<T>()
+        {
+            await Task.Yield();
+            return default(T);
+        }
+#else
+        [Fact]
+        public void NonGeneric_WithCurrentCulture_is_noop()
+        {
+            var task = (Task)Task.FromResult(true);
+
+            Assert.Same(task, task.WithCurrentCulture());
+        }
+
+        [Fact]
+        public void Generic_WithCurrentCulture_is_noop()
+        {
+            var task = Task.FromResult(true);
+
+            Assert.Same(task, task.WithCurrentCulture());
+        }
+#endif
+    }
+}
