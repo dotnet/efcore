7b47c447d24a59b9128466b3b44a9e67ed7fc92c
maumar <maumar@microsoft.com>
2014-10-24 17:32:40 -0700

    add gears of war model to tests

---
+++ test/EntityFramework.FunctionalTests/TestModels/GearsOfWarModel/MilitaryRank.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Data.Entity.FunctionalTests.TestModels.GearsOfWarModel
+{
+    public enum MilitaryRank
+    {
+        Private,
+        Corporal,
+        Sergeant,
+        Lieutenant,
+        Captain,
+        Major,
+        Colonel,
+        General,
+    }
+}
