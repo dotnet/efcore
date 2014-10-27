195d4c7c0a08a10504aae4bb591570f3357a039a
maumar <maumar@microsoft.com>
2014-10-27 16:21:29 -0700

    tests for issue 963 - disabled for now, until the big is fixed

--- test/EntityFramework.SqlServer.FunctionalTests/QueryBugsTest.cs
+++ test/EntityFramework.SqlServer.FunctionalTests/QueryBugsTest.cs
@@ -10,6 +10,7 @@ using Microsoft.Framework.DependencyInjection.Advanced;
 using Microsoft.Framework.DependencyInjection.Fallback;
 using Microsoft.Data.Entity.Relational.FunctionalTests;
 using Xunit;
+using Microsoft.Data.Entity.Metadata;
 
 namespace Microsoft.Data.Entity.SqlServer.FunctionalTests
 {
@@ -205,13 +206,133 @@ LEFT JOIN [Customer] AS [c] ON ([c].[FirstName] = [o].[CustomerId0] AND [c].[Las
                 options.UseSqlServer(SqlServerTestStore.CreateConnectionString("Repro925"));
             }
 
-            protected override void OnModelCreating(Entity.Metadata.ModelBuilder modelBuilder)
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
             {
                 modelBuilder.Entity<Customer>(m =>
                     {
                         m.Key(c => new { c.FirstName, c.LastName });
                         m.OneToMany(c => c.Orders, o => o.Customer);
                     });
+            }
+        }
+
+        ////[Fact]
+        public void Include_on_optional_navigation_One_To_Many_963()
+        {
+            CreateDatabase963();
+
+            using (var ctx = new MyContext963())
+            {
+                ctx.Targaryens.Include(t => t.Dragons).ToList();
+            }
+        }
+
+        ////[Fact]
+        public void Include_on_optional_navigation_Many_To_One_963()
+        {
+            CreateDatabase963();
+
+            using (var ctx = new MyContext963())
+            {
+                ctx.Dragons.Include(d => d.Mother).ToList();
+            }
+        }
+
+        ////[Fact]
+        public void Include_on_optional_navigation_One_To_One_principal_963()
+        {
+            CreateDatabase963();
+
+            using (var ctx = new MyContext963())
+            {
+                ctx.Targaryens.Include(t => t.Details).ToList();
+            }
+        }
+
+        ////[Fact]
+        public void Include_on_optional_navigation_One_To_One_dependent_963()
+        {
+            CreateDatabase963();
+
+            using (var ctx = new MyContext963())
+            {
+                ctx.Details.Include(d => d.Targaryen).ToList();
+            }
+        }
+
+        private void CreateDatabase963()
+        {
+            using (var context = new MyContext963())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var drogon = new Dragon { Name = "Drogon" };
+                var rhaegal = new Dragon { Name = "Rhaegal" };
+                var viserion = new Dragon { Name = "Viserion" };
+                var balerion = new Dragon { Name = "Balerion" };
+
+                var aerys = new Targaryen { Name = "Aerys II" };
+                var details = new Details
+                {
+                    FullName = @"Daenerys Stormborn of the House Targaryen, the First of Her Name, the Unburnt, Queen of Meereen, 
+Queen of the Andals and the Rhoynar and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons"
+                };
+
+                var daenerys = new Targaryen { Name = "Daenerys", Details = details, Dragons = new List<Dragon> { drogon, rhaegal, viserion } };
+                context.Targaryens.AddRange(new[] { daenerys, aerys });
+                context.Dragons.AddRange(new[] { drogon, rhaegal, viserion, balerion });
+                context.Details.Add(details);
+
+                context.SaveChanges();
+            }
+        }
+
+        public class Targaryen
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Details Details { get; set; }
+
+            public List<Dragon> Dragons { get; set; }
+        }
+
+        public class Dragon
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int? MotherId { get; set; }
+            public Targaryen Mother { get; set; }
+        }
+
+        public class Details
+        {
+            public int Id { get; set; }
+            public int? TargaryenId { get; set; }
+            public Targaryen Targaryen { get; set; }
+            public string FullName { get; set; }
+        }
+
+        // TODO: replace with GearsOfWar context when it's refactored properly
+        public class MyContext963 : DbContext
+        {
+            public DbSet<Targaryen> Targaryens { get; set; }
+            public DbSet<Details> Details { get; set; }
+            public DbSet<Dragon> Dragons { get; set; }
+
+            protected override void OnConfiguring(DbContextOptions options)
+            {
+                options.UseSqlServer(SqlServerTestStore.CreateConnectionString("Repro963"));
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Targaryen>(m =>
+                {
+                    m.Key(t => t.Id);
+                    m.OneToMany(t => t.Dragons, d => d.Mother).ForeignKey(d => d.MotherId);
+                    m.OneToOne(t => t.Details, d => d.Targaryen).ForeignKey<Details>(d => d.TargaryenId);
+                });
             }
         }
     }
