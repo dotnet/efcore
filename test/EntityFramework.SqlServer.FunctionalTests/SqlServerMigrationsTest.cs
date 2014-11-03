27d28f0f017cc74b6339ac8636b696c72b5ba5b6
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2014-11-03 11:33:34 -0800

    Adding a simple FT for migrations on SQL Server

---
+++ test/EntityFramework.SqlServer.FunctionalTests/SqlServerMigrationsTest.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using Microsoft.Data.Entity.Migrations.Builders;
+using Microsoft.Data.Entity.Migrations.Infrastructure;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using Xunit;
+
+namespace Microsoft.Data.Entity.SqlServer.FunctionalTests
+{
+    public class SqlServerMigrationsTest
+    {
+        [Fact]
+        public async Task Empty_Migration_Creates_Database()
+        {
+            using (var testDatabase = await SqlServerTestStore.CreateScratchAsync(createDatabase: false))
+            {
+                using (var context = CreateContext(testDatabase))
+                {
+                    context.Database.AsRelational().ApplyMigrations();
+
+                    Assert.True(context.Database.AsRelational().Exists());
+                }
+            }
+        }
+
+        private static BloggingContext CreateContext(SqlServerTestStore testStore)
+        {
+            var serviceProvider =
+                new ServiceCollection()
+                    .AddEntityFramework()
+                    .AddSqlServer()
+                    .ServiceCollection
+                    .BuildServiceProvider();
+
+            return new BloggingContext(
+                serviceProvider,
+                new DbContextOptions().UseSqlServer(testStore.Connection.ConnectionString));
+        }
+
+        private class BloggingContext : DbContext
+        {
+            public BloggingContext(IServiceProvider serviceProvider, DbContextOptions options)
+                : base(serviceProvider, options)
+            {
+            }
+
+            public DbSet<Blog> Blogs { get; set; }
+
+            public class Blog
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+            }
+        }
+
+        [ContextType(typeof(BloggingContext))]
+        public class EmptyMigration : Migration, IMigrationMetadata
+        {
+            public override void Up(MigrationBuilder migrationBuilder)
+            {
+            }
+
+            public override void Down(MigrationBuilder migrationBuilder)
+            {
+            }
+
+            public string MigrationId
+            {
+                get { return "Empty"; }
+            }
+
+            public string ProductVersion
+            {
+                get { return "EF7"; }
+            }
+
+            public IModel TargetModel
+            {
+                get { return new Model(); }
+            }
+        }
+    }
+}
