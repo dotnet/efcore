3af1f6deff0473f863c49c8d28591e5ffaca2357
lajones <lajones@microsoft.com>
2014-12-05 15:54:37 -0800

    First working copy with EF.Core

---
+++ src/EntityFramework.ReverseEngineering/EntityTypeTemplateModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.Metadata;
+
+namespace Microsoft.Data.Entity.ReverseEngineering
+{
+    public class EntityTypeTemplateModel
+    {
+        public string Namespace { get; set; }
+        public string ProviderAssembly { get; set; }
+        public string ConnectionString { get; set; }
+        public string Filters { get; set; }
+        public IEntityType EntityType { get; set; }
+    }
+}
