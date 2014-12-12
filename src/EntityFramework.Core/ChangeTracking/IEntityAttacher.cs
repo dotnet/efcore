ab8bb02b27748da016f8966c3e5d5c344a5aea3a
ajcvickers <ajcvickers@hotmail.com>
2014-12-12 08:48:52 -0800

    Add higher-level AttachGraph methods
    Currently two methods, with async overloads:
    - AttachGraph: Adds each entity if its key value is not set, otherwise attaches the entity as Unchanged.
    - UpdateGraph: Adds each entity if its key value is not set, otherwise attaches the entity as Modified.
    
    We can discuss if both behaviors are needed, and if so whether it should be a single method with a flag.
    
    These methods make use of an implementation of IEntityAttacher and there is a factory to get the IEntityAttacher to use for attach/update. This means that the defaults can be changed by registering a different factory in DI.
    
    The default key checking is exposed as a IsKeySet property on EntityEntry, so it is easy to use this functionaliy either in the AttachGraph delegate or in implementations of IEntityAttacher.

---
+++ src/EntityFramework.Core/ChangeTracking/IEntityAttacher.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Microsoft.Data.Entity.ChangeTracking
+{
+    public interface IEntityAttacher
+    {
+        void HandleEntity([NotNull] EntityEntry entry);
+        Task HandleEntityAsync([NotNull] EntityEntry entry, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
