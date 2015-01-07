johntallon/EntityFramework

Moving Migrations into Relational

src/EntityFramework.SqlServer/SqlServerMigrationOperationFactory.cs
@@ -4,8 +4,8 @@
 using System;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
-using Microsoft.Data.Entity.Migrations;
-using Microsoft.Data.Entity.Migrations.Model;
+using Microsoft.Data.Entity.Relational;
+using Microsoft.Data.Entity.Relational.MigrationsModel;
 using Microsoft.Data.Entity.SqlServer.Metadata;
 
 namespace Microsoft.Data.Entity.SqlServer

src/EntityFramework.SqlServer/SqlServerMigrationOperationProcessor.cs
@@ -6,8 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
-using Microsoft.Data.Entity.Migrations;
-using Microsoft.Data.Entity.Migrations.Model;
+using Microsoft.Data.Entity.Relational;
+using Microsoft.Data.Entity.Relational.MigrationsModel;
 using Microsoft.Data.Entity.SqlServer.Metadata;
 using Microsoft.Data.Entity.Utilities;
 

test/EntityFramework.Relational.Tests/MigrationsModel/MoveSequenceOperationTest.cs
@@ -1,0 +1,47 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.Relational.MigrationsModel;
+using Microsoft.Data.Entity.Utilities;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Data.Entity.Relational.Tests.MigrationsModel
+{
+    public class MoveSequenceOperationTest
+    {
+        [Fact]
+        public void Create_and_initialize_operation()
+        {
+            var operation = new MoveSequenceOperation("dbo.MySequence", "RenamedSchema");
+
+            Assert.Equal("dbo.MySequence", operation.SequenceName);
+            Assert.Equal("RenamedSchema", operation.NewSchema);
+            Assert.False(operation.IsDestructiveChange);
+        }
+
+        [Fact]
+        public void Dispatches_visitor()
+        {
+            var operation = new MoveSequenceOperation("dbo.MySequence", "RenamedSchema");
+
+            var sqlGeneratorMock = MigrationsTestHelpers.MockSqlGenerator();
+            var sqlBuilder = new SqlBatchBuilder();
+            operation.GenerateSql(sqlGeneratorMock.Object, sqlBuilder);
+
+            sqlGeneratorMock.Verify(g => g.Generate(operation, sqlBuilder), Times.Once());
+
+            var codeGeneratorMock = new Mock<MigrationCodeGenerator>(new Mock<ModelCodeGenerator>().Object);
+            var codeBuilder = new IndentedStringBuilder();
+            operation.GenerateCode(codeGeneratorMock.Object, codeBuilder);
+
+            codeGeneratorMock.Verify(g => g.Generate(operation, codeBuilder), Times.Once());
+
+            var visitorMock = new Mock<MigrationOperationVisitor<object>>();
+            var context = new object();
+            operation.Accept(visitorMock.Object, context);
+
+            visitorMock.Verify(v => v.Visit(operation, context), Times.Once());
+        }
+    }
+}

dotnet/efcore

src/EntityFramework.SqlServer/SqlServerMigrationOperationFactory.cs
@@ -4,8 +4,8 @@
 using System;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
-using Microsoft.Data.Entity.Migrations;
-using Microsoft.Data.Entity.Migrations.Model;
+using Microsoft.Data.Entity.Relational.Migrations;
+using Microsoft.Data.Entity.Relational.Migrations.MigrationsModel;
 using Microsoft.Data.Entity.SqlServer.Metadata;
 
 namespace Microsoft.Data.Entity.SqlServer

src/EntityFramework.SqlServer/SqlServerMigrationOperationProcessor.cs
@@ -6,8 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
-using Microsoft.Data.Entity.Migrations;
-using Microsoft.Data.Entity.Migrations.Model;
+using Microsoft.Data.Entity.Relational.Migrations;
+using Microsoft.Data.Entity.Relational.Migrations.MigrationsModel;
 using Microsoft.Data.Entity.SqlServer.Metadata;
 using Microsoft.Data.Entity.Utilities;
 

test/EntityFramework.Relational.Tests/Migrations/MigrationsModel/MoveSequenceOperationTest.cs
@@ -1,0 +1,48 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.Relational.Migrations;
+using Microsoft.Data.Entity.Relational.Migrations.MigrationsModel;
+using Microsoft.Data.Entity.Utilities;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Data.Entity.Relational.Tests.Migrations.MigrationsModel
+{
+    public class MoveSequenceOperationTest
+    {
+        [Fact]
+        public void Create_and_initialize_operation()
+        {
+            var operation = new MoveSequenceOperation("dbo.MySequence", "RenamedSchema");
+
+            Assert.Equal("dbo.MySequence", operation.SequenceName);
+            Assert.Equal("RenamedSchema", operation.NewSchema);
+            Assert.False(operation.IsDestructiveChange);
+        }
+
+        [Fact]
+        public void Dispatches_visitor()
+        {
+            var operation = new MoveSequenceOperation("dbo.MySequence", "RenamedSchema");
+
+            var sqlGeneratorMock = MigrationsTestHelpers.MockSqlGenerator();
+            var sqlBuilder = new SqlBatchBuilder();
+            operation.GenerateSql(sqlGeneratorMock.Object, sqlBuilder);
+
+            sqlGeneratorMock.Verify(g => g.Generate(operation, sqlBuilder), Times.Once());
+
+            var codeGeneratorMock = new Mock<MigrationCodeGenerator>(new Mock<ModelCodeGenerator>().Object);
+            var codeBuilder = new IndentedStringBuilder();
+            operation.GenerateCode(codeGeneratorMock.Object, codeBuilder);
+
+            codeGeneratorMock.Verify(g => g.Generate(operation, codeBuilder), Times.Once());
+
+            var visitorMock = new Mock<MigrationOperationVisitor<object>>();
+            var context = new object();
+            operation.Accept(visitorMock.Object, context);
+
+            visitorMock.Verify(v => v.Visit(operation, context), Times.Once());
+        }
+    }
+}