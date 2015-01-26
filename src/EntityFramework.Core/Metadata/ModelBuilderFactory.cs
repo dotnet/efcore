fisnikb/EntityFramework

Add a convention to replace shadow foreign keys with non-shadow ones. Properties are be matched using the following patterns: 1. Navigation to principal name + corresponding principal key property name 2. Principal type name + corresponding principal key property name 3. Corresponding principal key property name 4. PK on the dependent entity for unique FKs
Add a way to specify non-primary key to InternalEntityBuilder.

src/EntityFramework.Core/Metadata/ModelBuilderFactory.cs
@@ -18,9 +18,12 @@ namespace Microsoft.Data.Entity.Metadata
         protected virtual ConventionsDispatcher CreateConventionsDispatcher()
         {
             var conventions = new ConventionsDispatcher();
-            conventions.EntityTypeConventions.Add(new PropertiesConvention());
-            conventions.EntityTypeConventions.Add(new KeyConvention());
-            conventions.EntityTypeConventions.Add(new RelationshipDiscoveryConvention());
+
+            conventions.OnEntityTypeAddedConventions.Add(new PropertiesConvention());
+            conventions.OnEntityTypeAddedConventions.Add(new KeyConvention());
+            conventions.OnEntityTypeAddedConventions.Add(new RelationshipDiscoveryConvention());
+
+            conventions.OnForeignKeyAddedConventions.Add(new ForeignKeyPropertyDiscoveryConvention());
 
             return conventions;
         }

src/EntityFramework.Core/Metadata/ModelConventions/ConventionsDispatcher.cs
@@ -12,16 +12,19 @@ namespace Microsoft.Data.Entity.Metadata.ModelConventions
     {
         public ConventionsDispatcher()
         {
-            EntityTypeConventions = new List<IEntityTypeConvention>();
+            OnEntityTypeAddedConventions = new List<IEntityTypeConvention>();
+            OnForeignKeyAddedConventions = new List<IRelationshipConvention>();
         }
 
-        public virtual IList<IEntityTypeConvention> EntityTypeConventions { get; }
+        public virtual IList<IEntityTypeConvention> OnEntityTypeAddedConventions { get; }
+
+        public virtual IList<IRelationshipConvention> OnForeignKeyAddedConventions { get; }
 
         public virtual InternalEntityBuilder OnEntityTypeAdded([NotNull] InternalEntityBuilder entityBuilder)
         {
             Check.NotNull(entityBuilder, "entityBuilder");
 
-            foreach (var entityTypeConvention in EntityTypeConventions)
+            foreach (var entityTypeConvention in OnEntityTypeAddedConventions)
             {
                 entityBuilder = entityTypeConvention.Apply(entityBuilder);
                 if (entityBuilder == null)
@@ -31,6 +34,22 @@ namespace Microsoft.Data.Entity.Metadata.ModelConventions
             }
 
             return entityBuilder;
+        }
+
+        public virtual InternalRelationshipBuilder OnRelationshipAdded([NotNull] InternalRelationshipBuilder relationshipBuilder)
+        {
+            Check.NotNull(relationshipBuilder, "relationshipBuilder");
+
+            foreach (var relationshipConvention in OnForeignKeyAddedConventions)
+            {
+                relationshipBuilder = relationshipConvention.Apply(relationshipBuilder);
+                if (relationshipBuilder == null)
+                {
+                    break;
+                }
+            }
+
+            return relationshipBuilder;
         }
     }
 }

dotnet/efcore

src/EntityFramework.Core/Metadata/ModelBuilderFactory.cs
@@ -18,9 +18,12 @@ namespace Microsoft.Data.Entity.Metadata
         protected virtual ConventionsDispatcher CreateConventionsDispatcher()
         {
             var conventions = new ConventionsDispatcher();
-            conventions.EntityTypeConventions.Add(new PropertiesConvention());
-            conventions.EntityTypeConventions.Add(new KeyConvention());
-            conventions.EntityTypeConventions.Add(new RelationshipDiscoveryConvention());
+
+            conventions.EntityTypeAddedConventions.Add(new PropertiesConvention());
+            conventions.EntityTypeAddedConventions.Add(new KeyConvention());
+            conventions.EntityTypeAddedConventions.Add(new RelationshipDiscoveryConvention());
+
+            conventions.ForeignKeyAddedConventions.Add(new ForeignKeyPropertyDiscoveryConvention());
 
             return conventions;
         }

src/EntityFramework.Core/Metadata/ModelConventions/ConventionsDispatcher.cs
@@ -12,16 +12,19 @@ namespace Microsoft.Data.Entity.Metadata.ModelConventions
     {
         public ConventionsDispatcher()
         {
-            EntityTypeConventions = new List<IEntityTypeConvention>();
+            EntityTypeAddedConventions = new List<IEntityTypeConvention>();
+            ForeignKeyAddedConventions = new List<IRelationshipConvention>();
         }
 
-        public virtual IList<IEntityTypeConvention> EntityTypeConventions { get; }
+        public virtual IList<IEntityTypeConvention> EntityTypeAddedConventions { get; }
+
+        public virtual IList<IRelationshipConvention> ForeignKeyAddedConventions { get; }
 
         public virtual InternalEntityBuilder OnEntityTypeAdded([NotNull] InternalEntityBuilder entityBuilder)
         {
             Check.NotNull(entityBuilder, "entityBuilder");
 
-            foreach (var entityTypeConvention in EntityTypeConventions)
+            foreach (var entityTypeConvention in EntityTypeAddedConventions)
             {
                 entityBuilder = entityTypeConvention.Apply(entityBuilder);
                 if (entityBuilder == null)
@@ -31,6 +34,22 @@ namespace Microsoft.Data.Entity.Metadata.ModelConventions
             }
 
             return entityBuilder;
+        }
+
+        public virtual InternalRelationshipBuilder OnRelationshipAdded([NotNull] InternalRelationshipBuilder relationshipBuilder)
+        {
+            Check.NotNull(relationshipBuilder, "relationshipBuilder");
+
+            foreach (var relationshipConvention in ForeignKeyAddedConventions)
+            {
+                relationshipBuilder = relationshipConvention.Apply(relationshipBuilder);
+                if (relationshipBuilder == null)
+                {
+                    break;
+                }
+            }
+
+            return relationshipBuilder;
         }
     }
 }