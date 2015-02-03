74c7c79cb74e21ea29aaa41160d2ba6a59d56f50
Maurycy Markowski <maumar@microsoft.com>
2015-02-02 19:39:10 -0800

    fix to include bugs:
    #1452 - Multiple multi-level collection/reference Include can fail
    #1460 - "Sequence contains more than one matching element" thrown in some cases of multi level include
    
    #1452 - problem appears in include queries in which Id columns of Included entities have same name. When this happens, we uniquefy them (see #1362). However, for more complex multi-level include, we use those keys in sort clause in one stage of the include but then we need to uniquefy it in the stage that comes after. This would result in SQL that tried to apply alias in the sort clause which is invalid. Fix is to not generate the alias for the order by during sql generation. Also another issue around this area was that we were passing original column name rather than aliased one to the order clause in the SelectExpression.
    
    #1460 - problem appears in multi-level include queries that include same entity type multiple times in the chain. We uniquefy the names, however when constructing join conditions we only have column original name and table it comes from. This would lead to errors, since we assumed there would always be only one column in the projection list, that would match the criteria for the join condition. Fix is to use the last column from the list of candidates - as we always stick the column from the most recently joined table at the end of the projection list.
    
    Some tests are currently blocked by another issue - #1528

---
+++ test/EntityFramework.Core.FunctionalTests/TestModels/ComplexNavigationsModel/Level4.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Data.Entity.FunctionalTests.TestModels.ComplexNavigationsModel
+{
+    public class Level4
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Level3_Required_Id { get; set; }
+        public int? Level3_Optional_Id { get; set; }
+
+        public Level3 OneToOne_Required_PK_Inverse { get; set; }
+        public Level3 OneToOne_Optional_PK_Inverse { get; set; }
+        public Level3 OneToOne_Required_FK_Inverse { get; set; }
+        public Level3 OneToOne_Optional_FK_Inverse { get; set; }
+
+        public Level3 OneToMany_Required_Inverse { get; set; }
+        public Level3 OneToMany_Optional_Inverse { get; set; }
+
+        // issue #1417
+        //public Level4 OneToOne_Optional_Self { get; set; }
+
+        public ICollection<Level4> OneToMany_Required_Self { get; set; }
+        public ICollection<Level4> OneToMany_Optional_Self { get; set; }
+        public Level4 OneToMany_Required_Self_Inverse { get; set; }
+        public Level4 OneToMany_Optional_Self_Inverse { get; set; }
+    }
+}
