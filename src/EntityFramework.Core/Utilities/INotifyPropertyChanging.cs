dd8dbe54f498fc390f5917135c1aba6147f091d2
ajcvickers <ajcvickers@hotmail.com>
2015-02-10 15:02:58 -0800

    Revert INotifyPropertyChanging changes to fix .csproj test running

--- src/EntityFramework.Core/Utilities/INotifyPropertyChanging.cs
+++ src/EntityFramework.Core/Utilities/INotifyPropertyChanging.cs
@@ -7,28 +7,23 @@ using JetBrains.Annotations;
 #if !NET45 && !ASPNET50 && !ASPNETCORE50
 // TODO: Remove this file when VS can build against ".NET Portable 5.0"
 
-namespace Microsoft.Data.Entity.Utilities
+namespace Microsoft.Data.Entity
 {
-    internal interface INotifyPropertyChanging
+    public interface INotifyPropertyChanging
     {
         event PropertyChangingEventHandler PropertyChanging;
     }
 
-    internal delegate void PropertyChangingEventHandler(object sender, PropertyChangingEventArgs e);
+    public delegate void PropertyChangingEventHandler(object sender, PropertyChangingEventArgs e);
 
-    internal class PropertyChangingEventArgs : EventArgs
+    public class PropertyChangingEventArgs : EventArgs
     {
-        private readonly string _propertyName;
-
         public PropertyChangingEventArgs([CanBeNull] string propertyName)
         {
-            _propertyName = propertyName;
+            PropertyName = propertyName;
         }
 
-        public virtual string PropertyName
-        {
-            get { return _propertyName; }
-        }
+        public virtual string PropertyName { get; }
     }
 }
 
