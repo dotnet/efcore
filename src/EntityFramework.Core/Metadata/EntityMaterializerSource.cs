Andorbal/EntityFramework

Metadata: Inheritance: Adds more fundamental inheritance support.
- EntityTypes can now be abstract.
- Fixed bug in entity type index maintenance.
- ForeignKey now maintains referenced entity type independently of referenced key so we can handle FKs to derived types.
- Added EntityLoadInfo to query pipeline as a way to track which materializer is associated with a given value reader.
- Modified InMemoryDatabase to maintain a table for each concrete entity type.
- Lots of C# 6 goodness.

src/EntityFramework.Core/Metadata/EntityMaterializerSource.cs
@@ -51,12 +51,17 @@ namespace Microsoft.Data.Entity.Metadata
             var materializer = entityType as IEntityMaterializer;
             if (materializer != null)
             {
-                return materializer.CreatEntity;
+                return materializer.CreateEntity;
             }
 
             if (!entityType.HasClrType)
             {
                 throw new InvalidOperationException(Strings.NoClrType(entityType.Name));
+            }
+
+            if (entityType.IsAbstract)
+            {
+                throw new InvalidOperationException(Strings.CantMaterializeAbstractType(entityType));
             }
 
             return _cache.GetOrAdd(entityType.Type, k => BuildDelegate(entityType));

src/EntityFramework.Core/Properties/Strings.Designer.cs
@@ -908,6 +908,22 @@ namespace Microsoft.Data.Entity.Internal
             return string.Format(CultureInfo.CurrentCulture, GetString("DerivedEntityCannotHaveKeys", "entityType"), entityType);
         }
 
+        /// <summary>
+        /// Unable to build a materializer for entity type '{entityType}' because it is abstract. Either make it non-abstract or consider mapping at least one derived type.
+        /// </summary>
+        public static string CantMaterializeAbstractType([CanBeNull] object entityType)
+        {
+            return string.Format(CultureInfo.CurrentCulture, GetString("CantMaterializeAbstractType", "entityType"), entityType);
+        }
+
+        /// <summary>
+        /// The provided referenced entity key '{referencedKey}' is not a key on the entity type '{referencedEntityType}'.
+        /// </summary>
+        public static string ForeignKeyReferencedEntityKeyMismatch([CanBeNull] object referencedKey, [CanBeNull] object referencedEntityType)
+        {
+            return string.Format(CultureInfo.CurrentCulture, GetString("ForeignKeyReferencedEntityKeyMismatch", "referencedKey", "referencedEntityType"), referencedKey, referencedEntityType);
+        }
+
         private static string GetString(string name, params string[] formatterNames)
         {
             var value = _resourceManager.GetString(name);

dotnet/efcore

src/EntityFramework.Core/Metadata/EntityMaterializerSource.cs
@@ -51,12 +51,17 @@ namespace Microsoft.Data.Entity.Metadata
             var materializer = entityType as IEntityMaterializer;
             if (materializer != null)
             {
-                return materializer.CreatEntity;
+                return materializer.CreateEntity;
             }
 
             if (!entityType.HasClrType)
             {
                 throw new InvalidOperationException(Strings.NoClrType(entityType.Name));
+            }
+
+            if (entityType.IsAbstract)
+            {
+                throw new InvalidOperationException(Strings.CannotMaterializeAbstractType(entityType));
             }
 
             return _cache.GetOrAdd(entityType.Type, k => BuildDelegate(entityType));

src/EntityFramework.Core/Properties/Strings.Designer.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Data.Entity.Internal
     using System.Globalization;
     using System.Reflection;
     using System.Resources;
-    using JetBrains.Annotations;
+	using JetBrains.Annotations;
 
     public static class Strings
     {
@@ -138,14 +138,6 @@ namespace Microsoft.Data.Entity.Internal
         public static string CollectionArgumentIsEmpty([CanBeNull] object argumentName)
         {
             return string.Format(CultureInfo.CurrentCulture, GetString("CollectionArgumentIsEmpty", "argumentName"), argumentName);
-        }
-
-        /// <summary>
-        /// The edge cannot be added because the graph does not contain vertex '{vertex}'.
-        /// </summary>
-        public static string GraphDoesNotContainVertex([CanBeNull] object vertex)
-        {
-            return string.Format(CultureInfo.CurrentCulture, GetString("GraphDoesNotContainVertex", "vertex"), vertex);
         }
 
         /// <summary>
@@ -906,6 +898,30 @@ namespace Microsoft.Data.Entity.Internal
         public static string DerivedEntityCannotHaveKeys([CanBeNull] object entityType)
         {
             return string.Format(CultureInfo.CurrentCulture, GetString("DerivedEntityCannotHaveKeys", "entityType"), entityType);
+        }
+
+        /// <summary>
+        /// The edge cannot be added because the graph does not contain vertex '{vertex}'.
+        /// </summary>
+        public static string GraphDoesNotContainVertex([CanBeNull] object vertex)
+        {
+            return string.Format(CultureInfo.CurrentCulture, GetString("GraphDoesNotContainVertex", "vertex"), vertex);
+        }
+
+        /// <summary>
+        /// Unable to create an instance of type entity type '{entityType}' because it is abstract. Either make it non-abstract or consider mapping at least one derived type.
+        /// </summary>
+        public static string CannotMaterializeAbstractType([CanBeNull] object entityType)
+        {
+            return string.Format(CultureInfo.CurrentCulture, GetString("CannotMaterializeAbstractType", "entityType"), entityType);
+        }
+
+        /// <summary>
+        /// The provided referenced entity key '{referencedKey}' is not a key on the entity type '{referencedEntityType}'.
+        /// </summary>
+        public static string ForeignKeyReferencedEntityKeyMismatch([CanBeNull] object referencedKey, [CanBeNull] object referencedEntityType)
+        {
+            return string.Format(CultureInfo.CurrentCulture, GetString("ForeignKeyReferencedEntityKeyMismatch", "referencedKey", "referencedEntityType"), referencedKey, referencedEntityType);
         }
 
         private static string GetString(string name, params string[] formatterNames)