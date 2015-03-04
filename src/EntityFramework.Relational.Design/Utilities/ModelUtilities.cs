f4094233c0bb0ff3d2c9b55ad7170dc976fd2701
lajones <lajones@microsoft.com>
2015-03-04 09:36:52 -0800

    Make ModelUtilities a service and change the algorithm for producing candidate NavProp names.

--- src/EntityFramework.Relational.Design/Utilities/ModelUtilities.cs
+++ src/EntityFramework.Relational.Design/Utilities/ModelUtilities.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +9,10 @@ using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Utilities;
 
-namespace Microsoft.Data.Entity.Relational.Design.ReverseEngineering
+namespace Microsoft.Data.Entity.Relational.Design.Utilities
 {
     public class ModelUtilities
     {
-        private static readonly ModelUtilities _instance = new ModelUtilities();
-
-        public static ModelUtilities Instance
-        {
-            get
-            {
-                return _instance;
-            }
-        }
-
         public virtual string GenerateLambdaToKey(
             [NotNull] IEnumerable<IProperty> properties,
             [NotNull] string lambdaIdentifier)
@@ -49,7 +40,9 @@ namespace Microsoft.Data.Entity.Relational.Design.ReverseEngineering
         {
             Check.NotNull(entityType, nameof(entityType));
 
-            var primaryKeyProperties = entityType.GetPrimaryKey().Properties.ToList();
+            var primaryKeyProperties =
+                entityType.TryGetPrimaryKey()?.Properties.ToList()
+                ?? new List<IProperty>();
             foreach (var property in primaryKeyProperties)
             {
                 yield return property;
@@ -65,6 +58,35 @@ namespace Microsoft.Data.Entity.Relational.Design.ReverseEngineering
             {
                 yield return property;
             }
+        }
+
+        public virtual string GetDependentEndCandidateNavigationPropertyName([NotNull] ForeignKey foreignKey)
+        {
+            Check.NotNull(foreignKey, nameof(foreignKey));
+
+            if (foreignKey.Properties.Count == 1)
+            {
+                var foreignKeyPropertyName = foreignKey.Properties.Single().Name;
+                if (foreignKeyPropertyName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
+                {
+                    foreignKeyPropertyName =
+                        foreignKeyPropertyName.Substring(0, foreignKeyPropertyName.Length - 2);
+                }
+
+                if (!string.IsNullOrEmpty(foreignKeyPropertyName))
+                {
+                    return foreignKeyPropertyName;
+                }
+            }
+
+            return foreignKey.ReferencedEntityType.SimpleName;
+        }
+
+        public virtual string GetPrincipalEndCandidateNavigationPropertyName([NotNull] ForeignKey foreignKey)
+        {
+            Check.NotNull(foreignKey, nameof(foreignKey));
+
+            return foreignKey.EntityType.SimpleName;
         }
     }
 }
