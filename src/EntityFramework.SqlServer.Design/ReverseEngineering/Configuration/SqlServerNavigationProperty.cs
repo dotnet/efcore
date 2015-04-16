5a66106c8aa2cf080b7b78742d89b3491ab75724
lajones <lajones@microsoft.com>
2015-04-16 15:37:31 -0700

    Fix for issue 1933 - combine relationship config with other config.

---
+++ src/EntityFramework.SqlServer.Design/ReverseEngineering/Configuration/SqlServerNavigationProperty.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Utilities;
+
+namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering.Configuration
+{
+    public class SqlServerNavigationProperty
+    {
+        public SqlServerNavigationProperty([NotNull]string errorAnnotation)
+        {
+            Check.NotEmpty(errorAnnotation, nameof(errorAnnotation));
+
+            ErrorAnnotation = errorAnnotation;
+        }
+
+        public SqlServerNavigationProperty([NotNull]string type, [NotNull]string name)
+        {
+            Check.NotNull(type, nameof(type));
+            Check.NotEmpty(name, nameof(name));
+
+            Type = type;
+            Name = name;
+        }
+
+        public virtual string ErrorAnnotation { get;[param: NotNull]private set; }
+        public virtual string Type { get;[param: NotNull]private set; }
+        public virtual string Name { get;[param: NotNull]private set; }
+    }
+}
---
+++ src/EntityFramework.SqlServer.Design/ReverseEngineering/Configuration/SqlServerNavigationPropertyInitializer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Utilities;
+
+namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering.Configuration
+{
+    public class SqlServerNavigationPropertyInitializer
+    {
+        public SqlServerNavigationPropertyInitializer([NotNull]string navPropName, [NotNull]string principalEntityTypeName)
+        {
+            Check.NotEmpty(navPropName, nameof(navPropName));
+            Check.NotEmpty(principalEntityTypeName, nameof(principalEntityTypeName));
+
+            NavigationPropertyName = navPropName;
+            PrincipalEntityTypeName = principalEntityTypeName;
+        }
+
+        public virtual string NavigationPropertyName { get;[param: NotNull]private set; }
+        public virtual string PrincipalEntityTypeName { get;[param: NotNull]private set; }
+    }
+}
