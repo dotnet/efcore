65361759e78b7d371180b416f7c316b02e8030aa
ajcvickers <ajcvickers@hotmail.com>
2015-04-24 15:17:35 -0700

    Post-process DBNulls when using non-typed value reader factory
    This is a precursor to replacement of IValueReader with ValueBuffer struct. Attempted to do this only when the property value can be null, but this is hard to determine so talked with Andrew and decided to process all values for now.

--- src/EntityFramework.Relational/NonTypedValueReaderFactory.cs
+++ src/EntityFramework.Relational/NonTypedValueReaderFactory.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Data.Common;
 using System.Diagnostics;
 using Microsoft.Data.Entity.Storage;
@@ -10,10 +11,12 @@ namespace Microsoft.Data.Entity.Relational
     public class NonTypedValueReaderFactory : IRelationalValueReaderFactory
     {
         private readonly int _offset;
+        private readonly int _bufferSize;
 
-        public NonTypedValueReaderFactory(int offset)
+        public NonTypedValueReaderFactory(int offset, int count)
         {
             _offset = offset;
+            _bufferSize = offset + count;
         }
 
         public virtual IValueReader CreateValueReader(DbDataReader dataReader)
@@ -24,7 +27,15 @@ namespace Microsoft.Data.Entity.Relational
 
             dataReader.GetValues(values);
 
-            return new RelationalObjectArrayValueReader(values, _offset);
+            for (var i = _offset; i < _bufferSize; i++)
+            {
+                if (ReferenceEquals(values[i], DBNull.Value))
+                {
+                    values[i] = null;
+                }
+            }
+
+            return new ObjectArrayValueReader(values, _offset);
         }
     }
 }
