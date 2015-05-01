7fbd782a49e1e2967f9b57e9cf704c5e4210f8e6
lajones <lajones@microsoft.com>
2015-05-01 14:21:42 -0700

    Adding E2E Tests for SqlServer.Design. Adding FileService to abstract outputting files so can override for tests. Ensuring resources have same name when built using VS as when built using command line. Fixing some error messages.

---
+++ test/EntityFramework.SqlServer.Design.FunctionalTests/ReverseEngineering/E2EFixture.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.Entity.SqlServer.FunctionalTests;
+
+namespace EntityFramework.SqlServer.Design.ReverseEngineering.FunctionalTests
+{
+    public class E2EFixture
+    {
+        public E2EFixture()
+        {
+            SqlServerTestStore.CreateDatabase(
+                "SqlServerReverseEngineerTestE2E", @"ReverseEngineering\E2E.sql", true);
+        }
+    }
+}
