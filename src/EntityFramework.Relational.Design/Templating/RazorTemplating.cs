e8bbd6fcacc3856aa693f206858735dc9f0d7ad2
danielmarbach <daniel.marbach@openplace.net>
2015-05-21 00:12:23 +0200

    Use CancellationToken when possible

--- src/EntityFramework.Relational.Design/Templating/RazorTemplating.cs
+++ src/EntityFramework.Relational.Design/Templating/RazorTemplating.cs
@@ -71,7 +71,7 @@ namespace Microsoft.Data.Entity.Relational.Design.Templating
                 {
                     razorTemplate.Model = templateModel;
                     //ToDo: If there are errors executing the code, they are missed here.
-                    result = await razorTemplate.ExecuteTemplateAsync();
+                    result = await razorTemplate.ExecuteTemplateAsync(cancellationToken);
                 }
 
                 return new TemplateResult()
