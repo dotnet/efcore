a2682a0b31d3195e761bd921d27bd83d47c48a88
Brice Lambson <bricelam@microsoft.com>
2015-05-22 15:47:26 -0700

    Use IRelationalMetadataExtensionProvider in ModelDiffer
    Fixes #1919 & fixes #1975

--- test/EntityFramework.Relational.Tests/Migrations/Infrastructure/ModelDifferTest.cs
+++ test/EntityFramework.Relational.Tests/Migrations/Infrastructure/ModelDifferTest.cs
@@ -1,47 +1,57 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Data.Entity.Relational.Migrations.Infrastructure;
 using Microsoft.Data.Entity.Relational.Migrations.Operations;
 using Xunit;
 
-namespace Microsoft.Data.Entity.Relational.Tests.Migrations.Infrastructure
+namespace Microsoft.Data.Entity.Relational.Migrations.Infrastructure
 {
-    public class ModelDifferTest
+    public class ModelDifferTest : ModelDifferTestBase
     {
         [Fact]
         public void Model_differ_breaks_foreign_key_cycles()
         {
-            var model = new Entity.Metadata.Model();
+            Execute(
+                _ => { },
+                modelBuilder =>
+                {
+                    modelBuilder.Entity(
+                        "First",
+                        x =>
+                        {
+                            x.Property<int>("ID");
+                            x.Key("ID");
+                            x.Property<int>("FK");
+                        });
 
-            var firstType = model.AddEntityType("First");
-            var firstKey = firstType.SetPrimaryKey(firstType.AddProperty("ID", typeof(int), true));
-            var firstFk = firstType.AddProperty("FK", typeof(int), true);
+                    modelBuilder.Entity(
+                        "Second",
+                        x =>
+                        {
+                            x.Property<int>("ID");
+                            x.Key("ID");
+                            x.Property<int>("FK");
+                        });
 
-            var secondType = model.AddEntityType("Second");
-            var secondKey = secondType.SetPrimaryKey(secondType.AddProperty("ID", typeof(int), true));
-            var secondFk = secondType.AddProperty("FK", typeof(int), true);
+                    modelBuilder.Entity("First").Reference("Second").InverseCollection().ForeignKey("FK").PrincipalKey("ID");
+                    modelBuilder.Entity("Second").Reference("First").InverseCollection().ForeignKey("FK").PrincipalKey("ID");
+                },
+                result =>
+                {
+                    Assert.Equal(3, result.Count);
 
-            firstType.AddForeignKey(firstFk, secondKey);
-            secondType.AddForeignKey(secondFk, firstKey);
+                    var firstOperation = result[0] as CreateTableOperation;
+                    var secondOperation = result[1] as CreateTableOperation;
+                    var thirdOperation = result[2] as AddForeignKeyOperation;
 
-            var modelDiffer = new ModelDiffer(new RelationalTypeMapper());
+                    Assert.NotNull(firstOperation);
+                    Assert.NotNull(secondOperation);
+                    Assert.NotNull(thirdOperation);
 
-            var result = modelDiffer.GetDifferences(null, model);
-
-            Assert.Equal(3, result.Count);
-
-            var firstOperation = result[0] as CreateTableOperation;
-            var secondOperation = result[1] as CreateTableOperation;
-            var thirdOperation = result[2] as AddForeignKeyOperation;
-
-            Assert.NotNull(firstOperation);
-            Assert.NotNull(secondOperation);
-            Assert.NotNull(thirdOperation);
-
-            Assert.Equal(0, firstOperation.ForeignKeys.Count);
-            Assert.Equal(1, secondOperation.ForeignKeys.Count);
-            Assert.Equal(firstOperation.Name, thirdOperation.Table);
+                    Assert.Equal(0, firstOperation.ForeignKeys.Count);
+                    Assert.Equal(1, secondOperation.ForeignKeys.Count);
+                    Assert.Equal(firstOperation.Name, thirdOperation.Table);
+                });
         }
     }
 }
