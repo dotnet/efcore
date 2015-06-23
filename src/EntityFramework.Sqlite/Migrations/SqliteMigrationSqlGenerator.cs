rafaelsc/EntityFramework

Implementation of dropcolumn for SQLite using migration transforms.

src/EntityFramework.Sqlite/Migrations/SqliteMigrationSqlGenerator.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Relational;
+using Microsoft.Data.Entity.Relational.Migrations;
 using Microsoft.Data.Entity.Relational.Migrations.Operations;
 using Microsoft.Data.Entity.Relational.Migrations.Sql;
 using Microsoft.Data.Entity.Utilities;
@@ -14,12 +17,27 @@ namespace Microsoft.Data.Entity.Sqlite.Migrations
     public class SqliteMigrationSqlGenerator : MigrationSqlGenerator
     {
         private readonly ISqlGenerator _sql;
+        private readonly MigrationOperationTransformer _transformer;
 
         public SqliteMigrationSqlGenerator(
-            [NotNull] ISqlGenerator sqlGenerator)
+            [NotNull] ISqlGenerator sqlGenerator,
+            [CanBeNull] MigrationOperationTransformer transformer)
             : base(sqlGenerator)
         {
             _sql = sqlGenerator;
+            _transformer = transformer;
+        }
+
+        public override IReadOnlyList<SqlBatch> Generate(IReadOnlyList<MigrationOperation> operations, IModel model = null)
+        {
+            Check.NotNull(operations, nameof(operations));
+
+            if (_transformer != null)
+            {
+                operations = _transformer.Transform(operations, model);
+            }
+
+            return base.Generate(operations, model);
         }
 
         public override void Generate(DropIndexOperation operation, IModel model, SqlBatchBuilder builder)
@@ -45,6 +63,29 @@ namespace Microsoft.Data.Entity.Sqlite.Migrations
                     .Append(" RENAME TO ")
                     .Append(_sql.DelimitIdentifier(operation.NewName));
             }
+        }
+
+        public virtual void Generate([NotNull] MoveDataOperation operation, [CanBeNull] IModel model, [NotNull] SqlBatchBuilder builder)
+        {
+            Check.NotNull(operation, nameof(operation));
+            Check.NotNull(builder, nameof(builder));
+
+            if (operation.Columns?.Length == 0)
+            {
+                return;
+            }
+
+            var columnList = string.Join(", ", operation.Columns.Select(s => _sql.DelimitIdentifier(s)));
+
+            builder.Append("INSERT INTO ")
+                .Append(_sql.DelimitIdentifier(operation.NewTable))
+                .Append(" (")
+                .Append(columnList)
+                .AppendLine(")")
+                .Append("SELECT ")
+                .Append(columnList)
+                .Append(" FROM ")
+                .Append(_sql.DelimitIdentifier(operation.OldTable));
         }
 
         #region Invalid migration operations

src/EntityFramework.Sqlite/SqliteEntityFrameworkServicesBuilderExtensions.cs
@@ -28,6 +28,7 @@ namespace Microsoft.Framework.DependencyInjection
                     .AddSingleton<SqliteMetadataExtensionProvider>()
                     .AddSingleton<SqliteTypeMapper>()
                     .AddSingleton<SqliteModelSource>()
+                    .AddSingleton<SqliteOperationTransformer>()
                     .AddScoped<SqliteModificationCommandBatchFactory>()
                     .AddScoped<SqliteDataStoreServices>()
                     .AddScoped<SqliteDataStore>()

dotnet/efcore

src/EntityFramework.Sqlite/Migrations/SqliteMigrationSqlGenerator.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Relational;
@@ -14,12 +16,27 @@ namespace Microsoft.Data.Entity.Sqlite.Migrations
     public class SqliteMigrationSqlGenerator : MigrationSqlGenerator
     {
         private readonly ISqlGenerator _sql;
+        private readonly SqliteOperationTransformer _transformer;
 
         public SqliteMigrationSqlGenerator(
-            [NotNull] ISqlGenerator sqlGenerator)
+            [NotNull] ISqlGenerator sqlGenerator,
+            [CanBeNull] SqliteOperationTransformer transformer)
             : base(sqlGenerator)
         {
             _sql = sqlGenerator;
+            _transformer = transformer;
+        }
+
+        public override IReadOnlyList<SqlBatch> Generate(IReadOnlyList<MigrationOperation> operations, IModel model = null)
+        {
+            Check.NotNull(operations, nameof(operations));
+
+            if (_transformer != null)
+            {
+                operations = _transformer.Transform(operations, model);
+            }
+
+            return base.Generate(operations, model);
         }
 
         public override void Generate(DropIndexOperation operation, IModel model, SqlBatchBuilder builder)
@@ -45,6 +62,29 @@ namespace Microsoft.Data.Entity.Sqlite.Migrations
                     .Append(" RENAME TO ")
                     .Append(_sql.DelimitIdentifier(operation.NewName));
             }
+        }
+
+        public virtual void Generate([NotNull] MoveDataOperation operation, [CanBeNull] IModel model, [NotNull] SqlBatchBuilder builder)
+        {
+            Check.NotNull(operation, nameof(operation));
+            Check.NotNull(builder, nameof(builder));
+
+            if (operation.Columns?.Length == 0)
+            {
+                return;
+            }
+
+            var columnList = string.Join(", ", operation.Columns.Select(s => _sql.DelimitIdentifier(s)));
+
+            builder.Append("INSERT INTO ")
+                .Append(_sql.DelimitIdentifier(operation.NewTable))
+                .Append(" (")
+                .Append(columnList)
+                .AppendLine(")")
+                .Append("SELECT ")
+                .Append(columnList)
+                .Append(" FROM ")
+                .Append(_sql.DelimitIdentifier(operation.OldTable));
         }
 
         #region Invalid migration operations

src/EntityFramework.Sqlite/SqliteEntityFrameworkServicesBuilderExtensions.cs
@@ -4,6 +4,7 @@
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Data.Entity.Relational;
+using Microsoft.Data.Entity.Relational.Migrations;
 using Microsoft.Data.Entity.Sqlite;
 using Microsoft.Data.Entity.Sqlite.Metadata;
 using Microsoft.Data.Entity.Sqlite.Migrations;
@@ -32,6 +33,7 @@ namespace Microsoft.Framework.DependencyInjection
                     .AddScoped<SqliteDataStoreServices>()
                     .AddScoped<SqliteDataStore>()
                     .AddScoped<SqliteDataStoreConnection>()
+                    .AddScoped<SqliteOperationTransformer>()
                     .AddScoped<SqliteMigrationSqlGenerator>()
                     .AddScoped<SqliteDataStoreCreator>()
                     .AddScoped<SqliteHistoryRepository>()