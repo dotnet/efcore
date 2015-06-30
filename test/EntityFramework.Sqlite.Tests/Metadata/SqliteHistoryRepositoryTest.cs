055ee695f7c06c3b4ee33f9e43468de03a326836
Nate McMaster <nate.mcmaster@microsoft.com>
2015-06-30 16:00:03 -0700

    Fix broken tests for SQLite migrations. Remove usage of Moq.

--- test/EntityFramework.Sqlite.Tests/Metadata/SqliteHistoryRepositoryTest.cs
+++ test/EntityFramework.Sqlite.Tests/Metadata/SqliteHistoryRepositoryTest.cs
@@ -2,12 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
+using System.Threading;
+using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Data.Entity.Migrations.History;
 using Microsoft.Data.Entity.Migrations.Operations;
 using Microsoft.Data.Entity.Sqlite.Migrations;
-using Microsoft.Data.Entity.Storage;
-using Microsoft.Data.Sqlite;
-using Moq;
+using Microsoft.Framework.Logging;
 using Xunit;
 
 namespace Microsoft.Data.Entity.Sqlite.Metadata
@@ -83,17 +84,16 @@ namespace Microsoft.Data.Entity.Sqlite.Metadata
         [Fact]
         public void Exists_finds_existing_table()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            using (var cmd = connection.CreateCommand())
+            var connection = SqliteTestConnection.CreateScratch();
+            connection.Open();
+            using (var cmd = connection.DbConnection.CreateCommand())
             {
                 cmd.CommandText = "CREATE TABLE __migrationHistory (column1);";
                 connection.Open();
                 cmd.ExecuteNonQuery();
             }
-            var mockConnection = new Mock<IRelationalConnection>();
-            mockConnection.SetupGet(p => p.DbConnection).Returns(connection);
 
-            var hp = new SqliteHistoryRepository(mockConnection.Object, new TestContext(), new SqliteUpdateSqlGenerator());
+            var hp = new SqliteHistoryRepository(connection, new TestContext(), new SqliteUpdateSqlGenerator());
 
             Assert.True(hp.Exists());
         }
@@ -101,11 +101,9 @@ namespace Microsoft.Data.Entity.Sqlite.Metadata
         [Fact]
         public void Exists_no_table()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            var mockConnection = new Mock<IRelationalConnection>();
-            mockConnection.SetupGet(p => p.DbConnection).Returns(connection);
+            var connection = SqliteTestConnection.CreateScratch();
 
-            var hp = new SqliteHistoryRepository(mockConnection.Object, new TestContext(), new SqliteUpdateSqlGenerator());
+            var hp = new SqliteHistoryRepository(connection, new TestContext(), new SqliteUpdateSqlGenerator());
 
             Assert.False(hp.Exists());
         }
@@ -113,23 +111,21 @@ namespace Microsoft.Data.Entity.Sqlite.Metadata
         [Fact]
         public void GetAppliedMigrations_finds_migrations()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            var mockConnection = new Mock<IRelationalConnection>();
-            mockConnection.SetupGet(p => p.DbConnection).Returns(connection);
-            using (var command = connection.CreateCommand())
+            var testConnection = SqliteTestConnection.CreateScratch();
+            testConnection.Open();
+            using (var command = testConnection.DbConnection.CreateCommand())
             {
                 command.CommandText = CreateSqliteHistoryRepo().Create(true);
-                connection.Open();
                 command.ExecuteNonQuery();
             }
-            using (var command = connection.CreateCommand())
+            using (var command = testConnection.DbConnection.CreateCommand())
             {
                 command.CommandText = "INSERT INTO __migrationHistory VALUES ('different_context','SomeFakeContext','1');";
                 command.ExecuteNonQuery();
             }
 
             var row = new HistoryRow("Mig1", "7");
-            using (var command = connection.CreateCommand())
+            using (var command = testConnection.DbConnection.CreateCommand())
             {
                 var operation = CreateSqliteHistoryRepo()
                     .GetInsertOperation(row) as SqlOperation;
@@ -137,16 +133,59 @@ namespace Microsoft.Data.Entity.Sqlite.Metadata
                 command.ExecuteNonQuery();
             }
 
-            var hp = new SqliteHistoryRepository(mockConnection.Object, new TestContext(), new SqliteUpdateSqlGenerator());
+            var hp = new SqliteHistoryRepository(testConnection, new TestContext(), new SqliteUpdateSqlGenerator());
 
             Assert.Collection(hp.GetAppliedMigrations(), p =>
                 {
                     Assert.Equal(row.MigrationId, p.MigrationId);
                     Assert.Equal(row.ProductVersion, p.ProductVersion);
                 });
+            testConnection.Close();
         }
 
-        private static SqliteHistoryRepository CreateSqliteHistoryRepo() => new SqliteHistoryRepository(Mock.Of<IRelationalConnection>(), new TestContext(), new SqliteUpdateSqlGenerator());
+        public class SqliteTestConnection : SqliteDatabaseConnection
+        {
+            public SqliteTestConnection(IDbContextOptions options)
+                : base(options, new LoggerFactory())
+            {
+            }
+
+            private static int _scratchCount;
+
+            private string _fileName;
+
+            public static SqliteTestConnection CreateScratch()
+            {
+                var options = new DbContextOptionsBuilder();
+                string name;
+                do
+                {
+                    name = "scratch-" + Interlocked.Increment(ref _scratchCount) + ".db";
+                }
+                while (File.Exists(name));
+                options.UseSqlite("Data Source=" + name);
+                var connection = new SqliteTestConnection(options.Options) { _fileName = name };
+                return connection;
+            }
+
+            public override void Dispose()
+            {
+                base.Dispose();
+                if (_fileName != null)
+                {
+                    File.Delete(_fileName);
+                }
+            }
+
+            public static SqliteTestConnection InMemory()
+            {
+                var options = new DbContextOptionsBuilder();
+                options.UseSqlite("Data Source=:memory:");
+                return new SqliteTestConnection(options.Options);
+            }
+        }
+
+        private static SqliteHistoryRepository CreateSqliteHistoryRepo() => new SqliteHistoryRepository(SqliteTestConnection.InMemory(), new TestContext(), new SqliteUpdateSqlGenerator());
 
         private class TestContext : DbContext
         {
