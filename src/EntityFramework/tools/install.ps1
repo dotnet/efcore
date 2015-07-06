a2cfc4c8363611bfa2feca04bf7131f9e575e1c4
Brice Lambson <bricelam@microsoft.com>
2015-07-06 10:11:36 -0700

    Remove EntityFramework meta-package

--- src/EntityFramework/tools/install.ps1
+++
@@ -1,4 +0,0 @@
-param ($installPath, $toolsPath, $package, $project)
-
-$project.ProjectItems.Item('deleteme').Delete()
-$DTE.ItemOperations.Navigate((Join-Path $installPath readme.html)) | Out-Null
