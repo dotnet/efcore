7f864e02957d6562178e9a8e6fc7e0ea6e3d7322
laskoviymishka <laskoviymishka@gmail.com>
2015-07-08 00:20:10 +0300

    Resolves #2212 Add ApplyMigrationsAsync (and related async methods)

--- src/EntityFramework.Relational/Migrations/IMigrator.cs
+++ src/EntityFramework.Relational/Migrations/IMigrator.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 
 namespace Microsoft.Data.Entity.Migrations
@@ -11,6 +12,8 @@ namespace Microsoft.Data.Entity.Migrations
         IReadOnlyList<Migration> GetUnappliedMigrations();
 
         bool HasPendingModelChanges();
+
+        Task ApplyMigrationsAsync([CanBeNull] string targetMigration = null);
 
         void ApplyMigrations([CanBeNull] string targetMigration = null);
 
