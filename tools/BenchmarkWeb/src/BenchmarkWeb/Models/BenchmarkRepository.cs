cd85e445123729df6c24e2710b28654691eca219
Rowan Miller <mail@romiller.com>
2015-08-06 13:51:20 -0700

    :bar_chart: Reporting for benchmark results
    This is a pragmatic start at some reporting for benchmark results. There
    is certainly lots of room for improvement (adopting a plugin to render
    pivot tables, ajax loading of data for charts, etc.) but we can evolve
    as we work out what information is useful to us.
    
    The site will be hosted hooked up to our performance CI results, but you
    can also just run it locally and it will look at the same local database
    used to store local benchmark runs (which should be useful when working
    on local perf improvements).

---
+++ tools/BenchmarkWeb/src/BenchmarkWeb/Models/BenchmarkRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BenchmarkWeb.Models
+{
+    public class BenchmarkRepository
+    {
+        private readonly BenchmarkContext _context;
+        private static readonly string _latestResultsSql =
+@"SELECT Runs.*
+FROM 
+	dbo.Runs AS Runs
+	INNER JOIN (SELECT TestClass, TestMethod, Variation, ProductReportingVersion, Framework, MAX(RunStarted) LastRun
+				FROM dbo.Runs
+                WHERE RunStarted > GETDATE() - 30
+				GROUP BY TestClass, TestMethod, Variation, ProductReportingVersion, Framework) AS LastRuns
+	ON
+		Runs.TestClass = LastRuns.TestClass
+		AND Runs.TestMethod = LastRuns.TestMethod
+		AND Runs.Variation = LastRuns.Variation
+		AND Runs.ProductReportingVersion = LastRuns.ProductReportingVersion
+		AND Runs.Framework = LastRuns.Framework
+		AND Runs.RunStarted = LastRuns.LastRun";
+
+        public BenchmarkRepository(BenchmarkContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Run> GetLatestResults()
+        {
+            return _context.Runs
+                .FromSql(_latestResultsSql)
+                .ToList();
+        }
+
+        public IEnumerable<Run> GetTestHistory(string testClass, string testMethod)
+        {
+            return _context.Runs
+                .Where(r => r.TestClass == testClass && r.TestMethod == testMethod && r.Framework == ".NETFramework")
+                .OrderByDescending(r => r.RunStarted)
+                .Take(30);
+        }
+    }
+}
---
+++ tools/BenchmarkWeb/src/BenchmarkWeb/Models/Run.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BenchmarkWeb.Models
+{
+    public class Run
+    {
+        public int Id { get; set; }
+
+        // Dimensions
+        public string TestClassFullName { get; set; }
+        public string TestClass { get; set; }
+        public string TestMethod { get; set; }
+        public string Variation { get; set; }
+        public string MachineName { get; set; }
+        public string ProductReportingVersion { get; set; }
+        public string Framework { get; set; }
+        public string CustomData { get; set; }
+        public DateTime RunStarted { get; set; }
+        public int WarmupIterations { get; set; }
+        public int Iterations { get; set; }
+
+        // Metrics
+        public long TimeElapsedAverage { get; private set; }
+        public long TimeElapsedPercentile99 { get; private set; }
+        public long TimeElapsedPercentile95 { get; private set; }
+        public long TimeElapsedPercentile90 { get; private set; }
+        public double TimeElapsedStandardDeviation { get; private set; }
+
+        public long MemoryDeltaAverage { get; private set; }
+        public long MemoryDeltaPercentile99 { get; private set; }
+        public long MemoryDeltaPercentile95 { get; private set; }
+        public long MemoryDeltaPercentile90 { get; private set; }
+        public double MemoryDeltaStandardDeviation { get; private set; }
+
+        public string ToolTip
+        {
+            get
+            {
+                return $"Collected: {RunStarted}{Environment.NewLine}{CustomData}";
+            }
+        }
+    }
+}
