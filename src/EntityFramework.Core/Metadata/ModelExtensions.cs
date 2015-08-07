wangchez/EntityFramework

Allow to set Property.ClrType Remove ClrType and IsShadowProperty from the Property constructor Set default ClrType to string and make properties shadow by default Move CLR type validation to ModelValidator Make IModel.GetReferencingForeignKeys extension methods and rename to FindReferencingForeignKeys

src/EntityFramework.Core/Metadata/ModelExtensions.cs
@@ -10,19 +10,61 @@ namespace Microsoft.Data.Entity.Metadata
 {
     public static class ModelExtensions
     {
-        // TODO: Perf: consider not needing to do a full scan here
         public static IEnumerable<INavigation> GetNavigations(
             [NotNull] this IModel model, [NotNull] IForeignKey foreignKey)
-            => model.EntityTypes.SelectMany(e => e.GetNavigations()).Where(n => n.ForeignKey == foreignKey);
+        {
+            Check.NotNull(model, nameof(model));
+
+            // TODO: Perf: consider not needing to do a full scan here
+            return model.EntityTypes.SelectMany(e => e.GetNavigations()).Where(n => n.ForeignKey == foreignKey);
+        }
 
         public static string GetProductVersion([NotNull] this IModel model)
-            => model[CoreAnnotationNames.ProductVersionAnnotation] as string;
+        {
+            Check.NotNull(model, nameof(model));
+
+            return model[CoreAnnotationNames.ProductVersionAnnotation] as string;
+        }
 
         public static void SetProductVersion([NotNull] this Model model, [NotNull] string value)
         {
+            Check.NotNull(model, nameof(model));
             Check.NotEmpty(value, nameof(value));
 
             model[CoreAnnotationNames.ProductVersionAnnotation] = value;
+        }
+        
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IEntityType entityType)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(entityType, nameof(entityType));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes.SelectMany(et => et.GetForeignKeys())
+                .Where(fk => fk.PrincipalEntityType.IsAssignableFrom(entityType));
+        }
+
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IKey key)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(key, nameof(key));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes.SelectMany(e => e.GetForeignKeys()).Where(fk => fk.PrincipalKey == key);
+        }
+
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IProperty property)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(property, nameof(property));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes
+                .SelectMany(e => e.GetForeignKeys()
+                    .Where(f => f.PrincipalKey.Properties.Contains(property)));
         }
     }
 }

src/EntityFramework.SqlServer.Design/ReverseEngineering/SqlServerMetadataModelProvider.cs
@@ -272,9 +272,8 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                         Strings.CannotFindTableForColumn(tc.Id, tc.TableId));
                     continue;
                 }
-
-                // IModel will not allow Properties to be created without a Type, so map to CLR type here.
-                // This means if we come across a column with a SQL Server type which we can't map we will ignore it.
+                
+                // If we come across a column with a SQL Server type which we can't map we will ignore it.
                 // Note: foreign key properties appear just like any other property in the relational model.
                 Type clrPropertyType;
                 if (!SqlServerTypeMapping._sqlTypeToClrTypeMap.TryGetValue(tc.DataType, out clrPropertyType))
@@ -289,7 +288,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                     clrPropertyType = clrPropertyType.MakeNullable();
                 }
 
-                var relationalProperty = entityType.AddProperty(tc.Id, clrPropertyType, shadowProperty: true);
+                var relationalProperty = entityType.AddProperty(tc.Id, clrPropertyType);
                 _relationalColumnIdToRelationalPropertyMap[tc.Id] = relationalProperty;
             }
 
@@ -343,10 +342,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                         }
                     }
 
-                    var codeGenProperty = codeGenEntityType.AddProperty(
-                        nameMapper.PropertyToPropertyNameMap[relationalProperty],
-                        relationalProperty.ClrType,
-                        shadowProperty: true);
+                    var codeGenProperty = codeGenEntityType.AddProperty(nameMapper.PropertyToPropertyNameMap[relationalProperty], ((IProperty)relationalProperty).ClrType);
                     _relationalPropertyToCodeGenPropertyMap[relationalProperty] = codeGenProperty;
                     ApplyPropertyProperties(codeGenProperty, _tableColumns[relationalProperty.Name]);
                 } // end of loop over all relational properties for given EntityType
@@ -625,7 +621,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                 var defaultExpressionOrValue =
                     _sqlServerLiteralUtilities
                         .ConvertSqlServerDefaultValue(
-                            property.ClrType, tableColumn.DefaultValue);
+                            ((IProperty)property).ClrType, tableColumn.DefaultValue);
                 if (defaultExpressionOrValue != null
                     && defaultExpressionOrValue.DefaultExpression != null)
                 {
@@ -641,7 +637,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                     _logger.LogWarning(
                         Strings.UnableToConvertDefaultValue(
                             tableColumn.Id, tableColumn.DefaultValue,
-                            property.ClrType, property.Name, property.DeclaringEntityType.Name));
+                            ((IProperty)property).ClrType, property.Name, property.DeclaringEntityType.Name));
                 }
             }
         }

dotnet/efcore

src/EntityFramework.Core/Metadata/ModelExtensions.cs
@@ -10,19 +10,61 @@ namespace Microsoft.Data.Entity.Metadata
 {
     public static class ModelExtensions
     {
-        // TODO: Perf: consider not needing to do a full scan here
         public static IEnumerable<INavigation> GetNavigations(
             [NotNull] this IModel model, [NotNull] IForeignKey foreignKey)
-            => model.EntityTypes.SelectMany(e => e.GetNavigations()).Where(n => n.ForeignKey == foreignKey);
+        {
+            Check.NotNull(model, nameof(model));
+
+            // TODO: Perf: consider not needing to do a full scan here
+            return model.EntityTypes.SelectMany(e => e.GetNavigations()).Where(n => n.ForeignKey == foreignKey);
+        }
 
         public static string GetProductVersion([NotNull] this IModel model)
-            => model[CoreAnnotationNames.ProductVersionAnnotation] as string;
+        {
+            Check.NotNull(model, nameof(model));
+
+            return model[CoreAnnotationNames.ProductVersionAnnotation] as string;
+        }
 
         public static void SetProductVersion([NotNull] this Model model, [NotNull] string value)
         {
+            Check.NotNull(model, nameof(model));
             Check.NotEmpty(value, nameof(value));
 
             model[CoreAnnotationNames.ProductVersionAnnotation] = value;
+        }
+        
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IEntityType entityType)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(entityType, nameof(entityType));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes.SelectMany(et => et.GetDeclaredForeignKeys())
+                .Where(fk => fk.PrincipalEntityType.IsAssignableFrom(entityType));
+        }
+
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IKey key)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(key, nameof(key));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes.SelectMany(e => e.GetDeclaredForeignKeys()).Where(fk => fk.PrincipalKey == key);
+        }
+
+        public static IEnumerable<IForeignKey> FindReferencingForeignKeys([NotNull] this IModel model, [NotNull] IProperty property)
+        {
+            Check.NotNull(model, nameof(model));
+            Check.NotNull(property, nameof(property));
+
+            // TODO: Perf: Add additional indexes so that this isn't a linear lookup
+            // Issue #1179
+            return model.EntityTypes
+                .SelectMany(e => e.GetDeclaredForeignKeys()
+                    .Where(f => f.PrincipalKey.Properties.Contains(property)));
         }
     }
 }

src/EntityFramework.SqlServer.Design/ReverseEngineering/SqlServerMetadataModelProvider.cs
@@ -212,9 +212,8 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                         Strings.CannotFindTableForColumn(tc.Id, tc.TableId));
                     continue;
                 }
-
-                // IModel will not allow Properties to be created without a Type, so map to CLR type here.
-                // This means if we come across a column with a SQL Server type which we can't map we will ignore it.
+                
+                // If we come across a column with a SQL Server type which we can't map we will ignore it.
                 // Note: foreign key properties appear just like any other property in the relational model.
                 Type clrPropertyType;
                 if (!SqlServerTypeMapping._sqlTypeToClrTypeMap.TryGetValue(tc.DataType, out clrPropertyType))
@@ -229,7 +228,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                     clrPropertyType = clrPropertyType.MakeNullable();
                 }
 
-                var property = entityType.AddProperty(tc.Id, clrPropertyType, shadowProperty: true);
+                var property = entityType.AddProperty(tc.Id, clrPropertyType);
                 property.Relational().ColumnName = _tableColumns[tc.Id].ColumnName;
                 _columnIdToProperty[tc.Id] = property;
                 AddFacetsOnProperty(property, _tableColumns[tc.Id]);
@@ -412,7 +411,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                 var defaultExpressionOrValue =
                     _sqlServerLiteralUtilities
                         .ConvertSqlServerDefaultValue(
-                            property.ClrType, tableColumn.DefaultValue);
+                            ((IProperty)property).ClrType, tableColumn.DefaultValue);
                 if (defaultExpressionOrValue != null
                     && defaultExpressionOrValue.DefaultExpression != null)
                 {
@@ -428,7 +427,7 @@ namespace Microsoft.Data.Entity.SqlServer.Design.ReverseEngineering
                     Logger.LogWarning(
                         Strings.UnableToConvertDefaultValue(
                             tableColumn.Id, tableColumn.DefaultValue,
-                            property.ClrType, property.Name, property.DeclaringEntityType.Name));
+                            ((IProperty)property).ClrType, property.Name, property.DeclaringEntityType.Name));
                 }
             }
         }