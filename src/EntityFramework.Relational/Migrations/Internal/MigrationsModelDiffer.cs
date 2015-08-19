ca634e5177d23f265f6941e98778e619c2bb81b3
Brice Lambson <bricelam@microsoft.com>
2015-08-19 09:51:24 -0700

    Migrations: Restart sequence when start value changes
    Resolves #2811

--- src/EntityFramework.Relational/Migrations/Internal/MigrationsModelDiffer.cs
+++ src/EntityFramework.Relational/Migrations/Internal/MigrationsModelDiffer.cs
@@ -33,7 +33,8 @@ namespace Microsoft.Data.Entity.Migrations.Internal
             typeof(AddUniqueConstraintOperation),
             typeof(AlterColumnOperation),
             typeof(AlterSequenceOperation),
-            typeof(CreateIndexOperation)
+            typeof(CreateIndexOperation),
+            typeof(RestartSequenceOperation)
         };
 
         private static readonly Type[] _renameOperationTypes =
@@ -736,6 +737,16 @@ namespace Microsoft.Data.Entity.Migrations.Internal
                     Name = source.Name,
                     NewSchema = schemaChanged ? target.Schema : null,
                     NewName = renamed ? target.Name : null
+                };
+            }
+
+            if (source.StartValue != target.StartValue)
+            {
+                yield return new RestartSequenceOperation
+                {
+                    Schema = source.Schema,
+                    Name = source.Name,
+                    StartValue = target.StartValue
                 };
             }
 
