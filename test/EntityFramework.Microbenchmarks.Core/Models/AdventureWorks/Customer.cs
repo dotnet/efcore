4be602716f677ce8542c7480ee84335c6b2d2181
Brice Lambson <bricelam@microsoft.com>
2015-08-27 21:44:01 -0700

    Remove unnecessary dependency

--- test/EntityFramework.Microbenchmarks.Core/Models/AdventureWorks/Customer.cs
+++ test/EntityFramework.Microbenchmarks.Core/Models/AdventureWorks/Customer.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.Data.Entity;
-using Microsoft.Data.Entity.Metadata;
 
 namespace EntityFramework.Microbenchmarks.Core.Models.AdventureWorks
 {
