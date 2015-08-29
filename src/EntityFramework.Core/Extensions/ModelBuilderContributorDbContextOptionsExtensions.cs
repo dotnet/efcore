f9de019ae2f2049659466c6135c614749af1e978
Nero Sule <sulenero@gmail.com>
2015-08-29 18:21:05 +0100

    Copyright headers

--- src/EntityFramework.Core/Extensions/ModelBuilderContributorDbContextOptionsExtensions.cs
+++ src/EntityFramework.Core/Extensions/ModelBuilderContributorDbContextOptionsExtensions.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Data.Entity.Internal;
 
--- src/EntityFramework.Core/Internal/IModelBuilderConventionSource.cs
+++ src/EntityFramework.Core/Internal/IModelBuilderConventionSource.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
-using JetBrains.Annotations;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
 using Microsoft.Data.Entity.Infrastructure;
 
 namespace Microsoft.Data.Entity.Internal
