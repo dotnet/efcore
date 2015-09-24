4c98cfef1a4dfd6bab08f7498b448e11f3b882d9
Arthur Vickers <ajcvickers@hotmail.com>
2015-09-24 14:10:39 -0700

    Re-enable in-memory graph update test
    Fix was to re-add the code that was removed and change it to clear data from the in-memory database directly rather than going through EnsureDeleted.

--- test/EntityFramework.InMemory.FunctionalTests/GraphUpdatesInMemoryTest.cs
+++ test/EntityFramework.InMemory.FunctionalTests/GraphUpdatesInMemoryTest.cs
@@ -1,19 +1,18 @@
-// TODO won't work until we get named in-memory database
-//// Copyright (c) .NET Foundation. All rights reserved.
-//// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-//namespace Microsoft.Data.Entity.InMemory.FunctionalTests
-//{
-//    public class GraphUpdatesInMemoryTest : GraphUpdatesInMemoryTestBase<GraphUpdatesInMemoryTest.GraphUpdatesInMemoryFixture>
-//    {
-//        public GraphUpdatesInMemoryTest(GraphUpdatesInMemoryFixture fixture)
-//            : base(fixture)
-//        {
-//        }
+namespace Microsoft.Data.Entity.InMemory.FunctionalTests
+{
+    public class GraphUpdatesInMemoryTest : GraphUpdatesInMemoryTestBase<GraphUpdatesInMemoryTest.GraphUpdatesInMemoryFixture>
+    {
+        public GraphUpdatesInMemoryTest(GraphUpdatesInMemoryFixture fixture)
+            : base(fixture)
+        {
+        }
 
-//        public class GraphUpdatesInMemoryFixture : GraphUpdatesInMemoryFixtureBase
-//        {
-//            public override int IntSentinel => 0;
-//        }
-//    }
-//}
+        public class GraphUpdatesInMemoryFixture : GraphUpdatesInMemoryFixtureBase
+        {
+            public override int IntSentinel => 0;
+        }
+    }
+}
