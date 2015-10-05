b0a8e7cd03157405710045dda768a2fa437c7665
Arthur Vickers <ajcvickers@hotmail.com>
2015-10-05 09:58:42 -0700

    Enable cascade delete tests on SQLite
    The issue was that the test store is creating and opening the connection and then opening a transaction, which meant that our code to enable FKs for the connection had no effect, which is, as I understand it, by design. The fix is to have the test store enable FKs before starting the Transaction.

--- test/EntityFramework.Sqlite.FunctionalTests/GraphUpdatesSqliteTestBase.cs
+++ test/EntityFramework.Sqlite.FunctionalTests/GraphUpdatesSqliteTestBase.cs
@@ -3,9 +3,7 @@
 
 using System;
 using Microsoft.Data.Entity.FunctionalTests;
-using Microsoft.Data.Entity.FunctionalTests.TestUtilities.Xunit;
 using Microsoft.Framework.DependencyInjection;
-using Xunit;
 
 namespace Microsoft.Data.Entity.Sqlite.FunctionalTests
 {
@@ -15,120 +13,6 @@ namespace Microsoft.Data.Entity.Sqlite.FunctionalTests
         protected GraphUpdatesSqliteTestBase(TFixture fixture)
             : base(fixture)
         {
-        }
-
-        [ConditionalTheory]
-        public override void Save_required_one_to_one_changed_by_reference_with_alternate_key(ChangeMechanism changeMechanism, bool useExistingEntities)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Save_required_non_PK_one_to_one_changed_by_reference_with_alternate_key(ChangeMechanism changeMechanism, bool useExistingEntities)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Save_required_one_to_one_changed_by_reference(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Save_removed_required_many_to_one_dependents(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Save_required_non_PK_one_to_one_changed_by_reference(ChangeMechanism changeMechanism, bool useExistingEntities)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Sever_required_one_to_one_with_alternate_key(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Sever_required_one_to_one(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Sever_required_non_PK_one_to_one(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalTheory]
-        public override void Sever_required_non_PK_one_to_one_with_alternate_key(ChangeMechanism changeMechanism)
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_many_to_one_dependents_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_one_to_one_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_non_PK_one_to_one_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_many_to_one_dependents_with_alternate_key_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_one_to_one_with_alternate_key_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Required_non_PK_one_to_one_with_alternate_key_are_cascade_deleted_in_store()
-        {
-            // TODO: Cascade delete not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Optional_many_to_one_dependents_are_orphaned_in_store()
-        {
-            // TODO: Cascade nulls not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Optional_one_to_one_are_orphaned_in_store()
-        {
-            // TODO: Cascade nulls not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Optional_many_to_one_dependents_with_alternate_key_are_orphaned_in_store()
-        {
-            // TODO: Cascade nulls not yet supported by SQLite provider
-        }
-
-        [ConditionalFact]
-        public override void Optional_one_to_one_with_alternate_key_are_orphaned_in_store()
-        {
-            // TODO: Cascade nulls not yet supported by SQLite provider
         }
 
         public abstract class GraphUpdatesSqliteFixtureBase : GraphUpdatesFixtureBase
