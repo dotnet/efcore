519ac7317538726e66a8a248e558c82d8e358b01
Rowan Miller <mail@romiller.com>
2015-10-07 13:27:21 -0700

    :racehorse: Benchmarks drop and recreate invalid db
    Updating benchmarks to automatically drop the test database if the data
    is not as expected.

--- test/EntityFramework.Microbenchmarks/Models/Orders/OrdersSeedData.cs
+++ test/EntityFramework.Microbenchmarks/Models/Orders/OrdersSeedData.cs
@@ -1,66 +1,92 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Linq;
 using EntityFramework.Microbenchmarks.Core.Models.Orders;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Storage;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace EntityFramework.Microbenchmarks.Models.Orders
 {
     public class OrdersSeedData : OrdersSeedDataBase
     {
-        public void EnsureCreated(
+        private readonly string _connectionString;
+        private readonly int _productCount;
+        private readonly int _customerCount;
+        private readonly int _ordersPerCustomer;
+        private readonly int _linesPerOrder;
+
+        public OrdersSeedData(
             string connectionString,
             int productCount,
             int customerCount,
             int ordersPerCustomer,
             int linesPerOrder)
         {
-            using (var context = new OrdersContext(connectionString))
+            _connectionString = connectionString;
+            _productCount = productCount;
+            _customerCount = customerCount;
+            _ordersPerCustomer = ordersPerCustomer;
+            _linesPerOrder = linesPerOrder;
+        }
+
+        public void EnsureCreated()
+        {
+            using (var context = new OrdersContext(_connectionString))
             {
-                if (context.Database.EnsureCreated())
+                var database = ((IAccessor<IServiceProvider>)context).GetService<IRelationalDatabaseCreator>();
+                if(!database.Exists())
                 {
-                    InsertSeedData(connectionString, productCount, customerCount, ordersPerCustomer, linesPerOrder);
+                    context.Database.EnsureCreated();
+                    InsertSeedData();
+                }
+                else if(!IsDatabaseCorrect(context))
+                {
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                    InsertSeedData();
                 }
 
-                Assert.Equal(productCount, context.Products.Count());
-                Assert.Equal(customerCount, context.Customers.Count());
-                Assert.Equal(customerCount * ordersPerCustomer, context.Orders.Count());
-                Assert.Equal(customerCount * ordersPerCustomer * linesPerOrder, context.OrderLines.Count());
+                Assert.True(IsDatabaseCorrect(context));
             }
         }
 
-        public void InsertSeedData(
-            string connectionString,
-            int productCount,
-            int customerCount,
-            int ordersPerCustomer,
-            int linesPerOrder)
+        private bool IsDatabaseCorrect(OrdersContext context)
         {
-            var products = CreateProducts(productCount);
-            using (var context = new OrdersContext(connectionString))
+            return _productCount == context.Products.Count()
+                && _customerCount == context.Customers.Count()
+                && _customerCount * _ordersPerCustomer == context.Orders.Count()
+                && _customerCount * _ordersPerCustomer * _linesPerOrder == context.OrderLines.Count();
+        }
+
+        public void InsertSeedData()
+        {
+            var products = CreateProducts(_productCount);
+            using (var context = new OrdersContext(_connectionString))
             {
                 context.Products.AddRange(products);
                 context.SaveChanges();
             }
 
-            var customers = CreateCustomers(customerCount);
-            using (var context = new OrdersContext(connectionString))
+            var customers = CreateCustomers(_customerCount);
+            using (var context = new OrdersContext(_connectionString))
             {
                 context.Customers.AddRange(customers);
                 context.SaveChanges();
             }
 
-            var orders = CreateOrders(ordersPerCustomer, customers);
-            using (var context = new OrdersContext(connectionString))
+            var orders = CreateOrders(_ordersPerCustomer, customers);
+            using (var context = new OrdersContext(_connectionString))
             {
                 context.Orders.AddRange(orders);
                 context.SaveChanges();
             }
 
-            var lines = CreateOrderLines(linesPerOrder, products, orders);
+            var lines = CreateOrderLines(_linesPerOrder, products, orders);
 
-            using (var context = new OrdersContext(connectionString))
+            using (var context = new OrdersContext(_connectionString))
             {
                 context.OrderLines.AddRange(lines);
                 context.SaveChanges();
