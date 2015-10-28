f2669b274244a5adc3746bcb5311f7983317e839
Arthur Vickers <ajcvickers@hotmail.com>
2015-10-28 16:03:12 -0700

    Fix bad merge

--- test/EntityFramework.Core.Tests/Metadata/Internal/ClrPropertySetterFactoryTest.cs
+++ test/EntityFramework.Core.Tests/Metadata/Internal/ClrPropertySetterFactoryTest.cs
@@ -1,13 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Reflection;
 using Moq;
 using Xunit;
 
 namespace Microsoft.Data.Entity.Metadata.Internal
 {
-    public class ClrPropertySetterSourceTest
+    public class ClrPropertySetterFactoryTest
     {
         [Fact]
         public void Property_is_returned_if_it_implements_IClrPropertySetter()
