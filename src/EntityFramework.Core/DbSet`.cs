dotnet/efcore

commit 951933fc0c72e8bcd5795648fa204de7eefcd090

Add DNX451 to EntityFramework.Core
Fixes #3553

src/EntityFramework.Core/DbSet`.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-#if NET451
+#if NET451 || DNX451
 using System.ComponentModel;
 #endif
 using System.Linq;
@@ -12,7 +12,7 @@ using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.ChangeTracking;
 using Microsoft.Data.Entity.Infrastructure;
-#if NET451
+#if NET451 || DNX451
 using Microsoft.Data.Entity.Internal;
 #endif
 using Microsoft.Data.Entity.Query;

commit 741138a07c13ee1176680920312de4e6f655cccf

Re-add DNX451 compile symbols
Fixes bug introduced with 951933fc0c72e8bcd5795648fa204de7eefcd090.

src/EntityFramework.Core/DbSet`.cs
@@ -317,7 +317,7 @@ namespace Microsoft.Data.Entity
         }
     }
 
-#if NET451
+#if NET451 || DNX451
 
     public abstract partial class DbSet<TEntity> : IListSource
         where TEntity : class

src/EntityFramework.Relational/Storage/RelationalConnection.cs
@@ -205,7 +205,7 @@ namespace Microsoft.Data.Entity.Storage
 
         private void CheckForAmbientTransactions()
         {
-#if NET451
+#if NET451 || DNX451
             if (_throwOnAmbientTransaction
                 && System.Transactions.Transaction.Current != null)
             {