bodell/EntityFramework

Fix #2891 - Promote transaction API to core

src/EntityFramework.InMemory/Extensions/InMemoryEntityFrameworkServicesBuilderExtensions.cs
@@ -34,6 +34,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddScoped<InMemoryValueGeneratorSelector>()
                 .AddScoped<InMemoryDatabaseProviderServices>()
                 .AddScoped<IInMemoryDatabase, InMemoryDatabase>()
+                .AddScoped<InMemoryConnection>()
                 .AddScoped<InMemoryDatabaseCreator>()
                 .AddQuery());
 

src/EntityFramework.InMemory/Storage/Internal/InMemoryDatabaseProviderServices.cs
@@ -24,6 +24,7 @@ namespace Microsoft.Data.Entity.Storage.Internal
 
         public override string InvariantName => GetType().GetTypeInfo().Assembly.GetName().Name;
         public override IDatabase Database => GetService<IInMemoryDatabase>();
+        public override IConnection Connection => GetService<InMemoryConnection>();
         public override IQueryContextFactory QueryContextFactory => GetService<InMemoryQueryContextFactory>();
         public override IDatabaseCreator Creator => GetService<InMemoryDatabaseCreator>();
         public override IValueGeneratorSelector ValueGeneratorSelector => GetService<InMemoryValueGeneratorSelector>();

src/EntityFramework.Relational/Storage/RelationalDatabaseProviderServices.cs
@@ -25,6 +25,7 @@ namespace Microsoft.Data.Entity.Storage
         }
 
         public override IDatabase Database => GetService<RelationalDatabase>();
+        public override IConnection Connection => GetService<IRelationalConnection>();
         public override IModelValidator ModelValidator => GetService<RelationalModelValidator>();
         public override ICompiledQueryCacheKeyGenerator CompiledQueryCacheKeyGenerator => GetService<RelationalCompiledQueryCacheKeyGenerator>();
         public override IValueGeneratorSelector ValueGeneratorSelector => GetService<RelationalValueGeneratorSelector>();

dotnet/efcore

src/EntityFramework.InMemory/Extensions/InMemoryEntityFrameworkServicesBuilderExtensions.cs
@@ -34,6 +34,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddScoped<InMemoryValueGeneratorSelector>()
                 .AddScoped<InMemoryDatabaseProviderServices>()
                 .AddScoped<IInMemoryDatabase, InMemoryDatabase>()
+                .AddScoped<InMemoryTransactionManager>()
                 .AddScoped<InMemoryDatabaseCreator>()
                 .AddQuery());
 

src/EntityFramework.InMemory/Storage/Internal/InMemoryDatabaseProviderServices.cs
@@ -24,6 +24,7 @@ namespace Microsoft.Data.Entity.Storage.Internal
 
         public override string InvariantName => GetType().GetTypeInfo().Assembly.GetName().Name;
         public override IDatabase Database => GetService<IInMemoryDatabase>();
+        public override IDbContextTransactionManager TransactionManager => GetService<InMemoryTransactionManager>();
         public override IQueryContextFactory QueryContextFactory => GetService<InMemoryQueryContextFactory>();
         public override IDatabaseCreator Creator => GetService<InMemoryDatabaseCreator>();
         public override IValueGeneratorSelector ValueGeneratorSelector => GetService<InMemoryValueGeneratorSelector>();

src/EntityFramework.Relational/Storage/RelationalDatabaseProviderServices.cs
@@ -25,6 +25,7 @@ namespace Microsoft.Data.Entity.Storage
         }
 
         public override IDatabase Database => GetService<RelationalDatabase>();
+        public override IDbContextTransactionManager TransactionManager => GetService<IRelationalConnection>();
         public override IModelValidator ModelValidator => GetService<RelationalModelValidator>();
         public override ICompiledQueryCacheKeyGenerator CompiledQueryCacheKeyGenerator => GetService<RelationalCompiledQueryCacheKeyGenerator>();
         public override IValueGeneratorSelector ValueGeneratorSelector => GetService<RelationalValueGeneratorSelector>();