904d6ac38f5abb01cc4e72238eda3140d71194b0
lajones <lajones@microsoft.com>
2015-12-08 16:18:30 -0800

    Don't produce HasIndex() API if it would already be assumed due to it supporting a primary or foreign key, but make allowance for the extra API which is necessary if the default name of the index would be different.

--- src/EntityFramework.MicrosoftSqlServer.Design/SqlServerDatabaseModelFactory.cs
+++ src/EntityFramework.MicrosoftSqlServer.Design/SqlServerDatabaseModelFactory.cs
@@ -223,7 +223,6 @@ FROM sys.indexes i
     INNER JOIN sys.index_columns ic  ON i.object_id = ic.object_id AND i.index_id = ic.index_id
     INNER JOIN sys.columns c ON ic.object_id = c.object_id AND c.column_id = ic.column_id
 WHERE object_schema_name(i.object_id) <> 'sys'
-    AND i.is_primary_key <> 1
     AND object_name(i.object_id) <> '" + HistoryRepository.DefaultTableName + @"'
 ORDER BY object_schema_name(i.object_id), object_name(i.object_id), i.name, ic.key_ordinal";
 
--- src/EntityFramework.Relational/Metadata/RelationalKeyAnnotations.cs
+++ src/EntityFramework.Relational/Metadata/RelationalKeyAnnotations.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using JetBrains.Annotations;
@@ -36,18 +37,24 @@ namespace Microsoft.Data.Entity.Metadata
 
         protected virtual string GetDefaultName()
         {
-            var builder = new StringBuilder();
             var entityType = new RelationalEntityTypeAnnotations(Key.DeclaringEntityType, Annotations.ProviderPrefix);
 
-            builder
-                .Append(Key.IsPrimaryKey() ? "PK_" : "AK_")
-                .Append(entityType.TableName);
+            return GetDefaultKeyName(entityType.TableName, Key.IsPrimaryKey(), Key.Properties.Select(p => p.Name));
+        }
 
-            if (!Key.IsPrimaryKey())
+        public static string GetDefaultKeyName(
+            [NotNull] string tableName, bool isPrimaryKey, [NotNull] IEnumerable<string> propertyNames)
+        {
+            var builder = new StringBuilder();
+            builder
+                .Append(isPrimaryKey ? "PK_" : "AK_")
+                .Append(tableName);
+
+            if (!isPrimaryKey)
             {
                 builder
                     .Append("_")
-                    .Append(string.Join("_", Key.Properties.Select(p => p.Name)));
+                    .Append(string.Join("_", propertyNames));
             }
 
             return builder.ToString();
