863033a13ddd5a40a6c1e96759992f858a685177
Nate McMaster <nate.mcmaster@microsoft.com>
2015-12-17 13:44:24 -0800

    [reveng] Use x-plat consistent property ordering

--- src/EntityFramework.Commands/Scaffolding/Internal/ScaffoldingUtilities.cs
+++ src/EntityFramework.Commands/Scaffolding/Internal/ScaffoldingUtilities.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -169,7 +170,7 @@ namespace Microsoft.Data.Entity.Scaffolding.Internal
             foreach (var property in
                 entityType.GetProperties()
                     .Except(primaryKeyProperties)
-                    .OrderBy(p => p.Name))
+                    .OrderBy(p => p.Name, StringComparer.Ordinal))
             {
                 yield return property;
             }
