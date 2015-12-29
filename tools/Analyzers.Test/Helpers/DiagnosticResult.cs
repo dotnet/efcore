d94b6428d95d651a970748db42861bd0ef6ee099
Nate McMaster <nate.mcmaster@microsoft.com>
2015-12-29 15:58:08 -0800

    Create analyzer for MakeGenericMethod

---
+++ tools/Analyzers.Test/Helpers/DiagnosticResult.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.Data.Entity.Internal
+{
+    /// <summary>
+    ///     Location where the diagnostic appears, as determined by path, line number, and column number.
+    /// </summary>
+    public struct DiagnosticResultLocation
+    {
+        public DiagnosticResultLocation(string path, int line, int column)
+        {
+            if (line < -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(line), "line must be >= -1");
+            }
+
+            if (column < -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(column), "column must be >= -1");
+            }
+
+            Path = path;
+            Line = line;
+            Column = column;
+        }
+
+        public string Path { get; }
+        public int Line { get; }
+        public int Column { get; }
+    }
+
+    /// <summary>
+    ///     Struct that stores information about a Diagnostic appearing in a source
+    /// </summary>
+    public struct DiagnosticResult
+    {
+        private DiagnosticResultLocation[] locations;
+
+        public DiagnosticResultLocation[] Locations
+        {
+            get
+            {
+                if (locations == null)
+                {
+                    locations = new DiagnosticResultLocation[] { };
+                }
+                return locations;
+            }
+
+            set { locations = value; }
+        }
+
+        public DiagnosticSeverity Severity { get; set; }
+
+        public string Id { get; set; }
+
+        public string Message { get; set; }
+
+        public string Path
+        {
+            get { return Locations.Length > 0 ? Locations[0].Path : ""; }
+        }
+
+        public int Line
+        {
+            get { return Locations.Length > 0 ? Locations[0].Line : -1; }
+        }
+
+        public int Column
+        {
+            get { return Locations.Length > 0 ? Locations[0].Column : -1; }
+        }
+    }
+}
