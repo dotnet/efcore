c44e62c861f3ea3eeab9a56f64a2dbf87c8ee63a
Nate McMaster <nate.mcmaster@microsoft.com>
2016-01-20 10:27:35 -0800

    Fix #3365 - add more sqlite testing

---
+++ test/EntityFramework.Sqlite.FunctionalTests/InheritanceRelationshipsQuerySqliteFixture.cs
@@ -0,0 +1,62 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Data.Entity.FunctionalTests.TestModels.InheritanceRelationships;
+using Microsoft.Data.Entity.FunctionalTests;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Data.Entity.Sqlite.FunctionalTests
+{
+    public class InheritanceRelationshipsQuerySqliteFixture : InheritanceRelationshipsQueryRelationalFixture<SqliteTestStore>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InheritanceRelationshipsQuerySqliteFixture()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddEntityFramework()
+                .AddSqlite()
+                .ServiceCollection()
+                .AddSingleton(TestSqliteModelSource.GetFactory(OnModelCreating))
+                .AddSingleton<ILoggerFactory>(new TestSqlLoggerFactory())
+                .BuildServiceProvider();
+        }
+
+        public override InheritanceRelationshipsContext CreateContext(SqliteTestStore testStore)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseSqlite(testStore.Connection);
+
+            var context = new InheritanceRelationshipsContext(_serviceProvider, optionsBuilder.Options);
+
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            context.Database.UseTransaction(testStore.Transaction);
+
+            return context;
+        }
+
+        public override SqliteTestStore CreateTestStore()
+            => SqliteTestStore.GetOrCreateShared(
+                nameof(InheritanceRelationshipsQuerySqliteTest),
+                () =>
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder();
+                    optionsBuilder.UseSqlite(SqliteTestStore.CreateConnectionString(nameof(InheritanceRelationshipsQuerySqliteTest)));
+
+                    using (var context = new InheritanceRelationshipsContext(_serviceProvider, optionsBuilder.Options))
+                    {
+                        // TODO: Delete DB if model changed
+                        context.Database.EnsureDeleted();
+                        if (context.Database.EnsureCreated())
+                        {
+                            InheritanceRelationshipsModelInitializer.Seed(context);
+                        }
+
+                        TestSqlLoggerFactory.SqlStatements.Clear();
+                    }
+                });
+    }
+}
