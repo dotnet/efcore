093997e7a23b684f3035be1e570c9eb9c1c01a7b
Nate McMaster <nate.mcmaster@microsoft.com>
2016-02-03 15:16:22 -0800

    Build script tweaks to support dotnet build on Linux and OSX

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ koreBuildFolder=$buildFolder/KoreBuild-dotnet
 
 nugetPath=$buildFolder/nuget.exe
 
-if test `uname` = Darwin; then
+if [ "$(uname)" = "Darwin" ]; then
     cachedir=~/Library/Caches/KBuild
+    ulimit -n 2048
 else
     if [ -z $XDG_DATA_HOME ]; then
         cachedir=$HOME/.local/share
@@ -20,19 +21,19 @@ cacheNuget=$cachedir/nuget.$nugetVersion.exe
 
 nugetUrl=https://dist.nuget.org/win-x86-commandline/$nugetVersion/nuget.exe
 
-if test ! -d $buildFolder; then
+if [ ! -d $buildFolder ]; then
     mkdir $buildFolder
 fi
 
-if test ! -f $nugetPath; then
-    if test ! -f $cacheNuget; then
+if [ ! -f $nugetPath ]; then
+    if [ ! -f $cacheNuget ]; then
         wget -O $cacheNuget $nugetUrl 2>/dev/null || curl -o $cacheNuget --location $nugetUrl /dev/null
     fi
 
     cp $cacheNuget $nugetPath
 fi
 
-if test ! -d $koreBuildFolder; then
+if [ ! -d $koreBuildFolder ]; then
     mono $nugetPath install KoreBuild-dotnet -ExcludeVersion -o $buildFolder -nocache -pre
     chmod +x $koreBuildFolder/build/KoreBuild.sh
 fi
