42904d6e4d7574bf5a588d7eab7517ea422264e8
Rowan Miller <mail@romiller.com>
2016-02-05 14:53:19 -0800

    :white_check_mark: Fixes for benchmark CI
    Because we now consume artifacts from the central CI build (rather than
    cloning and building on the perf agent), we need to copy config files so
    that they are part of the artifacts from the build.
    
    Also removing redundant TestFramework attribute from EF6 tests.

--- test/Microsoft.EntityFrameworkCore.Microbenchmarks.EF6/Properties/AssemblyInfo.cs
+++ test/Microsoft.EntityFrameworkCore.Microbenchmarks.EF6/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using Xunit;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
@@ -18,8 +18,8 @@ using Xunit;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
@@ -31,15 +31,14 @@ using Xunit;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: TestFramework("Microsoft.EntityFrameworkCore.Microbenchmarks.Core.PerfTestFramework", "Microsoft.EntityFrameworkCore.Microbenchmarks.Core")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
