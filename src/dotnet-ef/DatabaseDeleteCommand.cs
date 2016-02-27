9a89612c38ffef904fcf2ce1e2e816bb8ad78caf
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-02-27 20:12:30 +0300

    Add Command: Drop Database. Usage sample - dnx ef database delete
    Add [CanBeNull] attribute to fix api consistency test.
    
    Change database property usage instead of creating new database facade object.
    
    Fix: Script-Migrations throws when no migrations.
    
    Undo mistake commit for different issue.
    
    Add confirm message for dropping database.
    If force (-f or --force) option presented skip confirm check.
    
    Enhance confirm message - now it show database name and data source.
    Refactor confirm implementation - move away from extensions to delete command itself.
    Refactor operation executor - move drop database implementation to context operations.
    
    Add missed virtal modifier for dropdatabase method in db context operations.
    
    Fix incorrect message for force db delete option.

---
+++ src/dotnet-ef/DatabaseDeleteCommand.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using JetBrains.Annotations;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Microsoft.EntityFrameworkCore.Commands
+{
+    public class DatabaseDeleteCommand
+    {
+        public static void Configure([NotNull] CommandLineApplication command)
+        {
+            command.Description = "Drop the database for specific enviroment";
+
+            var startupProject = command.Option(
+                "-s|--startup-project <project>",
+                "The startup project to use. If omitted, the current project is used.");
+            var environment = command.Option(
+                "-e|--environment <environment>",
+                "The environment to use. If omitted, \"Development\" is used.");
+            var context = command.Option(
+                "-c|--context <context>",
+                "The DbContext to use. If omitted, the default DbContext is used");
+
+            var force = command.Option(
+                "-f|--force",
+                "The DbContext to use. If omitted, the default DbContext is used");
+            command.HelpOption();
+            command.VerboseOption();
+
+            command.OnExecute(
+                () => Execute(context.Value(), startupProject.Value(), environment.Value(), force.HasValue()));
+        }
+
+        private static int Execute(string context, string startupProject, string environment, bool isForced)
+        {
+            new OperationExecutor(startupProject, environment)
+                .DropDatabase(
+                    context,
+                    message =>
+                    {
+                        Reporter.Output.WriteLine(message);
+                        var readedKey = Console.ReadKey().KeyChar;
+
+                        return isForced || (readedKey == 'y') || (readedKey == 'Y');
+                    });
+
+            return 0;
+        }
+    }
+}
--- src/dotnet-ef/OperationExecutor.cs
+++ src/dotnet-ef/OperationExecutor.cs
@@ -12,9 +12,11 @@ using JetBrains.Annotations;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.ProjectModel;
 using Microsoft.DotNet.ProjectModel.Loader;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Design.Internal;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations.Design;
 using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
 using Microsoft.Extensions.PlatformAbstractions;
@@ -118,6 +120,9 @@ namespace Microsoft.EntityFrameworkCore.Commands
                     startupProjectDir,
                     rootNamespace));
         }
+
+        public virtual void DropDatabase([CanBeNull] string contextName, [NotNull] Func<string, bool> confirmCheck) 
+            => _contextOperations.Value.DropDatabase(contextName, confirmCheck);
 
         public virtual MigrationFiles AddMigration(
             [NotNull] string name,
