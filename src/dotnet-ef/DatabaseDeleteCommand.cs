9c8ab0b18207c59cfabbcfc49cd3e6057c4a3964
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-03-02 12:16:05 +0300

    Add confirm message for dropping database. If force (-f or --force) option presented skip confirm check.

--- src/dotnet-ef/DatabaseDeleteCommand.cs
+++ src/dotnet-ef/DatabaseDeleteCommand.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using JetBrains.Annotations;
+using Microsoft.DotNet.Cli.Utils;
 using Microsoft.Extensions.CommandLineUtils;
 
 namespace Microsoft.EntityFrameworkCore.Commands
@@ -21,15 +22,27 @@ namespace Microsoft.EntityFrameworkCore.Commands
             var context = command.Option(
                 "-c|--context <context>",
                 "The DbContext to use. If omitted, the default DbContext is used");
+            var force = command.Option(
+                "-f|--force",
+                "Force confirm message. If omitted, confirm message not used");
             command.HelpOption();
             command.VerboseOption();
 
-            command.OnExecute(
-                () => Execute(context.Value(), startupProject.Value(), environment.Value()));
+            command.Confirm(
+                "Are you sure? (y/N)",
+                () => Execute(context.Value(), startupProject.Value(), environment.Value()),
+                () =>
+                {
+                    Reporter.Output.WriteLine(" Cancel droping database. Use -f|--force to skip.");
+                    return 1;
+                },
+                () => force.HasValue());
         }
 
         private static int Execute(string context, string startupProject, string environment)
         {
+            Reporter.Output.WriteLine(" Start droping database");
+
             new OperationExecutor(startupProject, environment)
                 .DropDatabase(context);
 
