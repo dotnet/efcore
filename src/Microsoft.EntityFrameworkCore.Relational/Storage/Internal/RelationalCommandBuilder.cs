J-GEGE/EntityFramework

Fix #3704 - Explicit separation of command construction and execution Fix #3115 - Support passing DbParameter instances to ExecuteSqlCommand and FromSql Fix #2315 - Improve FromSql command caching

src/Microsoft.EntityFrameworkCore.Relational/Storage/Internal/RelationalCommandBuilder.cs
@@ -1,10 +1,7 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -16,10 +13,8 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
     {
         private readonly ISensitiveDataLogger _logger;
         private readonly DiagnosticSource _diagnosticSource;
-        private readonly IRelationalTypeMapper _typeMapper;
 
         private readonly IndentedStringBuilder _commandTextBuilder = new IndentedStringBuilder();
-        private readonly List<IRelationalParameter> _parameters = new List<IRelationalParameter>();
 
         public RelationalCommandBuilder(
             [NotNull] ISensitiveDataLogger logger,
@@ -32,42 +27,20 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
 
             _logger = logger;
             _diagnosticSource = diagnosticSource;
-            _typeMapper = typeMapper;
+            ParameterList = new RelationalParameterList(typeMapper);
         }
 
         IndentedStringBuilder IInfrastructure<IndentedStringBuilder>.Instance
             => _commandTextBuilder;
 
-        public virtual IRelationalParameter CreateParameter(
-            string name,
-            object value,
-            Func<IRelationalTypeMapper, RelationalTypeMapping> mapType,
-            bool? nullable,
-            string invariantName)
-            => new RelationalParameter(
-                name,
-                value,
-                mapType(_typeMapper),
-                nullable,
-                invariantName);
-
-        public virtual void AddParameter(IRelationalParameter relationalParameter)
-        {
-            Check.NotNull(relationalParameter, nameof(relationalParameter));
-
-            if (relationalParameter.InvariantName == null
-                || _parameters.All(p => p.InvariantName != relationalParameter.InvariantName))
-            {
-                _parameters.Add(relationalParameter);
-            }
-        }
+        public virtual IRelationalParameterList ParameterList { get; }
 
         public virtual IRelationalCommand Build()
             => new RelationalCommand(
                 _logger,
                 _diagnosticSource,
                 _commandTextBuilder.ToString(),
-                _parameters);
+                ParameterList.Parameters);
 
         public override string ToString() => _commandTextBuilder.ToString();
     }

dotnet/efcore

src/Microsoft.EntityFrameworkCore.Relational/Storage/Internal/RelationalCommandBuilder.cs
@@ -1,10 +1,7 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -16,10 +13,8 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
     {
         private readonly ISensitiveDataLogger _logger;
         private readonly DiagnosticSource _diagnosticSource;
-        private readonly IRelationalTypeMapper _typeMapper;
 
         private readonly IndentedStringBuilder _commandTextBuilder = new IndentedStringBuilder();
-        private readonly List<IRelationalParameter> _parameters = new List<IRelationalParameter>();
 
         public RelationalCommandBuilder(
             [NotNull] ISensitiveDataLogger logger,
@@ -32,42 +27,20 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
 
             _logger = logger;
             _diagnosticSource = diagnosticSource;
-            _typeMapper = typeMapper;
+            ParameterBuilder = new RelationalParameterBuilder(typeMapper);
         }
 
         IndentedStringBuilder IInfrastructure<IndentedStringBuilder>.Instance
             => _commandTextBuilder;
 
-        public virtual IRelationalParameter CreateParameter(
-            string name,
-            object value,
-            Func<IRelationalTypeMapper, RelationalTypeMapping> mapType,
-            bool? nullable,
-            string invariantName)
-            => new RelationalParameter(
-                name,
-                value,
-                mapType(_typeMapper),
-                nullable,
-                invariantName);
-
-        public virtual void AddParameter(IRelationalParameter relationalParameter)
-        {
-            Check.NotNull(relationalParameter, nameof(relationalParameter));
-
-            if (relationalParameter.InvariantName == null
-                || _parameters.All(p => p.InvariantName != relationalParameter.InvariantName))
-            {
-                _parameters.Add(relationalParameter);
-            }
-        }
+        public virtual IRelationalParameterBuilder ParameterBuilder { get; }
 
         public virtual IRelationalCommand Build()
             => new RelationalCommand(
                 _logger,
                 _diagnosticSource,
                 _commandTextBuilder.ToString(),
-                _parameters);
+                ParameterBuilder.Parameters);
 
         public override string ToString() => _commandTextBuilder.ToString();
     }