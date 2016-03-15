1eef28b7e93ffb02e354d6e37a4a6cdd370a8551
Pranav K <prkrishn@hotmail.com>
2016-03-14 17:49:55 -0700

    Reacting to Hosting changes

--- src/Microsoft.EntityFrameworkCore.Commands/Design/Internal/HostingEnvironment.cs
+++ src/Microsoft.EntityFrameworkCore.Commands/Design/Internal/HostingEnvironment.cs
@@ -2,16 +2,22 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
 
 namespace Microsoft.EntityFrameworkCore.Design.Internal
 {
     public class HostingEnvironment : IHostingEnvironment
     {
-        public virtual IConfiguration Configuration { get; set; }
-        public virtual string EnvironmentName { get; set; }
-        public virtual IFileProvider WebRootFileProvider { get; set; }
-        public virtual string WebRootPath { get; set; }
+        public string EnvironmentName { get; set; }
+
+        public string ApplicationName { get; set; }
+
+        public string WebRootPath { get; set; }
+
+        public IFileProvider WebRootFileProvider { get; set; }
+
+        public string ContentRootPath { get; set; }
+
+        public IFileProvider ContentRootFileProvider { get; set; }
     }
 }
