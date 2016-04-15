b3d05fd30fb5cc6c2646c2002b2b1f0dd0e00681
Smit Patel <smitpatel@users.noreply.github.com>
2016-04-15 14:53:44 -0700

    Populate Assembly Info for Specification.Tests

--- src/Microsoft.EntityFrameworkCore.Relational.Design.Specification.Tests/Properties/AssemblyInfo.cs
+++ src/Microsoft.EntityFrameworkCore.Relational.Design.Specification.Tests/Properties/AssemblyInfo.cs
@@ -6,3 +6,6 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: AssemblyCompany("Microsoft Corporation.")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyProduct("Microsoft EntityFramework Core")]
