2af1137acca85f9f53c8344bb813739ad3e727b0
Pranav K <prkrishn@hotmail.com>
2016-05-03 16:43:06 -0700

    Revert ":memo: Update xml docs for configuration changes"
    This reverts commit df4ee7fbc7263d9ecc9e62acd1fc65771d643099.

--- src/Microsoft.EntityFrameworkCore/EntityFrameworkServiceCollectionExtensions.cs
+++ src/Microsoft.EntityFrameworkCore/EntityFrameworkServiceCollectionExtensions.cs
@@ -19,9 +19,14 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class EntityFrameworkServiceCollectionExtensions
     {
         /// <summary>
-        ///     Registers the given context as a service in the <see cref="IServiceCollection" />.
-        ///     You use this method when using dependency injection in your application, such as with ASP.NET.
-        ///     For more information on setting up dependency injection, see http://go.microsoft.com/fwlink/?LinkId=526890.
+        ///     <para>
+        ///         Registers the given context as a service in the <see cref="IServiceCollection" /> and ensures services that the context
+        ///         uses are resolved from the <see cref="IServiceProvider" />.
+        ///     </para>
+        ///     <para>
+        ///         You use this method when using dependency injection in your application, such as with ASP.NET.
+        ///         For more information on setting up dependency injection, see http://go.microsoft.com/fwlink/?LinkId=526890.
+        ///     </para>
         /// </summary>
         /// <example>
         ///     <code>
@@ -47,8 +52,10 @@ namespace Microsoft.Extensions.DependencyInjection
         ///         in addition to configuration performed here.
         ///     </para>
         ///     <para>
-        ///         In order for the options to be passed into your context, you need to expose a constructor on your context that takes
-        ///         <see cref="DbContextOptions{TContext}" /> and passes it to the base constructor of <see cref="DbContext"/>.
+        ///         You do not need to expose a <see cref="DbContextOptions" /> constructor parameter for the options to be passed to the
+        ///         context. If you choose to expose a constructor parameter, we recommend typing it as the generic
+        ///         <see cref="DbContextOptions{TContext}" />. You can use the non-generic <see cref="DbContextOptions" /> but this will only
+        ///         work if you have one derived context type registered in your <see cref="IServiceProvider" />.
         ///     </para>
         /// </param>
         /// <param name="contextLifetime"> The lifetime with which to register the DbContext service in the container. </param>
@@ -62,52 +69,6 @@ namespace Microsoft.Extensions.DependencyInjection
             where TContext : DbContext
             => AddDbContext<TContext>(serviceCollection, (p, b) => optionsAction?.Invoke(b), contextLifetime);
 
-        /// <summary>
-        ///     <para>
-        ///         Registers the given context as a service in the <see cref="IServiceCollection" />.
-        ///         You use this method when using dependency injection in your application, such as with ASP.NET.
-        ///         For more information on setting up dependency injection, see http://go.microsoft.com/fwlink/?LinkId=526890.
-        ///     </para>
-        ///     <para>
-        ///         This overload has an <paramref name="optionsAction"/> that provides the applications <see cref="IServiceProvider"/>.
-        ///         This is useful if you want to setup EF to resolve its internal services from the primary application service provider.
-        ///         By default, we recommend using the other overload, which allows EF to create and maintain its own <see cref="IServiceProvider"/>
-        ///         for internal EF services.
-        ///     </para>
-        /// </summary>
-        /// <example>
-        ///     <code>
-        ///         public void ConfigureServices(IServiceCollection services) 
-        ///         {
-        ///             var connectionString = "connection string to database";
-        /// 
-        ///             services.AddDbContext&lt;MyContext&gt;((serviceProvider, options) => 
-        ///                 options.UseSqlServer(connectionString)
-        ///                        .UseInternalServiceProvider(serviceProvider)); 
-        ///         }
-        ///     </code>
-        /// </example>
-        /// <typeparam name="TContext"> The type of context to be registered. </typeparam>
-        /// <param name="serviceCollection"> The <see cref="IServiceCollection" /> to add services to. </param>
-        /// <param name="optionsAction">
-        ///     <para>
-        ///         An optional action to configure the <see cref="DbContextOptions" /> for the context. This provides an
-        ///         alternative to performing configuration of the context by overriding the
-        ///         <see cref="DbContext.OnConfiguring" /> method in your derived context.
-        ///     </para>
-        ///     <para>
-        ///         If an action is supplied here, the <see cref="DbContext.OnConfiguring" /> method will still be run if it has
-        ///         been overridden on the derived context. <see cref="DbContext.OnConfiguring" /> configuration will be applied
-        ///         in addition to configuration performed here.
-        ///     </para>
-        ///     <para>
-        ///         In order for the options to be passed into your context, you need to expose a constructor on your context that takes
-        ///         <see cref="DbContextOptions{TContext}" /> and passes it to the base constructor of <see cref="DbContext"/>.
-        ///     </para>
-        /// </param>
-        /// <returns>
-        ///     A builder that allows further Entity Framework specific setup of the <see cref="IServiceCollection" />.
-        /// </returns>
         public static IServiceCollection AddDbContext<TContext>(
             [NotNull] this IServiceCollection serviceCollection,
             ServiceLifetime contextLifetime)
