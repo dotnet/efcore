0e8e821b22091e4b30bddb04e7c632a8327fa923
Nate McMaster <nate.mcmaster@microsoft.com>
2016-05-24 12:17:12 -0700

    Rename PowerShell cmdlets module to EntityFrameworkCore

--- src/Microsoft.EntityFrameworkCore.Tools/tools/install.ps1
+++ src/Microsoft.EntityFrameworkCore.Tools/tools/install.ps1
@@ -1,5 +1,5 @@
 ﻿param ($installPath, $toolsPath, $package, $project)
 
 Write-Host
-Write-Host 'Type ''get-help EntityFramework'' to see all available Entity Framework commands.'
+Write-Host 'Type ''get-help EntityFrameworkCore'' to see all available Entity Framework Core commands.'
 Write-Host
