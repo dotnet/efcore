f3d0db99f86425a2cea06e34bb35f1dd31edd955
Nate McMaster <natemcmaster@users.noreply.github.com>
2016-05-31 16:48:50 -0700

    Remove dependency on Newtonsoft.Json from dotnet-ef (#5564)

---
+++ src/Microsoft.EntityFrameworkCore.Tools.Core/Utilities/Internal/JsonUtility.cs
@@ -0,0 +1,148 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Microsoft.EntityFrameworkCore.Tools.Core.Utilities.Internal
+{
+    public class JsonUtility
+    {
+        public static string Serialize([CanBeNull] object obj)
+        {
+            var sb = new IndentedStringBuilder();
+            SerializeObject(obj, sb);
+            return sb.ToString();
+        }
+
+        private static void SerializeObject(object obj, IndentedStringBuilder sb)
+        {
+            if (obj == null)
+            {
+                sb.Append("null");
+                return;
+            }
+            if (obj is string)
+            {
+                SerializeString(obj as string, sb);
+                return;
+            }
+            if (obj is bool)
+            {
+                sb.Append(((bool)obj)
+                    ? "true"
+                    : "false");
+                return;
+            }
+            if (obj is short
+                || obj is ushort
+                || obj is int
+                || obj is uint
+                || obj is long
+                || obj is ulong
+                || obj is decimal
+                || obj is float
+                || obj is double)
+            {
+                sb.Append(obj);
+                return;
+            }
+            if (obj is IEnumerable)
+            {
+                SerializeEnumerable(obj as IEnumerable, sb);
+                return;
+            }
+            if (obj.GetType().GetTypeInfo().IsClass)
+            {
+                SerializeClass(obj, sb);
+                return;
+            }
+
+            throw new ArgumentException($"Could not serialize {obj} [{obj.GetType().FullName}]");
+        }
+
+        private static void SerializeEnumerable(IEnumerable en, IndentedStringBuilder sb)
+        {
+            sb.AppendLine("[");
+            using (sb.Indent())
+            {
+                var e = en.GetEnumerator();
+                var next = e.MoveNext();
+                while (next)
+                {
+                    SerializeObject(e.Current, sb);
+                    if ((next = e.MoveNext()))
+                    {
+                        sb.AppendLine(",");
+                    }
+                    else
+                    {
+                        sb.AppendLine();
+                    }
+                }
+            }
+            sb.Append("]");
+        }
+
+        private static readonly IDictionary<char, string> _replaces
+            = new Dictionary<char, string>
+                {
+                    {'\n', @"\n"},
+                    {'\t', @"\t"},
+                    {'\r', @"\r"},
+                    {'\f', @"\f"},
+                    {'\b', @"\b"},
+                    {'"', @"\"""},
+                    {'\\', @"\\"}
+                };
+
+        private static void SerializeString(string s, IndentedStringBuilder sb)
+        {
+            sb.Append('"');
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (_replaces.ContainsKey(s[i]))
+                {
+                    sb.Append(_replaces[s[i]]);
+                }
+                else
+                {
+                    sb.Append(s[i]);
+                }
+            }
+            sb.Append('"');
+        }
+
+        private static void SerializeClass(object obj, IndentedStringBuilder sb)
+        {
+            sb.AppendLine("{");
+            using (sb.Indent())
+            {
+                var typeInfo = obj.GetType().GetTypeInfo();
+                var e = typeInfo.DeclaredProperties.GetEnumerator();
+                var next = e.MoveNext();
+                while (next)
+                {
+                    var p = e.Current;
+                    SerializeString(p.Name, sb);
+                    sb.Append(": ");
+                    SerializeObject(p.GetValue(obj), sb);
+
+                    if ((next = e.MoveNext()))
+                    {
+                        sb.AppendLine(",");
+                    }
+                    else
+                    {
+                        sb.AppendLine();
+                    }
+                }
+            }
+            sb.Append("}");
+        }
+    }
+}
---
+++ test/Microsoft.EntityFrameworkCore.Tools.Core.Tests/Utilities/JsonUtilityTest.cs
@@ -0,0 +1,105 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Migrations.Design;
+using Microsoft.EntityFrameworkCore.Tools.Core.Utilities.Internal;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.EntityFrameworkCore.Tests.Utilities
+{
+    public class JsonUtilityTest
+    {
+        private readonly ITestOutputHelper _output;
+        public JsonUtilityTest(ITestOutputHelper output)
+        {
+            // uncomment to debug test
+            //_output = output;
+        }
+
+        [Fact]
+        public void SerializesMigrationFiles()
+        {
+            var actual = JsonUtility.Serialize(new MigrationFiles
+            {
+                MigrationFile = @"C:\MigrationFile.cs",
+                MetadataFile = @"C:/MetadataFile.cs",
+                SnapshotFile = @"""C:\Folder A\SnapshotFile.cs"""
+            });
+            _output?.WriteLine(actual);
+            Assert.Equal(
+@"{
+    ""MigrationFile"": ""C:\\MigrationFile.cs"",
+    ""MetadataFile"": ""C:/MetadataFile.cs"",
+    ""SnapshotFile"": """ + "\\\"C:\\\\Folder A\\\\SnapshotFile.cs\\\"" + @"""
+}"
+            , actual);
+            Assert.NotNull(JsonConvert.DeserializeObject(actual));
+        }
+
+        [Fact]
+        public void SerializesArray()
+        {
+            var typeNames = new[] { typeof(JsonUtilityTest), typeof(string) }
+               .Select(t => new { fullName = t.FullName })
+               .ToArray();
+            var actual = JsonUtility.Serialize(typeNames);
+            _output?.WriteLine(actual);
+            Assert.Equal(
+@"[
+    {
+        ""fullName"": ""Microsoft.EntityFrameworkCore.Tests.Utilities.JsonUtilityTest""
+    },
+    {
+        ""fullName"": ""System.String""
+    }
+]"
+               , actual);
+            Assert.NotNull(JsonConvert.DeserializeObject(actual));
+        }
+
+        [Fact]
+        public void SerializesAnonymousType()
+        {
+            var t = new
+            {
+                id = "Microsoft.EntityFrameworkCore.EFCore10",
+                escapedChars = "EFCore\\\"\t\f\b\r\n",
+                floatNo = 1.89f,
+                decimalNo = 48.1m,
+                doubleNo = 48d,
+                uintNo = 12u,
+                intNo = -23,
+                ulongNo = 123UL,
+                longNo = -123L,
+                trueType = true,
+                falseType = false,
+                nullProp = (object)null,
+                emptyArray = new object[] { }
+            };
+            var actual = JsonUtility.Serialize(t);
+            _output?.WriteLine(actual);
+            Assert.Equal(
+@"{
+    ""id"": ""Microsoft.EntityFrameworkCore.EFCore10"",
+    ""escapedChars"": ""EFCore\\" + "\\\"" + @"\t\f\b\r\n"",
+    ""floatNo"": 1.89,
+    ""decimalNo"": 48.1,
+    ""doubleNo"": 48,
+    ""uintNo"": 12,
+    ""intNo"": -23,
+    ""ulongNo"": 123,
+    ""longNo"": -123,
+    ""trueType"": true,
+    ""falseType"": false,
+    ""nullProp"": null,
+    ""emptyArray"": [
+    ]
+}"
+              , actual);
+            Assert.NotNull(JsonConvert.DeserializeObject(actual));
+        }
+    }
+}
