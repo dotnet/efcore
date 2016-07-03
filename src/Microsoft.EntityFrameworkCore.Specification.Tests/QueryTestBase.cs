viktortat/EntityFramework

Query: Navigations: Three bug fixes.
Fix: #3676 - Usage of the "let" keyword breaks grouping.
 - Fixed compiler bug in tracked, grouped queries.
Fix: #5427 - A column has been specified more than once in the order by list thrown for a complex query with orderby and navigation.
 - Fixed bug in SelectExpression.PrependToOrderBy
Fix: #4539 - Query :: Join flattening fails for some complex cases involving SelectMany
 - SelectMany after GroupJoin should not cause client-eval.

Also addressed most of the R# warnings in nav. rewriter.

src/Microsoft.EntityFrameworkCore.Specification.Tests/QueryTestBase.cs
@@ -3153,7 +3153,7 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 select new { c, hasOrders });
         }
 
-        // TODO: Need to figure out how to do this 
+        // TODO: Need to figure out how to do this
         //        [ConditionalFact]
         //        public virtual void GroupBy_anonymous()
         //        {
@@ -4620,6 +4620,30 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 from c in cs.Take(1)
                 join o in os on c.CustomerID equals o.CustomerID into orders
                 from o in orders.DefaultIfEmpty()
+                select o);
+        }
+
+
+        [ConditionalFact]
+        public virtual void GroupJoin_DefaultIfEmpty_Where()
+        {
+            AssertQuery<Customer, Order>((cs, os) =>
+                from c in cs
+                join o in os on c.CustomerID equals o.CustomerID into orders
+                from o in orders
+                where o.CustomerID == "ALFKI"
+                select o);
+        }
+
+        [ConditionalFact]
+        public virtual void GroupJoin_DefaultIfEmpty_Where_OrderBy()
+        {
+            AssertQuery<Customer, Order>((cs, os) =>
+                from c in cs
+                join o in os on c.CustomerID equals o.CustomerID into orders
+                from o in orders
+                where o.CustomerID == "ALFKI" || c.CustomerID == "ANATR"
+                orderby c.City
                 select o);
         }
 

test/Microsoft.EntityFrameworkCore.SqlServer.FunctionalTests/QuerySqlServerTest.cs
@@ -2974,6 +2974,32 @@ ORDER BY [t].[CustomerID]",
                 Sql);
         }
 
+        public override void GroupJoin_DefaultIfEmpty_Where()
+        {
+            base.GroupJoin_DefaultIfEmpty_Where();
+
+            Assert.Equal(
+                @"SELECT [o].[OrderID], [o].[CustomerID], [o].[EmployeeID], [o].[OrderDate]
+FROM [Customers] AS [c]
+LEFT JOIN [Orders] AS [o] ON [c].[CustomerID] = [o].[CustomerID]
+WHERE [o].[CustomerID] = N'ALFKI'
+ORDER BY [c].[CustomerID]",
+                Sql);
+        }
+
+        public override void GroupJoin_DefaultIfEmpty_Where_OrderBy()
+        {
+            base.GroupJoin_DefaultIfEmpty_Where_OrderBy();
+
+            Assert.Equal(
+                @"SELECT [o].[OrderID], [o].[CustomerID], [o].[EmployeeID], [o].[OrderDate]
+FROM [Customers] AS [c]
+LEFT JOIN [Orders] AS [o] ON [c].[CustomerID] = [o].[CustomerID]
+WHERE ([o].[CustomerID] = N'ALFKI') OR ([c].[CustomerID] = N'ANATR')
+ORDER BY [c].[City], [c].[CustomerID]",
+                Sql);
+        }
+
         public override void GroupJoin_simple()
         {
             base.GroupJoin_simple();

dotnet/efcore

src/Microsoft.EntityFrameworkCore.Specification.Tests/QueryTestBase.cs
@@ -3153,7 +3153,7 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 select new { c, hasOrders });
         }
 
-        // TODO: Need to figure out how to do this 
+        // TODO: Need to figure out how to do this
         //        [ConditionalFact]
         //        public virtual void GroupBy_anonymous()
         //        {
@@ -4620,6 +4620,30 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 from c in cs.Take(1)
                 join o in os on c.CustomerID equals o.CustomerID into orders
                 from o in orders.DefaultIfEmpty()
+                select o);
+        }
+
+
+        [ConditionalFact]
+        public virtual void GroupJoin_Where()
+        {
+            AssertQuery<Customer, Order>((cs, os) =>
+                from c in cs
+                join o in os on c.CustomerID equals o.CustomerID into orders
+                from o in orders
+                where o.CustomerID == "ALFKI"
+                select o);
+        }
+
+        [ConditionalFact]
+        public virtual void GroupJoin_DefaultIfEmpty_Where_OrderBy()
+        {
+            AssertQuery<Customer, Order>((cs, os) =>
+                from c in cs
+                join o in os on c.CustomerID equals o.CustomerID into orders
+                from o in orders
+                where o.CustomerID == "ALFKI" || c.CustomerID == "ANATR"
+                orderby c.City
                 select o);
         }
 

test/Microsoft.EntityFrameworkCore.SqlServer.FunctionalTests/QuerySqlServerTest.cs
@@ -2975,6 +2975,32 @@ ORDER BY [t].[CustomerID]",
                 Sql);
         }
 
+        public override void GroupJoin_Where()
+        {
+            base.GroupJoin_Where();
+
+            Assert.Equal(
+                @"SELECT [o].[OrderID], [o].[CustomerID], [o].[EmployeeID], [o].[OrderDate]
+FROM [Customers] AS [c]
+LEFT JOIN [Orders] AS [o] ON [c].[CustomerID] = [o].[CustomerID]
+WHERE [o].[CustomerID] = N'ALFKI'
+ORDER BY [c].[CustomerID]",
+                Sql);
+        }
+
+        public override void GroupJoin_DefaultIfEmpty_Where_OrderBy()
+        {
+            base.GroupJoin_DefaultIfEmpty_Where_OrderBy();
+
+            Assert.Equal(
+                @"SELECT [o].[OrderID], [o].[CustomerID], [o].[EmployeeID], [o].[OrderDate]
+FROM [Customers] AS [c]
+LEFT JOIN [Orders] AS [o] ON [c].[CustomerID] = [o].[CustomerID]
+WHERE ([o].[CustomerID] = N'ALFKI') OR ([c].[CustomerID] = N'ANATR')
+ORDER BY [c].[City], [c].[CustomerID]",
+                Sql);
+        }
+
         public override void GroupJoin_simple()
         {
             base.GroupJoin_simple();