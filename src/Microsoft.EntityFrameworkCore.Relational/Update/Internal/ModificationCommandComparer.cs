037b8a622915e3380d6d1609884b4a2a39d8d745
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2016-10-04 18:03:23 -0700

    Introduce a deterministic order based on key values to entities being saved
    Fixes #6666

--- src/Microsoft.EntityFrameworkCore.Relational/Update/Internal/ModificationCommandComparer.cs
+++ src/Microsoft.EntityFrameworkCore.Relational/Update/Internal/ModificationCommandComparer.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using JetBrains.Annotations;
 
 namespace Microsoft.EntityFrameworkCore.Update.Internal
 {
@@ -52,7 +56,51 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
                 return result;
             }
 
+            if (x.EntityState != EntityState.Added
+                && x.Entries.Count > 0
+                && y.Entries.Count > 0)
+            {
+                var xEntry = x.Entries[0];
+                var yEntry = y.Entries[0];
+
+                var key = xEntry.EntityType.FindPrimaryKey();
+
+                for (var i = 0; i < key.Properties.Count; i++)
+                {
+                    var keyProperty = key.Properties[i];
+                    var compare = GetComparer(keyProperty.ClrType);
+
+                    result = compare(xEntry.GetCurrentValue(keyProperty), yEntry.GetCurrentValue(keyProperty));
+                    if (0 != result)
+                    {
+                        return result;
+                    }
+                }
+            }
+
             return result;
         }
+
+        private readonly ConcurrentDictionary<Type, Func<object, object, int>> _comparers =
+            new ConcurrentDictionary<Type, Func<object, object, int>>();
+
+        protected virtual Func<object, object, int> GetComparer([NotNull] Type type)
+            => _comparers.GetOrAdd(type, t =>
+                {
+                    var xParameter = Expression.Parameter(typeof(object), name: "x");
+                    var yParameter = Expression.Parameter(typeof(object), name: "y");
+                    return Expression.Lambda<Func<object, object, int>>(
+                            Expression.Call(null, _compareMethod.MakeGenericMethod(t),
+                                Expression.Convert(xParameter, t),
+                                Expression.Convert(yParameter, t)),
+                            xParameter,
+                            yParameter)
+                        .Compile();
+                });
+
+        private static readonly MethodInfo _compareMethod
+            = typeof(ModificationCommandComparer).GetTypeInfo().GetDeclaredMethod(nameof(CompareValue));
+
+        private static int CompareValue<T>(T x, T y) => Comparer<T>.Default.Compare(x, y);
     }
 }
