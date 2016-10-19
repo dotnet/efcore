5b23250ad41f0a29fc82db17cc610f1125f06061
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2016-10-18 22:13:35 -0700

    Don't attempt to null out PK parts of composite FK during fixup
    Because this either throws because the PK property cannot be set to null, or marks the property as conceptually null which then causes issues later.
    
    There were also two issues in the tests:
    - Equals method of one of the entities was wrong
    - Tests wouldn't run on SQLite because composite keys with store generated ints are not supported
    
    Issue #6840

--- src/Microsoft.EntityFrameworkCore/ChangeTracking/Internal/InternalEntityEntry.cs
+++ src/Microsoft.EntityFrameworkCore/ChangeTracking/Internal/InternalEntityEntry.cs
@@ -667,7 +667,8 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
 
                 if (!Equals(currentValue, value)
                     || (propertyIndex.HasValue
-                        && _stateData.IsPropertyFlagged(propertyIndex.Value, PropertyFlag.Unknown)))
+                        && (_stateData.IsPropertyFlagged(propertyIndex.Value, PropertyFlag.Unknown)
+                            || _stateData.IsPropertyFlagged(propertyIndex.Value, PropertyFlag.Null))))
                 {
                     var writeValue = true;
 
