SefazAS/EntityFramework

Query: Issue propert client eval warning for Last & LastOrDefault

src/Microsoft.EntityFrameworkCore.Relational.Specification.Tests/WarningsTestBase.cs
@@ -80,7 +80,62 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 Assert.NotNull(query2);
             }
         }
-      
+
+        [Fact]
+        public virtual void Last_with_order_by_does_not_issue_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                var query1 = context.Customers.OrderBy(c => c.CustomerID).Last();
+                Assert.NotNull(query1);
+
+                var query2 = context.Customers.Where(c => c.CustomerID == "ALFKI" && c.Orders.OrderBy(o => o.OrderID).Last().OrderID > 1000).ToList();
+                Assert.NotNull(query2);
+
+                var query3 = context.Customers.OrderBy(c => c.CustomerID).LastOrDefault();
+                Assert.NotNull(query3);
+            }
+        }
+
+        [Fact]
+        public virtual void Last_without_order_by_issues_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("Last()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.Last()).Message);
+            }
+        }
+
+        [Fact]
+        public virtual void LastOrDefault_with_order_by_issues_client_eval_warning_in_subquery()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("LastOrDefault()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.Where(c => c.CustomerID == "ALFKI" && c.Orders.OrderBy(o => o.OrderID).LastOrDefault().OrderID > 1000).ToList()).Message);
+            }
+        }
+
+        [Fact]
+        public virtual void LastOrDefault_without_order_by_issues_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("LastOrDefault()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.LastOrDefault()).Message);
+            }
+        }
+
         protected NorthwindContext CreateContext() => Fixture.CreateContext();
 
         protected WarningsTestBase(TFixture fixture)

dotnet/efcore

src/Microsoft.EntityFrameworkCore.Relational.Specification.Tests/WarningsTestBase.cs
@@ -80,7 +80,69 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests
                 Assert.NotNull(query2);
             }
         }
-      
+
+        [Fact]
+        public virtual void LastOrDefault_with_order_by_does_not_issue_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                var query1 = context.Customers.Where(c => c.CustomerID == "ALFKI" && c.Orders.OrderBy(o => o.OrderID).LastOrDefault().OrderID > 1000).ToList();
+                Assert.NotNull(query1);
+
+                var query2 = context.Customers.OrderBy(c => c.CustomerID).LastOrDefault();
+                Assert.NotNull(query2);
+            }
+        }
+
+        [Fact]
+        public virtual void Last_with_order_by_does_not_issue_client_eval_warning_if_at_top_level()
+        {
+            using (var context = CreateContext())
+            {
+                var query = context.Customers.OrderBy(c => c.CustomerID).Last();
+                Assert.NotNull(query);
+            }
+        }
+
+        [Fact]
+        public virtual void Last_without_order_by_issues_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("Last()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.Last()).Message);
+            }
+        }
+
+        [Fact]
+        public virtual void Last_with_order_by_issues_client_eval_warning_in_subquery()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("Last()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.Where(c => c.CustomerID == "ALFKI" && c.Orders.OrderBy(o => o.OrderID).Last().OrderID > 1000).ToList()).Message);
+            }
+        }
+
+        [Fact]
+        public virtual void LastOrDefault_without_order_by_issues_client_eval_warning()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(CoreStrings.WarningAsErrorTemplate(
+                        $"{nameof(RelationalEventId)}.{nameof(RelationalEventId.QueryClientEvaluationWarning)}",
+                        RelationalStrings.ClientEvalWarning("LastOrDefault()")),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context.Customers.LastOrDefault()).Message);
+            }
+        }
+
         protected NorthwindContext CreateContext() => Fixture.CreateContext();
 
         protected WarningsTestBase(TFixture fixture)