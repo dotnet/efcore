fd6f6d1ec397cbc4ec1365561d3b36f7f6391cf7
Arthur Vickers <ajcvickers@hotmail.com>
2016-11-15 13:25:10 -0800

    Make RelationalFullAnnotationNames  public
    Issue #6993

--- src/Microsoft.EntityFrameworkCore.Relational/Metadata/RelationalFullAnnotationNames.cs
+++ src/Microsoft.EntityFrameworkCore.Relational/Metadata/RelationalFullAnnotationNames.cs
@@ -1,7 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.EntityFrameworkCore.Metadata.Internal
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Microsoft.EntityFrameworkCore.Metadata
 {
     /// <summary>
     ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
