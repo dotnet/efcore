36a6e5dcb06165f604aba17321efbd84eb70ec28
Smit Patel <smitpatel@users.noreply.github.com>
2017-04-03 10:48:49 -0700

    Query: Update the resolution of string.Replace overload

--- src/EFCore.SqlServer/Query/ExpressionTranslators/Internal/SqlServerStringReplaceTranslator.cs
+++ src/EFCore.SqlServer/Query/ExpressionTranslators/Internal/SqlServerStringReplaceTranslator.cs
@@ -16,7 +16,7 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
     {
         private static readonly MethodInfo _methodInfo = typeof(string).GetTypeInfo()
             .GetDeclaredMethods(nameof(string.Replace))
-            .Single(m => m.GetParameters()[0].ParameterType == typeof(string));
+            .Single(m => m.GetParameters().Length == 2 && m.GetParameters()[0].ParameterType == typeof(string));
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
