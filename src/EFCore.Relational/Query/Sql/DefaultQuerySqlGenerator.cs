joshcomley/EntityFramework-archive

Fix to #7429 - StartsWith() filter slow with use of SQL CHARINDEX()
Fix is to use more permanent translation:

property.StartsWith(pattern) => property LIKE pattern + '%' AND LEFT(property, LEN(pattern)) = pattern

Also fixed minor issue with null semantics where we would apply C# null semantics for comparisons to null-valued parameters

src/EFCore.Relational/Query/Sql/DefaultQuerySqlGenerator.cs
@@ -1585,6 +1585,11 @@ namespace Microsoft.EntityFrameworkCore.Query.Sql
 
                 return base.VisitBinary(expression);
             }
+
+            protected override Expression VisitExtension(Expression node)
+                => node is NotNullableExpression
+                    ? node
+                    : base.VisitExtension(node);
         }
 
         private class BooleanExpressionTranslatingVisitor : RelinqExpressionVisitor

src/EFCore.SqlServer/Query/ExpressionTranslators/Internal/SqlServerEndsWithOptimizedTranslator.cs
@@ -23,26 +23,26 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
                 var patternExpression = methodCallExpression.Arguments[0];
                 var patternConstantExpression = patternExpression as ConstantExpression;
 
-                var endsWithExpression = Expression.Equal(
-                    new SqlFunctionExpression(
-                        "RIGHT",
-                        // ReSharper disable once PossibleNullReferenceException
-                        methodCallExpression.Object.Type,
-                        new[]
-                        {
-                            methodCallExpression.Object,
-                            new SqlFunctionExpression("LEN", typeof(int), new[] { patternExpression })
-                        }),
-                    patternExpression);
+                var endsWithExpression = new NotNullableExpression(
+                    Expression.Equal(
+                        new SqlFunctionExpression(
+                            "RIGHT",
+                            // ReSharper disable once PossibleNullReferenceException
+                            methodCallExpression.Object.Type,
+                            new[]
+                            {
+                                methodCallExpression.Object,
+                                new SqlFunctionExpression("LEN", typeof(int), new[] { patternExpression })
+                            }),
+                        patternExpression));
 
-                return new NotNullableExpression(
-                    patternConstantExpression != null
-                        ? (string)patternConstantExpression.Value == string.Empty
-                            ? (Expression)Expression.Constant(true)
-                            : endsWithExpression
-                        : Expression.OrElse(
-                            endsWithExpression,
-                            Expression.Equal(patternExpression, Expression.Constant(string.Empty))));
+                return patternConstantExpression != null
+                    ? (string)patternConstantExpression.Value == string.Empty
+                        ? (Expression)Expression.Constant(true)
+                        : endsWithExpression
+                    : Expression.OrElse(
+                        endsWithExpression,
+                        Expression.Equal(patternExpression, Expression.Constant(string.Empty)));
             }
 
             return null;

src/EFCore.Sqlite.Core/Query/ExpressionTranslators/Internal/SqliteEndsWithOptimizedTranslator.cs
@@ -23,26 +23,26 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
                 var patternExpression = methodCallExpression.Arguments[0];
                 var patternConstantExpression = patternExpression as ConstantExpression;
 
-                var endsWithExpression = Expression.Equal(
-                    new SqlFunctionExpression(
-                        "substr",
-                        // ReSharper disable once PossibleNullReferenceException
-                        methodCallExpression.Object.Type,
-                        new[]
-                        {
-                            methodCallExpression.Object,
-                            Expression.Negate(new SqlFunctionExpression("length", typeof(int), new[] { patternExpression }))
-                        }),
-                    patternExpression);
+                var endsWithExpression = new NotNullableExpression(
+                    Expression.Equal(
+                        new SqlFunctionExpression(
+                            "substr",
+                            // ReSharper disable once PossibleNullReferenceException
+                            methodCallExpression.Object.Type,
+                            new[]
+                            {
+                                methodCallExpression.Object,
+                                Expression.Negate(new SqlFunctionExpression("length", typeof(int), new[] { patternExpression }))
+                            }),
+                        patternExpression));
 
-                return new NotNullableExpression(
-                    patternConstantExpression != null
-                        ? (string)patternConstantExpression.Value == string.Empty
-                            ? (Expression)Expression.Constant(true)
-                            : endsWithExpression
-                        : Expression.OrElse(
-                            endsWithExpression,
-                            Expression.Equal(patternExpression, Expression.Constant(string.Empty))));
+                return patternConstantExpression != null
+                    ? (string)patternConstantExpression.Value == string.Empty
+                        ? (Expression)Expression.Constant(true)
+                        : endsWithExpression
+                    : Expression.OrElse(
+                        endsWithExpression,
+                        Expression.Equal(patternExpression, Expression.Constant(string.Empty)));
             }
 
             return null;

dotnet/efcore

src/EFCore.Relational/Query/Sql/DefaultQuerySqlGenerator.cs
@@ -1585,6 +1585,11 @@ namespace Microsoft.EntityFrameworkCore.Query.Sql
 
                 return base.VisitBinary(expression);
             }
+
+            protected override Expression VisitExtension(Expression node)
+                => node is NullCompensatedExpression
+                    ? node
+                    : base.VisitExtension(node);
         }
 
         private class BooleanExpressionTranslatingVisitor : RelinqExpressionVisitor

src/EFCore.SqlServer/Query/ExpressionTranslators/Internal/SqlServerEndsWithOptimizedTranslator.cs
@@ -23,26 +23,26 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
                 var patternExpression = methodCallExpression.Arguments[0];
                 var patternConstantExpression = patternExpression as ConstantExpression;
 
-                var endsWithExpression = Expression.Equal(
-                    new SqlFunctionExpression(
-                        "RIGHT",
-                        // ReSharper disable once PossibleNullReferenceException
-                        methodCallExpression.Object.Type,
-                        new[]
-                        {
-                            methodCallExpression.Object,
-                            new SqlFunctionExpression("LEN", typeof(int), new[] { patternExpression })
-                        }),
-                    patternExpression);
+                var endsWithExpression = new NullCompensatedExpression(
+                    Expression.Equal(
+                        new SqlFunctionExpression(
+                            "RIGHT",
+                            // ReSharper disable once PossibleNullReferenceException
+                            methodCallExpression.Object.Type,
+                            new[]
+                            {
+                                methodCallExpression.Object,
+                                new SqlFunctionExpression("LEN", typeof(int), new[] { patternExpression })
+                            }),
+                        patternExpression));
 
-                return new NotNullableExpression(
-                    patternConstantExpression != null
-                        ? (string)patternConstantExpression.Value == string.Empty
-                            ? (Expression)Expression.Constant(true)
-                            : endsWithExpression
-                        : Expression.OrElse(
-                            endsWithExpression,
-                            Expression.Equal(patternExpression, Expression.Constant(string.Empty))));
+                return patternConstantExpression != null
+                    ? (string)patternConstantExpression.Value == string.Empty
+                        ? (Expression)Expression.Constant(true)
+                        : endsWithExpression
+                    : Expression.OrElse(
+                        endsWithExpression,
+                        Expression.Equal(patternExpression, Expression.Constant(string.Empty)));
             }
 
             return null;

src/EFCore.Sqlite.Core/Query/ExpressionTranslators/Internal/SqliteEndsWithOptimizedTranslator.cs
@@ -23,26 +23,26 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
                 var patternExpression = methodCallExpression.Arguments[0];
                 var patternConstantExpression = patternExpression as ConstantExpression;
 
-                var endsWithExpression = Expression.Equal(
-                    new SqlFunctionExpression(
-                        "substr",
-                        // ReSharper disable once PossibleNullReferenceException
-                        methodCallExpression.Object.Type,
-                        new[]
-                        {
-                            methodCallExpression.Object,
-                            Expression.Negate(new SqlFunctionExpression("length", typeof(int), new[] { patternExpression }))
-                        }),
-                    patternExpression);
+                var endsWithExpression = new NullCompensatedExpression(
+                    Expression.Equal(
+                        new SqlFunctionExpression(
+                            "substr",
+                            // ReSharper disable once PossibleNullReferenceException
+                            methodCallExpression.Object.Type,
+                            new[]
+                            {
+                                methodCallExpression.Object,
+                                Expression.Negate(new SqlFunctionExpression("length", typeof(int), new[] { patternExpression }))
+                            }),
+                        patternExpression));
 
-                return new NotNullableExpression(
-                    patternConstantExpression != null
-                        ? (string)patternConstantExpression.Value == string.Empty
-                            ? (Expression)Expression.Constant(true)
-                            : endsWithExpression
-                        : Expression.OrElse(
-                            endsWithExpression,
-                            Expression.Equal(patternExpression, Expression.Constant(string.Empty))));
+                return patternConstantExpression != null
+                    ? (string)patternConstantExpression.Value == string.Empty
+                        ? (Expression)Expression.Constant(true)
+                        : endsWithExpression
+                    : Expression.OrElse(
+                        endsWithExpression,
+                        Expression.Equal(patternExpression, Expression.Constant(string.Empty)));
             }
 
             return null;