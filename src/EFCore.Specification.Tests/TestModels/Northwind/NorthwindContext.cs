evgeni0us/EntityFramework

Core: Initial work on model-level entity type filters. E.g. ```c# modelBuilder.Entity<Customer>().HasFilter(c => !c.IsDeleted); modelBuilder.Entity<Order>().HasFilter(o => o.CustomerId == this._tenantId); ``` Such filters are automatically applied by the query compiler to any query targeting the corresponding EntityType.
Some key aspects of impl. so far:

- Client eval'd expressions are allowed.
- Nav references are not allowed. This is mostly to constrain the impl. cost.
- Evaluatable sub-trees in filter expressions are eval'd early (not parameterized) except for member accesses on DbContext objects. This permits the pattern in the second example above.
- Filters are only allowed on the root of hierarchies, again to constrain the impl. cost.

Open questions:

- Should Find ignore filters when hitting the store?
- We likely need to add an opt-out query operator.

src/EFCore.Specification.Tests/TestModels/Northwind/NorthwindContext.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+// ReSharper disable StringStartsWithIsCultureSpecific
+
 namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
 {
     public class NorthwindContext : DbContext
@@ -8,9 +10,14 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
         public NorthwindContext(
             DbContextOptions options,
             QueryTrackingBehavior queryTrackingBehavior = QueryTrackingBehavior.TrackAll)
-            : base(queryTrackingBehavior == QueryTrackingBehavior.TrackAll
-                ? options
-                : new DbContextOptionsBuilder(options).UseQueryTrackingBehavior(queryTrackingBehavior).Options)
+            : base(
+                queryTrackingBehavior == QueryTrackingBehavior.TrackAll
+                    ? options
+                    : new DbContextOptionsBuilder(options).UseQueryTrackingBehavior(queryTrackingBehavior).Options)
+        {
+        }
+
+        internal NorthwindContext()
         {
         }
 
@@ -19,5 +26,23 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
         public virtual DbSet<Product> Products { get; set; }
+
+        public string TenantPrefix { get; set; } = "B";
+
+        public void ConfigureFilters(ModelBuilder modelBuilder)
+        {
+            // Called explictly from filter test fixtures. Code is here
+            // so we can capture TenantPrefix in filter exprs (simulates OnModelCreating).
+
+            modelBuilder.Entity<Customer>().HasFilter(c => c.CompanyName.StartsWith(TenantPrefix));
+            modelBuilder.Entity<OrderDetail>().HasFilter(od => od.Quantity > 50);
+            modelBuilder.Entity<Employee>().HasFilter(e => e.Address.StartsWith("A"));
+            modelBuilder.Entity<Product>().HasFilter(p => ClientMethod(p));
+        }
+
+        private static bool ClientMethod(Product product)
+        {
+            return !product.Discontinued;
+        }
     }
 }

src/EFCore/Metadata/Builders/EntityTypeBuilder.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -173,6 +174,19 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
             Check.NotEmpty(propertyName, nameof(propertyName));
 
             Builder.Ignore(propertyName, ConfigurationSource.Explicit);
+
+            return this;
+        }
+
+        /// <summary>
+        ///     Specifies a LINQ predicate expression that will automatically be applied to any queries targeting
+        ///     this entity type.
+        /// </summary>
+        /// <param name="filter">The LINQ predicate expression.</param>
+        /// <returns></returns>
+        public virtual EntityTypeBuilder HasFilter([CanBeNull] LambdaExpression filter)
+        {
+            Builder.HasFilter(filter);
 
             return this;
         }

src/EFCore/Metadata/Internal/InternalEntityTypeBuilder.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -599,6 +600,15 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Internal
             }
 
             return true;
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public virtual void HasFilter([CanBeNull] LambdaExpression filter)
+        {
+            Metadata.Filter = filter;
         }
 
         /// <summary>

dotnet/efcore

src/EFCore.Specification.Tests/TestModels/Northwind/NorthwindContext.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+// ReSharper disable StringStartsWithIsCultureSpecific
+
 namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
 {
     public class NorthwindContext : DbContext
@@ -8,9 +10,14 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
         public NorthwindContext(
             DbContextOptions options,
             QueryTrackingBehavior queryTrackingBehavior = QueryTrackingBehavior.TrackAll)
-            : base(queryTrackingBehavior == QueryTrackingBehavior.TrackAll
-                ? options
-                : new DbContextOptionsBuilder(options).UseQueryTrackingBehavior(queryTrackingBehavior).Options)
+            : base(
+                queryTrackingBehavior == QueryTrackingBehavior.TrackAll
+                    ? options
+                    : new DbContextOptionsBuilder(options).UseQueryTrackingBehavior(queryTrackingBehavior).Options)
+        {
+        }
+
+        internal NorthwindContext()
         {
         }
 
@@ -19,5 +26,23 @@ namespace Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
         public virtual DbSet<Product> Products { get; set; }
+
+        public string TenantPrefix { get; set; } = "B";
+
+        public void ConfigureFilters(ModelBuilder modelBuilder)
+        {
+            // Called explictly from filter test fixtures. Code is here
+            // so we can capture TenantPrefix in filter exprs (simulates OnModelCreating).
+
+            modelBuilder.Entity<Customer>().HasQueryFilter(c => c.CompanyName.StartsWith(TenantPrefix));
+            modelBuilder.Entity<OrderDetail>().HasQueryFilter(od => od.Quantity > 50);
+            modelBuilder.Entity<Employee>().HasQueryFilter(e => e.Address.StartsWith("A"));
+            modelBuilder.Entity<Product>().HasQueryFilter(p => ClientMethod(p));
+        }
+
+        private static bool ClientMethod(Product product)
+        {
+            return !product.Discontinued;
+        }
     }
 }

src/EFCore/Metadata/Builders/EntityTypeBuilder.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -173,6 +174,19 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
             Check.NotEmpty(propertyName, nameof(propertyName));
 
             Builder.Ignore(propertyName, ConfigurationSource.Explicit);
+
+            return this;
+        }
+
+        /// <summary>
+        ///     Specifies a LINQ predicate expression that will automatically be applied to any queries targeting
+        ///     this entity type.
+        /// </summary>
+        /// <param name="filter">The LINQ predicate expression.</param>
+        /// <returns></returns>
+        public virtual EntityTypeBuilder HasQueryFilter([CanBeNull] LambdaExpression filter)
+        {
+            Builder.HasQueryFilter(filter);
 
             return this;
         }

src/EFCore/Metadata/Internal/InternalEntityTypeBuilder.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -599,6 +600,15 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Internal
             }
 
             return true;
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public virtual void HasQueryFilter([CanBeNull] LambdaExpression filter)
+        {
+            Metadata.Filter = filter;
         }
 
         /// <summary>