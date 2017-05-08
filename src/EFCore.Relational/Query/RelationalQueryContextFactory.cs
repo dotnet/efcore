68d973c495b32fd08a257ef86f931dec6602d561
Andrew Peters <anpete@microsoft.com>
2017-05-08 10:58:59 -0700

    Make RelationalQueryContextFactory public.
    Fix: #6713

--- src/EFCore.Relational/Query/RelationalQueryContextFactory.cs
+++ src/EFCore.Relational/Query/RelationalQueryContextFactory.cs
@@ -4,7 +4,7 @@
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Storage;
 
-namespace Microsoft.EntityFrameworkCore.Query.Internal
+namespace Microsoft.EntityFrameworkCore.Query
 {
     /// <summary>
     ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
