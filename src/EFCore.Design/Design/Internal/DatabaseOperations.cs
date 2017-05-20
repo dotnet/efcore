f5e41855461f703a01ad9d61e1c26689edea5c42
Smit Patel <smitpatel@users.noreply.github.com>
2017-05-19 20:56:42 -0700

    Remove ReverseEngineerConfiguration Remove CustomConfiguration

--- src/EFCore.Design/Design/Internal/DatabaseOperations.cs
+++ src/EFCore.Design/Design/Internal/DatabaseOperations.cs
@@ -63,20 +63,17 @@ namespace Microsoft.EntityFrameworkCore.Design.Internal
 #pragma warning restore CS0618 // Type or member is obsolete
 
             var generator = services.GetRequiredService<ReverseEngineeringGenerator>();
-            var tableSelectionSet = new TableSelectionSet(tables, schemas);
-            var configuration = new ReverseEngineeringConfiguration
-            {
-                ConnectionString = connectionString,
-                ContextClassName = dbContextClassName,
-                ProjectPath = _projectDir,
-                ProjectRootNamespace = _rootNamespace,
-                OutputPath = outputDir,
-                TableSelectionSet = tableSelectionSet,
-                UseDataAnnotations = useDataAnnotations,
-                OverwriteFiles = overwriteFiles
-            };
 
-            return generator.GenerateAsync(configuration, cancellationToken);
+            return generator.GenerateAsync(
+                connectionString,
+                new TableSelectionSet(tables, schemas),
+                _projectDir,
+                outputDir,
+                _rootNamespace,
+                dbContextClassName,
+                useDataAnnotations,
+                overwriteFiles,
+                cancellationToken);
         }
     }
 }
