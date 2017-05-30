c7e6e771fa6f139846c4dbbeee4dc1797da3598b
Brice Lambson <bricelam@users.noreply.github.com>
2017-05-30 11:25:02 -0700

    ef.exe: Always use UTF-8 when redirected
    Fixes #8483

--- src/ef/Program.cs
+++ src/ef/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Text;
 using Microsoft.DotNet.Cli.CommandLine;
 using Microsoft.EntityFrameworkCore.Tools.Commands;
 
@@ -11,6 +12,11 @@ namespace Microsoft.EntityFrameworkCore.Tools
     {
         private static int Main(string[] args)
         {
+            if (Console.IsOutputRedirected)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+            }
+
             var app = new CommandLineApplication()
             {
                 Name = "ef"
