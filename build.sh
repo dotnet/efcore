447468ff887c2223b2c4275f1b04476554bd9a6c
Smit Patel <smitpatel@users.noreply.github.com>
2017-06-07 16:32:41 -0700

    Workaround Logger

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
+koreBuildZip="https://github.com/aspnet/KoreBuild/archive/namc/binary-logger.zip"
 if [ ! -z $KOREBUILD_ZIP ]; then
     koreBuildZip=$KOREBUILD_ZIP
 fi
