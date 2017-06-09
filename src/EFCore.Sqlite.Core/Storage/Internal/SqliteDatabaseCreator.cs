aspnet-ce/EntityFramework

Log connection error at debug level for Exists path

src/EFCore.Sqlite.Core/Storage/Internal/SqliteDatabaseCreator.cs
@@ -49,7 +49,7 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
             {
                 try
                 {
-                    readOnlyConnection.Open();
+                    readOnlyConnection.Open(logErrorAsDebug: true);
                 }
                 catch (SqliteException ex) when (ex.SqliteErrorCode == SQLITE_CANTOPEN)
                 {

src/EFCore.Sqlite.Core/Storage/Internal/SqliteRelationalConnection.cs
@@ -57,9 +57,9 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public override bool Open()
+        public override bool Open(bool logErrorAsDebug = false)
         {
-            if (base.Open())
+            if (base.Open(logErrorAsDebug))
             {
                 EnableForeignKeys();
                 return true;
@@ -72,9 +72,9 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public override async Task<bool> OpenAsync(CancellationToken cancellationToken = default(CancellationToken))
+        public override async Task<bool> OpenAsync(CancellationToken cancellationToken = default(CancellationToken), bool logErrorAsDebug = false)
         {
-            if (await base.OpenAsync(cancellationToken))
+            if (await base.OpenAsync(cancellationToken, logErrorAsDebug))
             {
                 EnableForeignKeys();
                 return true;

test/EFCore.SqlServer.FunctionalTests/Utilities/TestSqlServerConnection.cs
@@ -55,18 +55,18 @@ namespace Microsoft.EntityFrameworkCore.Utilities
 
         public virtual bool IsMultipleActiveResultSetsEnabled => _realConnection.IsMultipleActiveResultSetsEnabled;
 
-        public virtual bool Open()
+        public virtual bool Open(bool logErrorAsDebug = false)
         {
             PreOpen();
 
-            return _realConnection.Open();
+            return _realConnection.Open(logErrorAsDebug);
         }
 
-        public virtual Task<bool> OpenAsync(CancellationToken cancellationToken = new CancellationToken())
+        public virtual Task<bool> OpenAsync(CancellationToken cancellationToken = new CancellationToken(), bool logErrorAsDebug = false)
         {
             PreOpen();
 
-            return _realConnection.OpenAsync(cancellationToken);
+            return _realConnection.OpenAsync(cancellationToken, logErrorAsDebug);
         }
 
         private void PreOpen()

dotnet/efcore

src/EFCore.Sqlite.Core/Storage/Internal/SqliteDatabaseCreator.cs
@@ -49,7 +49,7 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
             {
                 try
                 {
-                    readOnlyConnection.Open();
+                    readOnlyConnection.Open(errorsExpected: true);
                 }
                 catch (SqliteException ex) when (ex.SqliteErrorCode == SQLITE_CANTOPEN)
                 {

src/EFCore.Sqlite.Core/Storage/Internal/SqliteRelationalConnection.cs
@@ -57,9 +57,9 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public override bool Open()
+        public override bool Open(bool errorsExpected = false)
         {
-            if (base.Open())
+            if (base.Open(errorsExpected))
             {
                 EnableForeignKeys();
                 return true;
@@ -72,9 +72,9 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public override async Task<bool> OpenAsync(CancellationToken cancellationToken = default(CancellationToken))
+        public override async Task<bool> OpenAsync(bool errorsExpected = false, CancellationToken cancellationToken = default(CancellationToken))
         {
-            if (await base.OpenAsync(cancellationToken))
+            if (await base.OpenAsync(errorsExpected, cancellationToken))
             {
                 EnableForeignKeys();
                 return true;

test/EFCore.SqlServer.FunctionalTests/Utilities/TestSqlServerConnection.cs
@@ -55,18 +55,18 @@ namespace Microsoft.EntityFrameworkCore.Utilities
 
         public virtual bool IsMultipleActiveResultSetsEnabled => _realConnection.IsMultipleActiveResultSetsEnabled;
 
-        public virtual bool Open()
+        public virtual bool Open(bool errorsExpected = false)
         {
             PreOpen();
 
-            return _realConnection.Open();
+            return _realConnection.Open(errorsExpected);
         }
 
-        public virtual Task<bool> OpenAsync(CancellationToken cancellationToken = new CancellationToken())
+        public virtual Task<bool> OpenAsync(bool errorsExpected = false, CancellationToken cancellationToken = new CancellationToken())
         {
             PreOpen();
 
-            return _realConnection.OpenAsync(cancellationToken);
+            return _realConnection.OpenAsync(errorsExpected, cancellationToken);
         }
 
         private void PreOpen()
@@ -106,7 +106,7 @@ namespace Microsoft.EntityFrameworkCore.Utilities
         public virtual async Task<IDbContextTransaction> BeginTransactionAsync(
             IsolationLevel isolationLevel, CancellationToken cancellationToken = new CancellationToken())
         {
-            await OpenAsync(cancellationToken);
+            await OpenAsync(cancellationToken: cancellationToken);
             CurrentTransaction = new TestRelationalTransaction(this, await _realConnection.BeginTransactionAsync(isolationLevel, cancellationToken));
             return CurrentTransaction;
         }