2a5127cba4cbfb304d1353c097c7f01910526f52
Smit Patel <smitpatel@users.noreply.github.com>
2017-06-22 17:18:05 -0700

    RevEng: Remove DbDataReaderExtension

--- src/EFCore.SqlServer/Scaffolding/Internal/SqlDataReaderExtension.cs
+++ src/EFCore.SqlServer/Scaffolding/Internal/SqlDataReaderExtension.cs
@@ -1,12 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Data.SqlClient;
+using System.Data.Common;
 using JetBrains.Annotations;
 
 namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
 {
-    // TODO merge with DbDataReaderExtension.GetValueOrDefault when Mono supports GetFieldValue. See #2079
     /// <summary>
     ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
@@ -17,13 +16,12 @@ namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public static T GetValueOrDefault<T>([NotNull] this SqlDataReader reader, [NotNull] string name)
+        public static T GetValueOrDefault<T>([NotNull] this DbDataReader reader, [NotNull] string name)
         {
             var idx = reader.GetOrdinal(name);
             return reader.IsDBNull(idx)
                 ? default(T)
-                // : reader.GetFieldValue<T>(idx);
-                : (T)reader.GetValue(idx);
+                : reader.GetFieldValue<T>(idx);
         }
     }
 }
