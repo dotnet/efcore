2a74fbda9ce1ac672abf67d1da08fe22fe4daf49
Jass Bagga <jbagga93@live.ca>
2017-06-23 17:57:52 -0700

    Remove GetTempFileName

--- src/dotnet-ef/Project.cs
+++ src/dotnet-ef/Project.cs
@@ -67,7 +67,7 @@ namespace Microsoft.EntityFrameworkCore.Tools
             }
 
             IDictionary<string, string> metadata;
-            var metadataFile = Path.GetTempFileName();
+            var metadataFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             try
             {
                 var propertyArg = "/property:EFProjectMetadataFile=" + metadataFile;
