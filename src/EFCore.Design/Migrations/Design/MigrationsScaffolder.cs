wan-qy/EntityFramework

Post-process the snapshot model to remove/combine legacy provider-specific annotations
Issue #8925

Currently relies on the model elements from the snapshot being mutable, but API could return a new IModel instead if this ever becomes not true.

src/EFCore.Design/Migrations/Design/MigrationsScaffolder.cs
@@ -98,7 +98,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
             }
 
             var modelSnapshot = Dependencies.MigrationsAssembly.ModelSnapshot;
-            var lastModel = modelSnapshot?.Model;
+            var lastModel = Dependencies.ModelProcessor.Process(modelSnapshot?.Model);
             var upOperations = Dependencies.MigrationsModelDiffer.GetDifferences(lastModel, Dependencies.Model);
             var downOperations = upOperations.Any()
                 ? Dependencies.MigrationsModelDiffer.GetDifferences(Dependencies.Model, lastModel)
@@ -184,7 +184,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
                 var migration = migrations[migrations.Count - 1];
                 model = migration.TargetModel;
 
-                if (!Dependencies.MigrationsModelDiffer.HasDifferences(model, modelSnapshot.Model))
+                if (!Dependencies.MigrationsModelDiffer.HasDifferences(model, Dependencies.ModelProcessor.Process(modelSnapshot.Model)))
                 {
                     if (force)
                     {

test/EFCore.Design.Tests/Migrations/Design/MigrationScaffolderTest.cs
@@ -45,6 +45,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
             var currentContext = new CurrentDbContext(new TContext());
             var idGenerator = new MigrationsIdGenerator();
             var code = new CSharpHelper();
+            var reporter = new TestOperationReporter();
 
             return new MigrationsScaffolder(
                 new MigrationsScaffolderDependencies(
@@ -66,8 +67,9 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
                                 new CSharpMigrationOperationGeneratorDependencies(code)),
                             new CSharpSnapshotGenerator(new CSharpSnapshotGeneratorDependencies(code)))),
                     new MockHistoryRepository(),
-                    new TestOperationReporter(),
-                    new MockProvider()));
+                    reporter,
+                    new MockProvider(),
+                    new ModelProcessor(reporter)));
         }
 
         private class GenericContext<T> : DbContext

dotnet/efcore

src/EFCore.Design/Migrations/Design/MigrationsScaffolder.cs
@@ -98,7 +98,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
             }
 
             var modelSnapshot = Dependencies.MigrationsAssembly.ModelSnapshot;
-            var lastModel = modelSnapshot?.Model;
+            var lastModel = Dependencies.SnapshotModelProcessor.Process(modelSnapshot?.Model);
             var upOperations = Dependencies.MigrationsModelDiffer.GetDifferences(lastModel, Dependencies.Model);
             var downOperations = upOperations.Any()
                 ? Dependencies.MigrationsModelDiffer.GetDifferences(Dependencies.Model, lastModel)
@@ -184,7 +184,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
                 var migration = migrations[migrations.Count - 1];
                 model = migration.TargetModel;
 
-                if (!Dependencies.MigrationsModelDiffer.HasDifferences(model, modelSnapshot.Model))
+                if (!Dependencies.MigrationsModelDiffer.HasDifferences(model, Dependencies.SnapshotModelProcessor.Process(modelSnapshot.Model)))
                 {
                     if (force)
                     {

test/EFCore.Design.Tests/Migrations/Design/MigrationScaffolderTest.cs
@@ -45,6 +45,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
             var currentContext = new CurrentDbContext(new TContext());
             var idGenerator = new MigrationsIdGenerator();
             var code = new CSharpHelper();
+            var reporter = new TestOperationReporter();
 
             return new MigrationsScaffolder(
                 new MigrationsScaffolderDependencies(
@@ -66,8 +67,9 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Design
                                 new CSharpMigrationOperationGeneratorDependencies(code)),
                             new CSharpSnapshotGenerator(new CSharpSnapshotGeneratorDependencies(code)))),
                     new MockHistoryRepository(),
-                    new TestOperationReporter(),
-                    new MockProvider()));
+                    reporter,
+                    new MockProvider(),
+                    new SnapshotModelProcessor(reporter)));
         }
 
         private class GenericContext<T> : DbContext