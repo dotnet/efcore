0e87f8aec9577f485c9b70bbc3351b96cc3a3ce4
Smit Patel <smitpatel@users.noreply.github.com>
2017-07-26 14:38:56 -0700

    Benchmarks: Project for EF Core 2.0.0 Preview 2 Use 2.0 released packages for common library Update to 2.0 final release once available on NuGet.

---
+++ benchmarks/EFCore.Benchmarks.EFCore2/Models/Orders/OrdersContext.cs
@@ -0,0 +1,46 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.EntityFrameworkCore.Benchmarks.Models.Orders;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.Models.Orders
+{
+    public class OrdersContext : DbContext
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly string _connectionString;
+        private readonly bool _disableBatching;
+
+        public OrdersContext(string connectionString, bool disableBatching = false)
+        {
+            _connectionString = connectionString;
+            _disableBatching = disableBatching;
+        }
+
+        public OrdersContext(IServiceProvider serviceProvider, string connectionString, bool disableBatching = false)
+        {
+            _serviceProvider = serviceProvider;
+            _connectionString = connectionString;
+            _disableBatching = disableBatching;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseInternalServiceProvider(_serviceProvider)
+                .UseSqlServer(
+                    _connectionString,
+                    b =>
+                        {
+                            if (_disableBatching)
+                            {
+                                b.MaxBatchSize(1);
+                            }
+                        });
+    }
+}
