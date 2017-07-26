fb0c289e965e4665584219983177fb62113ca0a4
Smit Patel <smitpatel@users.noreply.github.com>
2017-07-26 14:38:56 -0700

    Benchmarks: Project for EF Core 2.0.0 Preview 2 Use 2.0 released packages for common library Update to 2.0 final release once available on NuGet.

---
+++ benchmarks/EFCore.Benchmarks.EFCore2/ChangeTracker/FixupTests.cs
@@ -0,0 +1,132 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.Models.Orders;
+using Microsoft.EntityFrameworkCore.Benchmarks.Models.Orders;
+using Xunit;
+
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.ChangeTracker
+{
+    public class FixupTests
+    {
+        public abstract class Base
+        {
+            protected static readonly FixupFixture Fixture = new FixupFixture();
+            protected List<Customer> Customers;
+            protected List<Order> OrdersWithoutPk;
+            protected List<Order> OrdersWithPk;
+            protected OrdersContext Context;
+
+            [Params(true, false)]
+            public bool AutoDetectChanges { get; set; }
+
+            [GlobalSetup]
+            public virtual void CreateData()
+            {
+                Customers = Fixture.CreateCustomers(5000, setPrimaryKeys: true);
+                OrdersWithoutPk = Fixture.CreateOrders(Customers, ordersPerCustomer: 2, setPrimaryKeys: false);
+                OrdersWithPk = Fixture.CreateOrders(Customers, ordersPerCustomer: 2, setPrimaryKeys: true);
+
+                using (var context = Fixture.CreateContext())
+                {
+                    Assert.Equal(5000, context.Customers.Count());
+                    Assert.Equal(10000, context.Orders.Count());
+                }
+            }
+
+            [IterationSetup]
+            public virtual void InitializeContext()
+            {
+                Context = Fixture.CreateContext();
+                Context.ChangeTracker.AutoDetectChangesEnabled = AutoDetectChanges;
+            }
+
+            [IterationCleanup]
+            public virtual void CleanupContext()
+            {
+                Context.Dispose();
+            }
+        }
+
+        public class ChildVariation : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+                Context.Customers.AttachRange(Customers);
+            }
+
+            [Benchmark]
+            public virtual void AddChildren()
+            {
+                foreach (var order in OrdersWithoutPk)
+                {
+                    Context.Orders.Add(order);
+                }
+            }
+
+            [Benchmark]
+            public virtual void AttachChildren()
+            {
+                foreach (var order in OrdersWithPk)
+                {
+                    Context.Orders.Attach(order);
+                }
+            }
+
+            [Benchmark]
+            public virtual void QueryChildren()
+            {
+                Context.Orders.ToList();
+            }
+        }
+
+        public class ParentVariation : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+                Context.Orders.AttachRange(OrdersWithPk);
+            }
+
+            [Benchmark]
+            public virtual void AddParents()
+            {
+                foreach (var customer in Customers)
+                {
+                    Context.Customers.Add(customer);
+                }
+            }
+
+            [Benchmark]
+            public virtual void AttachParents()
+            {
+                foreach (var customer in Customers)
+                {
+                    Context.Customers.Attach(customer);
+                }
+            }
+
+            [Benchmark]
+            public virtual void QueryParents()
+            {
+                Context.Customers.ToList();
+            }
+        }
+
+        public class FixupFixture : OrdersFixture
+        {
+            public FixupFixture()
+                : base("Perf_ChangeTracker_Fixup", 0, 5000, 2, 0)
+            {
+            }
+        }
+    }
+}
---
+++ benchmarks/EFCore.Benchmarks.EFCore2/UpdatePipeline/SimpleUpdatePipelineTests.cs
@@ -0,0 +1,126 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.Models.Orders;
+using Microsoft.EntityFrameworkCore.Storage;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.UpdatePipeline
+{
+    public class SimpleUpdatePipelineTests
+    {
+        public abstract class Base
+        {
+            protected static readonly SimpleUpdatePipelineFixture Fixture = new SimpleUpdatePipelineFixture();
+            protected OrdersContext Context;
+            protected IDbContextTransaction Transaction;
+            private int _recordsAffected = -1;
+
+            [Params(true, false)]
+            public bool Async;
+
+            [Params(true, false)]
+            public bool Batching;
+
+            [IterationSetup]
+            public virtual void InitializeContext()
+            {
+                Context = Fixture.CreateContext(Batching);
+                Transaction = Context.Database.BeginTransaction();
+            }
+
+            [IterationCleanup]
+            public virtual void CleanupContext()
+            {
+                if (_recordsAffected != -1)
+                {
+                    Assert.Equal(1000, _recordsAffected);
+                }
+
+                Transaction.Dispose();
+                Context.Dispose();
+            }
+
+            [Benchmark]
+            public async Task UpdatePipeline()
+            {
+                _recordsAffected = Async
+                    ? await Context.SaveChangesAsync()
+                    : Context.SaveChanges();
+            }
+        }
+
+        public class Insert : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+
+                var customers = Fixture.CreateCustomers(1000, setPrimaryKeys: false);
+                Context.Customers.AddRange(customers);
+            }
+        }
+
+        public class Update : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+
+                foreach (var customer in Context.Customers)
+                {
+                    customer.FirstName += " Modified";
+                }
+            }
+        }
+
+        public class Delete : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+
+                Context.Customers.RemoveRange(Context.Customers.ToList());
+            }
+        }
+
+        public class Mixed : Base
+        {
+            [IterationSetup]
+            public override void InitializeContext()
+            {
+                base.InitializeContext();
+
+                var existingCustomers = Context.Customers.ToArray();
+                var newCustomers = Fixture.CreateCustomers(333, setPrimaryKeys: false);
+                Context.Customers.AddRange(newCustomers);
+
+                for (var i = 0; i < 1000; i += 3)
+                {
+                    Context.Customers.Remove(existingCustomers[i]);
+                }
+
+                for (var i = 1; i < 1000; i += 3)
+                {
+                    existingCustomers[i].FirstName += " Modified";
+                }
+            }
+        }
+
+        public class SimpleUpdatePipelineFixture : OrdersFixture
+        {
+            public SimpleUpdatePipelineFixture()
+                : base("Perf_UpdatePipeline_Simple", 0, 1000, 0, 0)
+            {
+            }
+
+            public OrdersContext CreateContext(bool batching) => new OrdersContext(ConnectionString, disableBatching: !batching);
+        }
+    }
+}
