9c53cbf23ef158d7f73aa43a20067310cd587ff3
Smit Patel <smitpatel@users.noreply.github.com>
2017-07-26 14:38:56 -0700

    Benchmarks: Project for EF Core 2.0.0 Preview 2 Use 2.0 released packages for common library Update to 2.0 final release once available on NuGet.

---
+++ benchmarks/EFCore.Benchmarks.EFCore2/InitializationTests.cs
@@ -0,0 +1,130 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.Models.AdventureWorks;
+using Microsoft.EntityFrameworkCore.Benchmarks.Models.AdventureWorks;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+// ReSharper disable InconsistentNaming
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks.EFCore2
+{
+    public class InitializationTests
+    {
+#if NET461
+        private ColdStartSandbox _sandbox;
+#endif
+        private ColdStartEnabledTests _testClass;
+
+#if NET461
+        [Params(true, false)]
+#elif NETCOREAPP2_0
+        [Params(false)]
+#endif
+        public bool Cold { get; set; }
+
+        [GlobalSetup]
+        public virtual void Initialize()
+        {
+            if (Cold)
+            {
+#if NET461
+                _sandbox = new ColdStartSandbox();
+                _testClass = _sandbox.CreateInstance<ColdStartEnabledTests>();
+#endif
+            }
+            else
+            {
+                _testClass = new ColdStartEnabledTests();
+            }
+        }
+
+#if NET461
+        [GlobalCleanup]
+        public virtual void CleanupContext()
+        {
+            _sandbox?.Dispose();
+        }
+#endif
+
+        [Benchmark]
+        public virtual void CreateAndDisposeUnusedContext()
+        {
+            _testClass.CreateAndDisposeUnusedContext(Cold ? 1 : 10000);
+        }
+
+        [Benchmark]
+        public virtual void InitializeAndQuery_AdventureWorks()
+        {
+            _testClass.InitializeAndQuery_AdventureWorks(Cold ? 1 : 1000);
+        }
+
+        [Benchmark]
+        public virtual void InitializeAndSaveChanges_AdventureWorks()
+        {
+            _testClass.InitializeAndSaveChanges_AdventureWorks(Cold ? 1 : 100);
+        }
+
+        [Benchmark]
+        public virtual void BuildModel_AdventureWorks()
+        {
+            var builder = new ModelBuilder(SqlServerConventionSetBuilder.Build());
+            AdventureWorksContext.ConfigureModel(builder);
+
+            // ReSharper disable once UnusedVariable
+            var model = builder.Model;
+        }
+
+        private class ColdStartEnabledTests : MarshalByRefObject
+        {
+            public void CreateAndDisposeUnusedContext(int count)
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    // ReSharper disable once UnusedVariable
+                    using (var context = AdventureWorksFixture.CreateContext())
+                    {
+                    }
+                }
+            }
+
+            public void InitializeAndQuery_AdventureWorks(int count)
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    using (var context = AdventureWorksFixture.CreateContext())
+                    {
+                        // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                        context.Department.First();
+                    }
+                }
+            }
+
+            public void InitializeAndSaveChanges_AdventureWorks(int count)
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    using (var context = AdventureWorksFixture.CreateContext())
+                    {
+                        context.Currency.Add(
+                            new Currency
+                            {
+                                CurrencyCode = "TMP",
+                                Name = "Temporary"
+                            });
+
+                        using (context.Database.BeginTransaction())
+                        {
+                            context.SaveChanges();
+
+                            // TODO: Don't mesure transaction rollback
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ benchmarks/EFCore.Benchmarks.EFCore2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkDotNet.Running;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.ChangeTracker;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.Query;
+using Microsoft.EntityFrameworkCore.Benchmarks.EFCore2.UpdatePipeline;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks.EFCore2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var benchmarkSummaryProcessor = new BenchmarkSummaryProcessor();
+
+            // Calibration
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<CalibrationTests>());
+
+            // Initialization
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<InitializationTests>());
+
+            // ChangeTracker
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<DbSetOperationTests.AddDataVariations>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<DbSetOperationTests.ExistingDataVariations>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<FixupTests.ChildVariation>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<FixupTests.ParentVariation>());
+
+            // Query
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<FuncletizationTests>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<NavigationsQueryTests>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<QueryCompilationTests>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<RawSqlQueryTests>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<SimpleQueryTests>());
+
+            // Update
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<SimpleUpdatePipelineTests.Insert>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<SimpleUpdatePipelineTests.Update>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<SimpleUpdatePipelineTests.Delete>());
+            benchmarkSummaryProcessor.Process(BenchmarkRunner.Run<SimpleUpdatePipelineTests.Mixed>());
+        }
+    }
+}
