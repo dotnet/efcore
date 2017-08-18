6076b1f7ed56f7c0a601b18342bdf80c6514de2e
Smit Patel <smitpatel@users.noreply.github.com>
2017-08-18 16:39:00 -0700

    Remove QueryableExtensions.Select method which was being used for internal purposes only

--- src/EFCore/Extensions/Internal/QueryableExtensions.cs
+++ src/EFCore/Extensions/Internal/QueryableExtensions.cs
@@ -4,8 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Query.Internal;
@@ -39,33 +37,6 @@ namespace Microsoft.EntityFrameworkCore.Extensions.Internal
             }
 
             throw new InvalidOperationException(CoreStrings.IQueryableNotAsync(typeof(TSource)));
-        }
-
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public static IQueryable Select(
-            [NotNull] this IQueryable source, [NotNull] string propertyName, [NotNull] Type sourceType, [NotNull] Type resultType)
-            => (IQueryable)_selectMethod.MakeGenericMethod(sourceType, resultType).Invoke(null, new object[] { source, propertyName });
-
-        private static readonly MethodInfo _selectMethod
-            = typeof(QueryableExtensions).GetTypeInfo().GetDeclaredMethods(nameof(Select)).Single(mi => mi.IsGenericMethodDefinition);
-
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public static IQueryable<TResult> Select<TSource, TResult>(
-            [NotNull] this IQueryable<TSource> source, [NotNull] string propertyName)
-            where TResult : class
-            where TSource : class
-        {
-            var parameter = Expression.Parameter(typeof(TSource), "e");
-            return source.Select(
-                Expression.Lambda<Func<TSource, TResult>>(
-                    Expression.MakeMemberAccess(parameter, typeof(TSource).GetAnyProperty(propertyName)),
-                    parameter));
         }
     }
 }
--- src/EFCore/Internal/EntityFinder.cs
+++ src/EFCore/Internal/EntityFinder.cs
@@ -5,11 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
-using Microsoft.EntityFrameworkCore.Extensions.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -285,8 +285,30 @@ namespace Microsoft.EntityFrameworkCore.Internal
                 return (IQueryable)((IDbSetCache)context).GetOrAddSet(context.GetDependencies().SetSource, entityType.ClrType);
             }
 
-            return BuildQueryRoot(context, definingEntityType)
-                .Select(entityType.DefiningNavigationName, definingEntityType.ClrType, entityType.ClrType);
+            return BuildQueryRoot(context, definingEntityType, entityType);
+        }
+
+        private static IQueryable BuildQueryRoot(DbContext context, IEntityType definingEntityType, IEntityType entityType)
+        {
+            var queryRoot = BuildQueryRoot(context, definingEntityType);
+
+            return (IQueryable)_selectMethod.MakeGenericMethod(definingEntityType.ClrType, entityType.ClrType)
+                .Invoke(null, new object[] { queryRoot, entityType.DefiningNavigationName });
+        }
+
+        private static readonly MethodInfo _selectMethod
+            = typeof(EntityFinder<TEntity>).GetTypeInfo().GetDeclaredMethods(nameof(Select)).Single(mi => mi.IsGenericMethodDefinition);
+
+        private static IQueryable<TResult> Select<TSource, TResult>(
+            [NotNull] IQueryable<TSource> source, [NotNull] string propertyName)
+            where TResult : class
+            where TSource : class
+        {
+            var parameter = Expression.Parameter(typeof(TSource), "e");
+            return source.Select(
+                Expression.Lambda<Func<TSource, TResult>>(
+                    Expression.MakeMemberAccess(parameter, typeof(TSource).GetAnyProperty(propertyName)),
+                    parameter));
         }
 
         private static BinaryExpression BuildPredicate(
