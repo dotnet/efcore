c1bf9cc33d0b9b3b2b1f4c74a65daa202c3bd06c
Arthur Vickers <ajcvickers@hotmail.com>
2017-08-31 16:42:17 -0700

    Allow passing a root for in-memory databases
    Alternate fix for issue #9613
    
    The internal service provider created by AddDbContext is typically different than the one created when `new` is used. This is because AddDbContext automatically sets ILoggerFactory, etc. from the application service provider, while `new` does not have access to this and so uses a different ILoggerFactory. The same thing can happen with other 'singleton options' like EnableSensitiveDataLogging.
    
    For the in-memory database, this has two consequences:
    * Since the store is rooted in the service provider, a new store is created when a situation like above forces a new internal service provider
    * Since the store uses `IEntityType` instances to key its table dictionary, these instances must come from the same model to match, which is typically not the case when the model is built twice; once for each service provider
    
    This fix adds a new object that can be passed to UseInMemoryDatabase such that the database can be rooted globally, however the application wishes. Entity type names are uses as keys instead of IEntityType instances.

---
+++ src/EFCore.InMemory/Storage/InMemoryDatabaseRoot.cs
@@ -0,0 +1,27 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.EntityFrameworkCore.Storage
+{
+    /// <summary>
+    ///     Acts as a root for all in-memory databases such that they will be available
+    ///     across context instances and service providers as long as the same instance
+    ///     of this type is passed to
+    ///     <see
+    ///         cref="InMemoryDbContextOptionsExtensions.UseInMemoryDatabase{TContext}(DbContextOptionsBuilder{TContext},string,System.Action{Infrastructure.InMemoryDbContextOptionsBuilder})" />
+    /// </summary>
+    public sealed class InMemoryDatabaseRoot
+    {
+        /// <summary>
+        /// <para>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </para>
+        /// <para>
+        ///     Entity Framework code will set this instance as needed. It should be considered opaque to
+        ///     application code; the type of object may change at any time.
+        /// </para>
+        /// </summary>
+        public object Instance;
+    }
+}
--- src/EFCore.InMemory/Storage/Internal/InMemoryStoreCache.cs
+++ src/EFCore.InMemory/Storage/Internal/InMemoryStoreCache.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Threading;
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 namespace Microsoft.EntityFrameworkCore.Storage.Internal
 {
@@ -15,18 +16,43 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
     public class InMemoryStoreCache : IInMemoryStoreCache
     {
         private readonly IInMemoryTableFactory _tableFactory;
-
-        private readonly Lazy<ConcurrentDictionary<string, IInMemoryStore>> _namedStores
-            = new Lazy<ConcurrentDictionary<string, IInMemoryStore>>(
-                () => new ConcurrentDictionary<string, IInMemoryStore>(), LazyThreadSafetyMode.PublicationOnly);
+        private readonly bool _useNameMatching;
+        private readonly ConcurrentDictionary<string, IInMemoryStore> _namedStores;
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [Obsolete("Use the constructor that also accepts options.")]
         public InMemoryStoreCache([NotNull] IInMemoryTableFactory tableFactory)
+            : this(tableFactory, null)
+        {
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public InMemoryStoreCache(
+            [NotNull] IInMemoryTableFactory tableFactory, 
+            [CanBeNull] IInMemorySingletonOptions options)
         {
             _tableFactory = tableFactory;
+
+            if (options?.DatabaseRoot != null)
+            {
+                _useNameMatching = true;
+
+                LazyInitializer.EnsureInitialized(
+                    ref options.DatabaseRoot.Instance,
+                    () => new ConcurrentDictionary<string, IInMemoryStore>());
+
+                _namedStores = (ConcurrentDictionary<string, IInMemoryStore>)options.DatabaseRoot.Instance;
+            }
+            else
+            {
+                _namedStores = new ConcurrentDictionary<string, IInMemoryStore>();
+            }
         }
 
         /// <summary>
@@ -34,6 +60,6 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public virtual IInMemoryStore GetStore(string name)
-            => _namedStores.Value.GetOrAdd(name, n => new InMemoryStore(_tableFactory));
+            => _namedStores.GetOrAdd(name, n => new InMemoryStore(_tableFactory, _useNameMatching));
     }
 }
