dfa00f2a3325c2c913638367b72ab5e2b6be890c
Smit Patel <smitpatel@users.noreply.github.com>
2017-09-12 11:23:11 -0700

    RevEng: Don't generate nullable bool if default value is false

--- test/EFCore.SqlServer.Design.FunctionalTests/ReverseEngineering/Expected/NonNullBoolWithDefault.cs
+++ test/EFCore.SqlServer.Design.FunctionalTests/ReverseEngineering/Expected/NonNullBoolWithDefault.cs
@@ -7,6 +7,11 @@ namespace E2ETest.Namespace
     {
         public int Id { get; set; }
         public bool? BoolWithDefaultValueSql { get; set; }
+        public bool BoolWithDefaultValueSqlFalse1 { get; set; }
+        public bool BoolWithDefaultValueSqlFalse2 { get; set; }
+        public bool BoolWithDefaultValueSqlFalse3 { get; set; }
+        public bool BoolWithDefaultValueSqlFalse4 { get; set; }
+        public bool BoolWithDefaultValueSqlFalse5 { get; set; }
         public bool BoolWithoutDefaultValueSql { get; set; }
     }
 }
