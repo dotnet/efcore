f7a71fff5a9a76dbd3ef574370fbcdbaf2cf4f9f
Andrew Peters <anpete@microsoft.com>
2017-09-16 11:21:01 -0700

    Restore GetAwaiter behavior of TaskBlockingExpressionVisitor.

--- src/EFCore/Query/ExpressionVisitors/Internal/TaskBlockingExpressionVisitor.cs
+++ src/EFCore/Query/ExpressionVisitors/Internal/TaskBlockingExpressionVisitor.cs
@@ -4,6 +4,7 @@
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
 {
@@ -26,11 +27,20 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
                 if (typeInfo.IsGenericType
                     && typeInfo.GetGenericTypeDefinition() == typeof(Task<>))
                 {
-                    return Expression.Property(expression, nameof(Task<object>.Result));
+                    return Expression.Call(
+                        ResultMethodInfo.MakeGenericMethod(typeInfo.GenericTypeArguments[0]),
+                        expression);
                 }
             }
 
             return expression;
         }
+
+        internal static MethodInfo ResultMethodInfo { get; }
+            = typeof(TaskBlockingExpressionVisitor).GetTypeInfo()
+                .GetDeclaredMethod(nameof(Result));
+
+        [UsedImplicitly]
+        private static T Result<T>(Task<T> task) => task.GetAwaiter().GetResult();
     }
 }
