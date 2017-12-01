ecb5066eb6e6415c043393bfc200f2f84d8aa604
Brice Lambson <bricelam@users.noreply.github.com>
2017-11-30 16:05:35 -0800

    Tests: Make TempDirectory more robust
    Part of #10456

--- test/ef.Tests/TestUtilities/TempDirectory.cs
+++ test/ef.Tests/TestUtilities/TempDirectory.cs
@@ -2,7 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
+using Xunit;
 using IOPath = System.IO.Path;
 
 namespace Microsoft.EntityFrameworkCore.Tools.TestUtilities
@@ -12,12 +15,27 @@ namespace Microsoft.EntityFrameworkCore.Tools.TestUtilities
         public TempDirectory()
         {
             Path = IOPath.Combine(IOPath.GetTempPath(), IOPath.GetRandomFileName());
+            Assert.False(Directory.Exists(Path), $"Temporary directory '{Path}' already exists.");
             Directory.CreateDirectory(Path);
         }
 
         public string Path { get; }
 
         public void Dispose()
-            => Directory.Delete(Path, recursive: true);
+        {
+            var stopwatch = Stopwatch.StartNew();
+            while (true)
+            {
+                try
+                {
+                    Directory.Delete(Path, recursive: true);
+                    return;
+                }
+                catch when (stopwatch.ElapsedMilliseconds < 30000)
+                {
+                    Thread.Sleep(150);
+                }
+            }
+        }
     }
 }
