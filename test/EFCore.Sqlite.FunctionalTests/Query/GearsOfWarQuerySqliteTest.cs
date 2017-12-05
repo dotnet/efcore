10cc965c193917f550060e00e4bec96419dea5e2
ralmsdeveloper <ralms@ralms.net>
2017-12-05 01:22:10 -0200

    Remove Skip

--- test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
@@ -11,47 +11,5 @@ namespace Microsoft.EntityFrameworkCore.Query
             : base(fixture)
         {
         }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddYears()
-        {
-            base.DateTimeOffset_DateAdd_AddYears();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddMinutes()
-        {
-            base.DateTimeOffset_DateAdd_AddMinutes();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddSeconds()
-        {
-            base.DateTimeOffset_DateAdd_AddSeconds();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddHours()
-        {
-            base.DateTimeOffset_DateAdd_AddHours();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddMonths()
-        {
-            base.DateTimeOffset_DateAdd_AddMonths();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddDays()
-        {
-            base.DateTimeOffset_DateAdd_AddDays();
-        }
-
-        [Fact(Skip = "Partially implementing for SQLite")]
-        public override void DateTimeOffset_DateAdd_AddMilliseconds()
-        {
-            base.DateTimeOffset_DateAdd_AddMilliseconds();
-        }
     }
 }
