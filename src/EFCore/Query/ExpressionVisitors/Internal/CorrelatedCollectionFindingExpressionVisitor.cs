BaceK95/EntityFrameworkCore

Fix to #9282 - Query: optimize queries projecting correlated collections, so that they don't result in N+1 database queries
This feature optimizes a number of queries that project correlated collections. Previously those would produce N+1 queries. Now, we rewrite queries similarly to how Include pipeline does it, producing only two queries and correlating them on the client.
To enable the feature the inner subquery needs to be wrapped around ToList() or ToArray() call.

Current limitations:
- only works for sync queries,
- child entities are not being tracked,
- no fixup between parent and child,
- doesn't work if the parent query results in a CROSS JOIN,
- doesn't work with result operators (i.e. Skip/Take/Distinct)
- doesn't work if outer query needs client evaluation anywhere outside projection (e.g. order by or filter by NonMapped property)
- doesn't work if inner query is correlated with query two (or more) levels up, (e.g. customers.Select(c => c.Orders.Select(o => o.OrderDetails.Where(od => od.Name == c.Name).ToList()).ToList())
- doesn't work in nested scenarios where the outer collection is streaming (e.g. customers.Select(c => c.Orders.Select(o => o.OrderDetails.Where(od => od.Name != "Foo").ToList())) - to make it work, outer collection must also be wrapped in ToList(). However it is OK to stream inner collection - in that case outer collection will take advantage of the optimization.

Optimization process:

original query:

from c in ctx.Customers
where c.CustomerID != "ALFKI"
orderby c.City descending
select (from o in c.Orders
        where o.OrderID > 100
        orderby o.EmployeeID
        select new { o.OrderID, o.CustomerID }).ToList()

nav rewrite converts it to:

from c in customers
where c.CustomerID != "ALFKI"
order by c.City descending
select
   (from o in orders
    where o.OrderID > 100
    order by o.EmployeeID
    where c.CustomerID ?= o.CustomerID
    select new { o.OrderID, o.CustomerID }).ToList()

which gets rewritten to (simplified):

src/EFCore/Query/ExpressionVisitors/Internal/CorrelatedCollectionFindingExpressionVisitor.cs
@@ -1,0 +1,243 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Extensions.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using JetBrains.Annotations;
+using Remotion.Linq.Clauses.Expressions;
+using Remotion.Linq.Parsing;
+using Remotion.Linq;
+using Remotion.Linq.Clauses;
+
+namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
+{
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class CorrelatedCollectionFindingExpressionVisitor : RelinqExpressionVisitor
+    {
+        private EntityQueryModelVisitor _queryModelVisitor;
+        private CorrelatedSubqueryOptimizationValidator _validator;
+
+        private static readonly MethodInfo _toListMethodInfo
+            = typeof(Enumerable).GetTypeInfo().GetDeclaredMethod(nameof(Enumerable.ToList));
+
+        private static readonly MethodInfo _toArrayMethodInfo
+            = typeof(Enumerable).GetTypeInfo().GetDeclaredMethod(nameof(Enumerable.ToArray));
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public CorrelatedCollectionFindingExpressionVisitor([NotNull] EntityQueryModelVisitor queryModelVisitor)
+        {
+            _queryModelVisitor = queryModelVisitor;
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.Name.StartsWith(nameof(IQueryBuffer.IncludeCollection), StringComparison.Ordinal))
+            {
+                return node;
+            }
+
+            SubQueryExpression subQueryExpression = null;
+            if ((node.Method.MethodIsClosedFormOf(_toListMethodInfo) || node.Method.MethodIsClosedFormOf(_toArrayMethodInfo))
+                && node.Arguments[0] is SubQueryExpression)
+            {
+                subQueryExpression = (SubQueryExpression)node.Arguments[0];
+            }
+
+            if (node.Method.MethodIsClosedFormOf(CollectionNavigationSubqueryInjector.MaterializeCollectionNavigationMethodInfo)
+                && node.Arguments[1] is SubQueryExpression)
+            {
+                subQueryExpression = (SubQueryExpression)node.Arguments[1];
+            }
+
+            if (subQueryExpression != null)
+            {
+                TryMarkSubQuery(subQueryExpression);
+
+                return node;
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        protected override Expression VisitSubQuery(SubQueryExpression expression)
+            // prune subqueries (and potential subqueries inside them) that are not wrapped around ToList/ToArray
+            // we can't optimize correlated collection if it's parent is streaming
+            => expression; 
+
+        private void TryMarkSubQuery(SubQueryExpression expression)
+        {
+            var subQueryModel = expression.QueryModel;
+            subQueryModel.SelectClause.TransformExpressions(Visit);
+
+            if (_validator == null)
+            {
+                _validator = new CorrelatedSubqueryOptimizationValidator();
+            }
+
+            if (_validator.CanTryOptimizeCorreltedSubquery(subQueryModel))
+            {
+                // if the query passes validation it becomes a candidate for future optimization
+                // optimiation can't always be performed, e.g. when client-eval is needed
+                // but we need to collect metadata (i.e. INavigations) before nav rewrite converts them into joins
+                _queryModelVisitor.BindNavigationPathPropertyExpression(
+                    subQueryModel.MainFromClause.FromExpression,
+                    (properties, querySource) =>
+                    {
+                        var collectionNavigation = properties.OfType<INavigation>().SingleOrDefault(n => n.IsCollection());
+
+                        if (collectionNavigation != null)
+                        {
+                            _queryModelVisitor.QueryCompilationContext.CorrelatedSubqueryMetadataMap[subQueryModel.MainFromClause] = new CorrelatedSubqueryMetadata
+                            {
+                                Index = _queryModelVisitor.QueryCompilationContext.CorrelatedSubqueryMetadataMap.Count,
+                                FirstNavigation = properties.OfType<INavigation>().First(),
+                                CollectionNavigation = collectionNavigation,
+                                ParentQuerySource = querySource
+                            };
+
+                            return expression;
+                        }
+
+                        return default;
+                    });
+            }
+        }
+
+        private class CorrelatedSubqueryOptimizationValidator
+        {
+            public bool CanTryOptimizeCorreltedSubquery(QueryModel queryModel)
+            {
+                if (queryModel.ResultOperators.Any())
+                {
+                    return false;
+                }
+
+                // first pass finds all the query sources defined in this scope (i.e. from clauses)
+                var declaredQuerySourcesFinder = new DefinedQuerySourcesFindingVisitor();
+                declaredQuerySourcesFinder.VisitQueryModel(queryModel);
+
+                // second pass makes sure that all qsres reference only query sources that were discovered in the first step, i.e. nothing from the outside
+                var qsreScopeValidator = new ReferencedQuerySourcesScopeValidatingVisitor(queryModel.MainFromClause, declaredQuerySourcesFinder.QuerySources);
+                qsreScopeValidator.VisitQueryModel(queryModel);
+
+                return qsreScopeValidator.AllQuerySourceReferencesInScope;
+            }
+
+            private class DefinedQuerySourcesFindingVisitor : QueryModelVisitorBase
+            {
+                public ISet<IQuerySource> QuerySources { get; } = new HashSet<IQuerySource>();
+
+                public override void VisitQueryModel(QueryModel queryModel)
+                {
+                    queryModel.TransformExpressions(new TransformingQueryModelExpressionVisitor<DefinedQuerySourcesFindingVisitor>(this).Visit);
+
+                    base.VisitQueryModel(queryModel);
+                }
+
+                public override void VisitMainFromClause(MainFromClause fromClause, QueryModel queryModel)
+                {
+                    QuerySources.Add(fromClause);
+
+                    base.VisitMainFromClause(fromClause, queryModel);
+                }
+
+                public override void VisitAdditionalFromClause(AdditionalFromClause fromClause, QueryModel queryModel, int index)
+                {
+                    QuerySources.Add(fromClause);
+
+                    base.VisitAdditionalFromClause(fromClause, queryModel, index);
+                }
+            }
+
+            private class ReferencedQuerySourcesScopeValidatingVisitor : QueryModelVisitorBase
+            {
+                private class InnerVisitor : TransformingQueryModelExpressionVisitor<ReferencedQuerySourcesScopeValidatingVisitor>
+                {
+                    private ISet<IQuerySource> _querySourcesInScope;
+
+                    public InnerVisitor(ISet<IQuerySource> querySourcesInScope, ReferencedQuerySourcesScopeValidatingVisitor transformingQueryModelVisitor)
+                        : base(transformingQueryModelVisitor)
+                    {
+                        _querySourcesInScope = querySourcesInScope;
+                    }
+
+                    public bool AllQuerySourceReferencesInScope { get; private set; } = true;
+
+                    protected override Expression VisitQuerySourceReference(QuerySourceReferenceExpression expression)
+                    {
+                        if (!_querySourcesInScope.Contains(expression.ReferencedQuerySource))
+                        {
+                            AllQuerySourceReferencesInScope = false;
+                        }
+
+                        return base.VisitQuerySourceReference(expression);
+                    }
+                }
+
+                // query source that can reference something outside the scope, e.g. main from clause that contains the correlated navigation
+                private IQuerySource _exemptQuerySource;
+                private InnerVisitor _innerVisitor;
+
+                public ReferencedQuerySourcesScopeValidatingVisitor(IQuerySource exemptQuerySource, ISet<IQuerySource> querySourcesInScope)
+                {
+                    _exemptQuerySource = exemptQuerySource;
+                    _innerVisitor = new InnerVisitor(querySourcesInScope, this);
+                }
+
+                public bool AllQuerySourceReferencesInScope => _innerVisitor.AllQuerySourceReferencesInScope;
+
+                public override void VisitMainFromClause(MainFromClause fromClause, QueryModel queryModel)
+                {
+                    if (fromClause != _exemptQuerySource)
+                    {
+                        fromClause.TransformExpressions(_innerVisitor.Visit);
+                    }
+                }
+
+                protected override void VisitBodyClauses(ObservableCollection<IBodyClause> bodyClauses, QueryModel queryModel)
+                {
+                    foreach (var bodyClause in bodyClauses)
+                    {
+                        if (bodyClause != _exemptQuerySource)
+                        {
+                            bodyClause.TransformExpressions(_innerVisitor.Visit);
+                        }
+                    }
+                }
+
+                public override void VisitSelectClause(SelectClause selectClause, QueryModel queryModel)
+                {
+                    selectClause.TransformExpressions(_innerVisitor.Visit);
+                }
+
+                public override void VisitResultOperator(ResultOperatorBase resultOperator, QueryModel queryModel, int index)
+                {
+                    // it is not necessary to visit result ops at the moment, since we don't optimize subqueries that contain any result ops
+                    // however, we might support some result ops in the future
+                    resultOperator.TransformExpressions(_innerVisitor.Visit);
+                }
+            }
+        }
+    }
+}

dotnet/efcore

src/EFCore/Query/ExpressionVisitors/Internal/CorrelatedCollectionFindingExpressionVisitor.cs
@@ -1,0 +1,249 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Extensions.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using JetBrains.Annotations;
+using Remotion.Linq.Clauses.Expressions;
+using Remotion.Linq.Parsing;
+using Remotion.Linq;
+using Remotion.Linq.Clauses;
+
+namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
+{
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class CorrelatedCollectionFindingExpressionVisitor : RelinqExpressionVisitor
+    {
+        private EntityQueryModelVisitor _queryModelVisitor;
+        private CorrelatedSubqueryOptimizationValidator _validator;
+
+        private static readonly MethodInfo _toListMethodInfo
+            = typeof(Enumerable).GetTypeInfo().GetDeclaredMethod(nameof(Enumerable.ToList));
+
+        private static readonly MethodInfo _toArrayMethodInfo
+            = typeof(Enumerable).GetTypeInfo().GetDeclaredMethod(nameof(Enumerable.ToArray));
+
+        private bool _trackingQuery;
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public CorrelatedCollectionFindingExpressionVisitor(
+            [NotNull] EntityQueryModelVisitor queryModelVisitor,
+            bool trackingQuery)
+        {
+            _queryModelVisitor = queryModelVisitor;
+            _trackingQuery = trackingQuery;
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.Name.StartsWith(nameof(IQueryBuffer.IncludeCollection), StringComparison.Ordinal))
+            {
+                return node;
+            }
+
+            SubQueryExpression subQueryExpression = null;
+            if ((node.Method.MethodIsClosedFormOf(_toListMethodInfo) || node.Method.MethodIsClosedFormOf(_toArrayMethodInfo))
+                && node.Arguments[0] is SubQueryExpression)
+            {
+                subQueryExpression = (SubQueryExpression)node.Arguments[0];
+            }
+
+            if (node.Method.MethodIsClosedFormOf(CollectionNavigationSubqueryInjector.MaterializeCollectionNavigationMethodInfo)
+                && node.Arguments[1] is SubQueryExpression)
+            {
+                subQueryExpression = (SubQueryExpression)node.Arguments[1];
+            }
+
+            if (subQueryExpression != null)
+            {
+                TryMarkSubQuery(subQueryExpression);
+
+                return node;
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        protected override Expression VisitSubQuery(SubQueryExpression expression)
+            // prune subqueries (and potential subqueries inside them) that are not wrapped around ToList/ToArray
+            // we can't optimize correlated collection if it's parent is streaming
+            => expression;
+
+        private void TryMarkSubQuery(SubQueryExpression expression)
+        {
+            var subQueryModel = expression.QueryModel;
+            subQueryModel.SelectClause.TransformExpressions(Visit);
+
+            if (_validator == null)
+            {
+                _validator = new CorrelatedSubqueryOptimizationValidator();
+            }
+
+            if (_validator.CanTryOptimizeCorrelatedSubquery(subQueryModel))
+            {
+                // if the query passes validation it becomes a candidate for future optimization
+                // optimization can't always be performed, e.g. when client-eval is needed
+                // but we need to collect metadata (i.e. INavigations) before nav rewrite converts them into joins
+                _queryModelVisitor.BindNavigationPathPropertyExpression(
+                    subQueryModel.MainFromClause.FromExpression,
+                    (properties, querySource) =>
+                    {
+                        var collectionNavigation = properties.OfType<INavigation>().SingleOrDefault(n => n.IsCollection());
+
+                        if (collectionNavigation != null)
+                        {
+                            _queryModelVisitor.QueryCompilationContext.CorrelatedSubqueryMetadataMap[subQueryModel.MainFromClause] = new CorrelatedSubqueryMetadata
+                            {
+                                Index = _queryModelVisitor.QueryCompilationContext.CorrelatedSubqueryMetadataMap.Count,
+                                TrackingQuery = _trackingQuery,
+                                FirstNavigation = properties.OfType<INavigation>().First(),
+                                CollectionNavigation = collectionNavigation,
+                                ParentQuerySource = querySource
+                            };
+
+                            return expression;
+                        }
+
+                        return default;
+                    });
+            }
+        }
+
+        private class CorrelatedSubqueryOptimizationValidator
+        {
+            public bool CanTryOptimizeCorrelatedSubquery(QueryModel queryModel)
+            {
+                if (queryModel.ResultOperators.Count > 0)
+                {
+                    return false;
+                }
+
+                // first pass finds all the query sources defined in this scope (i.e. from clauses)
+                var definedQuerySourcesFinder = new DefinedQuerySourcesFindingVisitor();
+                definedQuerySourcesFinder.VisitQueryModel(queryModel);
+
+                // second pass makes sure that all qsres reference only query sources that were discovered in the first step, i.e. nothing from the outside
+                var qsreScopeValidator = new ReferencedQuerySourcesScopeValidatingVisitor(queryModel.MainFromClause, definedQuerySourcesFinder.QuerySources);
+                qsreScopeValidator.VisitQueryModel(queryModel);
+
+                return qsreScopeValidator.AllQuerySourceReferencesInScope;
+            }
+
+            private class DefinedQuerySourcesFindingVisitor : QueryModelVisitorBase
+            {
+                public ISet<IQuerySource> QuerySources { get; } = new HashSet<IQuerySource>();
+
+                public override void VisitQueryModel(QueryModel queryModel)
+                {
+                    queryModel.TransformExpressions(new TransformingQueryModelExpressionVisitor<DefinedQuerySourcesFindingVisitor>(this).Visit);
+
+                    base.VisitQueryModel(queryModel);
+                }
+
+                public override void VisitMainFromClause(MainFromClause fromClause, QueryModel queryModel)
+                {
+                    QuerySources.Add(fromClause);
+
+                    base.VisitMainFromClause(fromClause, queryModel);
+                }
+
+                public override void VisitAdditionalFromClause(AdditionalFromClause fromClause, QueryModel queryModel, int index)
+                {
+                    QuerySources.Add(fromClause);
+
+                    base.VisitAdditionalFromClause(fromClause, queryModel, index);
+                }
+            }
+
+            private class ReferencedQuerySourcesScopeValidatingVisitor : QueryModelVisitorBase
+            {
+                private class InnerVisitor : TransformingQueryModelExpressionVisitor<ReferencedQuerySourcesScopeValidatingVisitor>
+                {
+                    private ISet<IQuerySource> _querySourcesInScope;
+
+                    public InnerVisitor(ISet<IQuerySource> querySourcesInScope, ReferencedQuerySourcesScopeValidatingVisitor transformingQueryModelVisitor)
+                        : base(transformingQueryModelVisitor)
+                    {
+                        _querySourcesInScope = querySourcesInScope;
+                    }
+
+                    public bool AllQuerySourceReferencesInScope { get; private set; } = true;
+
+                    protected override Expression VisitQuerySourceReference(QuerySourceReferenceExpression expression)
+                    {
+                        if (!_querySourcesInScope.Contains(expression.ReferencedQuerySource))
+                        {
+                            AllQuerySourceReferencesInScope = false;
+                        }
+
+                        return base.VisitQuerySourceReference(expression);
+                    }
+                }
+
+                // query source that can reference something outside the scope, e.g. main from clause that contains the correlated navigation
+                private IQuerySource _exemptQuerySource;
+                private InnerVisitor _innerVisitor;
+
+                public ReferencedQuerySourcesScopeValidatingVisitor(IQuerySource exemptQuerySource, ISet<IQuerySource> querySourcesInScope)
+                {
+                    _exemptQuerySource = exemptQuerySource;
+                    _innerVisitor = new InnerVisitor(querySourcesInScope, this);
+                }
+
+                public bool AllQuerySourceReferencesInScope => _innerVisitor.AllQuerySourceReferencesInScope;
+
+                public override void VisitMainFromClause(MainFromClause fromClause, QueryModel queryModel)
+                {
+                    if (fromClause != _exemptQuerySource)
+                    {
+                        fromClause.TransformExpressions(_innerVisitor.Visit);
+                    }
+                }
+
+                protected override void VisitBodyClauses(ObservableCollection<IBodyClause> bodyClauses, QueryModel queryModel)
+                {
+                    foreach (var bodyClause in bodyClauses)
+                    {
+                        if (bodyClause != _exemptQuerySource)
+                        {
+                            bodyClause.TransformExpressions(_innerVisitor.Visit);
+                        }
+                    }
+                }
+
+                public override void VisitSelectClause(SelectClause selectClause, QueryModel queryModel)
+                {
+                    selectClause.TransformExpressions(_innerVisitor.Visit);
+                }
+
+                public override void VisitResultOperator(ResultOperatorBase resultOperator, QueryModel queryModel, int index)
+                {
+                    // it is not necessary to visit result ops at the moment, since we don't optimize subqueries that contain any result ops
+                    // however, we might support some result ops in the future
+                    resultOperator.TransformExpressions(_innerVisitor.Visit);
+                }
+            }
+        }
+    }
+}