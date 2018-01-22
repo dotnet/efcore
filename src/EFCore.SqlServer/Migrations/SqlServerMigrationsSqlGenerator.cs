3394dc635304e0a56397e514105023d988e0abaa
ralmsdeveloper <ralms@ralms.net>
2018-01-22 14:51:25 -0300

    Revert "Fix Issue:10740"
    This reverts commit 1c530b95a2334221c653eaab2c417e922d38d07c.

--- src/EFCore.SqlServer/Migrations/SqlServerMigrationsSqlGenerator.cs
+++ src/EFCore.SqlServer/Migrations/SqlServerMigrationsSqlGenerator.cs
@@ -657,20 +657,12 @@ namespace Microsoft.EntityFrameworkCore.Migrations
             builder
                 .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator)
                 .EndCommand(suppressTransaction: true)
-                .AppendLine("IF SERVERPROPERTY('EngineEdition') <> 5")
-                .AppendLine("BEGIN");
-
-            using (builder.Indent())
-            {
-                builder
-                    .Append("ALTER DATABASE ")
-                    .Append(Dependencies.SqlGenerationHelper.DelimitIdentifier(operation.Name))
-                    .Append(" SET READ_COMMITTED_SNAPSHOT ON")
-                    .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
-            }
-
-            builder
-                .Append("END")
+                .Append("IF SERVERPROPERTY('EngineEdition') <> 5 EXEC(N'ALTER DATABASE ")
+                .Append(Dependencies.SqlGenerationHelper.DelimitIdentifier(operation.Name))
+                .Append(" SET READ_COMMITTED_SNAPSHOT ON")
+                .Append(Dependencies.SqlGenerationHelper.StatementTerminator)
+                .Append("')")
+                .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator)
                 .EndCommand(suppressTransaction: true);
         }
 
