fef00eaa8038417ddca36b283fb16c869d237395
Shay Rojansky <roji@roji.org>
2018-02-26 17:46:06 +0200

    Another test OrderBy fix
    Like #9581 but for async

--- src/EFCore.Specification.Tests/Query/AsyncGroupByQueryTestBase.cs
+++ src/EFCore.Specification.Tests/Query/AsyncGroupByQueryTestBase.cs
@@ -1121,7 +1121,7 @@ namespace Microsoft.EntityFrameworkCore.Query
         public virtual async Task OrderBy_Skip_GroupBy()
         {
             await AssertQuery<Order>(
-                os => os.OrderBy(o => o.OrderDate).Skip(800).GroupBy(o => o.CustomerID),
+                os => os.OrderBy(o => o.OrderDate).ThenBy(o => o.OrderID).Skip(800).GroupBy(o => o.CustomerID),
                 elementSorter: GroupingSorter<string, object>(),
                 elementAsserter: GroupingAsserter<string, dynamic>(d => d.OrderDate),
                 entryCount: 30);
