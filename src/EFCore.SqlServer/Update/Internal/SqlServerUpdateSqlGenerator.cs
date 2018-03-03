7ea1ca64531f1cd3663a6d5e818d3c977716e969
ralmsdeveloper <ralms@ralms.net>
2018-03-02 23:03:57 -0300

    Small Code Cleanup

--- src/EFCore.SqlServer/Update/Internal/SqlServerUpdateSqlGenerator.cs
+++ src/EFCore.SqlServer/Update/Internal/SqlServerUpdateSqlGenerator.cs
@@ -10,7 +10,6 @@ using System.Text;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Microsoft.EntityFrameworkCore.Update.Internal
 {
