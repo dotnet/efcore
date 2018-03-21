ab0f724f0e9dad0ca2366abd70e2e72c822f6bc2
Shay Rojansky <roji@roji.org>
2018-03-21 10:45:09 +0200

    Don't unwrap enums in MigrationsModelDiffer

--- src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
+++ src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
@@ -953,7 +953,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Internal
         {
             columnOperation.ClrType
                 = typeMapping.Converter?.ProviderClrType
-                  ?? typeMapping.ClrType.UnwrapNullableType().UnwrapEnumType();
+                  ?? typeMapping.ClrType.UnwrapNullableType();
 
             columnOperation.ColumnType = property.GetConfiguredColumnType();
             columnOperation.MaxLength = property.GetMaxLength();
