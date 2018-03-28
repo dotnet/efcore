85f0ae9fb5c1ecf964be6d434e3416a01bf302a2
John Luo <johluo@microsoft.com>
2018-03-28 02:49:12 -0700

    Update Core runtime version

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ErrorActionPreference = "Stop"
 
 function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $retries) 
@@ -65,4 +66,5 @@ if (!(Test-Path $buildFolder)) {
 }
 
 &"$buildFile" $args
+
 
