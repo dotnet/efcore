560b6d0c48f5281ae0a050d95ae56d17f992886c
Arthur Vickers <ajcvickers@hotmail.com>
2018-03-29 19:34:31 -0700

    Perf: make TypeMappingInfo a struct
    Fixes #11424
    
    There is still some code duplication that could be refactored, but hopefully the shape is right.

--- src/EFCore/Storage/TypeMappingSource.cs
+++ src/EFCore/Storage/TypeMappingSource.cs
@@ -8,7 +8,6 @@ using System.Reflection;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.EntityFrameworkCore.Utilities;
 
 namespace Microsoft.EntityFrameworkCore.Storage
@@ -16,14 +15,14 @@ namespace Microsoft.EntityFrameworkCore.Storage
     /// <summary>
     ///     <para>
     ///         The base class for non-relational type mapping starting with version 2.1. Non-relational providers
-    ///         should derive from this class and override <see cref="FindMapping(TypeMappingInfo)" />
+    ///         should derive from this class and override <see cref="TypeMappingSourceBase.FindMapping(TypeMappingInfo)" />
     ///     </para>
     ///     <para>
     ///         This type is typically used by database providers (and other extensions). It is generally
     ///         not used in application code.
     ///     </para>
     /// </summary>
-    public abstract class TypeMappingSource : ITypeMappingSource
+    public abstract class TypeMappingSource : TypeMappingSourceBase
     {
         private readonly ConcurrentDictionary<TypeMappingInfo, CoreTypeMapping> _explicitMappings
             = new ConcurrentDictionary<TypeMappingInfo, CoreTypeMapping>();
@@ -33,55 +32,12 @@ namespace Microsoft.EntityFrameworkCore.Storage
         /// </summary>
         /// <param name="dependencies"> Parameter object containing dependencies for this service. </param>
         protected TypeMappingSource([NotNull] TypeMappingSourceDependencies dependencies)
-        {
-            Check.NotNull(dependencies, nameof(dependencies));
-
-            Dependencies = dependencies;
-        }
-
-        /// <summary>
-        ///     Dependencies used to create this <see cref="TypeMappingSource" />
-        /// </summary>
-        protected virtual TypeMappingSourceDependencies Dependencies { get; }
-
-        /// <summary>
-        ///     <para>
-        ///         Overridden by database providers to find a type mapping for the given info.
-        ///     </para>
-        ///     <para>
-        ///         The mapping info is populated with as much information about the required type mapping as
-        ///         is available. Use all the information necessary to create the best mapping. Return <c>null</c>
-        ///         if no mapping is available.
-        ///     </para>
-        /// </summary>
-        /// <param name="mappingInfo"> The mapping info to use to create the mapping. </param>
-        /// <returns> The type mapping, or <c>null</c> if none could be found. </returns>
-        protected abstract CoreTypeMapping FindMapping([NotNull] TypeMappingInfo mappingInfo);
-
-        /// <summary>
-        ///     Called after a mapping has been found so that it can be validated for the given property.
-        /// </summary>
-        /// <param name="mapping"> The mapping, if any. </param>
-        /// <param name="property"> The property, if any. </param>
-        protected virtual void ValidateMapping(
-            [CanBeNull] CoreTypeMapping mapping,
-            [CanBeNull] IProperty property)
+            : base(dependencies)
         {
         }
 
-        /// <summary>
-        ///     <para>
-        ///         Uses any available <see cref="ValueConverter" /> to help find a mapping that works.
-        ///     </para>
-        ///     <para>
-        ///         Note: providers should typically not need to override this method.
-        ///     </para>
-        /// </summary>
-        /// <param name="mappingInfo"> The mapping info. </param>
-        /// <param name="property"> The property for which the type mapping is needed, if any. </param>
-        /// <returns> The type mapping with conversions applied, or <c>null</c> if none could be found. </returns>
-        protected virtual CoreTypeMapping FindMappingWithConversion(
-            [NotNull] TypeMappingInfo mappingInfo,
+        private CoreTypeMapping FindMappingWithConversion(
+            TypeMappingInfo mappingInfo,
             [CanBeNull] IProperty property)
         {
             Check.NotNull(mappingInfo, nameof(mappingInfo));
@@ -169,9 +125,9 @@ namespace Microsoft.EntityFrameworkCore.Storage
         /// </summary>
         /// <param name="property"> The property. </param>
         /// <returns> The type mapping, or <c>null</c> if none was found. </returns>
-        public virtual CoreTypeMapping FindMapping(IProperty property)
+        public override CoreTypeMapping FindMapping(IProperty property)
             => property.FindMapping()
-               ?? FindMappingWithConversion(new ConcreteTypeMappingInfo(property), property);
+               ?? FindMappingWithConversion(new TypeMappingInfo(property), property);
 
         /// <summary>
         ///     <para>
@@ -188,8 +144,8 @@ namespace Microsoft.EntityFrameworkCore.Storage
         /// </summary>
         /// <param name="type"> The CLR type. </param>
         /// <returns> The type mapping, or <c>null</c> if none was found. </returns>
-        public virtual CoreTypeMapping FindMapping(Type type)
-            => FindMappingWithConversion(new ConcreteTypeMappingInfo(type), null);
+        public override CoreTypeMapping FindMapping(Type type)
+            => FindMappingWithConversion(new TypeMappingInfo(type), null);
 
         /// <summary>
         ///     <para>
@@ -206,33 +162,7 @@ namespace Microsoft.EntityFrameworkCore.Storage
         /// </summary>
         /// <param name="member"> The field or property. </param>
         /// <returns> The type mapping, or <c>null</c> if none was found. </returns>
-        public virtual CoreTypeMapping FindMapping(MemberInfo member)
-            => FindMappingWithConversion(new ConcreteTypeMappingInfo(member), null);
-
-        private sealed class ConcreteTypeMappingInfo : TypeMappingInfo
-        {
-            public ConcreteTypeMappingInfo([NotNull] IProperty property)
-                : base(property)
-            {
-            }
-
-            public ConcreteTypeMappingInfo([NotNull] Type type)
-                : base(type)
-            {
-            }
-
-            public ConcreteTypeMappingInfo([NotNull] MemberInfo member)
-                : base(member)
-            {
-            }
-
-            private ConcreteTypeMappingInfo(ConcreteTypeMappingInfo source, ValueConverterInfo builtInConverter)
-                : base(source, builtInConverter)
-            {
-            }
-
-            public override TypeMappingInfo WithConverter(ValueConverterInfo converterInfo)
-                => new ConcreteTypeMappingInfo(this, converterInfo);
-        }
+        public override CoreTypeMapping FindMapping(MemberInfo member)
+            => FindMappingWithConversion(new TypeMappingInfo(member), null);
     }
 }
