1d70534e6893dde5d894bf28a2f834f6360d340d
Andrei Amialchenia <flash2048@gmail.com>
2018-04-04 19:01:43 +0300

    Corrects spelling of some comments

--- src/EFCore/Metadata/Builders/QueryTypeBuilder`.cs
+++ src/EFCore/Metadata/Builders/QueryTypeBuilder`.cs
@@ -199,7 +199,7 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
         ///     <para>
         ///         By default, the backing field, if one is found by convention or has been specified, is used when
         ///         new objects are constructed, typically when entities are queried from the database.
-        ///         Properties are used for all other accesses.  Calling this method witll change that behavior
+        ///         Properties are used for all other accesses.  Calling this method will change that behavior
         ///         for all properties of this query type as described in the <see cref="PropertyAccessMode" /> enum.
         ///     </para>
         ///     <para>
