e7f9784a338ae4b2c3f7bf942eb4e6dd73147d42
Smit Patel <smitpatel@users.noreply.github.com>
2018-05-16 16:22:42 -0700

    CosmosDb: Add support for database creation/deletion - Add provider specific ContextOption and API `UseCosmosSql` to configure provider - Add provider specific implementations of required services - Add CollectionName annotation over entityType for target collection - Add implementation for EnsureCreated/Deleted_Async. (not sync versions)
    Part of #8443

---
+++ src/EFCore.Cosmos.Sql/Metadata/CosmosSqlAnnotationNames.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.Metadata
+{
+    public static class CosmosSqlAnnotationNames
+    {
+        public const string Prefix = "Cosmos.Sql:";
+        public const string CollectionName = Prefix + "CollectionName";
+    }
+}
