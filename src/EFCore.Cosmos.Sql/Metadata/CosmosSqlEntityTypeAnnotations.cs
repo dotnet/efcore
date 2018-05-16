36303ce6cacef0c1dd4498a06d417ca7397f8d68
Smit Patel <smitpatel@users.noreply.github.com>
2018-05-16 16:22:42 -0700

    CosmosDb: Add support for database creation/deletion - Add provider specific ContextOption and API `UseCosmosSql` to configure provider - Add provider specific implementations of required services - Add CollectionName annotation over entityType for target collection - Add implementation for EnsureCreated/Deleted_Async. (not sync versions)
    Part of #8443

---
+++ src/EFCore.Cosmos.Sql/Metadata/CosmosSqlEntityTypeAnnotations.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Utilities;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.Metadata
+{
+    public class CosmosSqlEntityTypeAnnotations : ICosmosSqlEntityTypeAnnotations
+    {
+        public CosmosSqlEntityTypeAnnotations(IEntityType entityType)
+            : this(new CosmosSqlAnnotations(entityType))
+        {
+        }
+
+        protected CosmosSqlEntityTypeAnnotations(CosmosSqlAnnotations annotations) => Annotations = annotations;
+
+        protected virtual CosmosSqlAnnotations Annotations { get; }
+
+        protected virtual IEntityType EntityType => (IEntityType)Annotations.Metadata;
+
+        protected virtual CosmosSqlEntityTypeAnnotations GetAnnotations([NotNull] IEntityType entityType)
+            => new CosmosSqlEntityTypeAnnotations(entityType);
+
+        public virtual string CollectionName
+        {
+            get => EntityType.BaseType != null
+                ? GetAnnotations(EntityType.RootType()).CollectionName
+                : ((string)Annotations.Metadata[CosmosSqlAnnotationNames.CollectionName])
+                    ?? GetDefaultCollectionName();
+
+            [param: CanBeNull]
+            set => SetCollectionName(value);
+        }
+
+        private static string GetDefaultCollectionName() => "Unicorn";
+
+        protected virtual bool SetCollectionName([CanBeNull] string value)
+        {
+            return Annotations.SetAnnotation(
+                CosmosSqlAnnotationNames.CollectionName,
+                Check.NullButNotEmpty(value, nameof(value)));
+        }
+    }
+}
