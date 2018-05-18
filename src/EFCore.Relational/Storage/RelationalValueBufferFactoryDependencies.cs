panyoujin/EntityFrameworkCore

Query: Relational: Adds DbContextOptionsBuilder.EnableRichDataErrorHandling and IsDbNull optimization in TypedRelationalValueBufferFactoryFactory.
- EnableRichDataErrorHandling (default false). Controls whether we generate try/catch error handling around value read expressions. Also removes rich handling from EntityMaterializerSource; we now only need it in TypedRelationalValueBufferFactoryFactory.
- No longer always generate IsDbNull calls when reading values.

src/EFCore.Relational/Storage/RelationalValueBufferFactoryDependencies.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Utilities;
 
@@ -38,12 +39,16 @@ namespace Microsoft.EntityFrameworkCore.Storage
         ///     </para>
         /// </summary>
         /// <param name="typeMappingSource"> The type mapping source. </param>
+        /// <param name="coreOptions"> The core options. </param>
         public RelationalValueBufferFactoryDependencies(
-            [NotNull] IRelationalTypeMappingSource typeMappingSource)
+            [NotNull] IRelationalTypeMappingSource typeMappingSource,
+            [NotNull] ICoreOptions coreOptions)
         {
             Check.NotNull(typeMappingSource, nameof(typeMappingSource));
+            Check.NotNull(coreOptions, nameof(coreOptions));
 
             TypeMappingSource = typeMappingSource;
+            CoreOptions = coreOptions;
         }
 
         /// <summary>
@@ -52,11 +57,24 @@ namespace Microsoft.EntityFrameworkCore.Storage
         public IRelationalTypeMappingSource TypeMappingSource { get; }
 
         /// <summary>
+        ///     Gets core options.
+        /// </summary>
+        public ICoreOptions CoreOptions { get; }
+
+        /// <summary>
         ///     Clones this dependency parameter object with one service replaced.
         /// </summary>
         /// <param name="typeMappingSource"> A replacement for the current dependency of this type. </param>
         /// <returns> A new parameter object with the given service replaced. </returns>
         public RelationalValueBufferFactoryDependencies With([NotNull] IRelationalTypeMappingSource typeMappingSource)
-            => new RelationalValueBufferFactoryDependencies(typeMappingSource);
+            => new RelationalValueBufferFactoryDependencies(typeMappingSource, CoreOptions);
+
+        /// <summary>
+        ///     Clones this dependency parameter object with one service replaced.
+        /// </summary>
+        /// <param name="coreOptions"> A replacement for the current dependency of this type. </param>
+        /// <returns> A new parameter object with the given service replaced. </returns>
+        public RelationalValueBufferFactoryDependencies With([NotNull] ICoreOptions coreOptions)
+            => new RelationalValueBufferFactoryDependencies(TypeMappingSource, coreOptions);
     }
 }

test/EFCore.SqlServer.Tests/Update/SqlServerModificationCommandBatchFactoryTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Update.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -36,7 +37,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreOptions())),
                 optionsBuilder.Options);
 
             var batch = factory.Create();
@@ -69,7 +70,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreOptions())),
                 optionsBuilder.Options);
 
             var batch = factory.Create();

test/EFCore.SqlServer.Tests/Update/SqlServerModificationCommandBatchTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Update.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -33,7 +34,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreOptions())),
                 1);
 
             Assert.True(

dotnet/efcore

src/EFCore.Relational/Storage/RelationalValueBufferFactoryDependencies.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Utilities;
 
@@ -38,12 +39,16 @@ namespace Microsoft.EntityFrameworkCore.Storage
         ///     </para>
         /// </summary>
         /// <param name="typeMappingSource"> The type mapping source. </param>
+        /// <param name="coreOptions"> The core options. </param>
         public RelationalValueBufferFactoryDependencies(
-            [NotNull] IRelationalTypeMappingSource typeMappingSource)
+            [NotNull] IRelationalTypeMappingSource typeMappingSource,
+            [NotNull] ICoreSingletonOptions coreOptions)
         {
             Check.NotNull(typeMappingSource, nameof(typeMappingSource));
+            Check.NotNull(coreOptions, nameof(coreOptions));
 
             TypeMappingSource = typeMappingSource;
+            CoreOptions = coreOptions;
         }
 
         /// <summary>
@@ -52,11 +57,24 @@ namespace Microsoft.EntityFrameworkCore.Storage
         public IRelationalTypeMappingSource TypeMappingSource { get; }
 
         /// <summary>
+        ///     Gets core options.
+        /// </summary>
+        public ICoreSingletonOptions CoreOptions { get; }
+
+        /// <summary>
         ///     Clones this dependency parameter object with one service replaced.
         /// </summary>
         /// <param name="typeMappingSource"> A replacement for the current dependency of this type. </param>
         /// <returns> A new parameter object with the given service replaced. </returns>
         public RelationalValueBufferFactoryDependencies With([NotNull] IRelationalTypeMappingSource typeMappingSource)
-            => new RelationalValueBufferFactoryDependencies(typeMappingSource);
+            => new RelationalValueBufferFactoryDependencies(typeMappingSource, CoreOptions);
+
+        /// <summary>
+        ///     Clones this dependency parameter object with one service replaced.
+        /// </summary>
+        /// <param name="coreOptions"> A replacement for the current dependency of this type. </param>
+        /// <returns> A new parameter object with the given service replaced. </returns>
+        public RelationalValueBufferFactoryDependencies With([NotNull] ICoreSingletonOptions coreOptions)
+            => new RelationalValueBufferFactoryDependencies(TypeMappingSource, coreOptions);
     }
 }

test/EFCore.SqlServer.Tests/Update/SqlServerModificationCommandBatchFactoryTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Update.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -36,7 +37,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreSingletonOptions())),
                 optionsBuilder.Options);
 
             var batch = factory.Create();
@@ -69,7 +70,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreSingletonOptions())),
                 optionsBuilder.Options);
 
             var batch = factory.Create();

test/EFCore.SqlServer.Tests/Update/SqlServerModificationCommandBatchTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Update.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -33,7 +34,7 @@ namespace Microsoft.EntityFrameworkCore.Update
                         typeMapper)),
                 new TypedRelationalValueBufferFactoryFactory(
                     new RelationalValueBufferFactoryDependencies(
-                        typeMapper)),
+                        typeMapper, new CoreSingletonOptions())),
                 1);
 
             Assert.True(