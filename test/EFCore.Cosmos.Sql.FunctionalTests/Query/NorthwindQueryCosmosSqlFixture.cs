3e14539efac7bf159ecabee7885e071f1ccdd742
Smit Patel <smitpatel@users.noreply.github.com>
2018-05-21 15:36:20 -0700

    Cosmos.Sql: Add northwind fixture & test base

---
+++ test/EFCore.Cosmos.Sql.FunctionalTests/Query/NorthwindQueryCosmosSqlFixture.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore.Cosmos.Sql.TestUtilities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.Query
+{
+    public class NorthwindQueryCosmosSqlFixture<TModelCustomizer> : NorthwindQueryFixtureBase<TModelCustomizer>
+        where TModelCustomizer : IModelCustomizer, new()
+    {
+        protected override ITestStoreFactory TestStoreFactory => CosmosSqlNorthwindTestStoreFactory.Instance;
+    }
+}
---
+++ test/EFCore.Cosmos.Sql.FunctionalTests/TestUtilities/CosmosSqlNorthwindTestStoreFactory.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.TestUtilities
+{
+    public class CosmosSqlNorthwindTestStoreFactory : CosmosSqlTestStoreFactory
+    {
+        private const string Name = "Northwind";
+
+        public new static CosmosSqlNorthwindTestStoreFactory Instance { get; }
+            = new CosmosSqlNorthwindTestStoreFactory();
+
+        protected CosmosSqlNorthwindTestStoreFactory()
+        {
+        }
+
+        public override TestStore GetOrCreate(string storeName)
+            => CosmosSqlTestStore.GetOrCreate(Name, "Northwind.json");
+    }
+}
