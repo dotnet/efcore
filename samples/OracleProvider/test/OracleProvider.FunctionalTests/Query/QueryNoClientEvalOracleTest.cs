f3c83403b6190ab32a74fdf6dae49293fe1228ec
ralmsdeveloper <ralms@ralms.net>
2018-05-22 11:28:41 -0300

    Override Test

--- samples/OracleProvider/test/OracleProvider.FunctionalTests/Query/QueryNoClientEvalOracleTest.cs
+++ samples/OracleProvider/test/OracleProvider.FunctionalTests/Query/QueryNoClientEvalOracleTest.cs
@@ -1,6 +1,12 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Internal;
+using Xunit;
+
 namespace Microsoft.EntityFrameworkCore.Query
 {
     public class QueryNoClientEvalOracleTest : QueryNoClientEvalTestBase<QueryNoClientEvalOracleFixture>
@@ -8,6 +14,37 @@ namespace Microsoft.EntityFrameworkCore.Query
         public QueryNoClientEvalOracleTest(QueryNoClientEvalOracleFixture fixture)
             : base(fixture)
         {
+        }
+
+        public override void Throws_when_from_sql_composed()
+        {
+            using (var context = CreateContext())
+            {
+                Assert.Equal(
+                    CoreStrings.WarningAsErrorTemplate(
+                        RelationalEventId.QueryClientEvaluationWarning,
+                        RelationalStrings.LogClientEvalWarning.GenerateMessage("where [c].IsLondon"),
+                        "RelationalEventId.QueryClientEvaluationWarning"),
+                    Assert.Throws<InvalidOperationException>(
+                        () => context
+                            .Customers
+                            .FromSql("select * from \"Customers\"")
+                            .Where(c => c.IsLondon)
+                            .ToList()).Message);
+            }
+        }
+
+        public override void Doesnt_throw_when_from_sql_not_composed()
+        {
+            using (var context = CreateContext())
+            {
+                var customers = context
+                    .Customers
+                    .FromSql("select * from \"Customers\"")
+                    .ToList();
+
+                Assert.Equal(91, customers.Count);
+            }
         }
     }
 }
