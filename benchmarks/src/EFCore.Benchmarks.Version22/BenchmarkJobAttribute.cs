edc93a90bcb745e9d2b5ba9fbf599da3b2f1faf9
Smit Patel <smitpatel@users.noreply.github.com>
2018-06-08 15:02:58 -0700

    Benchmarks: Add Matrix for 2.1 release
    Resolves #11997
    - EF6 on net461
    - EF Core 2.0 on net461;netcoreapp2.0
    - EF Core 2.1 on net461;netcoreapp2.1
    - EF Core 2.2 on net461;netcoreapp2.0;netcoreapp2.1;netcoreapp2.2
    
    Resolves #12286
    Resolves #12285
    
    Fix the path of Sqlite database files on benchmarks
    
    Also updated package versions to latest patch

---
+++ benchmarks/src/EFCore.Benchmarks.Version22/BenchmarkJobAttribute.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
+#pragma warning disable CA1813 // Avoid unsealed attributes
+    public class BenchmarkJobAttribute : Attribute, IConfigSource
+#pragma warning restore CA1813 // Avoid unsealed attributes
+    {
+        public BenchmarkJobAttribute(bool singleRun = false)
+        {
+            var job = new Job().ConfigureJob(singleRun);
+
+            Config = ManualConfig.CreateEmpty()
+                .With(job.With(CsProjClassicNetToolchain.Net461))
+                .With(job.With(CsProjCoreToolchain.NetCoreApp20))
+                .With(job.With(CsProjCoreToolchain.NetCoreApp21))
+                .With(job.With(CsProjCoreToolchain.NetCoreApp22));
+        }
+
+        public IConfig Config { get; }
+    }
+}
