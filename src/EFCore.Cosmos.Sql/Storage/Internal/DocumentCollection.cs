8a6d0f90554902fff41a747c685cbe78ce99c364
Smit Patel <smitpatel@users.noreply.github.com>
2018-07-09 14:05:14 -0700

    CosmosDb: Implement SaveChanges functionality
    Part of #8443

---
+++ src/EFCore.Cosmos.Sql/Storage/Internal/DocumentCollection.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Update;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.Storage.Internal
+{
+    public class DocumentCollection<TKey> : IDocumentCollection
+    {
+        private DocumentClient _documentClient;
+        private string _databaseId;
+        private string _collectionName;
+        private readonly IEntityType _entityType;
+        private IPrincipalKeyValueFactory<TKey> _principalKeyValueFactory;
+
+        public DocumentCollection(
+            CosmosClient cosmosClient,
+            IEntityType entityType,
+            IPrincipalKeyValueFactory<TKey> principalKeyValueFactory)
+        {
+            _documentClient = cosmosClient.DocumentClient;
+            _databaseId = cosmosClient.DatabaseId;
+            _collectionName = entityType.CosmosSql().CollectionName;
+            _entityType = entityType;
+            _principalKeyValueFactory = principalKeyValueFactory;
+        }
+
+        private string GetId(IUpdateEntry entry)
+        {
+            var key = _principalKeyValueFactory.CreateFromCurrentValues((InternalEntityEntry)entry);
+
+            // TODO: Escape | Bar|string.Empty
+            return key is object[] array ? string.Join("|", array) : key.ToString();
+        }
+
+        private Document CreateDocument(string id, IUpdateEntry entry)
+        {
+            var document = new Document
+            {
+                Id = id
+            };
+
+            foreach (var property in _entityType.GetProperties())
+            {
+                document.SetPropertyValue(property.Name, entry.GetCurrentValue(property));
+            }
+
+            return document;
+        }
+
+        public Task SaveAsync(IUpdateEntry entry, CancellationToken cancellationToken = default)
+        {
+            var id = GetId(entry);
+
+            switch (entry.EntityState)
+            {
+                case EntityState.Added:
+                    return _documentClient.CreateDocumentAsync(
+                        UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionName),
+                        CreateDocument(id, entry));
+
+                case EntityState.Modified:
+                    var document = CreateDocument(id, entry);
+
+                    // Set Discriminator Property for updates
+                    document.SetPropertyValue(
+                        _entityType.CosmosSql().DiscriminatorProperty.Name,
+                        _entityType.CosmosSql().DiscriminatorValue);
+
+                    return _documentClient.ReplaceDocumentAsync(
+                        UriFactory.CreateDocumentUri(_databaseId, _collectionName, id),
+                        document);
+
+                case EntityState.Deleted:
+                    return _documentClient.DeleteDocumentAsync(
+                        UriFactory.CreateDocumentUri(_databaseId, _collectionName, id));
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ test/EFCore.Cosmos.Sql.FunctionalTests/CosmosSqlEndToEndTest.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Cosmos.Sql.TestUtilities;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Sql
+{
+    public class CosmosSqlEndToEndTest : IClassFixture<CosmosSqlEndToEndTest.CosmosSqlFixture>
+    {
+        private const string DatabaseName = "CosmosSqlEndToEndTest";
+
+        protected CosmosSqlFixture Fixture { get; }
+
+        public CosmosSqlEndToEndTest(CosmosSqlFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        [Fact]
+        // TODO: Remove ToList when Single/Count works
+        public async Task Can_add_update_delete_end_to_end()
+        {
+            using (var testDatabase = CosmosSqlTestStore.CreateInitialized(DatabaseName))
+            {
+                var options = Fixture.CreateOptions(testDatabase);
+
+                var customer = new Customer { Id = 42, Name = "Theon" };
+
+                using (var context = new CustomerContext(options))
+                {
+                    await context.Database.EnsureCreatedAsync();
+
+                    context.Add(customer);
+
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new CustomerContext(options))
+                {
+                    var customerFromStore = context.Set<Customer>().ToList().Single();
+
+                    Assert.Equal(42, customerFromStore.Id);
+                    Assert.Equal("Theon", customerFromStore.Name);
+                }
+
+                using (var context = new CustomerContext(options))
+                {
+                    customer.Name = "Theon Greyjoy";
+                    context.Update(customer);
+
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new CustomerContext(options))
+                {
+                    var customerFromStore = context.Set<Customer>().ToList().Single();
+
+                    Assert.Equal(42, customerFromStore.Id);
+                    Assert.Equal("Theon Greyjoy", customerFromStore.Name);
+                }
+
+                using (var context = new CustomerContext(options))
+                {
+                    context.Remove(customer);
+
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new CustomerContext(options))
+                {
+                    Assert.Equal(0, context.Set<Customer>().ToList().Count());
+                }
+            }
+        }
+
+        private class Customer
+        {
+            public Customer()
+            {
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CustomerContext : DbContext
+        {
+            public CustomerContext(DbContextOptions dbContextOptions)
+                : base(dbContextOptions)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Customer>();
+            }
+        }
+
+        public class CosmosSqlFixture : ServiceProviderFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory => CosmosSqlTestStoreFactory.Instance;
+        }
+    }
+}
