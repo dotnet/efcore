7cec620052228f3d2c13d6233abcf22a87c1706f
Smit Patel <smitpatel@users.noreply.github.com>
2018-08-14 14:29:17 -0700

    Query: Infer typemapping for ExpressionType.Coalesce
    Partial fix for #12797

--- test/EFCore.InMemory.FunctionalTests/Query/SimpleQueryInMemoryTest.cs
+++ test/EFCore.InMemory.FunctionalTests/Query/SimpleQueryInMemoryTest.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.TestUtilities.Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.EntityFrameworkCore.Query
@@ -15,6 +17,12 @@ namespace Microsoft.EntityFrameworkCore.Query
             : base(fixture)
         {
             //TestLoggerFactory.TestOutputHelper = testOutputHelper;
+        }
+
+        [ConditionalTheory(Skip = "See issue#13004")]
+        public override Task Select_GetValueOrDefault_on_DateTime_with_null_values(bool isAsync)
+        {
+            return base.Select_GetValueOrDefault_on_DateTime_with_null_values(isAsync);
         }
     }
 }
