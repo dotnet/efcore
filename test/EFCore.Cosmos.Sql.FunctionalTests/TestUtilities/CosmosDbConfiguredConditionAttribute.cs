c13764e384947c96cb775ce873c213345c752816
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2018-08-15 12:09:04 -0700

    Fix tests on net461

--- test/EFCore.Cosmos.Sql.FunctionalTests/TestUtilities/CosmosDbConfiguredConditionAttribute.cs
+++ test/EFCore.Cosmos.Sql.FunctionalTests/TestUtilities/CosmosDbConfiguredConditionAttribute.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Net;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.EntityFrameworkCore.TestUtilities.Xunit;
 
@@ -11,13 +10,23 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.TestUtilities
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
     public class CosmosDbConfiguredConditionAttribute : Attribute, ITestCondition
     {
-        private static readonly bool _connectionAvailable = Connect();
+        private static bool? _connectionAvailable;
 
         public string SkipReason => "Unable to connect to CosmosDb Emulator. Please install/start emulator service.";
 
-        public bool IsMet => _connectionAvailable;
+        public bool IsMet
+        {
+            get
+            {
+                if (_connectionAvailable == null)
+                {
+                    _connectionAvailable = TryConnect();
+                }
+                return _connectionAvailable.Value;
+            }
+        }
 
-        private static bool Connect()
+        private static bool TryConnect()
         {
             var documentClient = new DocumentClient(
                     new Uri(TestEnvironment.DefaultConnection),
@@ -29,7 +38,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Sql.TestUtilities
 
                 return true;
             }
-            catch (WebException)
+            catch (Exception)
             {
                 return false;
             }
