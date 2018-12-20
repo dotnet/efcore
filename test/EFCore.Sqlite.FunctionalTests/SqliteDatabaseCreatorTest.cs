7e4c9184676ed11499d7f8cfa529c5935ea292a8
Neil Bostrom <github@silvergames.net>
2018-12-20 20:38:59 +0000

    Resolved formatting on test file

--- test/EFCore.Sqlite.FunctionalTests/SqliteDatabaseCreatorTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/SqliteDatabaseCreatorTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -58,7 +56,7 @@ namespace Microsoft.EntityFrameworkCore
                 }
             }
         }
-        
+
         private DbContext CreateContext(string connectionString)
             => new DbContext(
                     new DbContextOptionsBuilder()
