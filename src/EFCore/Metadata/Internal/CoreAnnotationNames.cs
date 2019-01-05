046057cab17da601d207e256551e17d53ac3e749
Arthur Vickers <ajcvickers@hotmail.com>
2019-01-05 15:40:19 -0800

    Move `Relational:` from TypeMapping annotation name
    Fixes #9913

--- src/EFCore/Metadata/Internal/CoreAnnotationNames.cs
+++ src/EFCore/Metadata/Internal/CoreAnnotationNames.cs
@@ -62,7 +62,7 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public const string TypeMapping = "Relational:TypeMapping"; // "Relational:" prefix to prevent breaking changes from 2.0
+        public const string TypeMapping = "TypeMapping";
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
