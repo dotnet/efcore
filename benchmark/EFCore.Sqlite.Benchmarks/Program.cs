cd8c3e74a58dfd1d9a7efed8dbe23d51925b2468
Shay Rojansky <roji@roji.org>
2019-02-06 14:00:10 +0100

    Benchmark refresh
    * Our previous config used to run multiple TFMs as part of a single
      benchmark run. Removed this - a single run is now on a single TFM.
      It is still possible run older TFMs via the commandline.
    * Running different versions of EF Core is now possible via build
      configs, rather than different projects. This simplifies things.
    * Code common to the two EF Core projects - and to the EF6 project -
      is now in Shared.EFCore and Shared respectively.
    * Integrated the new benchmark projects into EFCore.sln and removed the
      older ones.
    * Other various cleanup and update tasks

---
+++ benchmark/EFCore.Sqlite.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks
+{
+    public static class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
---
+++ benchmark/Shared.EFCore/Extensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+
+namespace Microsoft.EntityFrameworkCore.Benchmarks
+{
+    public static class Extensions
+    {
+        public static IQueryable<TEntity> ApplyTracking<TEntity>(this IQueryable<TEntity> query, bool tracking)
+            where TEntity : class
+        {
+            return tracking
+                ? query
+                : query.AsNoTracking();
+        }
+    }
+}
