2bb3fcc1cc457678cc8544d89dd9d3da327ce76d
Arthur Vickers <ajcvickers@hotmail.com>
2019-02-16 12:09:40 -0800

    Associate in-memory integer value generation with property
    Issue #6872
    Also fixes #14646
    
    Previous a single integer generator was used for all in-memory databases. This change instead associates a generator with each column/property in each database. This allows:
    * For a new database, all generators start at 1
    * When a database is deleted (EnsureDeleted) all generators are reset
    * Each column/property with a generator has its own key space and starts at 1
    * If the database column already has a larger value than the current generator value, then the generator is bumped up to start at the next available value.
      * This allows seeding with explicit values, followed by normal use with generated values
      * One thing that may not work is generated values and explicit values in the same unit-of-work if those explicit values, once saved, would conflict with the generated values. If this is needed, I think it is better covered by the in-memory database explicitly supporting store-generated values, if we chose to do that.

--- src/EFCore.InMemory/ValueGeneration/Internal/InMemoryValueGeneratorSelector.cs
+++ src/EFCore.InMemory/ValueGeneration/Internal/InMemoryValueGeneratorSelector.cs
@@ -3,7 +3,10 @@
 
 using System;
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Utilities;
 using Microsoft.EntityFrameworkCore.ValueGeneration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,29 +27,88 @@ namespace Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal
     /// </summary>
     public class InMemoryValueGeneratorSelector : ValueGeneratorSelector
     {
-        private readonly InMemoryIntegerValueGeneratorFactory _inMemoryFactory = new InMemoryIntegerValueGeneratorFactory();
+        private readonly IInMemoryStore _inMemoryStore;
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public InMemoryValueGeneratorSelector([NotNull] ValueGeneratorSelectorDependencies dependencies)
+        public InMemoryValueGeneratorSelector(
+            [NotNull] ValueGeneratorSelectorDependencies dependencies,
+            [NotNull] IInMemoryDatabase inMemoryDatabase)
             : base(dependencies)
         {
+            _inMemoryStore = inMemoryDatabase.Store;
         }
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public override ValueGenerator Create(IProperty property, IEntityType entityType)
+        public override ValueGenerator Select(IProperty property, IEntityType entityType)
         {
             Check.NotNull(property, nameof(property));
             Check.NotNull(entityType, nameof(entityType));
 
-            return property.ClrType.IsInteger()
-                ? _inMemoryFactory.Create(property)
-                : base.Create(property, entityType);
+            return property.GetValueGeneratorFactory() == null
+                   && property.ClrType.IsInteger()
+                ? GetOrCreate(property)
+                : base.Select(property, entityType);
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        private ValueGenerator GetOrCreate(IProperty property)
+        {
+            Check.NotNull(property, nameof(property));
+
+            var type = property.ClrType.UnwrapNullableType().UnwrapEnumType();
+
+            if (type == typeof(long))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<long>(property);
+            }
+
+            if (type == typeof(int))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<int>(property);
+            }
+
+            if (type == typeof(short))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<short>(property);
+            }
+
+            if (type == typeof(byte))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<byte>(property);
+            }
+
+            if (type == typeof(ulong))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<ulong>(property);
+            }
+
+            if (type == typeof(uint))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<uint>(property);
+            }
+
+            if (type == typeof(ushort))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<ushort>(property);
+            }
+
+            if (type == typeof(sbyte))
+            {
+                return _inMemoryStore.GetIntegerValueGenerator<sbyte>(property);
+            }
+
+            throw new ArgumentException(
+                CoreStrings.InvalidValueGeneratorFactoryProperty(
+                    "InMemoryIntegerValueGeneratorFactory", property.Name, property.DeclaringEntityType.DisplayName()));
         }
     }
 }
