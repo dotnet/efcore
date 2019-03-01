d293f668b2369de66c2ef3afac3e9a830c538cda
Jiri Cincura <jiri@cincura.net>
2019-03-01 08:26:44 +0100

    Explicit culture (#14847)

--- test/EFCore.Relational.Specification.Tests/TestUtilities/RelationalTestStoreFactory.cs
+++ test/EFCore.Relational.Specification.Tests/TestUtilities/RelationalTestStoreFactory.cs
@@ -2,12 +2,20 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Globalization;
+using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.EntityFrameworkCore.TestUtilities
 {
     public abstract class RelationalTestStoreFactory : ITestStoreFactory
     {
+        static RelationalTestStoreFactory()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        }
+
         public abstract TestStore Create(string storeName);
         public abstract TestStore GetOrCreate(string storeName);
         public abstract IServiceCollection AddProviderServices(IServiceCollection serviceCollection);
