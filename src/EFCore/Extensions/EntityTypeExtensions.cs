MareevOleg/EntityFrameworkCore

Create a public abstraction for managing seed data
Part of #15096 and #15126

To avoid using the state manager directly.

src/EFCore/Extensions/EntityTypeExtensions.cs
@@ -437,6 +437,15 @@ namespace Microsoft.EntityFrameworkCore
         }
 
         /// <summary>
+        ///    Gets the data stored in the model for the given entity type.
+        /// </summary>
+        /// <param name="entityType"> The entity type. </param>
+        /// <param name="providerValues"> If true, then provider values are used. </param>
+        /// <returns> The data. </returns>
+        public static IEnumerable<IDictionary<string, object>> GetData([NotNull] this IEntityType entityType, bool providerValues = false)
+            => entityType.AsEntityType().GetData(providerValues);
+
+        /// <summary>
         ///    Gets the LINQ expression filter automatically applied to queries for this entity type.
         /// </summary>
         /// <param name="entityType"> The entity type to get the query filter for. </param>

test/EFCore.Relational.Tests/Migrations/Internal/MigrationsModelDifferTestBase.cs
@@ -4,13 +4,13 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Xunit;
 
@@ -121,7 +121,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Internal
                 new MigrationsAnnotationProvider(
                     new MigrationsAnnotationProviderDependencies()),
                 ctx.GetService<IChangeDetector>(),
-                ctx.GetService<StateManagerDependencies>(),
+                ctx.GetService<IModelDataTrackerFactory>(),
                 ctx.GetService<CommandBatchPreparerDependencies>());
         }
     }

test/EFCore.SqlServer.Tests/Migrations/SqlServerModelDifferTest.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -14,6 +13,7 @@ using Microsoft.EntityFrameworkCore.SqlServer.Migrations.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Xunit;
 
@@ -832,7 +832,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                 new SqlServerMigrationsAnnotationProvider(
                     new MigrationsAnnotationProviderDependencies()),
                 ctx.GetService<IChangeDetector>(),
-                ctx.GetService<StateManagerDependencies>(),
+                ctx.GetService<IModelDataTrackerFactory>(),
                 ctx.GetService<CommandBatchPreparerDependencies>());
         }
 

dotnet/efcore

src/EFCore/Extensions/EntityTypeExtensions.cs
@@ -460,6 +460,15 @@ namespace Microsoft.EntityFrameworkCore
         }
 
         /// <summary>
+        ///    Gets the data stored in the model for the given entity type.
+        /// </summary>
+        /// <param name="entityType"> The entity type. </param>
+        /// <param name="providerValues"> If true, then provider values are used. </param>
+        /// <returns> The data. </returns>
+        public static IEnumerable<IDictionary<string, object>> GetSeedData([NotNull] this IEntityType entityType, bool providerValues = false)
+            => entityType.AsEntityType().GetSeedData(providerValues);
+
+        /// <summary>
         ///    Gets the LINQ expression filter automatically applied to queries for this entity type.
         /// </summary>
         /// <param name="entityType"> The entity type to get the query filter for. </param>

test/EFCore.Relational.Tests/Migrations/Internal/MigrationsModelDifferTestBase.cs
@@ -4,13 +4,13 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Xunit;
 
@@ -121,7 +121,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Internal
                 new MigrationsAnnotationProvider(
                     new MigrationsAnnotationProviderDependencies()),
                 ctx.GetService<IChangeDetector>(),
-                ctx.GetService<StateManagerDependencies>(),
+                ctx.GetService<IUpdateAdapterFactory>(),
                 ctx.GetService<CommandBatchPreparerDependencies>());
         }
     }

test/EFCore.SqlServer.Tests/Migrations/SqlServerModelDifferTest.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -14,6 +13,7 @@ using Microsoft.EntityFrameworkCore.SqlServer.Migrations.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Xunit;
 
@@ -832,7 +832,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                 new SqlServerMigrationsAnnotationProvider(
                     new MigrationsAnnotationProviderDependencies()),
                 ctx.GetService<IChangeDetector>(),
-                ctx.GetService<StateManagerDependencies>(),
+                ctx.GetService<IUpdateAdapterFactory>(),
                 ctx.GetService<CommandBatchPreparerDependencies>());
         }
 