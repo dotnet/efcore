7c662d5cabcbc5895413bd12b9f8d22d7b45e4f1
y-code <yasu.gemini@gmail.com>
2019-04-28 19:27:58 +1200

    fix condition in CSharpDbContextGenerator.GenerateSequence
    Correct the condition to append schema parameter to sequence creation code when generating DB context's OnModelCreating method. Even when sequence's schema is provide, schema parameter was not output to generated code. This change in the condition makes the code generation put sequence's schema to sequence creation code when it is provided.

--- src/EFCore.Design/Scaffolding/Internal/CSharpDbContextGenerator.cs
+++ src/EFCore.Design/Scaffolding/Internal/CSharpDbContextGenerator.cs
@@ -855,7 +855,7 @@ namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
 
             var parameters = _code.Literal(sequence.Name);
 
-            if (string.IsNullOrEmpty(sequence.Schema)
+            if (!string.IsNullOrEmpty(sequence.Schema)
                 && sequence.Model.Relational().DefaultSchema != sequence.Schema)
             {
                 parameters += $", {_code.Literal(sequence.Schema)}";
