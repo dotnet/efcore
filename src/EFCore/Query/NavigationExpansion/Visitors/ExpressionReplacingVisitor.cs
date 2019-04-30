rferolino/EntityFrameworkCore

Navigation expansion rewrite - first draft
Known limitations:
- GroupBy: not supported at all
- Include tracking/fixup: include method (one that suppose to do fixup and tracking) is just a stub currently - only does fixup one way and doesn't perform tracking at all,
- Various warnings and negative cases: we used to warn/throw particular exceptions for negative cases, those are now different and the messages themselves are not hardcoded and not polished
- include/project collection - correlated collection feature is disabled, so projecting collections will issue N+1 queries. Same goes for include collection.
- async + collections - no special handling of async, projecting/including collection may cause a deadlock

src/EFCore/Query/NavigationExpansion/Visitors/ExpressionReplacingVisitor.cs
@@ -1,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Microsoft.EntityFrameworkCore.Query.NavigationExpansion.Visitors
+{
+    public class ExpressionReplacingVisitor : NavigationExpansionVisitorBase
+    {
+        private Expression _searchedFor;
+        private Expression _replaceWith;
+
+        public ExpressionReplacingVisitor(Expression searchedFor, Expression replaceWith)
+        {
+            _searchedFor = searchedFor;
+            _replaceWith = replaceWith;
+        }
+
+        protected override Expression VisitLambda<T>(Expression<T> lambdaExpression)
+        {
+            var newParameters = new List<ParameterExpression>();
+            var parameterChanged = false;
+
+            foreach (var parameter in lambdaExpression.Parameters)
+            {
+                var newParameter = (ParameterExpression)Visit(parameter);
+                newParameters.Add(newParameter);
+                if (newParameter != parameter)
+                {
+                    parameterChanged = true;
+                }
+            }
+
+            var newBody = Visit(lambdaExpression.Body);
+
+            return parameterChanged || newBody != lambdaExpression.Body
+                ? Expression.Lambda(newBody, newParameters)
+                : lambdaExpression;
+        }
+
+        public override Expression Visit(Expression expression)
+            => expression == _searchedFor
+            ? _replaceWith
+            : base.Visit(expression);
+    }
+}

dotnet/efcore

src/EFCore/Query/NavigationExpansion/Visitors/ExpressionReplacingVisitor.cs
@@ -1,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Microsoft.EntityFrameworkCore.Query.NavigationExpansion.Visitors
+{
+    public class ExpressionReplacingVisitor : ExpressionVisitor
+    {
+        private Expression _searchedFor;
+        private Expression _replaceWith;
+
+        public ExpressionReplacingVisitor(Expression searchedFor, Expression replaceWith)
+        {
+            _searchedFor = searchedFor;
+            _replaceWith = replaceWith;
+        }
+
+        protected override Expression VisitLambda<T>(Expression<T> lambdaExpression)
+        {
+            var newParameters = new List<ParameterExpression>();
+            var parameterChanged = false;
+
+            foreach (var parameter in lambdaExpression.Parameters)
+            {
+                var newParameter = (ParameterExpression)Visit(parameter);
+                newParameters.Add(newParameter);
+                if (newParameter != parameter)
+                {
+                    parameterChanged = true;
+                }
+            }
+
+            var newBody = Visit(lambdaExpression.Body);
+
+            return parameterChanged || newBody != lambdaExpression.Body
+                ? Expression.Lambda(newBody, newParameters)
+                : lambdaExpression;
+        }
+
+        public override Expression Visit(Expression expression)
+            => expression == _searchedFor
+            ? _replaceWith
+            : base.Visit(expression);
+    }
+}