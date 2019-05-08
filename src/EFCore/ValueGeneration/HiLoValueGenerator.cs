dc723424e0c4e0610c9a324697f432ee8f4668a8
Arthur Vickers <ajcvickers@hotmail.com>
2019-05-08 15:18:21 -0700

    Update docs for now-public logger extensions
    These were made public but the docs had not been updated.

--- src/EFCore/ValueGeneration/HiLoValueGenerator.cs
+++ src/EFCore/ValueGeneration/HiLoValueGenerator.cs
@@ -60,11 +60,10 @@ namespace Microsoft.EntityFrameworkCore.ValueGeneration
         protected abstract long GetNewLowValue();
 
         /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        ///     Gets the low value for the next block of values to be used.
         /// </summary>
+        /// <param name="cancellationToken"> The cancellation token. </param>
+        /// <returns> The low value for the next block of values to be used. </returns>
         protected virtual Task<long> GetNewLowValueAsync(CancellationToken cancellationToken = default)
             => Task.FromResult(GetNewLowValue());
     }
