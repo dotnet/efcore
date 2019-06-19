412fe437acbe6ce9d4a50c70dfeb8ed0ee072d02
Smit Patel <smitpatel@users.noreply.github.com>
2019-06-19 07:46:23 -0700

    Update SimpleQueryTestBase.cs

--- test/EFCore.Specification.Tests/Query/SimpleQueryTestBase.cs
+++ test/EFCore.Specification.Tests/Query/SimpleQueryTestBase.cs
@@ -1461,7 +1461,7 @@ namespace Microsoft.EntityFrameworkCore.Query
                 entryCount: 19);
         }
 
-        [ConditionalTheory(Skip = "Issue#16157")]
+        [ConditionalTheory]
         [MemberData(nameof(IsAsyncData))]
         public virtual Task Where_subquery_anon(bool isAsync)
         {
@@ -1487,7 +1487,7 @@ namespace Microsoft.EntityFrameworkCore.Query
                 entryCount: 2);
         }
 
-        [ConditionalTheory]
+        [ConditionalTheory(Skip = "Issue#16157")]
         [MemberData(nameof(IsAsyncData))]
         public virtual Task Where_subquery_anon_nested(bool isAsync)
         {
