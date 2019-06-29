6045997902cca54baf08d9d6c113c0bdc75fbe54
Nick Schonning <nschonni@gmail.com>
2019-06-29 01:17:14 -0400

    typo: incomming -> incoming

--- test/EFCore.Tests/Utilities/MultigraphTest.cs
+++ test/EFCore.Tests/Utilities/MultigraphTest.cs
@@ -266,7 +266,7 @@ namespace Microsoft.EntityFrameworkCore.Utilities
         }
 
         [ConditionalFact]
-        public void AddEdge_updates_incomming_and_outgoing_neighbors()
+        public void AddEdge_updates_incoming_and_outgoing_neighbors()
         {
             var vertexOne = new Vertex
             {
