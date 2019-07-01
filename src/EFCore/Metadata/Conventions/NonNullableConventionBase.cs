e734e5b9658c5cb098fd663a987c1fac608b63b2
Shay Rojansky <roji@roji.org>
2019-07-01 21:33:26 +0200

    Update nullability convention to new nullability metadata
    See https://github.com/dotnet/roslyn/blob/master/docs/features/nullable-metadata.md.
    
    Makes #16370 pass.

--- src/EFCore/Metadata/Conventions/NonNullableConventionBase.cs
+++ src/EFCore/Metadata/Conventions/NonNullableConventionBase.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
@@ -15,9 +16,18 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions
     /// </summary>
     public abstract class NonNullableConventionBase
     {
+        // For the interpretation of nullability metadata, see
+        // https://github.com/dotnet/roslyn/blob/master/docs/features/nullable-metadata.md
+
         private const string NullableAttributeFullName = "System.Runtime.CompilerServices.NullableAttribute";
+        private const string NullableContextAttributeFullName = "System.Runtime.CompilerServices.NullableContextAttribute";
         private Type _nullableAttrType;
+        private Type _nullableContextAttrType;
         private FieldInfo _nullableFlagsFieldInfo;
+        private FieldInfo _nullableContextFlagFieldInfo;
+
+        private readonly Dictionary<Type, byte?> _typeNullabilityCache;
+        private readonly Dictionary<Module, byte?> _moduleNullabilityCache;
 
         /// <summary>
         ///     Creates a new instance of <see cref="NonNullableConventionBase" />.
@@ -26,12 +36,35 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions
         protected NonNullableConventionBase([NotNull] ProviderConventionSetBuilderDependencies dependencies)
         {
             Dependencies = dependencies;
+
+            _typeNullabilityCache = new Dictionary<Type, byte?>();
+            _moduleNullabilityCache = new Dictionary<Module, byte?>();
         }
 
         /// <summary>
         ///     Parameter object containing service dependencies.
         /// </summary>
         protected virtual ProviderConventionSetBuilderDependencies Dependencies { get; }
+
+        private byte? GetNullabilityContextFlag(Attribute[] attributes)
+        {
+            if (attributes.FirstOrDefault(a => a.GetType().FullName == NullableContextAttributeFullName) is object attribute)
+            {
+                var attributeType = attribute.GetType();
+                if (attributeType != _nullableContextAttrType)
+                {
+                    _nullableContextFlagFieldInfo = attributeType.GetField("Flag");
+                    _nullableContextAttrType = attributeType;
+                }
+
+                if (_nullableContextFlagFieldInfo?.GetValue(attribute) is byte flag)
+                {
+                    return flag;
+                }
+            }
+
+            return null;
+        }
 
         /// <summary>
         ///     Returns a value indicating whether the member type is a non-nullable reference type.
@@ -44,28 +77,57 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions
             // it produces. If the model is spread across more than one assembly, there will be multiple versions of this attribute,
             // so look for it by name, caching to avoid reflection on every check.
             // Note that this may change - if https://github.com/dotnet/corefx/issues/36222 is done we can remove all of this.
-            if (!(Attribute.GetCustomAttributes(memberInfo, true)
-                    .FirstOrDefault(a => a.GetType().FullName == NullableAttributeFullName)
-                is { } attribute))
+
+            if (Attribute.GetCustomAttributes(memberInfo, true)
+                    .FirstOrDefault(a => a.GetType().FullName == NullableAttributeFullName) is { } attribute)
             {
-                return false;
+                var attributeType = attribute.GetType();
+                if (attributeType != _nullableAttrType)
+                {
+                    _nullableFlagsFieldInfo = attributeType.GetField("NullableFlags");
+                    _nullableAttrType = attributeType;
+                }
+
+                if (_nullableFlagsFieldInfo?.GetValue(attribute) is byte[] flags
+                    && flags.FirstOrDefault() == 1)
+                {
+                    return true;
+                }
             }
 
-            var attributeType = attribute.GetType();
-            if (attributeType != _nullableAttrType)
+            // No attribute on the member, try to find a NullableContextAttribute on the declaring type
+            var type = memberInfo.DeclaringType;
+            if (type != null)
             {
-                _nullableFlagsFieldInfo = attributeType.GetField("NullableFlags");
-                _nullableAttrType = attributeType;
+                if (!_typeNullabilityCache.TryGetValue(type, out var typeContextFlag))
+                {
+                    typeContextFlag = _typeNullabilityCache[type] =
+                        GetNullabilityContextFlag(Attribute.GetCustomAttributes(type));
+                }
+                if (typeContextFlag.HasValue)
+                {
+                    return typeContextFlag.Value == 1;
+                }
             }
 
-            // For the interpretation of NullableFlags, see
-            // https://github.com/dotnet/roslyn/blob/master/docs/features/nullable-reference-types.md#annotations
-            if (_nullableFlagsFieldInfo?.GetValue(attribute) is byte[] flags
-                && flags.FirstOrDefault() == 1)
+            // Finally, try at the module level.
+            var module = memberInfo.Module;
+            if (!_moduleNullabilityCache.TryGetValue(module, out var moduleContextFlag))
             {
-                return true;
+                moduleContextFlag = _moduleNullabilityCache[module] =
+                    GetNullabilityContextFlag(Attribute.GetCustomAttributes(memberInfo.Module));
+
+                if (type != null)
+                {
+                    _typeNullabilityCache[type] = moduleContextFlag;
+                }
+            }
+            if (moduleContextFlag.HasValue)
+            {
+                return moduleContextFlag.Value == 1;
             }
 
+            // No NullableAttribute on the member or NullableContextAttribute on the type or module - we're oblivious.
             return false;
         }
     }
