75a366ac77cbb89dceaee695e28ca3527712c419
Shay Rojansky <roji@roji.org>
2019-07-30 12:42:06 +0200

    Entity equality support for non-extension Contains
    EE handled the extension version of {IQueryable,IEnumerable}.Contains,
    but not instance methods such as List.Contains.
    
    Fixes #15554

--- src/EFCore/Extensions/Internal/ExpressionExtensions.cs
+++ src/EFCore/Extensions/Internal/ExpressionExtensions.cs
@@ -8,9 +8,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.EntityFrameworkCore.Utilities;
 
