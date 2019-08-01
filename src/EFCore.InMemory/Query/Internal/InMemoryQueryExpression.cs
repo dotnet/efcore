garfbradaz/EntityFrameworkCore

Query: Navigation Expansion take 2
- Add TransparentIdentifier to uniquify them across the pipeline
- Convert all Enumerable based method which we can translate to Queryable
- Add QueryableMethodProvider to get methodInfos on Queryable robustly
- Split out subquery member pushdown out of navigation expansion
- Lastly expand navigations

src/EFCore.InMemory/Query/Internal/InMemoryQueryExpression.cs
@@ -380,24 +380,6 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             _projectionMapping = projectionMapping;
         }
 
-        public readonly struct TransparentIdentifier<TOuter, TInner>
-        {
-            [UsedImplicitly]
-#pragma warning disable IDE0051 // Remove unused private members
-            private TransparentIdentifier(TOuter outer, TInner inner)
-#pragma warning restore IDE0051 // Remove unused private members
-            {
-                Outer = outer;
-                Inner = inner;
-            }
-
-            [UsedImplicitly]
-            public readonly TOuter Outer;
-
-            [UsedImplicitly]
-            public readonly TInner Inner;
-        }
-
         public virtual void AddLeftJoin(
             InMemoryQueryExpression innerQueryExpression,
             LambdaExpression outerKeySelector,
@@ -405,8 +387,8 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             Type transparentIdentifierType)
         {
             // GroupJoin phase
-            var groupTransparentIdentifierType = typeof(TransparentIdentifier<,>)
-                .MakeGenericType(typeof(ValueBuffer), typeof(IEnumerable<ValueBuffer>));
+            var groupTransparentIdentifierType = TransparentIdentifierType.Create(
+                typeof(ValueBuffer), typeof(IEnumerable<ValueBuffer>));
             var outerParameter = Parameter(typeof(ValueBuffer), "outer");
             var innerParameter = Parameter(typeof(IEnumerable<ValueBuffer>), "inner");
             var outerMemberInfo = groupTransparentIdentifierType.GetTypeInfo().GetDeclaredField("Outer");

src/EFCore.InMemory/Query/Internal/InMemoryQueryableMethodTranslatingExpressionVisitor.cs
@@ -199,7 +199,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             outerKeySelector = TranslateLambdaExpression(outer, outerKeySelector);
             innerKeySelector = TranslateLambdaExpression(inner, innerKeySelector);
 
-            var transparentIdentifierType = CreateTransparentIdentifierType(
+            var transparentIdentifierType = TransparentIdentifierType.Create(
                 resultSelector.Parameters[0].Type,
                 resultSelector.Parameters[1].Type);
 
@@ -233,7 +233,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             outerKeySelector = TranslateLambdaExpression(outer, outerKeySelector);
             innerKeySelector = TranslateLambdaExpression(inner, innerKeySelector);
 
-            var transparentIdentifierType = CreateTransparentIdentifierType(
+            var transparentIdentifierType = TransparentIdentifierType.Create(
                 resultSelector.Parameters[0].Type,
                 resultSelector.Parameters[1].Type);
 
@@ -345,7 +345,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             {
                 if (Visit(collectionSelectorBody) is ShapedQueryExpression inner)
                 {
-                    var transparentIdentifierType = CreateTransparentIdentifierType(
+                    var transparentIdentifierType = TransparentIdentifierType.Create(
                         resultSelector.Parameters[0].Type,
                         resultSelector.Parameters[1].Type);
 

src/EFCore.Relational/Query/RelationalQueryableMethodTranslatingExpressionVisitor.cs
@@ -7,7 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -51,9 +53,6 @@ namespace Microsoft.EntityFrameworkCore.Query
             _projectionBindingExpressionVisitor = new RelationalProjectionBindingExpressionVisitor(this, sqlTranslator);
             _sqlExpressionFactory = sqlExpressionFactory;
         }
-
-        private static Type CreateTransparentIdentifierType(Type outerType, Type innerType)
-            => typeof(TransparentIdentifier<,>).MakeGenericType(outerType, innerType);
 
         protected override Expression VisitMethodCall(MethodCallExpression methodCallExpression)
         {
@@ -438,7 +437,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             var joinPredicate = CreateJoinPredicate(outer, outerKeySelector, inner, innerKeySelector);
             if (joinPredicate != null)
             {
-                var transparentIdentifierType = CreateTransparentIdentifierType(
+                var transparentIdentifierType = TransparentIdentifierType.Create(
                     resultSelector.Parameters[0].Type,
                     resultSelector.Parameters[1].Type);
 
@@ -461,7 +460,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             var joinPredicate = CreateJoinPredicate(outer, outerKeySelector, inner, innerKeySelector);
             if (joinPredicate != null)
             {
-                var transparentIdentifierType = CreateTransparentIdentifierType(
+                var transparentIdentifierType = TransparentIdentifierType.Create(
                     resultSelector.Parameters[0].Type,
                     resultSelector.Parameters[1].Type);
 
@@ -726,7 +725,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             {
                 if (Visit(collectionSelectorBody) is ShapedQueryExpression inner)
                 {
-                    var transparentIdentifierType = CreateTransparentIdentifierType(
+                    var transparentIdentifierType = TransparentIdentifierType.Create(
                         resultSelector.Parameters[0].Type,
                         resultSelector.Parameters[1].Type);
 
@@ -938,6 +937,28 @@ namespace Microsoft.EntityFrameworkCore.Query
                 return memberExpression.Update(innerExpression);
             }
 
+            protected override Expression VisitMethodCall(MethodCallExpression methodCallExpression)
+            {
+                if (methodCallExpression.TryGetEFPropertyArguments(out var source, out var navigationName))
+                {
+                    source = Visit(source);
+                    if (source is EntityShaperExpression
+                        || (source is UnaryExpression innerUnaryExpression
+                        && innerUnaryExpression.NodeType == ExpressionType.Convert
+                        && innerUnaryExpression.Operand is EntityShaperExpression))
+                    {
+                        var collectionNavigation = Expand(source, MemberIdentity.Create(navigationName));
+                        if (collectionNavigation != null)
+                        {
+                            return collectionNavigation;
+                        }
+                    }
+
+                    return methodCallExpression.Update(null, new[] { source, methodCallExpression.Arguments[1] });
+                }
+                return base.VisitMethodCall(methodCallExpression);
+            }
+
             protected override Expression VisitExtension(Expression extensionExpression)
                 => extensionExpression is EntityShaperExpression
                     ? extensionExpression
@@ -974,7 +995,8 @@ namespace Microsoft.EntityFrameworkCore.Query
                         ? entityType.FindNavigation(member.MemberInfo)
                         : entityType.FindNavigation(member.Name);
 
-                    if (navigation != null)
+                    if (navigation != null
+                        && navigation.ForeignKey.IsOwnership)
                     {
                         if (navigation.IsCollection())
                         {

dotnet/efcore

src/EFCore.InMemory/Query/Internal/InMemoryQueryExpression.cs
@@ -384,24 +384,6 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             _projectionMapping = projectionMapping;
         }
 
-        public readonly struct TransparentIdentifier<TOuter, TInner>
-        {
-            [UsedImplicitly]
-#pragma warning disable IDE0051 // Remove unused private members
-            private TransparentIdentifier(TOuter outer, TInner inner)
-#pragma warning restore IDE0051 // Remove unused private members
-            {
-                Outer = outer;
-                Inner = inner;
-            }
-
-            [UsedImplicitly]
-            public readonly TOuter Outer;
-
-            [UsedImplicitly]
-            public readonly TInner Inner;
-        }
-
         public virtual void AddLeftJoin(
             InMemoryQueryExpression innerQueryExpression,
             LambdaExpression outerKeySelector,
@@ -409,8 +391,8 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             Type transparentIdentifierType)
         {
             // GroupJoin phase
-            var groupTransparentIdentifierType = typeof(TransparentIdentifier<,>)
-                .MakeGenericType(typeof(ValueBuffer), typeof(IEnumerable<ValueBuffer>));
+            var groupTransparentIdentifierType = TransparentIdentifierFactory.Create(
+                typeof(ValueBuffer), typeof(IEnumerable<ValueBuffer>));
             var outerParameter = Parameter(typeof(ValueBuffer), "outer");
             var innerParameter = Parameter(typeof(IEnumerable<ValueBuffer>), "inner");
             var outerMemberInfo = groupTransparentIdentifierType.GetTypeInfo().GetDeclaredField("Outer");

src/EFCore.InMemory/Query/Internal/InMemoryQueryableMethodTranslatingExpressionVisitor.cs
@@ -203,7 +203,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             outerKeySelector = TranslateLambdaExpression(outer, outerKeySelector);
             innerKeySelector = TranslateLambdaExpression(inner, innerKeySelector);
 
-            var transparentIdentifierType = CreateTransparentIdentifierType(
+            var transparentIdentifierType = TransparentIdentifierFactory.Create(
                 resultSelector.Parameters[0].Type,
                 resultSelector.Parameters[1].Type);
 
@@ -237,7 +237,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             outerKeySelector = TranslateLambdaExpression(outer, outerKeySelector);
             innerKeySelector = TranslateLambdaExpression(inner, innerKeySelector);
 
-            var transparentIdentifierType = CreateTransparentIdentifierType(
+            var transparentIdentifierType = TransparentIdentifierFactory.Create(
                 resultSelector.Parameters[0].Type,
                 resultSelector.Parameters[1].Type);
 
@@ -349,7 +349,7 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Query.Internal
             {
                 if (Visit(collectionSelectorBody) is ShapedQueryExpression inner)
                 {
-                    var transparentIdentifierType = CreateTransparentIdentifierType(
+                    var transparentIdentifierType = TransparentIdentifierFactory.Create(
                         resultSelector.Parameters[0].Type,
                         resultSelector.Parameters[1].Type);
 

src/EFCore.Relational/Query/RelationalQueryableMethodTranslatingExpressionVisitor.cs
@@ -7,7 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -58,9 +60,6 @@ namespace Microsoft.EntityFrameworkCore.Query
             _projectionBindingExpressionVisitor = new RelationalProjectionBindingExpressionVisitor(this, sqlTranslator);
             _sqlExpressionFactory = sqlExpressionFactory;
         }
-
-        private static Type CreateTransparentIdentifierType(Type outerType, Type innerType)
-            => typeof(TransparentIdentifier<,>).MakeGenericType(outerType, innerType);
 
         protected override Expression VisitMethodCall(MethodCallExpression methodCallExpression)
         {
@@ -447,7 +446,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             var joinPredicate = CreateJoinPredicate(outer, outerKeySelector, inner, innerKeySelector);
             if (joinPredicate != null)
             {
-                var transparentIdentifierType = CreateTransparentIdentifierType(
+                var transparentIdentifierType = TransparentIdentifierFactory.Create(
                     resultSelector.Parameters[0].Type,
                     resultSelector.Parameters[1].Type);
 
@@ -470,7 +469,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             var joinPredicate = CreateJoinPredicate(outer, outerKeySelector, inner, innerKeySelector);
             if (joinPredicate != null)
             {
-                var transparentIdentifierType = CreateTransparentIdentifierType(
+                var transparentIdentifierType = TransparentIdentifierFactory.Create(
                     resultSelector.Parameters[0].Type,
                     resultSelector.Parameters[1].Type);
 
@@ -735,7 +734,7 @@ namespace Microsoft.EntityFrameworkCore.Query
             {
                 if (Visit(collectionSelectorBody) is ShapedQueryExpression inner)
                 {
-                    var transparentIdentifierType = CreateTransparentIdentifierType(
+                    var transparentIdentifierType = TransparentIdentifierFactory.Create(
                         resultSelector.Parameters[0].Type,
                         resultSelector.Parameters[1].Type);
 
@@ -947,6 +946,28 @@ namespace Microsoft.EntityFrameworkCore.Query
                 return memberExpression.Update(innerExpression);
             }
 
+            protected override Expression VisitMethodCall(MethodCallExpression methodCallExpression)
+            {
+                if (methodCallExpression.TryGetEFPropertyArguments(out var source, out var navigationName))
+                {
+                    source = Visit(source);
+                    if (source is EntityShaperExpression
+                        || (source is UnaryExpression innerUnaryExpression
+                        && innerUnaryExpression.NodeType == ExpressionType.Convert
+                        && innerUnaryExpression.Operand is EntityShaperExpression))
+                    {
+                        var collectionNavigation = Expand(source, MemberIdentity.Create(navigationName));
+                        if (collectionNavigation != null)
+                        {
+                            return collectionNavigation;
+                        }
+                    }
+
+                    return methodCallExpression.Update(null, new[] { source, methodCallExpression.Arguments[1] });
+                }
+                return base.VisitMethodCall(methodCallExpression);
+            }
+
             protected override Expression VisitExtension(Expression extensionExpression)
                 => extensionExpression is EntityShaperExpression
                     ? extensionExpression
@@ -983,7 +1004,8 @@ namespace Microsoft.EntityFrameworkCore.Query
                         ? entityType.FindNavigation(member.MemberInfo)
                         : entityType.FindNavigation(member.Name);
 
-                    if (navigation != null)
+                    if (navigation != null
+                        && navigation.ForeignKey.IsOwnership)
                     {
                         if (navigation.IsCollection())
                         {