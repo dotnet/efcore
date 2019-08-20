7dcc32f15d4f0448bf2601b695900147898fa129
Brice Lambson <brice@bricelam.net>
2019-08-20 09:50:23 -0700

    Use libsqlite3.dylib

--- src/Microsoft.Data.Sqlite.Core/Utilities/BundleInitializer.cs
+++ src/Microsoft.Data.Sqlite.Core/Utilities/BundleInitializer.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using SQLitePCL;
 
 namespace Microsoft.Data.Sqlite.Utilities
 {
@@ -9,18 +12,36 @@ namespace Microsoft.Data.Sqlite.Utilities
     {
         public static void Initialize()
         {
-            Assembly assembly;
-            try
+            SQLite3Provider_dynamic_cdecl.Setup("sqlite3", new MyGetFunctionPointer());
+            SQLitePCL.raw.SetProvider(new SQLite3Provider_dynamic_cdecl());
+
+            //Assembly assembly;
+            //try
+            //{
+            //    assembly = Assembly.Load(new AssemblyName("SQLitePCLRaw.batteries_v2"));
+            //}
+            //catch
+            //{
+            //    return;
+            //}
+
+            //assembly.GetType("SQLitePCL.Batteries_V2").GetTypeInfo().GetDeclaredMethod("Init")
+            //    .Invoke(null, null);
+        }
+
+        private class MyGetFunctionPointer : IGetFunctionPointer
+        {
+            private readonly IntPtr _dll;
+
+            public MyGetFunctionPointer()
             {
-                assembly = Assembly.Load(new AssemblyName("SQLitePCLRaw.batteries_v2"));
-            }
-            catch
-            {
-                return;
+                _dll = NativeLibrary.Load("sqlite3", typeof(SQLitePCL.raw).Assembly, null);
             }
 
-            assembly.GetType("SQLitePCL.Batteries_V2").GetTypeInfo().GetDeclaredMethod("Init")
-                .Invoke(null, null);
+            public IntPtr GetFunctionPointer(string name)
+                => NativeLibrary.TryGetExport(_dll, name, out var f)
+                    ? f
+                    : IntPtr.Zero;
         }
     }
 }
