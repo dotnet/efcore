9be06a02488700aa5b35dfc8c2a3907ba3605371
Brice Lambson <brice@bricelam.net>
2019-08-27 11:27:04 -0700

    RevEng: Fix bad logic around connection strings
    Fixes #17375

--- test/EFCore.Design.Tests/Scaffolding/Internal/ReverseEngineerScaffolderTest.cs
+++ test/EFCore.Design.Tests/Scaffolding/Internal/ReverseEngineerScaffolderTest.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Data.Common;
 using System.Globalization;
 using System.IO;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -181,5 +184,60 @@ namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
                 .AddSingleton<IScaffoldingModelFactory, FakeScaffoldingModelFactory>()
                 .BuildServiceProvider()
                 .GetRequiredService<IReverseEngineerScaffolder>();
+
+        [ConditionalFact]
+        public void ScaffoldModel_works_with_named_connection_string()
+        {
+            var resolver = new TestNamedConnectionStringResolver("Data Source=Test");
+            var databaseModelFactory = new TestDatabaseModelFactory();
+            var scaffolder = new ServiceCollection()
+                .AddEntityFrameworkDesignTimeServices()
+                .AddSingleton<INamedConnectionStringResolver>(resolver)
+                .AddSingleton<IDatabaseModelFactory>(databaseModelFactory)
+                .AddSingleton<IRelationalTypeMappingSource, TestRelationalTypeMappingSource>()
+                .AddSingleton<LoggingDefinitions, TestRelationalLoggingDefinitions>()
+                .AddSingleton<IProviderConfigurationCodeGenerator, TestProviderCodeGenerator>()
+                .AddSingleton<IAnnotationCodeGenerator, AnnotationCodeGenerator>()
+                .BuildServiceProvider()
+                .GetRequiredService<IReverseEngineerScaffolder>();
+
+            var result = scaffolder.ScaffoldModel(
+                "Name=DefaultConnection",
+                new DatabaseModelFactoryOptions(),
+                new ModelReverseEngineerOptions(),
+                new ModelCodeGenerationOptions());
+
+            Assert.Equal("Data Source=Test", databaseModelFactory.ConnectionString);
+
+            Assert.Contains("Name=DefaultConnection", result.ContextFile.Code);
+            Assert.DoesNotContain("Data Source=Test", result.ContextFile.Code);
+            Assert.DoesNotContain("#warning", result.ContextFile.Code);
+        }
+
+        private class TestNamedConnectionStringResolver : INamedConnectionStringResolver
+        {
+            private readonly string _resolvedConnectionString;
+
+            public TestNamedConnectionStringResolver(string resolvedConnectionString)
+                => _resolvedConnectionString = resolvedConnectionString;
+
+            public string ResolveConnectionString(string connectionString)
+                => _resolvedConnectionString;
+        }
+
+        private class TestDatabaseModelFactory : IDatabaseModelFactory
+        {
+            public string ConnectionString { get; set; }
+
+            public DatabaseModel Create(string connectionString, DatabaseModelFactoryOptions options)
+            {
+                ConnectionString = connectionString;
+
+                return new DatabaseModel();
+            }
+
+            public DatabaseModel Create(DbConnection connection, DatabaseModelFactoryOptions options)
+                => throw new System.NotImplementedException();
+        }
     }
 }
