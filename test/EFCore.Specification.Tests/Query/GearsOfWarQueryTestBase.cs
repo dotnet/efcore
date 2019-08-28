f2e118e01721be134096f9de73395d75e9bb7f57
Alexandre Murari Junior <murarialex@outlook.com>
2019-08-27 23:35:09 -0300

    Removed strange using

--- test/EFCore.Specification.Tests/Query/GearsOfWarQueryTestBase.cs
+++ test/EFCore.Specification.Tests/Query/GearsOfWarQueryTestBase.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Internal;
