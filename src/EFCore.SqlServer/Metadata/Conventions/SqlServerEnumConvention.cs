e863f1220a242eeddf611f1a5b6baf3192822208
Saravana Kumar <saravananbscitm@gmail.com>
2019-09-14 23:50:28 +0530

    Add support for Int Enum column and add test for Enum Conventions.

--- src/EFCore.SqlServer/Metadata/Conventions/SqlServerEnumConvention.cs
+++ src/EFCore.SqlServer/Metadata/Conventions/SqlServerEnumConvention.cs
@@ -1,8 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Text;
 using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
 
@@ -38,19 +39,36 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions
             {
                 foreach( var property in entityType.GetDeclaredProperties())
                 {
-                    if(property.GetIdentifyingMemberInfo()?.GetType().IsEnum ?? false
-                        && (property.FindTypeMapping()?.Converter ?? property.GetValueConverter())?.ProviderClrType == typeof(string))
+                    if(property?.PropertyInfo?.PropertyType.IsEnum ?? false)
                     {
-                        string sql = $"CHECK ({property.Name} IN(";
-                        var enumNames = property.PropertyInfo.GetType().GetEnumNames();
-                        foreach (var item in enumNames)
+
+                        StringBuilder sql = new StringBuilder($"CHECK ({property.Name} IN(");
+                        bool isStringColumn = (property.FindTypeMapping()?.Converter
+                                ?? property.GetValueConverter())?.ProviderClrType == typeof(string);
+                        if(isStringColumn)
                         {
-                            sql += $"'{item}', ";
+                            var enumNames = property.PropertyInfo.PropertyType.GetEnumNames();
+                            if(enumNames.Length <= 0)
+                                continue;
+                            foreach (var item in enumNames)
+                            {
+                                sql.Append($"'{item}', ");
+                            }
                         }
-                        sql = sql.Remove(sql.Length - 2);
-                        sql = sql + "))";
-                        string constraintName = $"CK_{entityType.Name}_{property.Name}_Enum_Constraint";
-                        entityType.AddCheckConstraint(constraintName, sql);
+                        else
+                        {
+                            var enumValues = Enum.GetValues(property.PropertyInfo.PropertyType);
+                            if(enumValues.Length <= 0)
+                                continue;
+                            foreach (int item in enumValues)
+                            {
+                                sql.Append($"{item}, ");
+                            }
+                        }
+                        sql.Remove(sql.Length - 2, 2);
+                        sql.Append("))");
+                        string constraintName = $"CK_{entityType.GetTableName()}_{property.GetColumnName()}_Enum_Constraint";
+                        entityType.AddCheckConstraint(constraintName, sql.ToString());
                     }
                 }
             }
--- test/EFCore.SqlServer.Tests/Metadata/Conventions/SqlServerEnumConventionTests.cs
+++ test/EFCore.SqlServer.Tests/Metadata/Conventions/SqlServerEnumConventionTests.cs
@@ -10,42 +10,107 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions
     public class SqlServerEnumConventionTests
     {
         [ConditionalFact]
-        public void GenerateConstraintWithAllEnumNames()
+        public void GenerateCheckConstraintWithAllEnumNames()
         {
-            var model = BuildModel();
+
+            var builder = SqlServerTestHelpers.Instance.CreateConventionBuilder();
+            builder.Entity<Order>()
+                        .Property(o => o.OrderStatus).HasConversion<string>();
+
+            var model = builder.FinalizeModel();
 
             var checkConstraint = model.FindEntityType(typeof(Order))
                                         .GetCheckConstraints()
                                         .FirstOrDefault<ICheckConstraint>(constraint =>
-                                            constraint.Name == "CK_Order_CustomerType_Enum_Constraint");
+                                            constraint.Name == "CK_Order_OrderStatus_Enum_Constraint");
 
-            Assert.Null(checkConstraint);
-            //Assert.Null(checkConstraint1);
-            // Assert.Equal(entityType, checkConstraint.EntityType);
-            // Assert.Equal("CK_Order_CustomerType_Enum_Constraint", checkConstraint.Name);
-            // Assert.Equal("CHECK (CustomerType IN('Standard', 'Premium'))", checkConstraint.Sql);
+            Assert.NotNull(checkConstraint);
+            Assert.Equal("CK_Order_OrderStatus_Enum_Constraint", checkConstraint.Name);
+            Assert.Equal("CHECK (OrderStatus IN('Active', 'Completed'))", checkConstraint.Sql);
 
         }
 
-        private static IModel BuildModel(bool generateValues = true)
+        [ConditionalFact]
+        public void GenerateCheckConstraintWithAllEnumValues()
         {
-            var builder = SqlServerTestHelpers.Instance.CreateConventionBuilder();
-            builder.Entity<Order>();
 
-            return builder.FinalizeModel();
+            var builder = SqlServerTestHelpers.Instance.CreateConventionBuilder();
+            builder.Entity<Customer>();
+
+            var model = builder.FinalizeModel();
+
+            var checkConstraint = model.FindEntityType(typeof(Customer))
+                                        .GetCheckConstraints()
+                                        .FirstOrDefault<ICheckConstraint>(constraint =>
+                                            constraint.Name == "CK_Customer_CustomerType_Enum_Constraint");
+
+            Assert.NotNull(checkConstraint);
+            Assert.Equal("CK_Customer_CustomerType_Enum_Constraint", checkConstraint.Name);
+            Assert.Equal("CHECK (CustomerType IN(0, 1))", checkConstraint.Sql);
+
+        }
+
+        [ConditionalFact]
+        public void ShouldNotGenerateCheckConstraintForEmptyEnum()
+        {
+
+            var builder = SqlServerTestHelpers.Instance.CreateConventionBuilder();
+            builder.Entity<Seller>()
+                        .Property(p => p.SellerStatusString)
+                        .HasConversion<string>();
+
+            var model = builder.FinalizeModel();
+
+            var checkConstraintString = model.FindEntityType(typeof(Seller))
+                                        .GetCheckConstraints()
+                                        .FirstOrDefault<ICheckConstraint>(constraint =>
+                                            constraint.Name == "CK_Seller_SellerStatusString_Enum_Constraint");
+
+            var checkConstraintInt = model.FindEntityType(typeof(Seller))
+                                        .GetCheckConstraints()
+                                        .FirstOrDefault<ICheckConstraint>(constraint =>
+                                            constraint.Name == "CK_Seller_checkConstraintInt_Enum_Constraint");
+
+            Assert.Null(checkConstraintString);
+            Assert.Null(checkConstraintInt);
+        }
+
+        private class Seller
+        {
+            public int Id{ get; set; }
+            public string SellerName { get; set; }
+            public SellerStatus SellerStatusString { get; set; }
+            public SellerStatus SellerStatusInt { get; set; }
+        }
+
+        private enum SellerStatus
+        {
         }
 
         private class Order
         {
             public int Id { get; set; }
             public int CustomerId { get; set; }
+            public OrderStatus OrderStatus{ get; set; }
+        }
+
+        private enum OrderStatus
+        {
+            Active,
+            Completed
+        }
+
+        private class Customer
+        {
+            public int Id { get; set; }
+            public int CustomerName { get; set; }
             public CustomerType CustomerType{ get; set; }
         }
 
         private enum CustomerType
         {
-            Standard,
-            Premium
+            Standard = 0,
+            Premium = 1
         }
     }
 }
