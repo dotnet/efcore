0936696349c9f721d0fc55529aa70c903f6b9b02
Shay Rojansky <roji@roji.org>
2019-11-12 11:27:19 -0800

    Add missing Ordinal to {Starts,Ends}With
    Fixes #18831

--- src/EFCore.Relational/Storage/RelationalSqlGenerationHelper.cs
+++ src/EFCore.Relational/Storage/RelationalSqlGenerationHelper.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Text;
 using JetBrains.Annotations;
@@ -57,7 +58,7 @@ namespace Microsoft.EntityFrameworkCore.Storage
         ///     A valid name based on the candidate name.
         /// </returns>
         public virtual string GenerateParameterName(string name)
-            => name.StartsWith("@")
+            => name.StartsWith("@", StringComparison.Ordinal)
                 ? name
                 : "@" + name;
 
