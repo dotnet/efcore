035dcdd301c0c79738f5aa6502c9def4cc741e01
PeterHuang <peter.huang@practiceevolve.com.au>
2019-11-14 00:52:30 +1100

    Fixed 12729 Flatten Case expression

---
+++ src/EFCore.Relational/Query/Internal/CaseWhenFlatteningExpressionVisitor.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace Microsoft.EntityFrameworkCore.Query.Internal
+{
+    public class CaseWhenFlatteningExpressionVisitor : ExpressionVisitor
+    {
+        private readonly ISqlExpressionFactory _sqlExpressionFactory;
+
+        public CaseWhenFlatteningExpressionVisitor(ISqlExpressionFactory sqlExpressionFactory)
+        {
+            _sqlExpressionFactory = sqlExpressionFactory;
+        }
+
+        protected override Expression VisitExtension(Expression node)
+        {
+            // Only applies to 'CASE WHEN condition...' not 'CASE operand WHEN...'
+            if (node is CaseExpression caseExpr && caseExpr.Operand == null)
+            {
+                if (caseExpr.ElseResult is CaseExpression nestedCaseExpr && nestedCaseExpr.Operand == null)
+                {
+                    return VisitExtension(_sqlExpressionFactory.Case(
+                        caseExpr.WhenClauses.Union(nestedCaseExpr.WhenClauses).ToList(),
+                        nestedCaseExpr.ElseResult));
+                }
+            }
+            return base.VisitExtension(node);
+        }
+
+    }
+}
--- test/EFCore.Specification.Tests/Query/NorthwindSelectQueryTestBase.cs
+++ test/EFCore.Specification.Tests/Query/NorthwindSelectQueryTestBase.cs
@@ -821,6 +821,28 @@ namespace Microsoft.EntityFrameworkCore.Query
 
         [ConditionalTheory]
         [MemberData(nameof(IsAsyncData))]
+        public virtual Task Select_over_10_nested_ternary_condition(bool isAsync)
+        {
+            return AssertQuery(
+                isAsync,
+                os => from c in os.Set<Customer>()
+                      select
+                      c.CustomerID == "1" ? "01" :
+                      c.CustomerID == "2" ? "02" :
+                      c.CustomerID == "3" ? "03" :
+                      c.CustomerID == "4" ? "04" :
+                      c.CustomerID == "5" ? "05" :
+                      c.CustomerID == "6" ? "06" :
+                      c.CustomerID == "7" ? "07" :
+                      c.CustomerID == "8" ? "08" :
+                      c.CustomerID == "9" ? "09" :
+                      c.CustomerID == "10" ? "10" :
+                      c.CustomerID == "11" ? "11" :
+                      null);
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
         public virtual Task Projection_in_a_subquery_should_be_liftable(bool async)
         {
             return AssertQuery(
