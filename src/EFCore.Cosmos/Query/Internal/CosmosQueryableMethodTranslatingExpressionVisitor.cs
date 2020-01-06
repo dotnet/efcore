tmvan/EntityFrameworkCore

Query: Remove public setters from customer expression properties
Resolves #19192

Changes
- Only server side query expression are in place updated using private setters, public methods
- Moved to Update method pattern in certain cases
- TableExpressionBase still has internal set which will be fixed in #17337
- ShapedQueryExpression.VisitChildren throws now. Since ShapedQuery contains query expression (server side) and shaper expression (client side), it is unlikely that a visitor needs to visit both components. Hence we force visitor to decide which component to visit. This improves perf as we wouldn't go into client side shaper when updating something in sql tree

src/EFCore.Cosmos/Query/Internal/CosmosQueryableMethodTranslatingExpressionVisitor.cs
@@ -184,14 +184,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             Check.NotNull(source, nameof(source));
             Check.NotNull(resultType, nameof(resultType));
 
-            if (source.ShaperExpression.Type == resultType)
-            {
-                return source;
-            }
-
-            source.ShaperExpression = Expression.Convert(source.ShaperExpression, resultType);
-
-            return source;
+            return source.ShaperExpression.Type != resultType
+                ? source.UpdateShaper(Expression.Convert(source.ShaperExpression, resultType))
+                : source;
         }
 
         /// <summary>
@@ -256,9 +251,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             selectExpression.ClearOrdering();
             selectExpression.ReplaceProjectionMapping(projectionMapping);
-            source.ShaperExpression = new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(int));
-
-            return source;
+            return source.UpdateShaper(new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(int)));
         }
 
         /// <summary>
@@ -342,12 +335,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             var selectExpression = (SelectExpression)source.QueryExpression;
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(1)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaper(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -449,12 +439,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             selectExpression.ReverseOrderings();
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(1)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaper(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -510,9 +497,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             selectExpression.ClearOrdering();
             selectExpression.ReplaceProjectionMapping(projectionMapping);
-            source.ShaperExpression = new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(long));
-
-            return source;
+            return source.UpdateShaper(new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(long)));
         }
 
         /// <summary>
@@ -649,10 +634,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             var newSelectorBody = ReplacingExpressionVisitor.Replace(selector.Parameters.Single(), source.ShaperExpression, selector.Body);
 
-            source.ShaperExpression = _projectionBindingExpressionVisitor
-                .Translate(selectExpression, newSelectorBody);
-
-            return source;
+            return source.UpdateShaper(_projectionBindingExpressionVisitor.Translate(selectExpression, newSelectorBody));
         }
 
         /// <summary>
@@ -709,12 +691,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             var selectExpression = (SelectExpression)source.QueryExpression;
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(2)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaper(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -940,9 +919,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
                 shaper = Expression.Convert(shaper, resultType);
             }
 
-            source.ShaperExpression = shaper;
-
-            return source;
+            return source.UpdateShaper(shaper);
         }
     }
 }

src/EFCore.Relational/Query/Internal/CollectionJoinApplyingExpressionVisitor.cs
@@ -39,14 +39,9 @@ namespace Microsoft.EntityFrameworkCore.Query.Internal
                     collectionShaperExpression.ElementType);
             }
 
-            if (extensionExpression is ShapedQueryExpression shapedQueryExpression)
-            {
-                shapedQueryExpression.ShaperExpression = Visit(shapedQueryExpression.ShaperExpression);
-
-                return shapedQueryExpression;
-            }
-
-            return base.VisitExtension(extensionExpression);
+            return extensionExpression is ShapedQueryExpression shapedQueryExpression
+                ? shapedQueryExpression.UpdateShaper(Visit(shapedQueryExpression.ShaperExpression))
+                : base.VisitExtension(extensionExpression);
         }
     }
 }

src/EFCore/Query/ShapedQueryExpression.cs
@@ -11,31 +11,26 @@ namespace Microsoft.EntityFrameworkCore.Query
 {
     public class ShapedQueryExpression : Expression, IPrintableExpression
     {
-        private Expression _queryExpression;
-        private Expression _shaperExpression;
-
         public ShapedQueryExpression([NotNull] Expression queryExpression, [NotNull] Expression shaperExpression)
+            : this(Check.NotNull(queryExpression, nameof(queryExpression)),
+                  Check.NotNull(shaperExpression, nameof(shaperExpression)),
+                  ResultCardinality.Enumerable)
         {
-            Check.NotNull(queryExpression, nameof(queryExpression));
-            Check.NotNull(shaperExpression, nameof(shaperExpression));
+        }
 
+        private ShapedQueryExpression(
+            [NotNull] Expression queryExpression, [NotNull] Expression shaperExpression, ResultCardinality resultCardinality)
+        {
             QueryExpression = queryExpression;
             ShaperExpression = shaperExpression;
+            ResultCardinality = resultCardinality;
         }
 
-        public virtual Expression QueryExpression
-        {
-            get => _queryExpression;
-            [param: NotNull] set => _queryExpression = Check.NotNull(value, nameof(value));
-        }
+        public virtual Expression QueryExpression { get; }
 
-        public virtual ResultCardinality ResultCardinality { get; set; }
+        public virtual ResultCardinality ResultCardinality { get; }
 
-        public virtual Expression ShaperExpression
-        {
-            get => _shaperExpression;
-            [param: NotNull] set => _shaperExpression = Check.NotNull(value, nameof(value));
-        }
+        public virtual Expression ShaperExpression { get; }
 
         public override Type Type => ResultCardinality == ResultCardinality.Enumerable
             ? typeof(IQueryable<>).MakeGenericType(ShaperExpression.Type)
@@ -44,13 +39,30 @@ namespace Microsoft.EntityFrameworkCore.Query
         public sealed override ExpressionType NodeType => ExpressionType.Extension;
 
         protected override Expression VisitChildren(ExpressionVisitor visitor)
+            => throw new InvalidOperationException("Calling ShapedQueryExpression.VisitChildren is not allowed. " +
+                "Visit expression manually for relevant part.");
+
+        public virtual ShapedQueryExpression Update([NotNull] Expression queryExpression, [NotNull] Expression shaperExpression)
         {
-            Check.NotNull(visitor, nameof(visitor));
+            Check.NotNull(queryExpression, nameof(queryExpression));
+            Check.NotNull(shaperExpression, nameof(shaperExpression));
 
-            QueryExpression = visitor.Visit(QueryExpression);
-
-            return this;
+            return queryExpression != QueryExpression || shaperExpression != ShaperExpression
+                ? new ShapedQueryExpression(queryExpression, shaperExpression, ResultCardinality)
+                : this;
         }
+
+        public virtual ShapedQueryExpression UpdateShaper([NotNull] Expression shaperExpression)
+        {
+            Check.NotNull(shaperExpression, nameof(shaperExpression));
+
+            return shaperExpression != ShaperExpression
+                ? new ShapedQueryExpression(QueryExpression, shaperExpression, ResultCardinality)
+                : this;
+        }
+
+        public virtual ShapedQueryExpression UpdateResultCardinality(ResultCardinality resultCardinality)
+            => new ShapedQueryExpression(QueryExpression, ShaperExpression, resultCardinality);
 
         public virtual void Print(ExpressionPrinter expressionPrinter)
         {

dotnet/efcore

src/EFCore.Cosmos/Query/Internal/CosmosQueryableMethodTranslatingExpressionVisitor.cs
@@ -184,14 +184,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             Check.NotNull(source, nameof(source));
             Check.NotNull(resultType, nameof(resultType));
 
-            if (source.ShaperExpression.Type == resultType)
-            {
-                return source;
-            }
-
-            source.ShaperExpression = Expression.Convert(source.ShaperExpression, resultType);
-
-            return source;
+            return source.ShaperExpression.Type != resultType
+                ? source.UpdateShaperExpression(Expression.Convert(source.ShaperExpression, resultType))
+                : source;
         }
 
         /// <summary>
@@ -256,9 +251,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             selectExpression.ClearOrdering();
             selectExpression.ReplaceProjectionMapping(projectionMapping);
-            source.ShaperExpression = new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(int));
-
-            return source;
+            return source.UpdateShaperExpression(new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(int)));
         }
 
         /// <summary>
@@ -342,12 +335,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             var selectExpression = (SelectExpression)source.QueryExpression;
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(1)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaperExpression(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -449,12 +439,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             selectExpression.ReverseOrderings();
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(1)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaperExpression(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -510,9 +497,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             selectExpression.ClearOrdering();
             selectExpression.ReplaceProjectionMapping(projectionMapping);
-            source.ShaperExpression = new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(long));
-
-            return source;
+            return source.UpdateShaperExpression(new ProjectionBindingExpression(source.QueryExpression, new ProjectionMember(), typeof(long)));
         }
 
         /// <summary>
@@ -657,10 +642,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
 
             var newSelectorBody = ReplacingExpressionVisitor.Replace(selector.Parameters.Single(), source.ShaperExpression, selector.Body);
 
-            source.ShaperExpression = _projectionBindingExpressionVisitor
-                .Translate(selectExpression, newSelectorBody);
-
-            return source;
+            return source.UpdateShaperExpression(_projectionBindingExpressionVisitor.Translate(selectExpression, newSelectorBody));
         }
 
         /// <summary>
@@ -717,12 +699,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
             var selectExpression = (SelectExpression)source.QueryExpression;
             selectExpression.ApplyLimit(TranslateExpression(Expression.Constant(2)));
 
-            if (source.ShaperExpression.Type != returnType)
-            {
-                source.ShaperExpression = Expression.Convert(source.ShaperExpression, returnType);
-            }
-
-            return source;
+            return source.ShaperExpression.Type != returnType
+                ? source.UpdateShaperExpression(Expression.Convert(source.ShaperExpression, returnType))
+                : source;
         }
 
         /// <summary>
@@ -948,9 +927,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal
                 shaper = Expression.Convert(shaper, resultType);
             }
 
-            source.ShaperExpression = shaper;
-
-            return source;
+            return source.UpdateShaperExpression(shaper);
         }
     }
 }

src/EFCore.Relational/Query/Internal/CollectionJoinApplyingExpressionVisitor.cs
@@ -39,14 +39,9 @@ namespace Microsoft.EntityFrameworkCore.Query.Internal
                     collectionShaperExpression.ElementType);
             }
 
-            if (extensionExpression is ShapedQueryExpression shapedQueryExpression)
-            {
-                shapedQueryExpression.ShaperExpression = Visit(shapedQueryExpression.ShaperExpression);
-
-                return shapedQueryExpression;
-            }
-
-            return base.VisitExtension(extensionExpression);
+            return extensionExpression is ShapedQueryExpression shapedQueryExpression
+                ? shapedQueryExpression.UpdateShaperExpression(Visit(shapedQueryExpression.ShaperExpression))
+                : base.VisitExtension(extensionExpression);
         }
     }
 }

src/EFCore/Query/ShapedQueryExpression.cs
@@ -11,31 +11,26 @@ namespace Microsoft.EntityFrameworkCore.Query
 {
     public class ShapedQueryExpression : Expression, IPrintableExpression
     {
-        private Expression _queryExpression;
-        private Expression _shaperExpression;
-
         public ShapedQueryExpression([NotNull] Expression queryExpression, [NotNull] Expression shaperExpression)
+            : this(Check.NotNull(queryExpression, nameof(queryExpression)),
+                  Check.NotNull(shaperExpression, nameof(shaperExpression)),
+                  ResultCardinality.Enumerable)
         {
-            Check.NotNull(queryExpression, nameof(queryExpression));
-            Check.NotNull(shaperExpression, nameof(shaperExpression));
+        }
 
+        private ShapedQueryExpression(
+            [NotNull] Expression queryExpression, [NotNull] Expression shaperExpression, ResultCardinality resultCardinality)
+        {
             QueryExpression = queryExpression;
             ShaperExpression = shaperExpression;
+            ResultCardinality = resultCardinality;
         }
 
-        public virtual Expression QueryExpression
-        {
-            get => _queryExpression;
-            [param: NotNull] set => _queryExpression = Check.NotNull(value, nameof(value));
-        }
+        public virtual Expression QueryExpression { get; }
 
-        public virtual ResultCardinality ResultCardinality { get; set; }
+        public virtual ResultCardinality ResultCardinality { get; }
 
-        public virtual Expression ShaperExpression
-        {
-            get => _shaperExpression;
-            [param: NotNull] set => _shaperExpression = Check.NotNull(value, nameof(value));
-        }
+        public virtual Expression ShaperExpression { get; }
 
         public override Type Type => ResultCardinality == ResultCardinality.Enumerable
             ? typeof(IQueryable<>).MakeGenericType(ShaperExpression.Type)
@@ -44,13 +39,30 @@ namespace Microsoft.EntityFrameworkCore.Query
         public sealed override ExpressionType NodeType => ExpressionType.Extension;
 
         protected override Expression VisitChildren(ExpressionVisitor visitor)
+            => throw new InvalidOperationException("Calling ShapedQueryExpression.VisitChildren is not allowed. " +
+                "Visit expression manually for relevant part.");
+
+        public virtual ShapedQueryExpression Update([NotNull] Expression queryExpression, [NotNull] Expression shaperExpression)
         {
-            Check.NotNull(visitor, nameof(visitor));
+            Check.NotNull(queryExpression, nameof(queryExpression));
+            Check.NotNull(shaperExpression, nameof(shaperExpression));
 
-            QueryExpression = visitor.Visit(QueryExpression);
-
-            return this;
+            return queryExpression != QueryExpression || shaperExpression != ShaperExpression
+                ? new ShapedQueryExpression(queryExpression, shaperExpression, ResultCardinality)
+                : this;
         }
+
+        public virtual ShapedQueryExpression UpdateShaperExpression([NotNull] Expression shaperExpression)
+        {
+            Check.NotNull(shaperExpression, nameof(shaperExpression));
+
+            return shaperExpression != ShaperExpression
+                ? new ShapedQueryExpression(QueryExpression, shaperExpression, ResultCardinality)
+                : this;
+        }
+
+        public virtual ShapedQueryExpression UpdateResultCardinality(ResultCardinality resultCardinality)
+            => new ShapedQueryExpression(QueryExpression, ShaperExpression, resultCardinality);
 
         public virtual void Print(ExpressionPrinter expressionPrinter)
         {