d9f331e3ca183933f22624713602e4e134514ce7
Aleksandar Manukov <34056917+aleksandar-manukov@users.noreply.github.com>
2020-02-08 11:13:09 +0200

    Skip TimeSpan members tests for Sqlite

--- test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
@@ -137,6 +137,54 @@ FROM ""Squads"" AS ""s""
 WHERE length(""s"".""Banner"") = length(@__byteArrayParam)");
         }
 
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task TimeSpan_Hours(bool async)
+        {
+            return base.TimeSpan_Hours(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task TimeSpan_Minutes(bool async)
+        {
+            return base.TimeSpan_Minutes(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task TimeSpan_Seconds(bool async)
+        {
+            return base.TimeSpan_Seconds(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task TimeSpan_Milliseconds(bool async)
+        {
+            return base.TimeSpan_Milliseconds(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task Where_TimeSpan_Hours(bool async)
+        {
+            return base.Where_TimeSpan_Hours(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task Where_TimeSpan_Minutes(bool async)
+        {
+            return base.Where_TimeSpan_Minutes(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task Where_TimeSpan_Seconds(bool async)
+        {
+            return base.Where_TimeSpan_Seconds(async);
+        }
+
+        [ConditionalTheory(Skip = "PR #19774")]
+        public override Task Where_TimeSpan_Milliseconds(bool async)
+        {
+            return base.Where_TimeSpan_Milliseconds(async);
+        }
+
         private void AssertSql(params string[] expected)
             => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
     }
