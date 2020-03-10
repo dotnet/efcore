01113673b2ec7e2762ed2cc07235fb7fedc45215
Brice Lambson <brice@bricelam.net>
2020-03-10 11:00:51 -0700

    Design: Use exact IDesignTimeDbContextFactory method
    Fixes #11611

--- test/EFCore.Design.Tests/TestUtilities/TestAppServiceProviderFactory.cs
+++ test/EFCore.Design.Tests/TestUtilities/TestAppServiceProviderFactory.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore.Design.Internal;
 
@@ -9,7 +8,7 @@ namespace Microsoft.EntityFrameworkCore.TestUtilities
 {
     public class TestAppServiceProviderFactory : AppServiceProviderFactory
     {
-        public TestAppServiceProviderFactory(Assembly startupAssembly, Type programType, IOperationReporter reporter = null)
+        public TestAppServiceProviderFactory(Assembly startupAssembly, IOperationReporter reporter = null)
             : base(startupAssembly, reporter ?? new TestOperationReporter())
         {
         }
