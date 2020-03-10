1d40504263179e00abee534e8c764c60eee51aa4
Smit Patel <smitpatel@users.noreply.github.com>
2020-03-09 18:02:53 -0700

    Add regression test for issue#20046
    Resolved by #19377
    Issue fixed since cast to base type is removed by nav expansion as it is redundant
    
    Resolves #20046

--- test/EFCore.Relational.Specification.Tests/Query/InheritanceRelationalTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/InheritanceRelationalTestBase.cs
@@ -32,6 +32,36 @@ namespace Microsoft.EntityFrameworkCore.Query
             context.Set<Eagle>().FromSqlRaw(NormalizeDelimitersInRawString("select * from [Animal]")).ToList();
         }
 
+        [ConditionalFact]
+        public virtual void Casting_to_base_type_joining_with_query_type_works()
+        {
+            using var context = CreateContext();
+            var query = context.Set<Eagle>();
+
+            GetEntityWithAuditHistoryQuery(context, query);
+        }
+
+        private void GetEntityWithAuditHistoryQuery<T>(InheritanceContext context, IQueryable<T> query)
+            where T : Animal
+        {
+            var queryTypeQuery = context.Set<AnimalQuery>().FromSqlRaw(NormalizeDelimitersInRawString("Select * from [Animal]"));
+
+            var animalQuery = query.Cast<Animal>();
+
+            var joinQuery =
+                from animal in animalQuery
+                join keylessanimal in queryTypeQuery on animal.Name equals keylessanimal.Name
+                select new
+                {
+                    animal,
+                    keylessanimal
+                };
+
+            var result = joinQuery.ToList();
+
+            Assert.Single(result);
+        }
+
         private string NormalizeDelimitersInRawString(string sql)
             => ((RelationalTestStore)Fixture.TestStore).NormalizeDelimitersInRawString(sql);
 
