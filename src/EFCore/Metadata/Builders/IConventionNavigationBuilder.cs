ca8c7351f52f3d254849c808120c0b3f70e1f85a
lajones <lajones@microsoft.com>
2020-03-17 17:41:40 -0700

    Updating based on review comments.

--- src/EFCore/Metadata/Builders/IConventionNavigationBuilder.cs
+++ src/EFCore/Metadata/Builders/IConventionNavigationBuilder.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using JetBrains.Annotations;
+
 namespace Microsoft.EntityFrameworkCore.Metadata.Builders
 {
     public interface IConventionNavigationBuilder : IConventionAnnotatableBuilder
@@ -9,6 +11,26 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
         ///     Gets the navigation being configured.
         /// </summary>
         new IConventionNavigation Metadata { get; }
+
+        /// <summary>
+        ///     Returns a value indicating whether the backing field can be set for this navigation
+        ///     from the given configuration source.
+        /// </summary>
+        /// <param name="fieldName"> The field name. </param>
+        /// <param name="fromDataAnnotation"> Indicates whether the configuration was specified using a data annotation. </param>
+        /// <returns> <c>true</c> if the backing field can be set for this property. </returns>
+        bool CanSetField([CanBeNull] string fieldName, bool fromDataAnnotation = false);
+
+        /// <summary>
+        ///     Sets the backing field to use for this navigation.
+        /// </summary>
+        /// <param name="fieldName"> The field name. </param>
+        /// <param name="fromDataAnnotation"> Indicates whether the configuration was specified using a data annotation. </param>
+        /// <returns>
+        ///     The same builder instance if the configuration was applied,
+        ///     <c>null</c> otherwise.
+        /// </returns>
+        IConventionNavigationBuilder HasField([CanBeNull] string fieldName, bool fromDataAnnotation = false);
 
         /// <summary>
         ///     Returns a value indicating whether the <see cref="PropertyAccessMode" /> can be set for this navigation
