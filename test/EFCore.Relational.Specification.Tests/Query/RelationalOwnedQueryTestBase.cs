0008b8020f5c284933585d98254bf88d659432e9
Smit Patel <smitpatel@users.noreply.github.com>
2020-03-19 13:53:09 -0700

    Test: Update Sql assertion in OwnedQuerySqlServerTest
    - Also add at least one additional property in each owned type. If there no columns other than shared PK then it is required dependent.
    - Add a property in owned type which would be sharing column for hierarchy. Currently disabled due to #20334
    
    Resolves #20343
    Resolves #20336
    Finishes left over test work from #20345

--- test/EFCore.Relational.Specification.Tests/Query/RelationalOwnedQueryTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/RelationalOwnedQueryTestBase.cs
@@ -28,6 +28,17 @@ namespace Microsoft.EntityFrameworkCore.Query
                     entitySorters,
                     entityAsserters,
                     CanExecuteQueryString);
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+            {
+                base.OnModelCreating(modelBuilder, context);
+
+                // TODO: See issue#20334
+                //modelBuilder.Entity<OwnedPerson>().OwnsOne(e => e.PersonAddress).Property(e => e.PlaceType).HasColumnName("PlaceType");
+                //modelBuilder.Entity<Branch>().OwnsOne(e => e.BranchAddress).Property(e => e.PlaceType).HasColumnName("PlaceType");
+                //modelBuilder.Entity<LeafA>().OwnsOne(e => e.LeafAAddress).Property(e => e.PlaceType).HasColumnName("PlaceType");
+                //modelBuilder.Entity<LeafB>().OwnsOne(e => e.LeafBAddress).Property(e => e.PlaceType).HasColumnName("PlaceType");
+            }
         }
     }
 }
