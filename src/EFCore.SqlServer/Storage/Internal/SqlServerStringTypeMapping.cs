dotnet/efcore

commit e6507f653abddc7076aedbe02949894620a341b3

Fix for 15256 - bad generated SQL for DefaultValueSql with line breaks. (#20304)
Fix for 15256 - bad generated SQL for DefaultValue with line breaks.

src/EFCore.SqlServer/Storage/Internal/SqlServerStringTypeMapping.cs
@@ -159,8 +159,24 @@ namespace Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         protected override string GenerateNonNullSqlLiteral(object value)
-            => IsUnicode
-                ? $"N'{EscapeSqlLiteral((string)value)}'" // Interpolation okay; strings
-                : $"'{EscapeSqlLiteral((string)value)}'";
+            => EscapeLineBreaks(EscapeSqlLiteral((string)value));
+
+        private static readonly char[] LineBreakChars = new char[] { '\r', '\n' };
+        private string EscapeLineBreaks(string value)
+        {
+            var unicodePrefix = IsUnicode ? "N" : string.Empty;
+
+            if (value == null
+                || value.IndexOfAny(LineBreakChars) == -1)
+            {
+                return $"{unicodePrefix}'{value}'";
+            }
+
+            return ($"CONCAT({unicodePrefix}'" + value
+                    .Replace("\r", $"', CHAR(13), {unicodePrefix}'")
+                    .Replace("\n", $"', CHAR(10), {unicodePrefix}'") + "')")
+                    .Replace($"{unicodePrefix}'', ", string.Empty)
+                    .Replace($", {unicodePrefix}''", string.Empty);
+        }
     }
 }

test/EFCore.SqlServer.FunctionalTests/MigrationsSqlServerTest.cs
@@ -59,8 +59,11 @@ namespace Microsoft.EntityFrameworkCore
     CONSTRAINT [CK_EmployerId] CHECK ([EmployerId] > 0),
     CONSTRAINT [FK_People_Employers_EmployerId] FOREIGN KEY ([EmployerId]) REFERENCES [Employers] ([Id]) ON DELETE NO ACTION
 );
-EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', N'dbo2', 'TABLE', N'People';
-EXEC sp_addextendedproperty 'MS_Description', N'Employer ID comment', 'SCHEMA', N'dbo2', 'TABLE', N'People', 'COLUMN', N'EmployerId';");
+DECLARE @description AS sql_variant;
+SET @description = N'Table comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', N'dbo2', 'TABLE', N'People';
+SET @description = N'Employer ID comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', N'dbo2', 'TABLE', N'People', 'COLUMN', N'EmployerId';");
         }
 
         public override async Task Create_table_no_key()
@@ -84,8 +87,11 @@ EXEC sp_addextendedproperty 'MS_Description', N'Employer ID comment', 'SCHEMA', 
 );
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
-EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', @defaultSchema, 'TABLE', N'People';
-EXEC sp_addextendedproperty 'MS_Description', N'Column comment', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Name';");
+DECLARE @description AS sql_variant;
+SET @description = N'Table comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People';
+SET @description = N'Column comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Name';");
         }
 
         public override async Task Create_table_with_multiline_comments()
@@ -99,14 +105,11 @@ EXEC sp_addextendedproperty 'MS_Description', N'Column comment', 'SCHEMA', @defa
 );
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
-EXEC sp_addextendedproperty 'MS_Description', N'This is a multi-line
-table comment.
-More information can
-be found in the docs.', 'SCHEMA', @defaultSchema, 'TABLE', N'People';
-EXEC sp_addextendedproperty 'MS_Description', N'This is a multi-line
-column comment.
-More information can
-be found in the docs.', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Name';");
+DECLARE @description AS sql_variant;
+SET @description = CONCAT(N'This is a multi-line', CHAR(13), CHAR(10), N'table comment.', CHAR(13), CHAR(10), N'More information can', CHAR(13), CHAR(10), N'be found in the docs.');
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People';
+SET @description = CONCAT(N'This is a multi-line', CHAR(13), CHAR(10), N'column comment.', CHAR(13), CHAR(10), N'More information can', CHAR(13), CHAR(10), N'be found in the docs.');
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Name';");
         }
 
         public override async Task Drop_table()
@@ -124,7 +127,9 @@ be found in the docs.', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', 
             AssertSql(
                 @"DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
-EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', @defaultSchema, 'TABLE', N'People';");
+DECLARE @description AS sql_variant;
+SET @description = N'Table comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People';");
         }
 
         public override async Task Alter_table_add_comment_non_default_schema()
@@ -132,7 +137,9 @@ EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', @defau
             await base.Alter_table_add_comment_non_default_schema();
 
             AssertSql(
-                @"EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', N'SomeOtherSchema', 'TABLE', N'People';");
+                @"DECLARE @description AS sql_variant;
+SET @description = N'Table comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', N'SomeOtherSchema', 'TABLE', N'People';");
         }
 
         public override async Task Alter_table_change_comment()
@@ -142,8 +149,10 @@ EXEC sp_addextendedproperty 'MS_Description', N'Table comment', 'SCHEMA', @defau
             AssertSql(
                 @"DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
+DECLARE @description AS sql_variant;
 EXEC sp_dropextendedproperty 'MS_Description', 'SCHEMA', @defaultSchema, 'TABLE', N'People';
-EXEC sp_addextendedproperty 'MS_Description', N'Table comment2', 'SCHEMA', @defaultSchema, 'TABLE', N'People';");
+SET @description = N'Table comment2';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People';");
         }
 
         public override async Task Alter_table_remove_comment()
@@ -153,6 +162,7 @@ EXEC sp_addextendedproperty 'MS_Description', N'Table comment2', 'SCHEMA', @defa
             AssertSql(
                 @"DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
+DECLARE @description AS sql_variant;
 EXEC sp_dropextendedproperty 'MS_Description', 'SCHEMA', @defaultSchema, 'TABLE', N'People';");
         }
 
@@ -373,7 +383,9 @@ EXEC(N'ALTER SCHEMA [' + @defaultSchema + N'] TRANSFER [TestTableSchema].[TestTa
                 @"ALTER TABLE [People] ADD [FullName] nvarchar(max) NULL;
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
-EXEC sp_addextendedproperty 'MS_Description', N'My comment', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'FullName';");
+DECLARE @description AS sql_variant;
+SET @description = N'My comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'FullName';");
         }
 
         public override async Task Add_column_shared()
@@ -531,7 +543,9 @@ IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [People] DROP CONSTRAINT [' + @var0 + ']
 ALTER TABLE [People] ALTER COLUMN [Id] int NOT NULL;
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
-EXEC sp_addextendedproperty 'MS_Description', N'Some comment', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';");
+DECLARE @description AS sql_variant;
+SET @description = N'Some comment';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';");
         }
 
         [ConditionalFact]
@@ -549,8 +563,10 @@ IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [People] DROP CONSTRAINT [' + @var0 + ']
 ALTER TABLE [People] ALTER COLUMN [Id] int NOT NULL;
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
+DECLARE @description AS sql_variant;
 EXEC sp_dropextendedproperty 'MS_Description', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';
-EXEC sp_addextendedproperty 'MS_Description', N'Some comment2', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';");
+SET @description = N'Some comment2';
+EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';");
         }
 
         [ConditionalFact]
@@ -568,6 +584,7 @@ IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [People] DROP CONSTRAINT [' + @var0 + ']
 ALTER TABLE [People] ALTER COLUMN [Id] int NOT NULL;
 DECLARE @defaultSchema AS sysname;
 SET @defaultSchema = SCHEMA_NAME();
+DECLARE @description AS sql_variant;
 EXEC sp_dropextendedproperty 'MS_Description', 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Id';");
         }
 

src/EFCore.SqlServer/Migrations/SqlServerMigrationsSqlGenerator.cs
@@ -362,7 +362,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                         operation.Schema,
                         operation.Table,
                         operation.Name,
-                        omitSchemaVariable: dropDescription);
+                        omitVariableDeclarations: dropDescription);
                 }
             }
 
@@ -516,7 +516,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                     operation.Schema,
                     operation.Name,
                     column.Name,
-                    omitSchemaVariable: !firstDescription);
+                    omitVariableDeclarations: !firstDescription);
 
                 firstDescription = false;
             }
@@ -1025,7 +1025,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                         operation.Comment,
                         operation.Schema,
                         operation.Name,
-                        omitSchemaVariable: dropDescription);
+                        omitVariableDeclarations: dropDescription);
                 }
             }
 
@@ -1709,8 +1709,8 @@ namespace Microsoft.EntityFrameworkCore.Migrations
         /// <param name="schema"> The schema of the table. </param>
         /// <param name="table"> The name of the table. </param>
         /// <param name="column"> The name of the column. </param>
-        /// <param name="omitSchemaVariable">
-        ///     Indicates whether the @defaultSchema variable declaraion should be omitted.
+        /// <param name="omitVariableDeclarations">
+        ///     Indicates whether the variable declarations should be omitted.
         /// </param>
         protected virtual void AddDescription(
             [NotNull] MigrationCommandListBuilder builder,
@@ -1718,14 +1718,14 @@ namespace Microsoft.EntityFrameworkCore.Migrations
             [CanBeNull] string schema,
             [NotNull] string table,
             [CanBeNull] string column = null,
-            bool omitSchemaVariable = false)
+            bool omitVariableDeclarations = false)
         {
             var stringTypeMapping = Dependencies.TypeMappingSource.GetMapping(typeof(string));
 
             string schemaLiteral;
             if (schema == null)
             {
-                if (!omitSchemaVariable)
+                if (!omitVariableDeclarations)
                 {
                     builder.Append("DECLARE @defaultSchema AS sysname")
                         .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
@@ -1740,9 +1740,18 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                 schemaLiteral = Literal(schema);
             }
 
+            if (!omitVariableDeclarations)
+            {
+                builder.Append("DECLARE @description AS sql_variant")
+                    .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
+            }
+
+            builder.Append("SET @description = ")
+                .Append(Literal(description))
+                .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
             builder
                 .Append("EXEC sp_addextendedproperty 'MS_Description', ")
-                .Append(Literal(description))
+                .Append("@description")
                 .Append(", 'SCHEMA', ")
                 .Append(schemaLiteral)
                 .Append(", 'TABLE', ")
@@ -1769,22 +1778,22 @@ namespace Microsoft.EntityFrameworkCore.Migrations
         /// <param name="schema"> The schema of the table. </param>
         /// <param name="table"> The name of the table. </param>
         /// <param name="column"> The name of the column. </param>
-        /// <param name="omitSchemaVariable">
-        ///     Indicates whether the @defaultSchema variable declaraion should be omitted.
+        /// <param name="omitVariableDeclarations">
+        ///     Indicates whether the variable declarations should be omitted.
         /// </param>
         protected virtual void DropDescription(
             [NotNull] MigrationCommandListBuilder builder,
             [CanBeNull] string schema,
             [NotNull] string table,
             [CanBeNull] string column = null,
-            bool omitSchemaVariable = false)
+            bool omitVariableDeclarations = false)
         {
             var stringTypeMapping = Dependencies.TypeMappingSource.GetMapping(typeof(string));
 
             string schemaLiteral;
             if (schema == null)
             {
-                if (!omitSchemaVariable)
+                if (!omitVariableDeclarations)
                 {
                     builder.Append("DECLARE @defaultSchema AS sysname")
                         .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
@@ -1797,6 +1806,12 @@ namespace Microsoft.EntityFrameworkCore.Migrations
             else
             {
                 schemaLiteral = Literal(schema);
+            }
+
+            if (!omitVariableDeclarations)
+            {
+                builder.Append("DECLARE @description AS sql_variant")
+                    .AppendLine(Dependencies.SqlGenerationHelper.StatementTerminator);
             }
 
             builder

src/EFCore.Sqlite.Core/Storage/Internal/SqliteStringTypeMapping.cs
@@ -1,0 +1,76 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Data;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal
+{
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public class SqliteStringTypeMapping : StringTypeMapping
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SqliteStringTypeMapping" /> class.
+        /// </summary>
+        /// <param name="storeType"> The name of the database type. </param>
+        /// <param name="dbType"> The <see cref="System.Data.DbType" /> to be used. </param>
+        /// <param name="unicode"> A value indicating whether the type should handle Unicode data or not. </param>
+        /// <param name="size"> The size of data the property is configured to store, or null if no size is configured. </param>
+        public SqliteStringTypeMapping(
+            [NotNull] string storeType,
+            DbType? dbType = null,
+            bool unicode = false,
+            int? size = null)
+            : base(storeType, dbType, unicode, size)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SqliteStringTypeMapping" /> class.
+        /// </summary>
+        /// <param name="parameters"> Parameter object for <see cref="RelationalTypeMapping" />. </param>
+        protected SqliteStringTypeMapping(RelationalTypeMappingParameters parameters)
+            : base(parameters)
+        {
+        }
+
+        /// <summary>
+        ///     Creates a copy of this mapping.
+        /// </summary>
+        /// <param name="parameters"> The parameters for this mapping. </param>
+        /// <returns> The newly created mapping. </returns>
+        protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
+            => new SqliteStringTypeMapping(parameters);
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        protected override string GenerateNonNullSqlLiteral(object value)
+            => EscapeLineBreaks(EscapeSqlLiteral((string)value));
+
+        private static readonly char[] LineBreakChars = new char[] { '\r', '\n' };
+        private static string EscapeLineBreaks(string value)
+        {
+            if (value == null
+                || value.IndexOfAny(LineBreakChars) == -1)
+            {
+                return $"'{value}'";
+            }
+
+            return ("('" + value
+                .Replace("\r", "' || CHAR(13) || '")
+                .Replace("\n", "' || CHAR(10) || '") + "')")
+                .Replace("'' || ", string.Empty)
+                .Replace(" || ''", string.Empty);
+        }
+    }
+}

src/EFCore.Sqlite.Core/Storage/Internal/SqliteTypeMappingSource.cs
@@ -46,7 +46,7 @@ namespace Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal
         private static readonly LongTypeMapping _integer = new LongTypeMapping(IntegerTypeName);
         private static readonly DoubleTypeMapping _real = new DoubleTypeMapping(RealTypeName);
         private static readonly ByteArrayTypeMapping _blob = new ByteArrayTypeMapping(BlobTypeName);
-        private static readonly StringTypeMapping _text = new StringTypeMapping(TextTypeName);
+        private static readonly SqliteStringTypeMapping _text = new SqliteStringTypeMapping(TextTypeName);
 
         private readonly Dictionary<Type, RelationalTypeMapping> _clrTypeMappings
             = new Dictionary<Type, RelationalTypeMapping>

test/EFCore.Relational.Tests/Migrations/MigrationSqlGeneratorTestBase.cs
@@ -141,6 +141,30 @@ namespace Microsoft.EntityFrameworkCore.Migrations
             => Generate(
                 new SqlOperation { Sql = "-- I <3 DDL" });
 
+        [ConditionalTheory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public virtual void DefaultValue_with_line_breaks(bool isUnicode)
+        {
+            Generate(
+                new CreateTableOperation
+                {
+                    Name = "TestLineBreaks",
+                    Schema = "dbo",
+                    Columns =
+                    {
+                        new AddColumnOperation
+                        {
+                            Name = "TestDefaultValue",
+                            Table = "Test",
+                            ClrType = typeof(string),
+                            DefaultValue = "\r\nVarious Line\rBreaks\n",
+                            IsUnicode = isUnicode
+                        }
+                    }
+                });
+        }
+
         protected TestHelpers TestHelpers { get; }
 
         protected MigrationSqlGeneratorTestBase(TestHelpers testHelpers)

test/EFCore.SqlServer.Tests/Migrations/SqlServerMigrationSqlGeneratorTest.cs
@@ -545,6 +545,19 @@ GO
 ");
         }
 
+        public override void DefaultValue_with_line_breaks(bool isUnicode)
+        {
+            base.DefaultValue_with_line_breaks(isUnicode);
+
+            var storeType = isUnicode ? "nvarchar(max)" : "varchar(max)";
+            var unicodePrefix = isUnicode ? "N" : string.Empty;
+            var expectedSql = @$"CREATE TABLE [dbo].[TestLineBreaks] (
+    [TestDefaultValue] {storeType} NOT NULL DEFAULT CONCAT(CHAR(13), CHAR(10), {unicodePrefix}'Various Line', CHAR(13), {unicodePrefix}'Breaks', CHAR(10))
+);
+";
+            AssertSql(expectedSql);
+        }
+
         public SqlServerMigrationSqlGeneratorTest()
             : base(SqlServerTestHelpers.Instance)
         {

test/EFCore.Sqlite.Tests/Migrations/SqliteMigrationSqlGeneratorTest.cs
@@ -71,6 +71,17 @@ namespace Microsoft.EntityFrameworkCore.Migrations
 ");
         }
 
+        public override void DefaultValue_with_line_breaks(bool isUnicode)
+        {
+            base.DefaultValue_with_line_breaks(isUnicode);
+
+            AssertSql(
+                @"CREATE TABLE ""TestLineBreaks"" (
+    ""TestDefaultValue"" TEXT NOT NULL DEFAULT (CHAR(13) || CHAR(10) || 'Various Line' || CHAR(13) || 'Breaks' || CHAR(10))
+);
+");
+        }
+
         [ConditionalTheory]
         [InlineData(true, null)]
         [InlineData(false, "PK_Id")]

commit 64361698325dbb17333c3e511a758ac7d9e13d9e

Fix Linux build breaks
Fix two issues building and testing on Linux introduced by #20304 (bad generated SQL for DefaultValue with line breaks /cc @lajones)
* Test literals were using machine line endings
* Replacement mechanism for new lines in literals did not handle a single `\n`

Fixes #20439
Fixes #20438

src/EFCore.SqlServer/Storage/Internal/SqlServerStringTypeMapping.cs
@@ -172,6 +172,11 @@ namespace Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal
                 return $"{unicodePrefix}'{value}'";
             }
 
+            if (value.Length == 1)
+            {
+                return value[0] == '\n' ? "CHAR(10)" : "CHAR(13)";
+            }
+
             return ($"CONCAT({unicodePrefix}'" + value
                     .Replace("\r", $"', CHAR(13), {unicodePrefix}'")
                     .Replace("\n", $"', CHAR(10), {unicodePrefix}'") + "')")

test/EFCore.Relational.Specification.Tests/MigrationsTestBase.cs
@@ -177,14 +177,8 @@ namespace Microsoft.EntityFrameworkCore
         [ConditionalFact]
         public virtual Task Create_table_with_multiline_comments()
         {
-            var tableComment = @"This is a multi-line
-table comment.
-More information can
-be found in the docs.";
-            var columnComment = @"This is a multi-line
-column comment.
-More information can
-be found in the docs.";
+            var tableComment = "This is a multi-line\r\ntable comment.\r\nMore information can\r\nbe found in the docs.";
+            var columnComment = "This is a multi-line\ncolumn comment.\nMore information can\nbe found in the docs.";
 
             return Test(
                 builder => { },

test/EFCore.SqlServer.FunctionalTests/MigrationsSqlServerTest.cs
@@ -108,7 +108,7 @@ SET @defaultSchema = SCHEMA_NAME();
 DECLARE @description AS sql_variant;
 SET @description = CONCAT(N'This is a multi-line', CHAR(13), CHAR(10), N'table comment.', CHAR(13), CHAR(10), N'More information can', CHAR(13), CHAR(10), N'be found in the docs.');
 EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People';
-SET @description = CONCAT(N'This is a multi-line', CHAR(13), CHAR(10), N'column comment.', CHAR(13), CHAR(10), N'More information can', CHAR(13), CHAR(10), N'be found in the docs.');
+SET @description = CONCAT(N'This is a multi-line', CHAR(10), N'column comment.', CHAR(10), N'More information can', CHAR(10), N'be found in the docs.');
 EXEC sp_addextendedproperty 'MS_Description', @description, 'SCHEMA', @defaultSchema, 'TABLE', N'People', 'COLUMN', N'Name';");
         }
 