37c439e2e80135519ded66e565ee6771e7513c9d
Arthur Vickers <ajcvickers@hotmail.com>
2020-03-29 12:51:01 -0700

    Make IDbSetFinder public
    Fixes #20385
    
    Also annotated internal service properties on public dependency objects.

--- src/EFCore/Infrastructure/DbSetFinderExtensions.cs
+++ src/EFCore/Infrastructure/DbSetFinderExtensions.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Microsoft.EntityFrameworkCore.Infrastructure
 {
