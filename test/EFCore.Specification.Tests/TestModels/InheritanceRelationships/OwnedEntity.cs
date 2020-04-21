e41f1f763e5f3e7de7cf564db3acf972e0357d55
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2020-04-20 21:15:40 -0700

    Add owned types to InheritanceRelationshipsQueryTest
    Part of #2266

--- test/EFCore.Specification.Tests/TestModels/InheritanceRelationships/OwnedEntity.cs
+++ test/EFCore.Specification.Tests/TestModels/InheritanceRelationships/OwnedEntity.cs
@@ -1,15 +1,11 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace Microsoft.EntityFrameworkCore.TestModels.InheritanceRelationships
 {
-    public class BaseCollectionOnDerived
+    public class OwnedEntity
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
-        public int? ParentId { get; set; }
-        public DerivedInheritanceRelationshipEntity BaseParent { get; set; }
     }
 }
