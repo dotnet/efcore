b449924ee1605a8fe009fc92a34854d09a366b91
Andriy Svyryd <AndriySvyryd@users.noreply.github.com>
2020-04-22 11:46:40 -0700

    Add owned types to InheritanceRelationshipsQueryTest
    Part of #2266

--- test/EFCore.Specification.Tests/TestModels/InheritanceRelationships/NestedReferenceBase.cs
+++ test/EFCore.Specification.Tests/TestModels/InheritanceRelationships/NestedReferenceBase.cs
@@ -1,13 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Microsoft.EntityFrameworkCore.TestModels.InheritanceRelationships
 {
     public class NestedReferenceBase
     {
-        [NotMapped]
         public int Id { get; set; }
 
         public string Name { get; set; }
