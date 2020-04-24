d7d1c350a2735e4ffa7ea740040755eafcf006c0
Shay Rojansky <roji@roji.org>
2020-04-24 17:42:24 +0200

    Add missing XML docs

--- src/EFCore.SqlServer/Storage/Internal/SqlServerTransaction.cs
+++ src/EFCore.SqlServer/Storage/Internal/SqlServerTransaction.cs
@@ -11,6 +11,12 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal
 {
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
     public class SqlServerTransaction : RelationalTransaction, IDbContextTransaction
     {
         private readonly DbTransaction _dbTransaction;
