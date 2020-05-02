fd84f1ce3edb40d8c4edda7c53d365152d85d2ec
Shay Rojansky <roji@roji.org>
2020-05-02 14:31:49 +0200

    Remove cold start benchmarking
    Closes #19879

--- benchmark/EFCore.Sqlite.Benchmarks/Initialization/InitializationSqliteTests.cs
+++ benchmark/EFCore.Sqlite.Benchmarks/Initialization/InitializationSqliteTests.cs
@@ -1,15 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.EntityFrameworkCore.Benchmarks.Models.AdventureWorks;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace Microsoft.EntityFrameworkCore.Benchmarks.Initialization
 {
-    public class InitializationSqliteTests : InitializationTests<ColdStartEnabledSqliteTest>
+    public class InitializationSqliteTests : InitializationTests
     {
-        protected override ConventionSet CreateConventionSet()
-        {
-            return SqliteConventionSetBuilder.Build();
-        }
+        protected override AdventureWorksContextBase CreateContext()=> AdventureWorksSqliteFixture.CreateContext();
+        protected override ConventionSet CreateConventionSet() => SqliteConventionSetBuilder.Build();
     }
 }
