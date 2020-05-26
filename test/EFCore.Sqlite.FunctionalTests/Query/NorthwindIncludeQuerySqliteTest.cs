6268c397a483eb1bba8274129461b5246fbacae2
Smit Patel <smitpatel@users.noreply.github.com>
2020-05-26 10:34:49 -0700

    Test: Use AssertQuery in NorthwindIncludeQueryTestBase (#21011)
    - Remove string based includes from base class. (will move to a different class)
    - Remove tests related to no tracking from base class. (will move to a different class)
    - Run tests for both sync/async

--- test/EFCore.Sqlite.FunctionalTests/Query/NorthwindIncludeQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/NorthwindIncludeQuerySqliteTest.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.EntityFrameworkCore.TestUtilities;
+using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 namespace Microsoft.EntityFrameworkCore.Query
@@ -14,19 +14,9 @@ namespace Microsoft.EntityFrameworkCore.Query
             //TestSqlLoggerFactory.CaptureOutput(testOutputHelper);
         }
 
-        [SqliteVersionCondition(Min = "3.8.8", SkipReason = "Distinct & Order By gives incorrect result in older version of Sqlite.")]
-        public override void Include_collection_order_by_non_key_with_take(bool useString)
-        {
-            base.Include_collection_order_by_non_key_with_take(useString);
-        }
-
         // Sqlite does not support Apply operations
-        public override void Include_collection_with_cross_apply_with_filter(bool useString)
-        {
-        }
+        public override Task Include_collection_with_cross_apply_with_filter(bool async) => Task.CompletedTask;
 
-        public override void Include_collection_with_outer_apply_with_filter(bool useString)
-        {
-        }
+        public override Task Include_collection_with_outer_apply_with_filter(bool async) => Task.CompletedTask;
     }
 }
