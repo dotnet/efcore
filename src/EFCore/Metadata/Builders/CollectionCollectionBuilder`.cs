7ed858d6aae4e8dbcdbc2078cec55541b3c26f45
lajones <lajones@microsoft.com>
2020-06-17 16:18:14 -0700

    Correcting order of param defintions.

--- src/EFCore/Metadata/Builders/CollectionCollectionBuilder`.cs
+++ src/EFCore/Metadata/Builders/CollectionCollectionBuilder`.cs
@@ -41,8 +41,8 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
         /// <summary>
         ///     Configures the relationships to the entity types participating in the many-to-many relationship.
         /// </summary>
-        /// <param name="configureLeft"> The configuration for the relationship to the left entity type. </param>
         /// <param name="configureRight"> The configuration for the relationship to the right entity type. </param>
+        /// <param name="configureLeft"> The configuration for the relationship to the left entity type. </param>
         /// <typeparam name="TAssociationEntity"> The type of the association entity. </typeparam>
         /// <returns> The builder for the association type. </returns>
         public virtual EntityTypeBuilder<TAssociationEntity> UsingEntity<TAssociationEntity>(
@@ -64,8 +64,8 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Builders
         /// <summary>
         ///     Configures the relationships to the entity types participating in the many-to-many relationship.
         /// </summary>
-        /// <param name="configureLeft"> The configuration for the relationship to the left entity type. </param>
         /// <param name="configureRight"> The configuration for the relationship to the right entity type. </param>
+        /// <param name="configureLeft"> The configuration for the relationship to the left entity type. </param>
         /// <param name="configureAssociation"> The configuration of the association type. </param>
         /// <typeparam name="TAssociationEntity"> The type of the association entity. </typeparam>
         /// <returns> The builder for the originating entity type so that multiple configuration calls can be chained. </returns>
