9d9c25e227c097c30a5a9cf14cad93eeb83797b8
Smit Patel <smitpatel@users.noreply.github.com>
2020-06-23 15:35:47 -0700

    Query: Add tests for Owned collections and AsSplitQuery (#21387)
    Resolves #21352

--- test/EFCore.Relational.Specification.Tests/Query/OwnedQueryRelationalTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/OwnedQueryRelationalTestBase.cs
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
 
 namespace Microsoft.EntityFrameworkCore.Query
 {
@@ -13,6 +14,101 @@ namespace Microsoft.EntityFrameworkCore.Query
         protected OwnedQueryRelationalTestBase(TFixture fixture)
             : base(fixture)
         {
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Query_for_base_type_loads_all_owned_navs_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().AsSplitQuery());
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Query_for_branch_type_loads_all_owned_navs_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<Branch>().AsSplitQuery());
+        }
+
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Query_when_subquery_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().Distinct().AsSplitQuery()
+                    .OrderBy(p => p.Id)
+                    .Take(5)
+                    .Select(op => new { op }),
+                assertOrder: true,
+                elementAsserter: (e, a) => AssertEqual(e.op, a.op));
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Project_multiple_owned_navigations_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().OrderBy(p => p.Id).AsSplitQuery()
+                    .Select(
+                        p => new
+                        {
+                            p.Orders,
+                            p.PersonAddress,
+                            p.PersonAddress.Country.Planet
+                        }),
+                assertOrder: true,
+                elementAsserter: (e, a) =>
+                {
+                    AssertCollection(e.Orders, a.Orders);
+                    AssertEqual(e.PersonAddress, a.PersonAddress);
+                    AssertEqual(e.Planet, a.Planet);
+                });
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Navigation_rewrite_on_owned_reference_followed_by_regular_entity_and_collection_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().OrderBy(p => p.Id).Select(p => p.PersonAddress.Country.Planet.Moons).AsSplitQuery(),
+                assertOrder: true,
+                elementAsserter: (e, a) => AssertCollection(e, a));
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Query_with_OfType_eagerly_loads_correct_owned_navigations_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().OfType<LeafA>().AsSplitQuery());
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Unmapped_property_projection_loads_owned_navigations_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().Where(e => e.Id == 1).AsTracking().Select(e => new { e.ReadOnlyProperty }).AsSplitQuery(),
+                entryCount: 5);
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual Task Can_query_on_indexer_properties_split(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => ss.Set<OwnedPerson>().Where(c => (string)c["Name"] == "Mona Cy").AsSplitQuery());
         }
 
         protected virtual bool CanExecuteQueryString => false;
