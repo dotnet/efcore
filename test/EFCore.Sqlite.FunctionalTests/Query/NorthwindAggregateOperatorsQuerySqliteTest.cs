2eacef051c2597ca0806d526d5074707b07b8bd2
maumar <maumar@microsoft.com>
2020-07-08 20:27:15 -0700

    adding and fixing tests

--- test/EFCore.Sqlite.FunctionalTests/Query/NorthwindAggregateOperatorsQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/NorthwindAggregateOperatorsQuerySqliteTest.cs
@@ -1,8 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.EntityFrameworkCore.Query
@@ -17,15 +19,15 @@ namespace Microsoft.EntityFrameworkCore.Query
         }
 
         public override Task Sum_with_division_on_decimal(bool async)
-            => AssertTranslationFailed(() => base.Sum_with_division_on_decimal(async));
+            => Assert.ThrowsAsync<NotSupportedException>(() => base.Sum_with_division_on_decimal(async));
 
         public override Task Sum_with_division_on_decimal_no_significant_digits(bool async)
-            => AssertTranslationFailed(() => base.Sum_with_division_on_decimal_no_significant_digits(async));
+            => Assert.ThrowsAsync<NotSupportedException>(() => base.Sum_with_division_on_decimal_no_significant_digits(async));
 
         public override Task Average_with_division_on_decimal(bool async)
-            => AssertTranslationFailed(() => base.Average_with_division_on_decimal(async));
+            => Assert.ThrowsAsync<NotSupportedException>(() => base.Average_with_division_on_decimal(async));
 
         public override Task Average_with_division_on_decimal_no_significant_digits(bool async)
-            => AssertTranslationFailed(() => base.Average_with_division_on_decimal_no_significant_digits(async));
+            => Assert.ThrowsAsync<NotSupportedException>(() => base.Average_with_division_on_decimal_no_significant_digits(async));
     }
 }
