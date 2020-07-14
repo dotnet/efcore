b75ead65cf170779e3be6cfc1457a3dd38c6aa9e
Youssef Victor <youssefvictor00@gmail.com>
2020-07-14 18:10:20 +0200

    Write DiagnosticSuppressor for CS8618

---
+++ src/EFCore.Analyzers/NullabilityDiagnosticSuppressor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class NullabilityDiagnosticSuppressor : DiagnosticSuppressor
+    {
+        public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions => ImmutableArray.Create(new SuppressionDescriptor("EFSup-CS8618", "CS8618", "Entity Framework initializes DbSet<T>, so they are not null."));
+
+        public override void ReportSuppressions(SuppressionAnalysisContext context)
+        {
+            foreach (var diagnostic in context.ReportedDiagnostics)
+            {
+                var root = diagnostic.Location.SourceTree.GetRoot(context.CancellationToken);
+                if (!(root.FindNode(diagnostic.Location.SourceSpan) is PropertyDeclarationSyntax node))
+                {
+                    continue;
+                }
+                if (!(node.Parent is ClassDeclarationSyntax classDeclaration))
+                {
+                    continue;
+                }
+
+                // TODO: Check if the class derives from DbContext. (Not sure how to do it yet)
+
+                if (!(node.Type is GenericNameSyntax genericSyntax))
+                {
+                    continue;
+                }
+                if (genericSyntax.Identifier.Text != "DbSet")
+                {
+                    continue;
+                }
+
+                context.ReportSuppression(Suppression.Create(SupportedSuppressions.First(), diagnostic));
+            }
+        }
+    }
+}
---
+++ test/EFCore.Analyzers.Tests/NullabilityDiagnosticSuppressorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Analyzers.Test.TestUtilities;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public class NullabilityDiagnosticSuppressorTest
+    {
+
+    }
+}
