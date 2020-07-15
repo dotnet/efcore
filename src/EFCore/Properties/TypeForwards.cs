90105b00dc4fda60a5df0d18bd21ab11abf179f9
Arthur Vickers <ajcvickers@hotmail.com>
2020-07-15 08:34:23 -0700

    Move ObservableCollectionListSource to Abstractions package (#21595)
    Fixes #20034

---
+++ src/EFCore/Properties/TypeForwards.cs
@@ -0,0 +1,12 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+[assembly:TypeForwardedToAttribute(typeof(ObservableBackedBindingList<>))]
+[assembly:TypeForwardedToAttribute(typeof(ObservableCollectionExtensions))]
+[assembly:TypeForwardedToAttribute(typeof(ObservableCollectionListSource<>))]
+[assembly:TypeForwardedToAttribute(typeof(SortableBindingList<>))]
