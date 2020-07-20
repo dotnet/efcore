roji/EntityFramework

Fix savepoint bugs
* Check that DbConnection is Open before attempting to rollback and/or
  release a savepoint, and swallow (log) any exceptions that bubble up
  from those operations.
* Call close on IRelationalConnection after releasing savepoint to make
  sure open/close are balanced.

Closes #21340

src/EFCore.Relational/Diagnostics/RelationalEventId.cs
@@ -88,7 +88,9 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics
 
             // Update events
             BatchReadyForExecution = CoreEventId.RelationalBaseId + 700,
-            BatchSmallerThanMinBatchSize
+            BatchSmallerThanMinBatchSize,
+            BatchExecutorFailedToRollbackToSavepoint,
+            BatchExecutorFailedToReleaseSavepoint,
         }
 
         private static readonly string _connectionPrefix = DbLoggerCategory.Database.Connection.Name + ".";
@@ -720,5 +722,25 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics
         ///     </para>
         /// </summary>
         public static readonly EventId BatchSmallerThanMinBatchSize = MakeUpdateId(Id.BatchSmallerThanMinBatchSize);
+
+        /// <summary>
+        ///     <para>
+        ///         An error occurred while the batch executor was rolling back the transaction to a savepoint, after an exception occured.
+        ///     </para>
+        ///     <para>
+        ///         This event is in the <see cref="DbLoggerCategory.Update" /> category.
+        ///     </para>
+        /// </summary>
+        public static readonly EventId BatchExecutorFailedToRollbackToSavepoint = MakeValidationId(Id.BatchExecutorFailedToRollbackToSavepoint);
+
+        /// <summary>
+        ///     <para>
+        ///         An error occurred while the batch executor was releasing a transaction savepoint.
+        ///     </para>
+        ///     <para>
+        ///         This event is in the <see cref="DbLoggerCategory.Update" /> category.
+        ///     </para>
+        /// </summary>
+        public static readonly EventId BatchExecutorFailedToReleaseSavepoint = MakeValidationId(Id.BatchExecutorFailedToReleaseSavepoint);
     }
 }

src/EFCore.Relational/Properties/RelationalStrings.Designer.cs
@@ -2195,5 +2195,53 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics.Internal
 
             return (EventDefinition)definition;
         }
+
+        /// <summary>
+        ///     An error occurred while the batch executor was rolling back the transaction to a savepoint, after an exception occured.
+        /// </summary>
+        public static EventDefinition BatchExecutorFailedToRollbackToSavepoint([NotNull] IDiagnosticsLogger logger)
+        {
+            var definition = ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToRollbackToSavepoint;
+            if (definition == null)
+            {
+                definition = LazyInitializer.EnsureInitialized<EventDefinitionBase>(
+                    ref ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToRollbackToSavepoint,
+                    () => new EventDefinition(
+                        logger.Options,
+                        RelationalEventId.BatchExecutorFailedToRollbackToSavepoint,
+                        LogLevel.Debug,
+                        "RelationalEventId.BatchExecutorFailedToRollbackToSavepoint",
+                        level => LoggerMessage.Define(
+                            level,
+                            RelationalEventId.BatchExecutorFailedToRollbackToSavepoint,
+                            _resourceManager.GetString("BatchExecutorFailedToRollbackToSavepoint"))));
+            }
+
+            return (EventDefinition)definition;
+        }
+
+        /// <summary>
+        ///     An error occurred while the batch executor was releasing a transaction savepoint.
+        /// </summary>
+        public static EventDefinition BatchExecutorFailedToReleaseSavepoint([NotNull] IDiagnosticsLogger logger)
+        {
+            var definition = ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToReleaseSavepoint;
+            if (definition == null)
+            {
+                definition = LazyInitializer.EnsureInitialized<EventDefinitionBase>(
+                    ref ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToReleaseSavepoint,
+                    () => new EventDefinition(
+                        logger.Options,
+                        RelationalEventId.BatchExecutorFailedToReleaseSavepoint,
+                        LogLevel.Debug,
+                        "RelationalEventId.BatchExecutorFailedToReleaseSavepoint",
+                        level => LoggerMessage.Define(
+                            level,
+                            RelationalEventId.BatchExecutorFailedToReleaseSavepoint,
+                            _resourceManager.GetString("BatchExecutorFailedToReleaseSavepoint"))));
+            }
+
+            return (EventDefinition)definition;
+        }
     }
 }

src/EFCore.Relational/Update/Internal/BatchExecutor.cs
@@ -1,11 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Transactions;
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,9 +39,12 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
-        public BatchExecutor([NotNull] ICurrentDbContext currentContext)
+        public BatchExecutor(
+            [NotNull] ICurrentDbContext currentContext,
+            [NotNull] IDiagnosticsLogger<DbLoggerCategory.Update> updateLogger)
         {
             CurrentContext = currentContext;
+            UpdateLogger = updateLogger;
         }
 
         /// <summary>
@@ -48,6 +54,11 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public virtual ICurrentDbContext CurrentContext { get; }
+
+        /// <summary>
+        ///     The logger.
+        /// </summary>
+        protected virtual IDiagnosticsLogger<DbLoggerCategory.Update> UpdateLogger { get; }
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -97,25 +108,43 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
             }
             catch
             {
-                if (createdSavepoint)
+                if (createdSavepoint && connection.DbConnection.State == ConnectionState.Open)
                 {
-                    transaction.RollbackToSavepoint(SavepointName);
+                    try
+                    {
+                        transaction.RollbackToSavepoint(SavepointName);
+                    }
+                    catch (Exception e)
+                    {
+                        UpdateLogger.LogBatchExecutorFailedToRollbackToSavepoint(CurrentContext.GetType(), e);
+                    }
                 }
 
                 throw;
             }
             finally
             {
-                if (createdSavepoint)
-                {
-                    transaction.ReleaseSavepoint(SavepointName);
-                }
-                else if (beganTransaction)
+                if (beganTransaction)
                 {
                     transaction.Dispose();
                 }
                 else
                 {
+                    if (createdSavepoint)
+                    {
+                        if (connection.DbConnection.State == ConnectionState.Open)
+                        {
+                            try
+                            {
+                                transaction.ReleaseSavepoint(SavepointName);
+                            }
+                            catch (Exception e)
+                            {
+                                UpdateLogger.LogBatchExecutorFailedToReleaseSavepoint(CurrentContext.GetType(), e);
+                            }
+                        }
+                    }
+
                     connection.Close();
                 }
             }
@@ -172,25 +201,43 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
             }
             catch
             {
-                if (createdSavepoint)
+                if (createdSavepoint && connection.DbConnection.State == ConnectionState.Open)
                 {
-                    await transaction.RollbackToSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        await transaction.RollbackToSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        UpdateLogger.LogBatchExecutorFailedToRollbackToSavepoint(CurrentContext.GetType(), e);
+                    }
                 }
 
                 throw;
             }
             finally
             {
-                if (createdSavepoint)
-                {
-                    await transaction.ReleaseSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
-                }
-                else if (beganTransaction)
+                if (beganTransaction)
                 {
                     await transaction.DisposeAsync().ConfigureAwait(false);
                 }
                 else
                 {
+                    if (createdSavepoint)
+                    {
+                        if (connection.DbConnection.State == ConnectionState.Open)
+                        {
+                            try
+                            {
+                                await transaction.ReleaseSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                            }
+                            catch (Exception e)
+                            {
+                                UpdateLogger.LogBatchExecutorFailedToReleaseSavepoint(CurrentContext.GetType(), e);
+                            }
+                        }
+                    }
+
                     await connection.CloseAsync().ConfigureAwait(false);
                 }
             }

dotnet/efcore

src/EFCore.Relational/Diagnostics/RelationalEventId.cs
@@ -88,7 +88,9 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics
 
             // Update events
             BatchReadyForExecution = CoreEventId.RelationalBaseId + 700,
-            BatchSmallerThanMinBatchSize
+            BatchSmallerThanMinBatchSize,
+            BatchExecutorFailedToRollbackToSavepoint,
+            BatchExecutorFailedToReleaseSavepoint,
         }
 
         private static readonly string _connectionPrefix = DbLoggerCategory.Database.Connection.Name + ".";
@@ -720,5 +722,25 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics
         ///     </para>
         /// </summary>
         public static readonly EventId BatchSmallerThanMinBatchSize = MakeUpdateId(Id.BatchSmallerThanMinBatchSize);
+
+        /// <summary>
+        ///     <para>
+        ///         An error occurred while the batch executor was rolling back the transaction to a savepoint, after an exception occured.
+        ///     </para>
+        ///     <para>
+        ///         This event is in the <see cref="DbLoggerCategory.Update" /> category.
+        ///     </para>
+        /// </summary>
+        public static readonly EventId BatchExecutorFailedToRollbackToSavepoint = MakeUpdateId(Id.BatchExecutorFailedToRollbackToSavepoint);
+
+        /// <summary>
+        ///     <para>
+        ///         An error occurred while the batch executor was releasing a transaction savepoint.
+        ///     </para>
+        ///     <para>
+        ///         This event is in the <see cref="DbLoggerCategory.Update" /> category.
+        ///     </para>
+        /// </summary>
+        public static readonly EventId BatchExecutorFailedToReleaseSavepoint = MakeUpdateId(Id.BatchExecutorFailedToReleaseSavepoint);
     }
 }

src/EFCore.Relational/Properties/RelationalStrings.Designer.cs
@@ -1,3 +1,4 @@
+
 // <auto-generated />
 
 using System;
@@ -2191,6 +2192,54 @@ namespace Microsoft.EntityFrameworkCore.Diagnostics.Internal
                             level,
                             RelationalEventId.MultipleCollectionIncludeWarning,
                             _resourceManager.GetString("LogMultipleCollectionIncludeWarning"))));
+            }
+
+            return (EventDefinition)definition;
+        }
+
+        /// <summary>
+        ///     An error occurred while the batch executor was rolling back the transaction to a savepoint, after an exception occured.
+        /// </summary>
+        public static EventDefinition LogBatchExecutorFailedToRollbackToSavepoint([NotNull] IDiagnosticsLogger logger)
+        {
+            var definition = ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToRollbackToSavepoint;
+            if (definition == null)
+            {
+                definition = LazyInitializer.EnsureInitialized<EventDefinitionBase>(
+                    ref ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToRollbackToSavepoint,
+                    () => new EventDefinition(
+                        logger.Options,
+                        RelationalEventId.BatchExecutorFailedToRollbackToSavepoint,
+                        LogLevel.Debug,
+                        "RelationalEventId.BatchExecutorFailedToRollbackToSavepoint",
+                        level => LoggerMessage.Define(
+                            level,
+                            RelationalEventId.BatchExecutorFailedToRollbackToSavepoint,
+                            _resourceManager.GetString("LogBatchExecutorFailedToRollbackToSavepoint"))));
+            }
+
+            return (EventDefinition)definition;
+        }
+
+        /// <summary>
+        ///     An error occurred while the batch executor was releasing a transaction savepoint.
+        /// </summary>
+        public static EventDefinition LogBatchExecutorFailedToReleaseSavepoint([NotNull] IDiagnosticsLogger logger)
+        {
+            var definition = ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToReleaseSavepoint;
+            if (definition == null)
+            {
+                definition = LazyInitializer.EnsureInitialized<EventDefinitionBase>(
+                    ref ((RelationalLoggingDefinitions)logger.Definitions).LogBatchExecutorFailedToReleaseSavepoint,
+                    () => new EventDefinition(
+                        logger.Options,
+                        RelationalEventId.BatchExecutorFailedToReleaseSavepoint,
+                        LogLevel.Debug,
+                        "RelationalEventId.BatchExecutorFailedToReleaseSavepoint",
+                        level => LoggerMessage.Define(
+                            level,
+                            RelationalEventId.BatchExecutorFailedToReleaseSavepoint,
+                            _resourceManager.GetString("LogBatchExecutorFailedToReleaseSavepoint"))));
             }
 
             return (EventDefinition)definition;

src/EFCore.Relational/Update/Internal/BatchExecutor.cs
@@ -1,11 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Transactions;
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,9 +39,12 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
-        public BatchExecutor([NotNull] ICurrentDbContext currentContext)
+        public BatchExecutor(
+            [NotNull] ICurrentDbContext currentContext,
+            [NotNull] IDiagnosticsLogger<DbLoggerCategory.Update> updateLogger)
         {
             CurrentContext = currentContext;
+            UpdateLogger = updateLogger;
         }
 
         /// <summary>
@@ -48,6 +54,11 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public virtual ICurrentDbContext CurrentContext { get; }
+
+        /// <summary>
+        ///     The logger.
+        /// </summary>
+        protected virtual IDiagnosticsLogger<DbLoggerCategory.Update> UpdateLogger { get; }
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -97,25 +108,43 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
             }
             catch
             {
-                if (createdSavepoint)
+                if (createdSavepoint && connection.DbConnection.State == ConnectionState.Open)
                 {
-                    transaction.RollbackToSavepoint(SavepointName);
+                    try
+                    {
+                        transaction.RollbackToSavepoint(SavepointName);
+                    }
+                    catch (Exception e)
+                    {
+                        UpdateLogger.BatchExecutorFailedToRollbackToSavepoint(CurrentContext.GetType(), e);
+                    }
                 }
 
                 throw;
             }
             finally
             {
-                if (createdSavepoint)
-                {
-                    transaction.ReleaseSavepoint(SavepointName);
-                }
-                else if (beganTransaction)
+                if (beganTransaction)
                 {
                     transaction.Dispose();
                 }
                 else
                 {
+                    if (createdSavepoint)
+                    {
+                        if (connection.DbConnection.State == ConnectionState.Open)
+                        {
+                            try
+                            {
+                                transaction.ReleaseSavepoint(SavepointName);
+                            }
+                            catch (Exception e)
+                            {
+                                UpdateLogger.BatchExecutorFailedToReleaseSavepoint(CurrentContext.GetType(), e);
+                            }
+                        }
+                    }
+
                     connection.Close();
                 }
             }
@@ -172,25 +201,43 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
             }
             catch
             {
-                if (createdSavepoint)
+                if (createdSavepoint && connection.DbConnection.State == ConnectionState.Open)
                 {
-                    await transaction.RollbackToSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        await transaction.RollbackToSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        UpdateLogger.BatchExecutorFailedToRollbackToSavepoint(CurrentContext.GetType(), e);
+                    }
                 }
 
                 throw;
             }
             finally
             {
-                if (createdSavepoint)
-                {
-                    await transaction.ReleaseSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
-                }
-                else if (beganTransaction)
+                if (beganTransaction)
                 {
                     await transaction.DisposeAsync().ConfigureAwait(false);
                 }
                 else
                 {
+                    if (createdSavepoint)
+                    {
+                        if (connection.DbConnection.State == ConnectionState.Open)
+                        {
+                            try
+                            {
+                                await transaction.ReleaseSavepointAsync(SavepointName, cancellationToken).ConfigureAwait(false);
+                            }
+                            catch (Exception e)
+                            {
+                                UpdateLogger.BatchExecutorFailedToReleaseSavepoint(CurrentContext.GetType(), e);
+                            }
+                        }
+                    }
+
                     await connection.CloseAsync().ConfigureAwait(false);
                 }
             }