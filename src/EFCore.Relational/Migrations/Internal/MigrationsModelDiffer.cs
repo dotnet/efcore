cd01806fe4ee61832508085ab1c57203eddd3b55
Christopher Haws <cribs2@gmail.com>
2020-07-20 23:39:27 -0700

    Fix issue where property with value generated on add is reseeded in every migration
    Fixes #21661

--- src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
+++ src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
@@ -1913,7 +1913,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Internal
 
                         foreach (var targetProperty in entry.EntityType.GetProperties())
                         {
-                            if (targetProperty.ValueGenerated != ValueGenerated.Never)
+                            if (!(targetProperty.ValueGenerated == ValueGenerated.Never || targetProperty.ValueGenerated == ValueGenerated.OnAdd))
                             {
                                 continue;
                             }
