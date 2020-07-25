59122460a8bc2d7230b98190af3a81f32853ddfe
Brice Lambson <brice@bricelam.net>
2020-07-25 10:52:17 -0700

    SQLite Migrations: Test table rebuilds (#21783)

--- test/EFCore.Sqlite.Tests/Migrations/SqliteMigrationsSqlGeneratorTest.cs
+++ test/EFCore.Sqlite.Tests/Migrations/SqliteMigrationsSqlGeneratorTest.cs
@@ -614,6 +614,425 @@ SELECT changes();
 ");
         }
 
+        [ConditionalFact]
+        public virtual void AddPrimaryKey_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new AddPrimaryKeyOperation
+                    {
+                        Table = "Blogs",
+                        Name = "PK_Blogs",
+                        Columns = new[] { "Id" }
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("AddPrimaryKeyOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void AddUniqueConstraint_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new AddUniqueConstraintOperation
+                    {
+                        Table = "Blogs",
+                        Name = "AK_Blogs_Uri",
+                        Columns = new[] { "Uri" }
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("AddUniqueConstraintOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void AddCheckConstraint_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new AddCheckConstraintOperation
+                    {
+                        Table = "Blogs",
+                        Name = "CK_Blogs_Rating",
+                        Sql = "Rating BETWEEN 1 AND 5"
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("AddCheckConstraintOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void AlterTable_mostly_works_when_no_model()
+        {
+            Generate(
+                new AlterTableOperation
+                {
+                    Name = "Blogs",
+                    Comment = "The Blogs table"
+                });
+
+            Assert.Empty(Sql);
+        }
+
+        [ConditionalFact]
+        public virtual void DropForeignKey_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new DropForeignKeyOperation
+                    {
+                        Table = "Posts",
+                        Name = "FK_Posts_BlogId"
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("DropForeignKeyOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void DropPrimaryKey_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new DropPrimaryKeyOperation
+                    {
+                        Table = "Blogs",
+                        Name = "PK_Blogs"
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("DropPrimaryKeyOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void DropUniqueConstraint_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new DropUniqueConstraintOperation
+                    {
+                        Table = "Blogs",
+                        Name = "AK_Blogs_Uri"
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("DropUniqueConstraintOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void DropColumn_throws_when_no_model()
+        {
+            var ex = Assert.Throws<NotSupportedException>(
+                () => Generate(
+                    new DropColumnOperation
+                    {
+                        Table = "Posts",
+                        Name = "Rating"
+                    }));
+
+            Assert.Equal(SqliteStrings.InvalidMigrationOperation("DropColumnOperation"), ex.Message);
+        }
+
+        [ConditionalFact]
+        public virtual void AddColumnOperation_with_comment_mostly_works_when_no_model()
+        {
+            Generate(
+                new AddColumnOperation
+                {
+                    Table = "Blogs",
+                    ClrType = typeof(string),
+                    Name = "Summary",
+                    Comment = "A short description"
+                });
+
+            AssertSql(
+                @"ALTER TABLE ""Blogs"" ADD ""Summary"" TEXT NOT NULL;
+");
+        }
+
+        [ConditionalFact]
+        public virtual void DropColumn_defers_subsequent_RenameColumn()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity("Blog").Property<string>("Name"),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.RenameColumn(
+                        name: "Title",
+                        table: "Blog",
+                        newName: "Name");
+                });
+
+            AssertSql(
+                @"CREATE TABLE ""ef_temp_Blog"" (
+    ""Name"" TEXT NULL
+);
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Name"")
+SELECT ""Title""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+");
+        }
+
+        [ConditionalFact]
+        public virtual void Deferred_RenameColumn_defers_subsequent_AddColumn()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity(
+                    "Blog", x =>
+                    {
+                        x.Property<string>("Title");
+                        x.Property<string>("Name");
+                    }),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.RenameColumn(
+                        name: "Title",
+                        table: "Blog",
+                        newName: "Name");
+                    migrationBuilder.AddColumn<string>(
+                        name: "Title",
+                        table: "Blog",
+                        nullable: true);
+                });
+
+            AssertSql(
+                @"CREATE TABLE ""ef_temp_Blog"" (
+    ""Name"" TEXT NULL,
+    ""Title"" TEXT NULL
+);
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Name"")
+SELECT ""Title""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+");
+        }
+
+        [ConditionalFact]
+        public virtual void Deferred_RenameColumn_defers_subsequent_CreateIndex_unique()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity(
+                    "Blog", x =>
+                    {
+                        x.Property<string>("Name");
+                        x.HasIndex("Name").IsUnique();
+                    }),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.RenameColumn(
+                        name: "Title",
+                        table: "Blog",
+                        newName: "Name");
+                    migrationBuilder.CreateIndex(
+                        name: "IX_Blog_Name",
+                        table: "Blog",
+                        column: "Name",
+                        unique: true);
+                });
+
+            AssertSql(
+                @"CREATE TABLE ""ef_temp_Blog"" (
+    ""Name"" TEXT NULL
+);
+GO
+
+CREATE UNIQUE INDEX ""IX_Blog_Name"" ON ""ef_temp_Blog"" (""Name"");
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Name"")
+SELECT ""Title""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+");
+        }
+
+        [ConditionalFact]
+        public virtual void DropColumn_defers_subsequent_AddColumn_required()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity(
+                    "Blog",
+                    x =>
+                    {
+                        x.Property<int>("Id");
+                        x.Property<string>("Name").IsRequired();
+                    }),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.AddColumn<string>(
+                        name: "Name",
+                        table: "Blog",
+                        nullable: false,
+                        defaultValue: "Overridden");
+                });
+
+            AssertSql(
+                @"CREATE TABLE ""ef_temp_Blog"" (
+    ""Id"" INTEGER NOT NULL CONSTRAINT ""PK_Blog"" PRIMARY KEY AUTOINCREMENT,
+    ""Name"" TEXT NOT NULL DEFAULT 'Overridden'
+);
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Id"")
+SELECT ""Id""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+");
+        }
+
+        [ConditionalFact]
+        public virtual void Deferred_AddColumn_defers_subsequent_CreateIndex()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity(
+                    "Blog",
+                    x =>
+                    {
+                        x.Property<int>("Id");
+                        x.Property<string>("Name");
+                        x.HasIndex("Name");
+                    }),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.AddColumn<string>(
+                        name: "Name",
+                        table: "Blog");
+                    migrationBuilder.CreateIndex(
+                        name: "IX_Blog_Name",
+                        table: "Blog",
+                        column: "Name");
+
+                });
+
+            AssertSql(
+                @"CREATE TABLE ""ef_temp_Blog"" (
+    ""Id"" INTEGER NOT NULL CONSTRAINT ""PK_Blog"" PRIMARY KEY AUTOINCREMENT,
+    ""Name"" TEXT NULL
+);
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Id"")
+SELECT ""Id""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+GO
+
+CREATE INDEX ""IX_Blog_Name"" ON ""Blog"" (""Name"");
+");
+        }
+
+        [ConditionalFact]
+        public virtual void RenameTable_preserves_pending_rebuilds()
+        {
+            Generate(
+                modelBuilder => modelBuilder.Entity("Blog").Property<int>("Id"),
+                migrationBuilder =>
+                {
+                    migrationBuilder.DropColumn(
+                        name: "Name",
+                        table: "Blogs");
+                    migrationBuilder.RenameTable(
+                        name: "Blogs",
+                        newName: "Blog");
+                });
+
+            AssertSql(
+                @"ALTER TABLE ""Blogs"" RENAME TO ""Blog"";
+GO
+
+CREATE TABLE ""ef_temp_Blog"" (
+    ""Id"" INTEGER NOT NULL CONSTRAINT ""PK_Blog"" PRIMARY KEY AUTOINCREMENT
+);
+GO
+
+INSERT INTO ""ef_temp_Blog"" (""Id"")
+SELECT ""Id""
+FROM Blog;
+GO
+
+PRAGMA foreign_keys = 0;
+GO
+
+DROP TABLE ""Blog"";
+GO
+
+ALTER TABLE ""ef_temp_Blog"" RENAME TO ""Blog"";
+GO
+
+PRAGMA foreign_keys = 1;
+");
+        }
+
         public SqliteMigrationsSqlGeneratorTest()
             : base(SqliteTestHelpers.Instance,
                   new ServiceCollection().AddEntityFrameworkSqliteNetTopologySuite(),
@@ -622,6 +1041,14 @@ SELECT changes();
                     new SqliteDbContextOptionsBuilder(new DbContextOptionsBuilder()).UseNetTopologySuite())
                   .OptionsBuilder).Options)
         {
+        }
+
+        protected virtual void Generate(Action<ModelBuilder> buildAction, Action<MigrationBuilder> migrateAction)
+        {
+            var migrationBuilder = new MigrationBuilder(activeProvider: null);
+            migrateAction(migrationBuilder);
+
+            Generate(buildAction, migrationBuilder.Operations.ToArray());
         }
     }
 }
