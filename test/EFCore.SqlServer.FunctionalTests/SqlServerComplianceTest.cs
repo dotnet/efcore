e2f72b60c3580208bb95fee63158bf4c70d15522
Smit Patel <smitpatel@users.noreply.github.com>
2020-07-30 21:13:57 -0700

    Skip flaky tests (#21872)

--- test/EFCore.SqlServer.FunctionalTests/SqlServerComplianceTest.cs
+++ test/EFCore.SqlServer.FunctionalTests/SqlServerComplianceTest.cs
@@ -1,12 +1,19 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Microsoft.EntityFrameworkCore
 {
     public class SqlServerComplianceTest : RelationalComplianceTestBase
     {
+        protected override ICollection<Type> IgnoredTestBases => new List<Type>
+        {
+            typeof(SaveChangesInterceptionTestBase)
+        };
+
         protected override Assembly TargetAssembly { get; } = typeof(SqlServerComplianceTest).Assembly;
     }
 }
