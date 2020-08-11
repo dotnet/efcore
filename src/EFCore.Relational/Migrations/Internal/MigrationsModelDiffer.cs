3514b515107381159797bf1582d460c93fc1314e
Shay Rojansky <roji@roji.org>
2020-08-11 16:41:06 +0300

    Suppress one more new warning

--- src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
+++ src/EFCore.Relational/Migrations/Internal/MigrationsModelDiffer.cs
@@ -139,7 +139,9 @@ namespace Microsoft.EntityFrameworkCore.Migrations.Internal
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
+#pragma warning disable EF1001 // Internal EF Core API usage.
         protected virtual IChangeDetector ChangeDetector { get; }
+#pragma warning restore EF1001 // Internal EF Core API usage.
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
