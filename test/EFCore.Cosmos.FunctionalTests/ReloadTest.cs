e75996402b53efeb4eb23b1e66b68a08448333b2
Alexander Fanat <alex.fanat@smus.ca>
2020-08-20 11:26:56 -0700

    Add test.

---
+++ test/EFCore.Cosmos.FunctionalTests/ReloadTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Xunit;
+using static Microsoft.EntityFrameworkCore.Cosmos.Storage.Internal.CosmosDatabaseCreatorTest;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos
+{
+    public class ReloadTest
+    {
+        public static IEnumerable<object[]> IsAsyncData = new[]
+        {
+            new object[] { true },
+            new object[] { false }
+        };
+
+        [Theory]
+        [MemberData(nameof(IsAsyncData))]
+        public async Task Entity_reference_can_be_reloaded(bool async)
+        {
+            await using var testDatabase = CosmosTestStore.CreateInitialized("Database");
+
+            using var context = new BloggingContext(testDatabase);
+            await context.Database.EnsureCreatedAsync();
+
+            var entry = context.Add(new Blog { Id = 1337 });
+
+            context.SaveChanges();
+
+            if (async)
+            {
+                await entry.ReloadAsync();
+            }
+            else
+            {
+                entry.Reload();
+            }
+        }
+    }
+}
