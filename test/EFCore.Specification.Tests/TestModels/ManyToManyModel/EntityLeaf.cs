8e3f8724cfe78ac5b613d0913179599e64cd4483
Arthur Vickers <ajcvickers@hotmail.com>
2020-09-08 09:37:42 -0700

    Automatically create notification collections when change-tracking proxies are being used (#22438)
    Fixes #21684

--- test/EFCore.Specification.Tests/TestModels/ManyToManyModel/EntityLeaf.cs
+++ test/EFCore.Specification.Tests/TestModels/ManyToManyModel/EntityLeaf.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyModel
 {
@@ -10,10 +9,7 @@ namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyModel
     {
         public virtual bool? IsGreen { get; set; }
 
-        public virtual ICollection<EntityCompositeKey> CompositeKeySkipFull { get; }
-            = new ObservableCollection<EntityCompositeKey>(); // #21684
-
-        public virtual ICollection<JoinCompositeKeyToLeaf> JoinCompositeKeyFull { get; }
-            = new ObservableCollection<JoinCompositeKeyToLeaf>(); // #21684
+        public virtual ICollection<EntityCompositeKey> CompositeKeySkipFull { get; set; }
+        public virtual ICollection<JoinCompositeKeyToLeaf> JoinCompositeKeyFull { get; set; }
     }
 }
--- test/EFCore.Specification.Tests/TestModels/ManyToManyModel/EntityOne.cs
+++ test/EFCore.Specification.Tests/TestModels/ManyToManyModel/EntityOne.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyModel
@@ -13,33 +12,20 @@ namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyModel
         public virtual string Name { get; set; }
 
         public virtual EntityTwo Reference { get; set; }
-        public virtual ICollection<EntityTwo> Collection { get; } = new ObservableCollection<EntityTwo>(); // #21684
-
-        public virtual ICollection<EntityTwo> TwoSkip { get; } = new ObservableCollection<EntityTwo>(); // #21684
-
-        public virtual ICollection<EntityThree> ThreeSkipPayloadFull { get; } = new ObservableCollection<EntityThree>(); // #21684
-
-        public virtual ICollection<JoinOneToThreePayloadFull> JoinThreePayloadFull { get; }
-            = new ObservableCollection<JoinOneToThreePayloadFull>(); // #21684
+        public virtual ICollection<EntityTwo> Collection { get; set; }
+        public virtual ICollection<EntityTwo> TwoSkip { get; set; }
+        public virtual ICollection<EntityThree> ThreeSkipPayloadFull { get; set; }
+        public virtual ICollection<JoinOneToThreePayloadFull> JoinThreePayloadFull { get; set; }
 
         [InverseProperty("OneSkipShared")]
-        public virtual ICollection<EntityTwo> TwoSkipShared { get; } = new ObservableCollection<EntityTwo>(); // #21684
+        public virtual ICollection<EntityTwo> TwoSkipShared { get; set; }
 
-        public virtual ICollection<EntityThree> ThreeSkipPayloadFullShared { get; } = new ObservableCollection<EntityThree>(); // #21684
-
-        public virtual ICollection<Dictionary<string, object>> JoinThreePayloadFullShared { get; }
-            = new ObservableCollection<Dictionary<string, object>>(); // #21684
-
-        public virtual ICollection<EntityOne> SelfSkipPayloadLeft { get; } = new ObservableCollection<EntityOne>(); // #21684
-
-        public virtual ICollection<JoinOneSelfPayload> JoinSelfPayloadLeft { get; }
-            = new ObservableCollection<JoinOneSelfPayload>(); // #21684
-
-        public virtual ICollection<EntityOne> SelfSkipPayloadRight { get; } = new ObservableCollection<EntityOne>(); // #21684
-
-        public virtual ICollection<JoinOneSelfPayload> JoinSelfPayloadRight { get; }
-            = new ObservableCollection<JoinOneSelfPayload>(); // #21684
-
-        public virtual ICollection<EntityBranch> BranchSkip { get; } = new ObservableCollection<EntityBranch>(); // #21684
+        public virtual ICollection<EntityThree> ThreeSkipPayloadFullShared { get; set; }
+        public virtual ICollection<Dictionary<string, object>> JoinThreePayloadFullShared { get; set; }
+        public virtual ICollection<EntityOne> SelfSkipPayloadLeft { get; set; }
+        public virtual ICollection<JoinOneSelfPayload> JoinSelfPayloadLeft { get; set; }
+        public virtual ICollection<EntityOne> SelfSkipPayloadRight { get; set; }
+        public virtual ICollection<JoinOneSelfPayload> JoinSelfPayloadRight { get; set; }
+        public virtual ICollection<EntityBranch> BranchSkip { get; set; }
     }
 }
