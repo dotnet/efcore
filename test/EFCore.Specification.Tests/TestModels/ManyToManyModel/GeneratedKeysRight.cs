89f0e61e1776505fcced79ccd83a1831305501d4
Arthur Vickers <ajcvickers@hotmail.com>
2020-09-22 13:23:01 -0700

    Mark join entities as Added when either side is Added (#22669)
    Fixes #22647
    
    **Description**
    
    If the entity on either end of a many-to-many relationship is Added, then any join entity instance must be new since it references a new entity via a required relationship. This change improves the many-to-many experience by detecting this case.
    
    **Customer Impact**
    
    This is an improvement to the experience for the new many-to-many feature. Doing this now instead of in the future avoids a breaking change after 5.0. Therefore, doing this now will help customers have a seamless update to 6.0.
    
    **How found**
    
    Customer reported on RC1.
    
    **Test coverage**
    
    Added additional test coverage around this case.
    
    **Regression?**
    
    No; new feature.
    
    **Risk**
    
    Low; small change in behavior.

---
+++ test/EFCore.Specification.Tests/TestModels/ManyToManyModel/GeneratedKeysRight.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyModel
+{
+    public class GeneratedKeysRight
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual ICollection<GeneratedKeysLeft> Lefts { get; } = new ObservableCollection<GeneratedKeysLeft>();
+    }
+}
