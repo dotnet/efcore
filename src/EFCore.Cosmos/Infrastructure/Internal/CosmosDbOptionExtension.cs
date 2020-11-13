dbe4b00b6bca0ed921bc6de49888f1584c084679
Umit Kavala <u.kavala@gmail.com>
2020-11-13 22:32:40 +0100

    Perf: unset EnableContentResponseOnWrite dotnet#22999

--- src/EFCore.Cosmos/Infrastructure/Internal/CosmosDbOptionExtension.cs
+++ src/EFCore.Cosmos/Infrastructure/Internal/CosmosDbOptionExtension.cs
@@ -39,6 +39,7 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Infrastructure.Internal
         private int? _gatewayModeMaxConnectionLimit;
         private int? _maxTcpConnectionsPerEndpoint;
         private int? _maxRequestsPerTcpConnection;
+        private bool? _enableContentResponseOnWrite;
         private DbContextOptionsExtensionInfo _info;
 
         /// <summary>
@@ -437,6 +438,30 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Infrastructure.Internal
             var clone = Clone();
 
             clone._maxRequestsPerTcpConnection = requestLimit;
+
+            return clone;
+        }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual bool? EnableContentResponseOnWrite
+            => _enableContentResponseOnWrite;
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual CosmosOptionsExtension ContentResponseOnWriteEnabled(bool enabled)
+        {
+            var clone = Clone();
+
+            clone._enableContentResponseOnWrite = enabled;
 
             return clone;
         }
