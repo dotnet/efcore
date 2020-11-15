1b5fed2b0ee360e8e49dc2d4e35fae60bdf3b2f3
cyril andreichuk <cyril.andreichuk@gmail.com>
2020-11-15 12:51:59 +0300

    added check for IImmutableSet<> interface
    fix #23123

--- src/Shared/MethodInfoExtensions.cs
+++ src/Shared/MethodInfoExtensions.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 #nullable enable
@@ -12,10 +13,44 @@ namespace System.Reflection
     internal static class MethodInfoExtensions
     {
         public static bool IsContainsMethod(this MethodInfo method)
-            => method.Name == nameof(IList.Contains)
-                && method.DeclaringType != null
-                && method.DeclaringType.GetInterfaces().Append(method.DeclaringType).Any(
-                    t => t == typeof(IList)
-                        || (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(ICollection<>)));
+        {
+            if (method.Name != nameof(IList.Contains))
+            {
+                return false;
+            }
+
+            if (method.DeclaringType is null)
+            {
+                return false;
+            }
+
+            return
+                method.DeclaringType
+                    .GetInterfaces()
+                    .Append(method.DeclaringType)
+                    .Any(t => IsCollection(t));
+        }
+
+        private static bool IsCollection(Type type)
+        {
+            if (type == typeof(IList))
+            {
+                return true;
+            }
+
+            if (type.IsGenericType == false)
+            {
+                return false;
+            }
+
+            var genericType = type.GetGenericTypeDefinition();
+
+            if (genericType == typeof(ICollection<>) || genericType == typeof(IImmutableSet<>))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
