3bd012b6dc645f6d4e14ddb473c42f3fa4e0b98b
the-wazz <wazztech@outlook.com>
2020-12-02 04:38:17 -0500

    Update Migration.cs (#23551)
    Two grammar/typos fixes. (Same typo in two different places.)

--- src/EFCore.Relational/Migrations/Migration.cs
+++ src/EFCore.Relational/Migrations/Migration.cs
@@ -96,7 +96,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
         ///         previous migration so that it is up-to-date with regard to this migration.
         ///     </para>
         ///     <para>
-        ///         This method must be overridden in each class the inherits from <see cref="Migration" />.
+        ///         This method must be overridden in each class that inherits from <see cref="Migration" />.
         ///     </para>
         /// </summary>
         /// <param name="migrationBuilder"> The <see cref="MigrationBuilder" /> that will build the operations. </param>
@@ -111,7 +111,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
         ///         this migration so that it returns to the state that it was in before this migration was applied.
         ///     </para>
         ///     <para>
-        ///         This method must be overridden in each class the inherits from <see cref="Migration" /> if
+        ///         This method must be overridden in each class that inherits from <see cref="Migration" /> if
         ///         both 'up' and 'down' migrations are to be supported. If it is not overridden, then calling it
         ///         will throw and it will not be possible to migrate in the 'down' direction.
         ///     </para>
