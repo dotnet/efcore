fcfab10dc8422b3d289079dbc31b4318234440f6
Felice Pollano <felice@felicepollano.com>
2020-12-27 10:58:45 +0100

    added unit test for #23569

--- test/EFCore.InMemory.FunctionalTests/OptimisticConcurrencyInMemoryTest.cs
+++ test/EFCore.InMemory.FunctionalTests/OptimisticConcurrencyInMemoryTest.cs
@@ -4,6 +4,7 @@
 using System.Threading.Tasks;
 using Xunit;
 using Microsoft.EntityFrameworkCore.TestModels.ConcurrencyModel;
+using System.Linq;
 
 namespace Microsoft.EntityFrameworkCore
 {
@@ -31,7 +32,7 @@ namespace Microsoft.EntityFrameworkCore
             : base(fixture)
         {
         }
-
+       
         [ConditionalFact(Skip = "Optimistic Offline Lock #2195")]
         public override Task Simple_concurrency_exception_can_be_resolved_with_store_values()
             => Task.FromResult(true);
@@ -78,7 +79,7 @@ namespace Microsoft.EntityFrameworkCore
         public override Task Two_concurrency_issues_in_one_to_one_related_entities_can_be_handled_by_dealing_with_dependent_first()
             => Task.FromResult(true);
 
-        [Fact]
+        [ConditionalFact]
         public override async Task Adding_the_same_entity_twice_results_in_DbUpdateException()
         {
             using var c = CreateF1Context();
@@ -106,20 +107,61 @@ namespace Microsoft.EntityFrameworkCore
 
                     await innerContext.SaveChangesAsync();
 
-                    await Assert.ThrowsAnyAsync<DbUpdateException>(() => context.SaveChangesAsync());
+                    await Assert.ThrowsAnyAsync<DbUpdateConcurrencyException>(() => context.SaveChangesAsync());
                 });
-            
         }
-        [ConditionalFact(Skip = "Throw DbUpdateException or DbUpdateConcurrencyException for in-memory database errors #23569")]
-        public override Task Deleting_the_same_entity_twice_results_in_DbUpdateConcurrencyException()
+        [ConditionalFact]
+        public override async Task Deleting_the_same_entity_twice_results_in_DbUpdateConcurrencyException()
         {
-            return Task.FromResult(true);
+            const string DRIVER="Fernando Alonso";
+            using var c = CreateF1Context();
+            await c.Database.CreateExecutionStrategy().ExecuteAsync(
+                c, async context =>
+                {
+                    using var prepareTransaction = BeginTransaction(context.Database);
+                    context.Drivers.Add(
+                        new Driver(){ Name=DRIVER}
+                    );
+                    await context.SaveChangesAsync();
+                    using var transaction = BeginTransaction(context.Database);
+                    context.Drivers.Remove(context.Drivers.Single(d => d.Name == DRIVER));
+                        
+
+                    using var innerContext = CreateF1Context();
+                    UseTransaction(innerContext.Database, transaction);
+                    context.Drivers.Remove(innerContext.Drivers.Single(d => d.Name == DRIVER));
+
+                    await innerContext.SaveChangesAsync();
+
+                    await Assert.ThrowsAnyAsync<DbUpdateConcurrencyException>(() => context.SaveChangesAsync());
+                });
         }
 
-        [ConditionalFact(Skip = "Throw DbUpdateException or DbUpdateConcurrencyException for in-memory database errors #23569")]
-        public override Task Deleting_then_updating_the_same_entity_results_in_DbUpdateConcurrencyException()
+        [ConditionalFact]
+        public override async Task Deleting_then_updating_the_same_entity_results_in_DbUpdateConcurrencyException()
         {
-            return Task.FromResult(true);
+            const string DRIVER="Fernando Alonso";
+            using var c = CreateF1Context();
+            await c.Database.CreateExecutionStrategy().ExecuteAsync(
+                c, async context =>
+                {
+                    using var prepareTransaction = BeginTransaction(context.Database);
+                    context.Drivers.Add(
+                        new Driver(){ Name=DRIVER}
+                    );
+                    await context.SaveChangesAsync();
+                    using var transaction = BeginTransaction(context.Database);
+                    context.Drivers.Remove(context.Drivers.Single(d => d.Name == DRIVER));
+                        
+
+                    using var innerContext = CreateF1Context();
+                    UseTransaction(innerContext.Database, transaction);
+                    var driver = context.Drivers.Single(d => d.Name == DRIVER);
+                    driver.Name="Felix";
+                    await innerContext.SaveChangesAsync();
+
+                    await Assert.ThrowsAnyAsync<DbUpdateConcurrencyException>(() => context.SaveChangesAsync());
+                });
         }
     }
 }
