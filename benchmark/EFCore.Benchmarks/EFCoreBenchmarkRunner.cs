123d4dfa6f97d8ede40c9ff4e59ed4501c44776d
Sébastien Ros <sebastienros@gmail.com>
2021-01-06 15:41:28 -0800

    Fix benchmarks

--- benchmark/EFCore.Benchmarks/EFCoreBenchmarkRunner.cs
+++ benchmark/EFCore.Benchmarks/EFCoreBenchmarkRunner.cs
@@ -1,17 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
 using System.Reflection;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Horology;
-using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 
 namespace Microsoft.EntityFrameworkCore.Benchmarks
@@ -20,14 +11,6 @@ namespace Microsoft.EntityFrameworkCore.Benchmarks
     {
         public static void Run(string[] args, Assembly assembly, IConfig config = null)
         {
-//             if (config == null)
-//             {
-//                 config = DefaultConfig.Instance;
-//             }
-
-//             config = config.With(DefaultConfig.Instance.GetDiagnosers().Concat(new[] { MemoryDiagnoser.Default }).ToArray());
-
-//             BenchmarkSwitcher.FromAssembly(assembly).Run(args, config);
             BenchmarkSwitcher.FromAssembly(assembly).Run(args);
         }
     }
