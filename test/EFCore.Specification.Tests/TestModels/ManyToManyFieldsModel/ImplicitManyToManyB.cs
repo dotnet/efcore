3437c75233d892008db03e1fd49a17743b262ca0
Arthur Vickers <ajcvickers@hotmail.com>
2021-04-03 15:03:28 -0700

    Allow explicit loading of field-only navigations (#24548)
    Fixes #23717

---
+++ test/EFCore.Specification.Tests/TestModels/ManyToManyFieldsModel/ImplicitManyToManyB.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.EntityFrameworkCore.TestModels.ManyToManyFieldsModel
+{
+    public class ImplicitManyToManyB
+    {
+        public int Id;
+        public string Name;
+
+        public ICollection<ImplicitManyToManyA> As = new ObservableCollection<ImplicitManyToManyA>();
+    }
+}
