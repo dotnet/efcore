72c2ac10aa9bd72da89b06f0f64d39af16fd98d1
Arthur Vickers <ajcvickers@hotmail.com>
2021-04-03 16:09:53 -0700

    Delay synthesis of join entities until entire graph is attached (#24572)
    Fixes #23339
    
    This stops the change tracker creating a join entity instance for the same join entity that is later discovered in the graph.

--- src/EFCore/ChangeTracking/Internal/StateManagerDependencies.cs
+++ src/EFCore/ChangeTracking/Internal/StateManagerDependencies.cs
@@ -78,7 +78,8 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
             ICoreSingletonOptions coreSingletonOptions,
             ILoggingOptions loggingOptions,
             IDiagnosticsLogger<DbLoggerCategory.Update> updateLogger,
-            IDiagnosticsLogger<DbLoggerCategory.ChangeTracking> changeTrackingLogger)
+            IDiagnosticsLogger<DbLoggerCategory.ChangeTracking> changeTrackingLogger,
+            INavigationFixer navigationFixer)
         {
             InternalEntityEntryFactory = internalEntityEntryFactory;
             InternalEntityEntrySubscriber = internalEntityEntrySubscriber;
@@ -96,6 +97,7 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
             LoggingOptions = loggingOptions;
             UpdateLogger = updateLogger;
             ChangeTrackingLogger = changeTrackingLogger;
+            NavigationFixer = navigationFixer;
         }
 
         /// <summary>
@@ -227,5 +229,13 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public IDiagnosticsLogger<DbLoggerCategory.ChangeTracking> ChangeTrackingLogger { get; init; }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public INavigationFixer NavigationFixer { get; init; }
     }
 }
