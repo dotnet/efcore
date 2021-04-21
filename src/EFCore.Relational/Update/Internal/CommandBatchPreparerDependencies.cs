216eada13cea470a2ff34d675ec5b3257eaa6a46
Kovalenko Dmitry <dmitry.lipetsk@gmail.com>
2021-04-21 19:09:31 +0300

    [correction] CommandBatchPreparerDependencies::ModificationCommandComparer returns IComparer<IModificationCommand>

--- src/EFCore.Relational/Update/Internal/CommandBatchPreparerDependencies.cs
+++ src/EFCore.Relational/Update/Internal/CommandBatchPreparerDependencies.cs
@@ -102,7 +102,7 @@ namespace Microsoft.EntityFrameworkCore.Update.Internal
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
-        public IComparer<ModificationCommand> ModificationCommandComparer { get; init; }
+        public IComparer<IModificationCommand> ModificationCommandComparer { get; init; }
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
