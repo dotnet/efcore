669929880afe1a21b354d5b99c19ac9beae500ca
Michał Czerwiński <mcr@spyro-soft.com>
2021-04-28 08:59:33 +0200

    SqlServer: Translate bool ToString
            - Adding boolean.ToString translation via CASE WHEN END
            - Adding test to cover a new feature
    
            Fixes #14205

---
+++ test/EFCore.SqlServer.Tests/Query/Boolean_ToString_Tests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class SqlServerObjectToStringTranslator_Tests
+    {
+        public class SimpleEntity
+        {
+            public long Id { get; set; }
+            
+            public bool IsEnabled { get; set; }
+        }
+
+        public class TestDbContext : DbContext
+        {
+            public DbSet<SimpleEntity> SimpleEntities { get; set; }
+
+            public TestDbContext(DbContextOptions dbContextOptions) :
+                base(dbContextOptions)
+            {
+            }
+        }
+
+        [ConditionalFact]
+        public void Test_boolean_ToString_translation()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseSqlServer("Database=Crunchie");
+
+            using var dbContext = new TestDbContext(optionsBuilder.Options);
+            
+            var actual = dbContext.SimpleEntities.Select(e => e.IsEnabled.ToString()).ToQueryString();
+            var expected =
+
+@"SELECT CASE
+    WHEN [s].[IsEnabled] = CAST(0 AS bit) THEN N'False'
+    ELSE N'True'
+END
+FROM [SimpleEntities] AS [s]";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
