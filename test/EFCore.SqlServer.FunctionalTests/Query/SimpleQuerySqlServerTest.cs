8799fc25d0e36f7640209f3a13fb926c240ae81a
Smit Patel <smitpatel@users.noreply.github.com>
2021-08-25 13:27:04 -0700

    Query: Add regression test for #24368
    Resolves #24368

---
+++ test/EFCore.SqlServer.FunctionalTests/Query/SimpleQuerySqlServerTest.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+// ReSharper disable InconsistentNaming
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class SimpleQuerySqlServerTest : SimpleQueryRelationalTestBase
+    {
+        protected override ITestStoreFactory TestStoreFactory => SqlServerTestStoreFactory.Instance;
+
+        public override async Task Multiple_nested_reference_navigations(bool async)
+        {
+            await base.Multiple_nested_reference_navigations(async);
+
+            AssertSql(
+                @"@__p_0='3'
+
+SELECT TOP(1) [s].[Id], [s].[Email], [s].[Logon], [s].[ManagerId], [s].[Name], [s].[SecondaryManagerId]
+FROM [Staff] AS [s]
+WHERE [s].[Id] = @__p_0",
+                //
+                @"@__id_0='1'
+
+SELECT TOP(2) [a].[Id], [a].[Complete], [a].[Deleted], [a].[PeriodEnd], [a].[PeriodStart], [a].[StaffId], [s].[Id], [s].[Email], [s].[Logon], [s].[ManagerId], [s].[Name], [s].[SecondaryManagerId], [s0].[Id], [s0].[Email], [s0].[Logon], [s0].[ManagerId], [s0].[Name], [s0].[SecondaryManagerId], [s1].[Id], [s1].[Email], [s1].[Logon], [s1].[ManagerId], [s1].[Name], [s1].[SecondaryManagerId]
+FROM [Appraisals] AS [a]
+INNER JOIN [Staff] AS [s] ON [a].[StaffId] = [s].[Id]
+LEFT JOIN [Staff] AS [s0] ON [s].[ManagerId] = [s0].[Id]
+LEFT JOIN [Staff] AS [s1] ON [s].[SecondaryManagerId] = [s1].[Id]
+WHERE [a].[Id] = @__id_0");
+        }
+    }
+}
