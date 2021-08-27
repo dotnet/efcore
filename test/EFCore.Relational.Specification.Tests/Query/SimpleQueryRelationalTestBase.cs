99a25a6ead6e0bec6358b82e200203eb497ab97f
Arthur Vickers <ajcvickers@hotmail.com>
2021-08-27 08:37:04 +0100

    Add back missing usings

--- test/EFCore.Relational.Specification.Tests/Query/SimpleQueryRelationalTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/SimpleQueryRelationalTestBase.cs
@@ -1,6 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Xunit;
 
