c6bfd032baa8c402396603e14c171dca896aab48
Smit Patel <smitpatel@users.noreply.github.com>
2021-09-17 13:11:30 -0700

    Add regression test for HasMany on keyless entity
    Resolves #24283

--- test/EFCore.Tests/ModelBuilding/TestModel.cs
+++ test/EFCore.Tests/ModelBuilding/TestModel.cs
@@ -1162,5 +1162,15 @@ namespace Microsoft.EntityFrameworkCore.ModelBuilding
         protected class DreJr : Dre
         {
         }
+
+        protected class Store
+        {
+            public int StoreId { get; set; }
+        }
+
+        protected class KeylessCollectionNavigation
+        {
+            public List<Store>? Stores { get; set; }
+        }
     }
 }
