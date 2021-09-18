ef68226896a946a35c5efd49b517fdd0a2ceb8c3
Arthur Vickers <ajcvickers@hotmail.com>
2021-09-18 07:52:37 +0100

    Adding grouping tests from EF6 (#26069)
    * Adding grouping tests from EF6
    
    As a crude measure of the gap between EF Core and EF6.
    
    Currently:
    - 12 tests pass
    - 2 tests fail with "Translation of 'Select' which contains grouping parameter without composition is not supported."
    - 17 tests fail with general "Could not be translated."
    - 8 tests fail with different results
    
    * Updated
    
    * Updates based on feedback.

---
+++ test/EFCore.InMemory.FunctionalTests/Query/Ef6GroupByInMemoryTest.cs
@@ -0,0 +1,99 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class Ef6GroupByInMemoryTest : Ef6GroupByTestBase<Ef6GroupByInMemoryTest.Ef6GroupByInMemoryFixture>
+    {
+        public Ef6GroupByInMemoryTest(Ef6GroupByInMemoryFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+        }
+
+        public override Task Average_Grouped_from_LINQ_101(bool async)
+        {
+            return AssertQuery(
+                async,
+                ss => from p in ss.Set<ProductForLinq>()
+                      group p by p.Category
+                      into g
+                      select new
+                      {
+                          Category = g.Key,
+                          AveragePrice = g.Average(p => p.UnitPrice)
+                      });
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_doesnt_produce_a_groupby_statement(bool async)
+        {
+            await base.Grouping_by_all_columns_doesnt_produce_a_groupby_statement(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_2(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_2(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_3(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_3(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_4(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_4(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_5(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_5(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_6(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_6(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_7(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_7(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_8(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_8(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_9(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_9(async);
+        }
+
+        [ConditionalTheory (Skip = "Issue #17653")]
+        public override async Task Grouping_by_all_columns_with_aggregate_function_works_10(bool async)
+        {
+            await base.Grouping_by_all_columns_with_aggregate_function_works_10(async);
+        }
+
+        public class Ef6GroupByInMemoryFixture : Ef6GroupByFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => InMemoryTestStoreFactory.Instance;
+        }
+    }
+}
