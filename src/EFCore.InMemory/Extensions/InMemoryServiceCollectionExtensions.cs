4c9de06081c9c8d3a4dcc954a7e8fbf34d2165c1
Weihan Li <weihanli@outlook.com>
2021-09-27 17:02:38 +0000

    Add simple in-memory provider configure extension

--- src/EFCore.InMemory/Extensions/InMemoryServiceCollectionExtensions.cs
+++ src/EFCore.InMemory/Extensions/InMemoryServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.ComponentModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -25,6 +26,113 @@ namespace Microsoft.Extensions.DependencyInjection
     /// </summary>
     public static class InMemoryServiceCollectionExtensions
     {
+        /// <summary>
+        ///     <para>
+        ///         Registers the given Entity Framework <see cref="DbContext" /> as a service in the <see cref="IServiceCollection" />
+        ///         and configures it to connect to an in-memory database.
+        ///     </para>
+        ///     <para>
+        ///         This method is a shortcut for configuring a <see cref="DbContext" /> to use in-memory database. It does not support all options.
+        ///         Use <see cref="M:EntityFrameworkServiceCollectionExtensions.AddDbContext" /> and related methods for full control of
+        ///         this process.
+        ///     </para>
+        ///     <para>
+        ///         Use this method when using dependency injection in your application, such as with ASP.NET Core.
+        ///         For applications that don't use dependency injection, consider creating <see cref="DbContext" />
+        ///         instances directly with its constructor. The <see cref="DbContext.OnConfiguring" /> method can then be
+        ///         overridden to configure the in-memory provider and connection string.
+        ///     </para>
+        ///     <para>
+        ///         To configure the <see cref="DbContextOptions{TContext}" /> for the context, either override the
+        ///         <see cref="DbContext.OnConfiguring" /> method in your derived context, or supply
+        ///         an optional action to configure the <see cref="DbContextOptions" /> for the context.
+        ///     </para>
+        ///     <para>
+        ///         See <see href="https://aka.ms/efcore-docs-di">Using DbContext with dependency injection</see> for more information.
+        ///     </para>
+        /// </summary>
+        /// <remarks>
+        ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+        ///     <see href="https://aka.ms/efcore-docs-in-memory">The EF Core in-memory database provider</see> for more information.
+        /// </remarks>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="databaseName">
+        ///     The name of the in-memory database. This allows the scope of the in-memory database to be controlled
+        ///     independently of the context. The in-memory database is shared anywhere the same name is used.
+        /// </param>
+        /// <param name="inMemoryOptionsAction">An optional action to allow additional in-memory specific configuration.</param>
+        /// <param name="optionsAction">An optional action to configure the <see cref="DbContextOptions" /> for the context.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
+        public static IServiceCollection AddInMemoryDatabase<TContext>(
+            this IServiceCollection serviceCollection,
+            string databaseName,
+            Action<InMemoryDbContextOptionsBuilder>? inMemoryOptionsAction = null,
+            Action<DbContextOptionsBuilder>? optionsAction = null)
+            where TContext : DbContext
+          => AddInMemoryDatabase<TContext>(serviceCollection, databaseName, null, inMemoryOptionsAction, optionsAction);
+
+        /// <summary>
+        ///     <para>
+        ///         Registers the given Entity Framework <see cref="DbContext" /> as a service in the <see cref="IServiceCollection" />
+        ///         and configures it to connect to an in-memory database.
+        ///     </para>
+        ///     <para>
+        ///         This method is a shortcut for configuring a <see cref="DbContext" /> to use in-memory database. It does not support all options.
+        ///         Use <see cref="M:EntityFrameworkServiceCollectionExtensions.AddDbContext" /> and related methods for full control of
+        ///         this process.
+        ///     </para>
+        ///     <para>
+        ///         Use this method when using dependency injection in your application, such as with ASP.NET Core.
+        ///         For applications that don't use dependency injection, consider creating <see cref="DbContext" />
+        ///         instances directly with its constructor. The <see cref="DbContext.OnConfiguring" /> method can then be
+        ///         overridden to configure the in-memory provider.
+        ///     </para>
+        ///     <para>
+        ///         To configure the <see cref="DbContextOptions{TContext}" /> for the context, either override the
+        ///         <see cref="DbContext.OnConfiguring" /> method in your derived context, or supply
+        ///         an optional action to configure the <see cref="DbContextOptions" /> for the context.
+        ///     </para>
+        ///     <para>
+        ///         See <see href="https://aka.ms/efcore-docs-di">Using DbContext with dependency injection</see> for more information.
+        ///     </para>
+        /// </summary>
+        /// <remarks>
+        ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+        ///     <see href="https://aka.ms/efcore-docs-in-memory">The EF Core in-memory database provider</see> for more information.
+        /// </remarks>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="databaseName">
+        ///     The name of the in-memory database. This allows the scope of the in-memory database to be controlled
+        ///     independently of the context. The in-memory database is shared anywhere the same name is used.
+        /// </param>
+        /// <param name="databaseRoot">
+        ///     All in-memory databases will be rooted in this object, allowing the application
+        ///     to control their lifetime. This is useful when sometimes the context instance
+        ///     is created explicitly with <see langword="new" /> while at other times it is resolved using dependency injection.
+        /// </param>
+        /// <param name="inMemoryOptionsAction">An optional action to allow additional in-memory specific configuration.</param>
+        /// <param name="optionsAction">An optional action to configure the <see cref="DbContextOptions" /> for the context.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
+        public static IServiceCollection AddInMemoryDatabase<TContext>(
+            this IServiceCollection serviceCollection,
+            string databaseName,
+            InMemoryDatabaseRoot? databaseRoot,
+            Action<InMemoryDbContextOptionsBuilder>? inMemoryOptionsAction = null,
+            Action<DbContextOptionsBuilder>? optionsAction = null
+            )
+            where TContext : DbContext
+        {
+            Check.NotNull(serviceCollection, nameof(serviceCollection));
+            Check.NotEmpty(databaseName, nameof(databaseName));
+
+            return serviceCollection.AddDbContext<TContext>(
+                (serviceProvider, options) =>
+                    {
+                        optionsAction?.Invoke(options);
+                        options.UseInMemoryDatabase(databaseName, databaseRoot, inMemoryOptionsAction);
+                    });
+        }
+
         /// <summary>
         ///     <para>
         ///         Adds the services required by the in-memory database provider for Entity Framework
