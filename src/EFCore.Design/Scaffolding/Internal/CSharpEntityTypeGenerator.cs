1b1e272ad0332596a5b1d4e2ece5757ee6c84ff9
Smit Patel <smitpatel@users.noreply.github.com>
2021-09-29 13:48:44 -0700

    RevEng: Initialize skip collection navigations in ctor
    Resolves #26206

--- src/EFCore.Design/Scaffolding/Internal/CSharpEntityTypeGenerator.cs
+++ src/EFCore.Design/Scaffolding/Internal/CSharpEntityTypeGenerator.cs
@@ -242,7 +242,10 @@ namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
         {
             Check.NotNull(entityType, nameof(entityType));
 
-            var collectionNavigations = entityType.GetNavigations().Where(n => n.IsCollection).ToList();
+            var collectionNavigations = entityType.GetNavigations().Where(n => n.IsCollection)
+                .Cast<INavigationBase>()
+                .Concat(entityType.GetSkipNavigations().Where(n => n.IsCollection))
+                .ToList();
 
             if (collectionNavigations.Count > 0)
             {
