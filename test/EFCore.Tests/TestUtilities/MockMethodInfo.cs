32f790c5aced3d1d2d0ca65c510e580853d581bb
Arthur Vickers <ajcvickers@hotmail.com>
2021-09-29 14:17:15 +0100

    Test changes to make sure applicationName is set when using new template pattern
    EF-side tests for https://github.com/dotnet/runtime/pull/59743
    
    These will fail until the new source package for HostFactoryResolver is available. I have tested locally that they work once this is done.

---
+++ test/EFCore.Tests/TestUtilities/MockMethodInfo.cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Microsoft.EntityFrameworkCore.TestUtilities
+{
+    public class MockMethodInfo : MethodInfo
+    {
+        private readonly Action<object[]> _invoke;
+
+        public MockMethodInfo(Type declaringType, Action<object[]> invoke = null)
+        {
+            _invoke = invoke;
+            DeclaringType = declaringType;
+        }
+
+        public override Type DeclaringType { get; }
+
+        public override ICustomAttributeProvider ReturnTypeCustomAttributes
+            => throw new NotImplementedException();
+
+        public override RuntimeMethodHandle MethodHandle
+            => throw new NotImplementedException();
+
+        public override MethodAttributes Attributes
+            => throw new NotImplementedException();
+
+        public override string Name
+            => throw new NotImplementedException();
+
+        public override Type ReflectedType
+            => throw new NotImplementedException();
+
+        public override MethodInfo GetBaseDefinition()
+            => throw new NotImplementedException();
+
+        public override object[] GetCustomAttributes(bool inherit)
+            => throw new NotImplementedException();
+
+        public override object[] GetCustomAttributes(Type attributeType, bool inherit)
+            => throw new NotImplementedException();
+
+        public override MethodImplAttributes GetMethodImplementationFlags()
+            => throw new NotImplementedException();
+
+        public override ParameterInfo[] GetParameters()
+            => new ParameterInfo[1];
+
+        public override object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture)
+        {
+            _invoke?.Invoke(parameters);
+
+            return null;
+        }
+        public override bool IsDefined(Type attributeType, bool inherit)
+            => throw new NotImplementedException();
+    }
+}
