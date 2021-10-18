6c2c153ae4a3f2efab9278d414656f639cd39047
Kev Ritchie <65573253+KevRitchie@users.noreply.github.com>
2021-10-18 20:11:36 +0100

    Add Migration name check

--- src/ef/Commands/MigrationsAddCommand.cs
+++ src/ef/Commands/MigrationsAddCommand.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections;
 using Microsoft.EntityFrameworkCore.Tools.Properties;
 
@@ -12,6 +13,11 @@ namespace Microsoft.EntityFrameworkCore.Tools.Commands
         protected override void Validate()
         {
             base.Validate();
+
+            if (string.Equals(_name!.Value, "migration", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new CommandException(Resources.CircularBaseClassDependency);
+            }
 
             if (string.IsNullOrEmpty(_name!.Value))
             {
