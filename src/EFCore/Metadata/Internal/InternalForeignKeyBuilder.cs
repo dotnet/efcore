f5d789529eefd886585a5e764da0cc71a13b8798
Andriy Svyryd <Andriy.Svyryd@microsoft.com>
2021-11-16 17:51:12 -0800

    Update the foreign key builder after IsRequired()
    Fixes #26611

--- src/EFCore/Metadata/Internal/InternalForeignKeyBuilder.cs
+++ src/EFCore/Metadata/Internal/InternalForeignKeyBuilder.cs
@@ -973,7 +973,13 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Internal
                 Metadata.DeclaringEntityType.Model.ScopedModelDependencies?.Logger.AmbiguousEndRequiredWarning(Metadata);
             }
 
-            Metadata.SetIsRequired(required, configurationSource);
+            IConventionForeignKey? foreignKey = Metadata;
+            var result = Metadata.DeclaringEntityType.Model.ConventionDispatcher.Track(
+                () => Metadata.SetIsRequired(required, configurationSource), ref foreignKey);
+            if (foreignKey != null)
+            {
+                return ((ForeignKey)foreignKey).Builder;
+            }
 
             return this;
         }
