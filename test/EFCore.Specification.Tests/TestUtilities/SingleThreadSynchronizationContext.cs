183830d0426fa43495d7986032e06de37048101f
Shay Rojansky <roji@roji.org>
2021-12-10 12:36:23 +0200

    Preserve synchronization context in ExecutionStrategy (#26953)
    Fixes #26763

---
+++ test/EFCore.Specification.Tests/TestUtilities/SingleThreadSynchronizationContext.cs
@@ -0,0 +1,43 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Concurrent;
+
+namespace Microsoft.EntityFrameworkCore.TestUtilities;
+
+public class SingleThreadSynchronizationContext : SynchronizationContext, IDisposable
+{
+    private readonly BlockingCollection<(SendOrPostCallback callback, object state)> _tasks = new();
+
+    public Thread Thread { get; }
+
+    public SingleThreadSynchronizationContext()
+    {
+        Thread = new Thread(WorkLoop);
+        Thread.Start();
+    }
+
+    public override void Post(SendOrPostCallback callback, object state)
+        => _tasks.Add((callback, state));
+
+    public void Dispose()
+        => _tasks.CompleteAdding();
+
+    private void WorkLoop()
+    {
+        SetSynchronizationContext(this);
+
+        try
+        {
+            while (true)
+            {
+                var (callback, state) = _tasks.Take();
+                callback(state);
+            }
+        }
+        catch (InvalidOperationException)
+        {
+            _tasks.Dispose();
+        }
+    }
+}
