8202ab2705d8ec190884a28a36a668715e18021e
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-12-14 19:05:59 +0100

    WithMany doc: fix the sample expression
    blog => blog.Posts

--- src/EFCore/Metadata/Builders/CollectionNavigationBuilder`.cs
+++ src/EFCore/Metadata/Builders/CollectionNavigationBuilder`.cs
@@ -112,7 +112,7 @@ public class CollectionNavigationBuilder<TEntity, TRelatedEntity> : CollectionNa
     /// </summary>
     /// <param name="navigationExpression">
     ///     A lambda expression representing the reference navigation property on the other end of this
-    ///     relationship (<c>post => post.Blog</c>). If no property is specified, the relationship will be
+    ///     relationship (<c>blog => blog.Posts</c>). If no property is specified, the relationship will be
     ///     configured without a navigation property on the other end of the relationship.
     /// </param>
     /// <returns>An object to further configure the relationship.</returns>
