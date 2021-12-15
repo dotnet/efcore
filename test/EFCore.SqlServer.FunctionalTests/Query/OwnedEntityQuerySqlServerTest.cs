d659dd87ac2321fe3509968f1d0a54437b27ee5c
Smit Patel <smitpatel@users.noreply.github.com>
2021-12-15 01:26:15 -0800

    [6.0.2] Query: Defer inline-ing owned navigation expansion till all joins are generated (#26617)

--- test/EFCore.SqlServer.FunctionalTests/Query/OwnedEntityQuerySqlServerTest.cs
+++ test/EFCore.SqlServer.FunctionalTests/Query/OwnedEntityQuerySqlServerTest.cs
@@ -99,5 +99,44 @@ FROM [Location25680] AS [l]
 WHERE [l].[Id] = @__id_0
 ORDER BY [l].[Id]");
         }
+
+        public override async Task Owned_reference_mapped_to_different_table_updated_correctly_after_subquery_pushdown(bool async)
+        {
+            await base.Owned_reference_mapped_to_different_table_updated_correctly_after_subquery_pushdown(async);
+
+            AssertSql(
+                @"@__p_0='10'
+
+SELECT [t].[Id], [t].[Name], [t].[CompanyId], [t].[AdditionalCustomerData], [t].[Id0], [s].[CompanyId], [s].[AdditionalSupplierData], [s].[Id]
+FROM (
+    SELECT TOP(@__p_0) [c].[Id], [c].[Name], [c0].[CompanyId], [c0].[AdditionalCustomerData], [c0].[Id] AS [Id0]
+    FROM [Companies] AS [c]
+    LEFT JOIN [CustomerData] AS [c0] ON [c].[Id] = [c0].[CompanyId]
+    WHERE [c0].[CompanyId] IS NOT NULL
+    ORDER BY [c].[Id]
+) AS [t]
+LEFT JOIN [SupplierData] AS [s] ON [t].[Id] = [s].[CompanyId]
+ORDER BY [t].[Id]");
+        }
+
+        public override async Task Owned_reference_mapped_to_different_table_nested_updated_correctly_after_subquery_pushdown(bool async)
+        {
+            await base.Owned_reference_mapped_to_different_table_nested_updated_correctly_after_subquery_pushdown(async);
+
+            AssertSql(
+                @"@__p_0='10'
+
+SELECT [t].[Id], [t].[Name], [t].[OwnerId], [t].[Id0], [t].[Name0], [t].[IntermediateOwnedEntityOwnerId], [t].[AdditionalCustomerData], [t].[Id1], [i1].[IntermediateOwnedEntityOwnerId], [i1].[AdditionalSupplierData], [i1].[Id]
+FROM (
+    SELECT TOP(@__p_0) [o].[Id], [o].[Name], [i].[OwnerId], [i].[Id] AS [Id0], [i].[Name] AS [Name0], [i0].[IntermediateOwnedEntityOwnerId], [i0].[AdditionalCustomerData], [i0].[Id] AS [Id1]
+    FROM [Owners] AS [o]
+    LEFT JOIN [IntermediateOwnedEntity] AS [i] ON [o].[Id] = [i].[OwnerId]
+    LEFT JOIN [IM_CustomerData] AS [i0] ON [i].[OwnerId] = [i0].[IntermediateOwnedEntityOwnerId]
+    WHERE [i0].[IntermediateOwnedEntityOwnerId] IS NOT NULL
+    ORDER BY [o].[Id]
+) AS [t]
+LEFT JOIN [IM_SupplierData] AS [i1] ON [t].[OwnerId] = [i1].[IntermediateOwnedEntityOwnerId]
+ORDER BY [t].[Id]");
+        }
     }
 }
