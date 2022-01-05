c5fdb5d708d31f36c8ef5a7b9f876efe5a2b207a
Shay Rojansky <roji@roji.org>
2022-01-05 10:25:26 +0100

    Add CI verification step

--- test/EFCore.Trimming.Tests/Program.cs
+++ test/EFCore.Trimming.Tests/Program.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Linq;
+using EFCore.Trimming.Tests;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.TestUtilities;
 
 await using var ctx = new BlogContext();
 await ctx.Database.EnsureDeletedAsync();
@@ -12,12 +14,26 @@ await ctx.Database.EnsureCreatedAsync();
 // Execute any query to make sure the basic query pipeline works
 _ = ctx.Blogs.Where(b => b.Name.StartsWith("foo")).ToList();
 
+Console.WriteLine("Database query executed successfully.");
+
 public class BlogContext : DbContext
 {
+    private static readonly string ConnectionString;
+
     public DbSet<Blog> Blogs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer(TestEnvironment.DefaultConnection);
+        => optionsBuilder.UseSqlServer(ConnectionString);
+
+    static BlogContext()
+    {
+        var builder = new SqlConnectionStringBuilder(TestEnvironment.DefaultConnection)
+        {
+            InitialCatalog = "TrimmingTests"
+        };
+
+        ConnectionString = builder.ToString();
+    }
 }
 
 public class Blog
--- test/EFCore.Trimming.Tests/TestEnvironment.cs
+++ test/EFCore.Trimming.Tests/TestEnvironment.cs
@@ -1,12 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.IO;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
-namespace Microsoft.EntityFrameworkCore.TestUtilities;
+namespace EFCore.Trimming.Tests;
 
 public static class TestEnvironment
 {
