9d1633357f11b9f38c9247eca692ae876c103f95
Arthur Vickers <ajcvickers@hotmail.com>
2022-01-10 08:43:09 +0000

    Remove quirk for #26602 (#27140)

--- src/EFCore.Proxies/Proxies/Internal/ProxyFactory.cs
+++ src/EFCore.Proxies/Proxies/Internal/ProxyFactory.cs
@@ -19,11 +19,7 @@ public class ProxyFactory : IProxyFactory
     private static readonly Type ProxyLazyLoaderInterface = typeof(IProxyLazyLoader);
     private static readonly Type NotifyPropertyChangedInterface = typeof(INotifyPropertyChanged);
     private static readonly Type NotifyPropertyChangingInterface = typeof(INotifyPropertyChanging);
-
-    private static readonly ProxyGenerationOptions GenerationOptions
-        = AppContext.TryGetSwitch("Microsoft.EntityFrameworkCore.Issue26602", out var enabled) && enabled
-            ? ProxyGenerationOptions.Default
-            : new(new ClonelessProxyGenerationHook());
+    private static readonly ProxyGenerationOptions GenerationOptions = new(new ClonelessProxyGenerationHook());
 
     private readonly ProxyGenerator _generator = new();
 
