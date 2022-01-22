4fa43cad9241af4bad2af41071b850f8281353d0
Shay Rojansky <roji@roji.org>
2022-01-22 01:10:32 +0100

    Fix typos in PropertyAccessMode
    Fixes #26923

--- src/EFCore/PropertyAccessMode.cs
+++ src/EFCore/PropertyAccessMode.cs
@@ -25,53 +25,48 @@ public enum PropertyAccessMode
     ///     Enforces that all accesses to the property must go through the field.
     /// </summary>
     /// <remarks>
-    ///     An exception will be thrown if this mode is set and it is not possible to read
-    ///     from or write to the field.
+    ///     An exception will be thrown if this mode is set and it is not possible to read from or write to the field.
     /// </remarks>
     Field,
 
     /// <summary>
-    ///     Enforces that all accesses to the property must go through the field when
-    ///     new instances are being constructed. New instances are typically constructed when
-    ///     entities are queried from the database.
-    ///     An exception will be thrown if this mode is set and it is not possible to
-    ///     write to the field.
+    ///     Enforces that all accesses to the property must go through the field when new instances are being constructed. New instances are
+    ///     typically constructed when entities are queried from the database. An exception will be thrown if this mode is set and it is not
+    ///     possible to write to the field.
     /// </summary>
     /// <remarks>
-    ///     All other uses of the property will go through the property getters and setters,
-    ///     unless this is not possible because, for example, the property is read-only, in which
-    ///     case these accesses will also use the field.
+    ///     All other uses of the property will go through the property getters and setters, unless this is not possible because, for
+    ///     example, the property is read-only, in which case these accesses will also use the field.
     /// </remarks>
     FieldDuringConstruction,
 
     /// <summary>
-    ///     Enforces that all accesses to the property must go through the property
-    ///     getters and setters, even when new objects are being constructed.
+    ///     Enforces that all accesses to the property must go through the property getters and setters, even when new objects are being
+    ///     constructed.
     /// </summary>
     /// <remarks>
-    ///     An exception will be thrown if this mode is set and it is not possible to read
-    ///     from or write to the property, for example because it is read-only.
+    ///     An exception will be thrown if this mode is set and it is not possible to read from or write to the property, for example
+    ///     because it is read-only.
     /// </remarks>
     Property,
 
     /// <summary>
-    ///     All accesses to the property goes directly to the field, unless the field is
-    ///     not known, in which as access goes through the property.
+    ///     All accesses to the property goes directly to the field, unless the field is not known, in which case access goes through the
+    ///     property.
     /// </summary>
     PreferField,
 
     /// <summary>
-    ///     All accesses to the property when constructing new entity instances goes directly
-    ///     to the field, unless the field is not known, in which as access goes through the property.
-    ///     All other uses of the property will go through the property getters and setters,
-    ///     unless this is not possible because, for example, the property is read-only, in which
-    ///     case these accesses will also use the field.
+    ///     All accesses to the property when constructing new entity instances goes directly to the field, unless the field is not known,
+    ///     in which case access goes through the property. All other uses of the property will go through the property getters and setters,
+    ///     unless this is not possible because, for example, the property is read-only, in which case these accesses will also use the
+    ///     field.
     /// </summary>
     PreferFieldDuringConstruction,
 
     /// <summary>
-    ///     All accesses to the property go through the property, unless there is no property or
-    ///     it is missing a setter/getter, in which as access goes directly to the field.
+    ///     All accesses to the property go through the property, unless there is no property or it is missing a setter/getter, in which
+    ///     case access goes directly to the field.
     /// </summary>
     PreferProperty
 }
