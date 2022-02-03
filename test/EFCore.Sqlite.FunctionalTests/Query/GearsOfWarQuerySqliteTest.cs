c577821b42fd8cf53ca6d01a39fc366357e2a4a4
Arthur Vickers <ajcvickers@hotmail.com>
2022-02-03 12:44:42 +0000

    Fix baseline.

--- test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/GearsOfWarQuerySqliteTest.cs
@@ -8073,14 +8073,26 @@ LEFT JOIN (
     {
         await base.Where_subquery_equality_to_null_with_composite_key(async);
 
-        AssertSql();
+        AssertSql(
+            @"SELECT ""s"".""Id"", ""s"".""Banner"", ""s"".""Banner5"", ""s"".""InternalNumber"", ""s"".""Name""
+FROM ""Squads"" AS ""s""
+WHERE NOT (EXISTS (
+    SELECT 1
+    FROM ""Gears"" AS ""g""
+    WHERE ""s"".""Id"" = ""g"".""SquadId""))");
     }
 
     public override async Task Where_subquery_equality_to_null_without_composite_key(bool async)
     {
         await base.Where_subquery_equality_to_null_without_composite_key(async);
 
-        AssertSql();
+        AssertSql(
+            @"SELECT ""g"".""Nickname"", ""g"".""SquadId"", ""g"".""AssignedCityName"", ""g"".""CityOfBirthName"", ""g"".""Discriminator"", ""g"".""FullName"", ""g"".""HasSoulPatch"", ""g"".""LeaderNickname"", ""g"".""LeaderSquadId"", ""g"".""Rank""
+FROM ""Gears"" AS ""g""
+WHERE NOT (EXISTS (
+    SELECT 1
+    FROM ""Weapons"" AS ""w""
+    WHERE ""g"".""FullName"" = ""w"".""OwnerFullName""))");
     }
 
     private void AssertSql(params string[] expected)
