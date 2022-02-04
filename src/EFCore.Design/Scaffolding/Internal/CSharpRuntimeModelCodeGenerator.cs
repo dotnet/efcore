80bb7b363cd6a180b23eb978d12b9b1db92549cd
leniency <levi.baker.101@gmail.com>
2022-02-03 18:45:30 -0800

    Fix for the optimizer when an entity has a collection property that references itself.
    Fixes #27301

--- src/EFCore.Design/Scaffolding/Internal/CSharpRuntimeModelCodeGenerator.cs
+++ src/EFCore.Design/Scaffolding/Internal/CSharpRuntimeModelCodeGenerator.cs
@@ -1376,7 +1376,7 @@ public class CSharpRuntimeModelCodeGenerator : ICompiledModelCodeGenerator
         }
 
         var sequenceType = type.TryGetSequenceType();
-        if (sequenceType != null)
+        if (sequenceType != null && sequenceType != type)
         {
             AddNamespace(sequenceType, namespaces);
         }
