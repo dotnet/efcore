a9034619d63239497a724869cf40c6110e7d0366
Artur Porowski <V4nII@hotmail.com>
2022-03-11 00:28:22 +0100

    Register DeleteBehaviorAttributeConvention in ConventionSet provider

--- src/EFCore/Metadata/Conventions/Infrastructure/ProviderConventionSetBuilder.cs
+++ src/EFCore/Metadata/Conventions/Infrastructure/ProviderConventionSetBuilder.cs
@@ -114,6 +114,7 @@ public class ProviderConventionSetBuilder : IProviderConventionSetBuilder
         var backingFieldAttributeConvention = new BackingFieldAttributeConvention(Dependencies);
         var unicodeAttributeConvention = new UnicodeAttributeConvention(Dependencies);
         var precisionAttributeConvention = new PrecisionAttributeConvention(Dependencies);
+        var deleteBehaviorAttributeConvention = new DeleteBehaviorAttributeConvention(Dependencies);
 
         conventionSet.PropertyAddedConventions.Add(backingFieldAttributeConvention);
         conventionSet.PropertyAddedConventions.Add(backingFieldConvention);
@@ -129,6 +130,7 @@ public class ProviderConventionSetBuilder : IProviderConventionSetBuilder
         conventionSet.PropertyAddedConventions.Add(foreignKeyPropertyDiscoveryConvention);
         conventionSet.PropertyAddedConventions.Add(unicodeAttributeConvention);
         conventionSet.PropertyAddedConventions.Add(precisionAttributeConvention);
+        conventionSet.PropertyAddedConventions.Add(deleteBehaviorAttributeConvention);
 
         conventionSet.EntityTypePrimaryKeyChangedConventions.Add(foreignKeyPropertyDiscoveryConvention);
         conventionSet.EntityTypePrimaryKeyChangedConventions.Add(valueGeneratorConvention);
