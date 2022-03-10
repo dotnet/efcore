c72a8af5f454781b824330f61960bc47ee4f8a4b
Shay Rojansky <roji@roji.org>
2022-03-10 19:33:35 +0100

    Fix scaffolding of reference navigations with NRT
    Fixes #27496

--- src/EFCore.Design/Scaffolding/Internal/CSharpEntityTypeGenerator.cs
+++ src/EFCore.Design/Scaffolding/Internal/CSharpEntityTypeGenerator.cs
@@ -444,7 +444,7 @@ public class CSharpEntityTypeGenerator : ICSharpEntityTypeGenerator
                 _sb.AppendLine(
                     !_useNullableReferenceTypes || navigation.IsCollection
                         ? $"public virtual {navigationType} {navigation.Name} {{ get; set; }}"
-                        : navigation.ForeignKey.IsRequired
+                        : navigation.ForeignKey.IsRequired && navigation.IsOnDependent
                             ? $"public virtual {navigationType} {navigation.Name} {{ get; set; }} = null!;"
                             : $"public virtual {navigationType}? {navigation.Name} {{ get; set; }}");
             }
