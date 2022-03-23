af2e118ef4be29a32b115834632dd812ac214f11
Artur Porowski <V4nII@hotmail.com>
2022-03-23 23:37:37 +0100

    Limit use of the attribute to only properties to accomodate only setting it on the navigation properties

--- src/EFCore.Abstractions/DeleteBehaviorAttribute.cs
+++ src/EFCore.Abstractions/DeleteBehaviorAttribute.cs
@@ -10,7 +10,7 @@ namespace Microsoft.EntityFrameworkCore;
 /// <remarks>
 ///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
 /// </remarks>
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+[AttributeUsage(AttributeTargets.Property)]
 public sealed class DeleteBehaviorAttribute : Attribute
 {
     /// <summary>
