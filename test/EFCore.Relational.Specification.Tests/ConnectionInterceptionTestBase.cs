7ad294cf61845105a64c26e1c32d7b6bd861c697
Arthur Vickers <ajcvickers@hotmail.com>
2022-05-04 17:30:36 +0100

    Add creation and disposal to connection interception (#27920)
    Co-authored-by: Andriy Svyryd <AndriySvyryd@users.noreply.github.com>

--- test/EFCore.Relational.Specification.Tests/ConnectionInterceptionTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/ConnectionInterceptionTestBase.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Data;
 
 namespace Microsoft.EntityFrameworkCore;
@@ -209,6 +211,419 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         AssertErrorOnOpen(context, interceptor, async);
     }
 
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public virtual async Task Intercept_connection_creation_passively(bool async)
+    {
+        var interceptor = new ConnectionCreationInterceptor();
+        var context = new ConnectionStringContext(ConfigureProvider);
+        var connectionDisposed = false;
+        context.Interceptors.Add(interceptor);
+
+        _ = context.Model;
+
+        Assert.False(interceptor.CreatingCalled);
+        Assert.False(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+
+        var connection = context.Database.GetDbConnection();
+        connection.Disposed += (_, __) => connectionDisposed = true;
+
+        Assert.True(interceptor.CreatingCalled);
+        Assert.True(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+        Assert.Same(context, interceptor.Context);
+        Assert.Same(connection, interceptor.Connection);
+
+        if (async)
+        {
+            await context.DisposeAsync();
+        }
+        else
+        {
+            context.Dispose();
+        }
+
+        Assert.True(interceptor.DisposingCalled);
+        Assert.True(interceptor.DisposedCalled);
+        Assert.Equal(async, interceptor.AsyncCalled);
+        Assert.NotEqual(async, interceptor.SyncCalled);
+        Assert.True(connectionDisposed);
+    }
+
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public virtual async Task Intercept_connection_to_override_creation(bool async)
+    {
+        using var tempContext = new ConnectionStringContext(ConfigureProvider);
+        var replacementConnection = tempContext.Database.GetDbConnection();
+        var connectionDisposed = false;
+        replacementConnection.Disposed += (_, __) => connectionDisposed = true;
+
+        var interceptor = new ConnectionCreationOverrideInterceptor(replacementConnection);
+        var context = new ConnectionStringContext(ConfigureProvider);
+        context.Interceptors.Add(interceptor);
+
+        _ = context.Model;
+
+        Assert.False(interceptor.CreatingCalled);
+        Assert.False(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+
+        var connection = context.Database.GetDbConnection();
+        Assert.Same(replacementConnection, connection);
+
+        Assert.True(interceptor.CreatingCalled);
+        Assert.True(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+        Assert.Same(context, interceptor.Context);
+        Assert.Same(connection, interceptor.Connection);
+
+        if (async)
+        {
+            await context.DisposeAsync();
+        }
+        else
+        {
+            context.Dispose();
+        }
+
+        Assert.True(interceptor.DisposingCalled);
+        Assert.True(interceptor.DisposedCalled);
+        Assert.Equal(async, interceptor.AsyncCalled);
+        Assert.NotEqual(async, interceptor.SyncCalled);
+        Assert.True(connectionDisposed);
+    }
+
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public virtual async Task Intercept_connection_to_override_connection_after_creation(bool async)
+    {
+        using var tempContext = new ConnectionStringContext(ConfigureProvider);
+        var replacementConnection = tempContext.Database.GetDbConnection();
+        var connectionDisposed = false;
+        replacementConnection.Disposed += (_, __) => connectionDisposed = true;
+
+        var interceptor = new ConnectionCreationReplaceInterceptor(replacementConnection);
+        var context = new ConnectionStringContext(ConfigureProvider);
+        context.Interceptors.Add(interceptor);
+
+        _ = context.Model;
+
+        Assert.False(interceptor.CreatingCalled);
+        Assert.False(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+
+        var connection = context.Database.GetDbConnection();
+        Assert.Same(replacementConnection, connection);
+
+        Assert.True(interceptor.CreatingCalled);
+        Assert.True(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+        Assert.Same(context, interceptor.Context);
+        Assert.Same(connection, interceptor.Connection);
+
+        if (async)
+        {
+            await context.DisposeAsync();
+        }
+        else
+        {
+            context.Dispose();
+        }
+
+        Assert.True(interceptor.DisposingCalled);
+        Assert.True(interceptor.DisposedCalled);
+        Assert.Equal(async, interceptor.AsyncCalled);
+        Assert.NotEqual(async, interceptor.SyncCalled);
+        Assert.True(connectionDisposed);
+    }
+
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public virtual async Task Intercept_connection_to_suppress_dispose(bool async)
+    {
+        var interceptor = new ConnectionCreationNoDisposeInterceptor();
+        var context = new ConnectionStringContext(ConfigureProvider);
+        var connectionDisposed = false;
+        context.Interceptors.Add(interceptor);
+
+        _ = context.Model;
+
+        Assert.False(interceptor.CreatingCalled);
+        Assert.False(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+
+        var connection = context.Database.GetDbConnection();
+        connection.Disposed += (_, __) => connectionDisposed = true;
+
+        Assert.True(interceptor.CreatingCalled);
+        Assert.True(interceptor.CreatedCalled);
+        Assert.False(interceptor.DisposingCalled);
+        Assert.False(interceptor.DisposedCalled);
+        Assert.Same(context, interceptor.Context);
+        Assert.Same(connection, interceptor.Connection);
+
+        if (async)
+        {
+            await context.DisposeAsync();
+        }
+        else
+        {
+            context.Dispose();
+        }
+
+        Assert.True(interceptor.DisposingCalled);
+        Assert.True(interceptor.DisposedCalled);
+        Assert.Equal(async, interceptor.AsyncCalled);
+        Assert.NotEqual(async, interceptor.SyncCalled);
+        Assert.False(connectionDisposed);
+    }
+
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public virtual async Task Intercept_connection_creation_with_multiple_interceptors(bool async)
+    {
+        using var tempContext1 = new ConnectionStringContext(ConfigureProvider);
+        var replacementConnection1 = tempContext1.Database.GetDbConnection();
+        var connectionDisposed1 = false;
+        replacementConnection1.Disposed += (_, __) => connectionDisposed1 = true;
+
+        using var tempContext2 = new ConnectionStringContext(ConfigureProvider);
+        var replacementConnection2 = tempContext2.Database.GetDbConnection();
+        var connectionDisposed2 = false;
+        replacementConnection2.Disposed += (_, __) => connectionDisposed2 = true;
+
+        var interceptors = new[]
+        {
+            new ConnectionCreationInterceptor(),
+            new ConnectionCreationOverrideInterceptor(replacementConnection1),
+            new ConnectionCreationInterceptor(),
+            new ConnectionCreationOverrideInterceptor(replacementConnection2)
+        };
+
+        var context = new ConnectionStringContext(ConfigureProvider);
+        context.Interceptors.AddRange(interceptors);
+
+        var connection = context.Database.GetDbConnection();
+        Assert.Same(replacementConnection2, connection);
+
+        foreach (var interceptor in interceptors)
+        {
+            Assert.True(interceptor.CreatingCalled);
+            Assert.True(interceptor.CreatedCalled);
+            Assert.False(interceptor.DisposingCalled);
+            Assert.False(interceptor.DisposedCalled);
+            Assert.Same(context, interceptor.Context);
+        }
+
+        if (async)
+        {
+            await context.DisposeAsync();
+        }
+        else
+        {
+            context.Dispose();
+        }
+
+        foreach (var interceptor in interceptors)
+        {
+            Assert.True(interceptor.DisposingCalled);
+            Assert.True(interceptor.DisposedCalled);
+            Assert.Equal(async, interceptor.AsyncCalled);
+            Assert.NotEqual(async, interceptor.SyncCalled);
+        }
+
+        Assert.False(connectionDisposed1);
+        Assert.True(connectionDisposed2);
+    }
+
+    protected abstract DbContextOptionsBuilder ConfigureProvider(DbContextOptionsBuilder optionsBuilder);
+
+    protected class ConnectionCreationInterceptor : IDbConnectionInterceptor
+    {
+        public DbContext? Context { get; set; }
+        public DbConnection? Connection { get; set; }
+        public Guid ConnectionId { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public bool CreatingCalled { get; set; }
+        public bool CreatedCalled { get; set; }
+        public bool AsyncCalled { get; set; }
+        public bool SyncCalled { get; set; }
+        public bool DisposingCalled { get; set; }
+        public bool DisposedCalled { get; set; }
+
+        public virtual InterceptionResult<DbConnection> ConnectionCreating(
+            ConnectionCreatingEventData eventData,
+            InterceptionResult<DbConnection> result)
+        {
+            Assert.NotNull(eventData.Context);
+            Assert.NotEqual(default, eventData.ConnectionId);
+            Assert.NotEqual(default, eventData.StartTime);
+
+            Context = eventData.Context;
+            ConnectionId = eventData.ConnectionId;
+            StartTime = eventData.StartTime;
+            CreatingCalled = true;
+
+            return result;
+        }
+
+        public virtual DbConnection ConnectionCreated(ConnectionCreatedEventData eventData, DbConnection result)
+        {
+            Assert.Same(Context, eventData.Context);
+            Assert.Equal(ConnectionId, eventData.ConnectionId);
+            Assert.Equal(StartTime, eventData.StartTime);
+            Assert.NotNull(result);
+
+            Connection = result;
+            CreatedCalled = true;
+
+            return result;
+        }
+
+        public virtual InterceptionResult ConnectionDisposing(
+            DbConnection connection,
+            ConnectionEventData eventData,
+            InterceptionResult result)
+        {
+            Assert.False(eventData.IsAsync);
+            SyncCalled = true;
+            AssertDisposing(eventData);
+
+            return result;
+        }
+
+        public virtual ValueTask<InterceptionResult> ConnectionDisposingAsync(
+            DbConnection connection,
+            ConnectionEventData eventData,
+            InterceptionResult result)
+        {
+            Assert.True(eventData.IsAsync);
+            AsyncCalled = true;
+            AssertDisposing(eventData);
+
+            return new ValueTask<InterceptionResult>(result);
+        }
+
+        public virtual void ConnectionDisposed(
+            DbConnection connection,
+            ConnectionEndEventData eventData)
+        {
+            Assert.False(eventData.IsAsync);
+            SyncCalled = true;
+            AssertDisposed(eventData);
+        }
+
+        public virtual Task ConnectionDisposedAsync(
+            DbConnection connection,
+            ConnectionEndEventData eventData)
+        {
+            Assert.True(eventData.IsAsync);
+            AsyncCalled = true;
+            AssertDisposed(eventData);
+
+            return Task.CompletedTask;
+        }
+
+        protected virtual void AssertDisposing(ConnectionEventData eventData)
+        {
+            Assert.Same(Context, eventData.Context);
+            Assert.Equal(ConnectionId, eventData.ConnectionId);
+
+            DisposingCalled = true;
+        }
+
+        protected virtual void AssertDisposed(ConnectionEndEventData eventData)
+        {
+            Assert.Same(Context, eventData.Context);
+            Assert.Equal(ConnectionId, eventData.ConnectionId);
+
+            DisposedCalled = true;
+        }
+    }
+
+    protected class ConnectionCreationOverrideInterceptor : ConnectionCreationInterceptor
+    {
+        private readonly DbConnection _replacementConnection;
+
+        public ConnectionCreationOverrideInterceptor(DbConnection replacementConnection)
+        {
+            _replacementConnection = replacementConnection;
+        }
+
+        public override InterceptionResult<DbConnection> ConnectionCreating(
+            ConnectionCreatingEventData eventData,
+            InterceptionResult<DbConnection> result)
+        {
+            base.ConnectionCreating(eventData, result);
+
+            return InterceptionResult<DbConnection>.SuppressWithResult(_replacementConnection);
+        }
+    }
+
+    protected class ConnectionCreationReplaceInterceptor : ConnectionCreationInterceptor
+    {
+        private readonly DbConnection _replacementConnection;
+
+        public ConnectionCreationReplaceInterceptor(DbConnection replacementConnection)
+        {
+            _replacementConnection = replacementConnection;
+        }
+
+        public override DbConnection ConnectionCreated(ConnectionCreatedEventData eventData, DbConnection result)
+        {
+            base.ConnectionCreated(eventData, result);
+
+            Connection = _replacementConnection;
+            return _replacementConnection;
+        }
+    }
+
+    protected class ConnectionCreationNoDisposeInterceptor : ConnectionCreationInterceptor
+    {
+        public override InterceptionResult ConnectionDisposing(DbConnection connection, ConnectionEventData eventData, InterceptionResult result)
+        {
+            base.ConnectionDisposing(connection, eventData, result);
+
+            return InterceptionResult.Suppress();
+        }
+
+        public override async ValueTask<InterceptionResult> ConnectionDisposingAsync(DbConnection connection, ConnectionEventData eventData, InterceptionResult result)
+        {
+            await base.ConnectionDisposingAsync(connection, eventData, result);
+
+            return InterceptionResult.Suppress();
+        }
+    }
+
+    protected class ConnectionStringContext : DbContext
+    {
+        private readonly Func<DbContextOptionsBuilder, DbContextOptionsBuilder> _configureProvider;
+
+        public ConnectionStringContext(Func<DbContextOptionsBuilder, DbContextOptionsBuilder> configureProvider)
+        {
+            _configureProvider = configureProvider;
+        }
+
+        public List<ConnectionCreationInterceptor> Interceptors { get; } = new List<ConnectionCreationInterceptor>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => _configureProvider(optionsBuilder).AddInterceptors(Interceptors);
+    }
+
     protected abstract BadUniverseContext CreateBadUniverse(DbContextOptionsBuilder optionsBuilder);
 
     protected class BadUniverseContext : UniverseContext
@@ -259,8 +674,8 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
 
     protected class ConnectionInterceptor : IDbConnectionInterceptor
     {
-        public DbContext Context { get; set; }
-        public Exception Exception { get; set; }
+        public DbContext? Context { get; set; }
+        public Exception? Exception { get; set; }
         public DbContextId ContextId { get; set; }
         public Guid ConnectionId { get; set; }
         public bool AsyncCalled { get; set; }
@@ -401,7 +816,7 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         {
             Assert.NotNull(eventData.Context);
             Assert.NotEqual(default, eventData.ConnectionId);
-            Assert.NotEqual(default, eventData.Context.ContextId);
+            Assert.NotEqual(default, eventData.Context!.ContextId);
 
             Context = eventData.Context;
             ContextId = Context.ContextId;
@@ -413,7 +828,7 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         {
             Assert.Same(Context, eventData.Context);
             Assert.Equal(ConnectionId, eventData.ConnectionId);
-            Assert.Equal(ContextId, eventData.Context.ContextId);
+            Assert.Equal(ContextId, eventData.Context!.ContextId);
 
             OpenedCalled = true;
         }
@@ -422,7 +837,7 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         {
             Assert.NotNull(eventData.Context);
             Assert.NotEqual(default, eventData.ConnectionId);
-            Assert.NotEqual(default, eventData.Context.ContextId);
+            Assert.NotEqual(default, eventData.Context!.ContextId);
 
             Context = eventData.Context;
             ContextId = Context.ContextId;
@@ -434,7 +849,7 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         {
             Assert.Same(Context, eventData.Context);
             Assert.Equal(ConnectionId, eventData.ConnectionId);
-            Assert.Equal(ContextId, eventData.Context.ContextId);
+            Assert.Equal(ContextId, eventData.Context!.ContextId);
 
             ClosedCalled = true;
         }
@@ -443,7 +858,7 @@ public abstract class ConnectionInterceptionTestBase : InterceptionTestBase
         {
             Assert.Same(Context, eventData.Context);
             Assert.Equal(ConnectionId, eventData.ConnectionId);
-            Assert.Equal(ContextId, eventData.Context.ContextId);
+            Assert.Equal(ContextId, eventData.Context!.ContextId);
             Assert.NotNull(eventData.Exception);
 
             Exception = eventData.Exception;
