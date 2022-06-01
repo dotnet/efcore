c537e2a933e5be769670e6edd109c2d605e81178
Arthur Vickers <ajcvickers@hotmail.com>
2022-06-01 09:36:32 +0100

    Update src/EFCore.Proxies/Proxies/Internal/IProxyFactory.cs
    Co-authored-by: Andriy Svyryd <AndriySvyryd@users.noreply.github.com>

--- src/EFCore.Proxies/Proxies/Internal/IProxyFactory.cs
+++ src/EFCore.Proxies/Proxies/Internal/IProxyFactory.cs
@@ -40,7 +40,7 @@ public interface IProxyFactory
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
-    Type CreateProxyType(IReadOnlyEntityType entityType);
+    Type CreateProxyType(IEntityType entityType);
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
