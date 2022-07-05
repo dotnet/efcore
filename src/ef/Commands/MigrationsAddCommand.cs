3a28ebd28383e6cfa862af578a234ad9870c11d7
CruderKat <71131016+BirajMainali@users.noreply.github.com>
2022-07-05 11:36:48 +0000

    feat: add validation to prevent invalid migrations name

--- src/ef/Commands/MigrationsAddCommand.cs
+++ src/ef/Commands/MigrationsAddCommand.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.IO;
 using System.Collections;
 using Microsoft.EntityFrameworkCore.Tools.Properties;
 
@@ -17,6 +18,10 @@ namespace Microsoft.EntityFrameworkCore.Tools.Commands
             if (string.IsNullOrEmpty(_name!.Value))
             {
                 throw new CommandException(Resources.MissingArgument(_name.Name));
+            }
+             if (_name?.Name?.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+            {
+                throw new CommandException("Invalid migration name. Names must not contain any invalid characters, e.g [-,/,:]");
             }
         }
 
