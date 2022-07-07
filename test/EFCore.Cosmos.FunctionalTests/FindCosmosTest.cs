e4b38dbe9392ec3491b2b3a59e9c15165e3d6ab7
Arthur Vickers <ajcvickers@hotmail.com>
2022-07-07 11:54:20 +0100

    Allow cancellation token to be passed as part of params array in FindAsync
    Fixes #22667

--- test/EFCore.Cosmos.FunctionalTests/FindCosmosTest.cs
+++ test/EFCore.Cosmos.FunctionalTests/FindCosmosTest.cs
@@ -14,15 +14,15 @@ public abstract class FindCosmosTest : FindTestBase<FindCosmosTest.FindCosmosFix
     [ConditionalFact(Skip = "#25886")]
     public override void Find_base_type_using_derived_set_tracked() { }
 
-    [ConditionalFact(Skip = "#25886")]
-    public override Task Find_base_type_using_derived_set_tracked_async()
+    [ConditionalTheory(Skip = "#25886")]
+    public override Task Find_base_type_using_derived_set_tracked_async(CancellationType cancellationType)
         => Task.CompletedTask;
 
     [ConditionalFact(Skip = "#25886")]
     public override void Find_derived_using_base_set_type_from_store() { }
 
-    [ConditionalFact(Skip = "#25886")]
-    public override Task Find_derived_using_base_set_type_from_store_async()
+    [ConditionalTheory(Skip = "#25886")]
+    public override Task Find_derived_using_base_set_type_from_store_async(CancellationType cancellationType)
         => Task.CompletedTask;
 
     public class FindCosmosTestSet : FindCosmosTest
@@ -32,11 +32,7 @@ public abstract class FindCosmosTest : FindTestBase<FindCosmosTest.FindCosmosFix
         {
         }
 
-        protected override TEntity Find<TEntity>(DbContext context, params object[] keyValues)
-            => context.Set<TEntity>().Find(keyValues);
-
-        protected override ValueTask<TEntity> FindAsync<TEntity>(DbContext context, params object[] keyValues)
-            => context.Set<TEntity>().FindAsync(keyValues);
+        protected override TestFinder Finder { get; } = new FindViaSetFinder();
     }
 
     public class FindCosmosTestContext : FindCosmosTest
@@ -46,11 +42,7 @@ public abstract class FindCosmosTest : FindTestBase<FindCosmosTest.FindCosmosFix
         {
         }
 
-        protected override TEntity Find<TEntity>(DbContext context, params object[] keyValues)
-            => context.Find<TEntity>(keyValues);
-
-        protected override ValueTask<TEntity> FindAsync<TEntity>(DbContext context, params object[] keyValues)
-            => context.FindAsync<TEntity>(keyValues);
+        protected override TestFinder Finder { get; } = new FindViaContextFinder();
     }
 
     public class FindCosmosTestNonGeneric : FindCosmosTest
@@ -60,11 +52,7 @@ public abstract class FindCosmosTest : FindTestBase<FindCosmosTest.FindCosmosFix
         {
         }
 
-        protected override TEntity Find<TEntity>(DbContext context, params object[] keyValues)
-            => (TEntity)context.Find(typeof(TEntity), keyValues);
-
-        protected override async ValueTask<TEntity> FindAsync<TEntity>(DbContext context, params object[] keyValues)
-            => (TEntity)await context.FindAsync(typeof(TEntity), keyValues);
+        protected override TestFinder Finder { get; } = new FindViaNonGenericContextFinder();
     }
 
     public class FindCosmosFixture : FindFixtureBase
