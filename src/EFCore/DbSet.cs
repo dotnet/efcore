5d640609b70d16eb4e986002ced129de7a5c2615
Arthur Vickers <ajcvickers@hotmail.com>
2022-07-15 07:42:43 +0100

    Add Entry method to DbSet to get entries for shared-type entity type instances (#28451)

--- src/EFCore/DbSet.cs
+++ src/EFCore/DbSet.cs
@@ -591,6 +591,19 @@ public abstract class DbSet<TEntity> : IQueryable<TEntity>, IInfrastructure<ISer
         => throw new NotSupportedException();
 
     /// <summary>
+    ///     Gets an <see cref="EntityEntry{TEntity}" /> for the given entity. The entry provides
+    ///     access to change tracking information and operations for the entity.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-entity-entries">Accessing tracked entities in EF Core</see> for more information and
+    ///     examples.
+    /// </remarks>
+    /// <param name="entity">The entity to get the entry for.</param>
+    /// <returns>The entry for the given entity.</returns>
+    public virtual EntityEntry<TEntity> Entry(TEntity entity)
+        => throw new NotSupportedException();
+
+    /// <summary>
     ///     Returns an <see cref="IEnumerator{T}" /> which when enumerated will execute a query against the database
     ///     to load all entities from the database.
     /// </summary>
