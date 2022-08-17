0a33473a3984fff7841f694160b84f9afbfcfdc3
Smit Patel <smitpatel@users.noreply.github.com>
2022-08-17 12:19:32 -0700

    ExecuteDelete: Use Any path for unsupported query operator (#28755)
    Work-around for #28745

---
+++ test/EFCore.Sqlite.FunctionalTests/BulkUpdates/NonSharedModelBulkUpdatesSqliteTest.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.BulkUpdates;
+
+public class NonSharedModelBulkUpdatesSqliteTest : NonSharedModelBulkUpdatesTestBase
+{
+    protected override ITestStoreFactory TestStoreFactory => SqliteTestStoreFactory.Instance;
+
+    [ConditionalFact]
+    public virtual void Check_all_tests_overridden()
+        => TestHelpers.AssertAllMethodsOverridden(GetType());
+
+    public override async Task Delete_predicate_based_on_optional_navigation(bool async)
+    {
+        await base.Delete_predicate_based_on_optional_navigation(async);
+
+        AssertSql(
+            @"DELETE FROM ""Posts"" AS ""p""
+WHERE EXISTS (
+    SELECT 1
+    FROM ""Posts"" AS ""p0""
+    LEFT JOIN ""Blogs"" AS ""b"" ON ""p0"".""BlogId"" = ""b"".""Id""
+    WHERE ""b"".""Title"" IS NOT NULL AND (""b"".""Title"" LIKE 'Arthur%') AND ""p0"".""Id"" = ""p"".""Id"")");
+    }
+
+    private void AssertSql(params string[] expected)
+        => TestSqlLoggerFactory.AssertBaseline(expected);
+
+    private void AssertExecuteUpdateSql(params string[] expected)
+        => TestSqlLoggerFactory.AssertBaseline(expected, forUpdate: true);
+}
