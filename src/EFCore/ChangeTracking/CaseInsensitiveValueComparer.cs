f577e1a4d94c8437a840d8f301dddc2d9bc2ebc0
Arthur Vickers <ajcvickers@hotmail.com>
2022-12-31 10:35:43 +0000

    Use case-insensitive string comparisons by default on SQL Server
    Fixes #27526

---
+++ src/EFCore/ChangeTracking/CaseInsensitiveValueComparer.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.ChangeTracking;
+
+/// <summary>
+///     Case-insensitive value comparison for strings.
+/// </summary>
+public class CaseInsensitiveValueComparer : ValueComparer<string?>
+{
+    /// <summary>
+    ///    Creates a value comparer instance.
+    /// </summary>
+    public CaseInsensitiveValueComparer()
+        : base(
+            (l, r) => string.Equals(l, r, StringComparison.OrdinalIgnoreCase),
+            v => v == null ? 0 : v.ToUpper().GetHashCode())
+    {
+    }
+}
