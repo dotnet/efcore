ec185c07f55b92015cdfb3985667b0b42df27634
Arthur Vickers <ajcvickers@hotmail.com>
2023-01-02 11:41:39 +0000

    Minor test refactoring and enabling (#29958)

--- test/EFCore.SqlServer.FunctionalTests/Query/SqlExecutorSqlServerTest.cs
+++ test/EFCore.SqlServer.FunctionalTests/Query/SqlExecutorSqlServerTest.cs
@@ -15,16 +15,16 @@ public class SqlExecutorSqlServerTest : SqlExecutorTestBase<NorthwindQuerySqlSer
         //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
     }
 
-    public override void Executes_stored_procedure()
+    public override async Task Executes_stored_procedure(bool async)
     {
-        base.Executes_stored_procedure();
+        await base.Executes_stored_procedure(async);
 
         AssertSql("[dbo].[Ten Most Expensive Products]");
     }
 
-    public override void Executes_stored_procedure_with_parameter()
+    public override async Task Executes_stored_procedure_with_parameter(bool async)
     {
-        base.Executes_stored_procedure_with_parameter();
+        await base.Executes_stored_procedure_with_parameter(async);
 
         AssertSql(
 """
@@ -34,9 +34,9 @@ public class SqlExecutorSqlServerTest : SqlExecutorTestBase<NorthwindQuerySqlSer
 """);
     }
 
-    public override void Executes_stored_procedure_with_generated_parameter()
+    public override async Task Executes_stored_procedure_with_generated_parameter(bool async)
     {
-        base.Executes_stored_procedure_with_generated_parameter();
+        await base.Executes_stored_procedure_with_generated_parameter(async);
 
         AssertSql(
 """
@@ -46,9 +46,9 @@ public class SqlExecutorSqlServerTest : SqlExecutorTestBase<NorthwindQuerySqlSer
 """);
     }
 
-    public override void Query_with_parameters()
+    public override async Task Query_with_parameters(bool async)
     {
-        base.Query_with_parameters();
+        await base.Query_with_parameters(async);
 
         AssertSql(
 """
@@ -59,9 +59,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @p0 AND "ContactTitle" = @p1
 """);
     }
 
-    public override void Query_with_dbParameter_with_name()
+    public override async Task Query_with_dbParameter_with_name(bool async)
     {
-        base.Query_with_dbParameter_with_name();
+        await base.Query_with_dbParameter_with_name(async);
 
         AssertSql(
 """
@@ -71,9 +71,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @city
 """);
     }
 
-    public override void Query_with_positional_dbParameter_with_name()
+    public override async Task Query_with_positional_dbParameter_with_name(bool async)
     {
-        base.Query_with_positional_dbParameter_with_name();
+        await base.Query_with_positional_dbParameter_with_name(async);
 
         AssertSql(
 """
@@ -83,9 +83,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @city
 """);
     }
 
-    public override void Query_with_positional_dbParameter_without_name()
+    public override async Task Query_with_positional_dbParameter_without_name(bool async)
     {
-        base.Query_with_positional_dbParameter_without_name();
+        await base.Query_with_positional_dbParameter_without_name(async);
 
         AssertSql(
 """
@@ -95,9 +95,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @p0
 """);
     }
 
-    public override void Query_with_dbParameters_mixed()
+    public override async Task Query_with_dbParameters_mixed(bool async)
     {
-        base.Query_with_dbParameters_mixed();
+        await base.Query_with_dbParameters_mixed(async);
 
         AssertSql(
 """
@@ -115,9 +115,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @city AND "ContactTitle" = @p0
 """);
     }
 
-    public override void Query_with_parameters_interpolated()
+    public override async Task Query_with_parameters_interpolated(bool async)
     {
-        base.Query_with_parameters_interpolated();
+        await base.Query_with_parameters_interpolated(async);
 
         AssertSql(
 """
@@ -128,9 +128,9 @@ SELECT COUNT(*) FROM "Customers" WHERE "City" = @p0 AND "ContactTitle" = @p1
 """);
     }
 
-    public override void Query_with_DbParameters_interpolated()
+    public override async Task Query_with_DbParameters_interpolated(bool async)
     {
-        base.Query_with_DbParameters_interpolated();
+        await base.Query_with_DbParameters_interpolated(async);
 
         AssertSql(
 """
@@ -138,32 +138,6 @@ city='London' (Nullable = false) (Size = 6)
 contactTitle='Sales Representative' (Nullable = false) (Size = 20)
 
 SELECT COUNT(*) FROM "Customers" WHERE "City" = @city AND "ContactTitle" = @contactTitle
-""");
-    }
-
-    public override async Task Query_with_parameters_async()
-    {
-        await base.Query_with_parameters_async();
-
-        AssertSql(
-"""
-@p0='London' (Size = 4000)
-@p1='Sales Representative' (Size = 4000)
-
-SELECT COUNT(*) FROM "Customers" WHERE "City" = @p0 AND "ContactTitle" = @p1
-""");
-    }
-
-    public override async Task Query_with_parameters_interpolated_async()
-    {
-        await base.Query_with_parameters_interpolated_async();
-
-        AssertSql(
-"""
-@p0='London' (Size = 4000)
-@p1='Sales Representative' (Size = 4000)
-
-SELECT COUNT(*) FROM "Customers" WHERE "City" = @p0 AND "ContactTitle" = @p1
 """);
     }
 
