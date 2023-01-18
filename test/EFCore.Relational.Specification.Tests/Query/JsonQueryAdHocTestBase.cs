a688b44db3d0d70c852015e75f5d617430f03225
maumar <maumar@microsoft.com>
2023-01-18 02:45:00 -0800

    Fix to #30028 - Added nullable property to Json mapped model resulting in errors instead of mapping non existing json property to null
    Problem was that we when accessing inner property on a JsonElement we used GetProperty. If property is not present (which should be allowed if we try to access optional navigation) KeyNotFound is thrown.
    
    Fix is to use TryGetProperty instead to gracefully handle this scenario.
    
    Fixes #30028

--- test/EFCore.Relational.Specification.Tests/Query/JsonQueryAdHocTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/JsonQueryAdHocTestBase.cs
@@ -88,6 +88,135 @@ public abstract class JsonQueryAdHocTestBase : NonSharedModelTestBase
 
     #endregion
 
+    #region 30028
+
+    protected abstract void Seed30028(MyContext30028 ctx);
+
+    protected class MyContext30028 : DbContext
+    {
+        public MyContext30028(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyEntity30028> Entities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyEntity30028>(b =>
+            {
+                b.Property(x => x.Id).ValueGeneratedNever();
+                b.OwnsOne(x => x.Json, nb =>
+                {
+                    nb.ToJson();
+                    nb.OwnsMany(x => x.Collection, nnb => nnb.OwnsOne(x => x.Nested));
+                    nb.OwnsOne(x => x.OptionalReference, nnb => nnb.OwnsOne(x => x.Nested));
+                    nb.OwnsOne(x => x.RequiredReference, nnb => nnb.OwnsOne(x => x.Nested));
+                    nb.Navigation(x => x.RequiredReference).IsRequired();
+                });
+            });
+        }
+    }
+
+    public class MyEntity30028
+    {
+        public int Id { get; set; }
+        public MyJsonRootEntity30028 Json { get; set; }
+    }
+
+    public class MyJsonRootEntity30028
+    {
+        public string RootName { get; set; }
+        public MyJsonBranchEntity30028 RequiredReference { get; set; }
+        public MyJsonBranchEntity30028 OptionalReference { get; set; }
+        public List<MyJsonBranchEntity30028> Collection { get; set; }
+    }
+
+    public class MyJsonBranchEntity30028
+    {
+        public string BranchName { get; set; }
+        public MyJsonLeafEntity30028 Nested { get; set; }
+    }
+
+    public class MyJsonLeafEntity30028
+    {
+        public string LeafName { get; set; }
+    }
+
+    [ConditionalTheory]
+    [MemberData(nameof(IsAsyncData))]
+    public virtual async Task Accessing_missing_navigation_works(bool async)
+    {
+        var contextFactory = await InitializeAsync<MyContext30028>(seed: Seed30028);
+        using (var context = contextFactory.CreateContext())
+        {
+            var result = context.Entities.OrderBy(x => x.Id).ToList();
+            Assert.Equal(4, result.Count);
+            Assert.NotNull(result[0].Json.Collection);
+            Assert.NotNull(result[0].Json.OptionalReference);
+            Assert.NotNull(result[0].Json.RequiredReference);
+
+            Assert.Null(result[1].Json.Collection);
+            Assert.NotNull(result[1].Json.OptionalReference);
+            Assert.NotNull(result[1].Json.RequiredReference);
+
+            Assert.NotNull(result[2].Json.Collection);
+            Assert.Null(result[2].Json.OptionalReference);
+            Assert.NotNull(result[2].Json.RequiredReference);
+
+            Assert.NotNull(result[3].Json.Collection);
+            Assert.NotNull(result[3].Json.OptionalReference);
+            Assert.Null(result[3].Json.RequiredReference);
+        }
+    }
+
+    [ConditionalTheory]
+    [MemberData(nameof(IsAsyncData))]
+    public virtual async Task Missing_navigation_works_with_deduplication(bool async)
+    {
+        var contextFactory = await InitializeAsync<MyContext30028>(seed: Seed30028);
+        using (var context = contextFactory.CreateContext())
+        {
+            var result = context.Entities.OrderBy(x => x.Id).Select(x => new
+            {
+                x,
+                x.Json,
+                x.Json.OptionalReference,
+                x.Json.RequiredReference,
+                NestedOptional = x.Json.OptionalReference.Nested,
+                NestedRequired = x.Json.RequiredReference.Nested,
+                x.Json.Collection,
+            }).ToList();
+
+            Assert.Equal(4, result.Count);
+            Assert.NotNull(result[0].OptionalReference);
+            Assert.NotNull(result[0].RequiredReference);
+            Assert.NotNull(result[0].NestedOptional);
+            Assert.NotNull(result[0].NestedRequired);
+            Assert.NotNull(result[0].Collection);
+
+            Assert.NotNull(result[1].OptionalReference);
+            Assert.NotNull(result[1].RequiredReference);
+            Assert.NotNull(result[1].NestedOptional);
+            Assert.NotNull(result[1].NestedRequired);
+            Assert.Null(result[1].Collection);
+
+            Assert.Null(result[2].OptionalReference);
+            Assert.NotNull(result[2].RequiredReference);
+            Assert.Null(result[2].NestedOptional);
+            Assert.NotNull(result[2].NestedRequired);
+            Assert.NotNull(result[2].Collection);
+
+            Assert.NotNull(result[3].OptionalReference);
+            Assert.Null(result[3].RequiredReference);
+            Assert.NotNull(result[3].NestedOptional);
+            Assert.Null(result[3].NestedRequired);
+            Assert.NotNull(result[3].Collection);
+        }
+    }
+
+    #endregion
+
     #region ArrayOfPrimitives
 
     [ConditionalTheory]
