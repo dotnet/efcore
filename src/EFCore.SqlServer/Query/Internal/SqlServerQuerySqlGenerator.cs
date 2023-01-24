dfe2bf60a3a19b0b350aa15f7c88f1772bef9905
maumar <maumar@microsoft.com>
2023-01-24 05:33:54 -0800

    Fix to #29667 - Count after Take throws "No column name was specified for column 1 of 't'."
    Problem was that in some cases (e.g. count over keyless entity that has been pushed down) we now generate empty projection in the subquery, where before we were projecting some columns.
    SQL Server doesn't allow unaliased projection in the subquery, so the fix is to simply add an alias to the empty projection that we generate.
    
    Fixes #29667

--- src/EFCore.SqlServer/Query/Internal/SqlServerQuerySqlGenerator.cs
+++ src/EFCore.SqlServer/Query/Internal/SqlServerQuerySqlGenerator.cs
@@ -130,6 +130,21 @@ public class SqlServerQuerySqlGenerator : QuerySqlGenerator
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
+    protected override void GenerateEmptyProjection(SelectExpression selectExpression)
+    {
+        base.GenerateEmptyProjection(selectExpression);
+        if (selectExpression.Alias != null)
+        {
+            Sql.Append(" AS empty");
+        }
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
     protected override void GenerateTop(SelectExpression selectExpression)
     {
         if (selectExpression.Limit != null
