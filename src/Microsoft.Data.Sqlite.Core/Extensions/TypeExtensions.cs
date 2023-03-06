ed8a0536f3754289711d7646ce66ef234932d9d4
Shay Rojansky <roji@roji.org>
2023-03-06 11:51:52 +0100

    Optimize SqliteDataReader.GetFieldValue

--- src/Microsoft.Data.Sqlite.Core/Extensions/TypeExtensions.cs
+++ src/Microsoft.Data.Sqlite.Core/Extensions/TypeExtensions.cs
@@ -6,10 +6,6 @@ namespace System
 {
     internal static class TypeExtensions
     {
-        public static bool IsNullable(this Type type)
-            => !type.IsValueType
-                || (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>));
-
         public static Type UnwrapEnumType(this Type type)
             => type.IsEnum ? Enum.GetUnderlyingType(type) : type;
 
