1899f1808f2a62b6f9e6db47c29cd2cf2187c47b
DoctorKrolic <mapmyp03@gmail.com>
2023-05-03 13:13:14 +0300

    justify-content: left; :)

--- test/EFCore.Analyzers.Tests/Suppressors/UninitializedDbSetDiagnosticSuppressorTests.cs
+++ test/EFCore.Analyzers.Tests/Suppressors/UninitializedDbSetDiagnosticSuppressorTests.cs
@@ -12,112 +12,112 @@ public class UninitializedDbSetDiagnosticSuppressorTests
     [ConditionalFact]
     public Task DbSet_property_on_DbContext_is_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
-            }
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""");
 
     [ConditionalFact]
     public Task Non_public_DbSet_property_on_DbContext_is_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                private Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
-            }
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    private Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""");
 
     [ConditionalFact]
     public Task DbSet_property_with_non_public_setter_on_DbContext_is_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; private set; }
-            }
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; private set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""");
 
     [ConditionalFact]
     public Task DbSet_property_without_setter_on_DbContext_is_not_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; }
-            }
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """, isSuppressed: false);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""", isSuppressed: false);
 
     [ConditionalFact]
     public Task Static_DbSet_property_on_DbContext_is_not_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public static Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
-            }
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public static Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """, isSuppressed: false);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""", isSuppressed: false);
 
     [ConditionalFact]
     public Task Non_DbSet_property_on_DbContext_is_not_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public string {|#0:Name|} { get; set; }
-            }
-            """, isSuppressed: false);
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public string {|#0:Name|} { get; set; }
+}
+""", isSuppressed: false);
 
     [ConditionalFact]
     public Task DbSet_property_on_non_DbContext_is_not_suppressed()
         => VerifySingleSuppressionAsync("""
-            public class Foo
-            {
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
-            }
+public class Foo
+{
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#0:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """, isSuppressed: false);
+public class Blog
+{
+    public int Id { get; set; }
+}
+""", isSuppressed: false);
 
     [ConditionalFact]
     public async Task DbSet_property_on_DbContext_with_ctor_is_suppressed()
     {
         var source = """
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public {|#0:MyDbContext|}() {}
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public {|#0:MyDbContext|}() {}
 
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#1:Blogs|} { get; set; }
-            }
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#1:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """;
+public class Blog
+{
+    public int Id { get; set; }
+}
+""";
 
         await new VerifyCS.Test
         {
@@ -137,20 +137,20 @@ public class UninitializedDbSetDiagnosticSuppressorTests
     public async Task DbSet_property_on_DbContext_with_ctors_is_suppressed()
     {
         var source = """
-            public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
-            {
-                public {|#0:MyDbContext|}() {}
+public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public {|#0:MyDbContext|}() {}
 
-                public {|#1:MyDbContext|}(int foo) {}
+    public {|#1:MyDbContext|}(int foo) {}
 
-                public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#2:Blogs|} { get; set; }
-            }
+    public Microsoft.EntityFrameworkCore.DbSet<Blog> {|#2:Blogs|} { get; set; }
+}
 
-            public class Blog
-            {
-                public int Id { get; set; }
-            }
-            """;
+public class Blog
+{
+    public int Id { get; set; }
+}
+""";
 
         await new VerifyCS.Test
         {
