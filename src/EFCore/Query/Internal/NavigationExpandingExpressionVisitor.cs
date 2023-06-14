f414770638bfef3e37350af618cdcc4a7af4ba8d
Aleksei Beliakov <106954729+beliakov-mb@users.noreply.github.com>
2023-06-15 01:42:56 +0300

    Fix navigation in UserDefinedFunction (#30776)
    * Fix navigation
    
    * add Visits_extention_childrens test

--- src/EFCore/Query/Internal/NavigationExpandingExpressionVisitor.cs
+++ src/EFCore/Query/Internal/NavigationExpandingExpressionVisitor.cs
@@ -225,7 +225,8 @@ public partial class NavigationExpandingExpressionVisitor : ExpressionVisitor
                 }
                 else
                 {
-                    navigationExpansionExpression = CreateNavigationExpansionExpression(entityQueryRootExpression, entityType);
+                    navigationExpansionExpression = CreateNavigationExpansionExpression(
+                        base.VisitExtension(entityQueryRootExpression), entityType);
                 }
 
                 return ApplyQueryFilter(entityType, navigationExpansionExpression);
