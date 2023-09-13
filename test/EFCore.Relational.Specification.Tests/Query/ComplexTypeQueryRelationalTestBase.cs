5e7113e96d688891236ef6062d4be3b9969d87d1
Shay Rojansky <roji@roji.org>
2023-09-13 12:26:38 +0300

    Adjust some exception messages (#31707)
    Part of #31679

--- test/EFCore.Relational.Specification.Tests/Query/ComplexTypeQueryRelationalTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/Query/ComplexTypeQueryRelationalTestBase.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.EntityFrameworkCore.TestModels.ComplexTypeModel;
-
 namespace Microsoft.EntityFrameworkCore.Query;
 
 public abstract class ComplexTypeQueryRelationalTestBase<TFixture> : ComplexTypeQueryTestBase<TFixture>
@@ -18,6 +16,8 @@ public abstract class ComplexTypeQueryRelationalTestBase<TFixture> : ComplexType
         var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => base.Subquery_over_complex_type(async));
 
         Assert.Equal(RelationalStrings.SubqueryOverComplexTypesNotSupported("Customer.ShippingAddress#Address"), exception.Message);
+
+        AssertSql();
     }
 
     public override async Task Concat_two_different_complex_type(bool async)
@@ -27,6 +27,8 @@ public abstract class ComplexTypeQueryRelationalTestBase<TFixture> : ComplexType
         Assert.Equal(
             RelationalStrings.SetOperationOverDifferentStructuralTypes(
                 "Customer.ShippingAddress#Address", "Customer.BillingAddress#Address"), exception.Message);
+
+        AssertSql();
     }
 
     public override async Task Union_two_different_complex_type(bool async)
@@ -36,5 +38,19 @@ public abstract class ComplexTypeQueryRelationalTestBase<TFixture> : ComplexType
         Assert.Equal(
             RelationalStrings.SetOperationOverDifferentStructuralTypes(
                 "Customer.ShippingAddress#Address", "Customer.BillingAddress#Address"), exception.Message);
+
+        AssertSql();
     }
+
+    public override async Task Complex_type_equals_null(bool async)
+    {
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => base.Complex_type_equals_null(async));
+
+        Assert.Equal(RelationalStrings.CannotCompareComplexTypeToNull, exception.Message);
+
+        AssertSql();
+    }
+
+    private void AssertSql(params string[] expected)
+        => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
 }
