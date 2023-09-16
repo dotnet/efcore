3f698170de0e3364c104f672140d0a24c25e8106
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-16 18:08:05 +0100

    Fix issues with the compiled model
    Fixes #31727
    Fixes #31720
    
    Also fixed a formatting issue introduced in #31709
    
    Added tests for all common types on SQLite and SQL Server.

--- src/EFCore.Sqlite.Core/Storage/Internal/SqliteGuidTypeMapping.cs
+++ src/EFCore.Sqlite.Core/Storage/Internal/SqliteGuidTypeMapping.cs
@@ -20,7 +20,7 @@ public class SqliteGuidTypeMapping : GuidTypeMapping
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
-    public static new readonly SqliteGuidTypeMapping Default = new(SqliteTypeMappingSource.TextTypeName);
+    public static new SqliteGuidTypeMapping Default { get; } = new(SqliteTypeMappingSource.TextTypeName);
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
--- src/EFCore.Sqlite.Core/Storage/Internal/SqliteULongTypeMapping.cs
+++ src/EFCore.Sqlite.Core/Storage/Internal/SqliteULongTypeMapping.cs
@@ -19,7 +19,7 @@ public class SqliteULongTypeMapping : ULongTypeMapping
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
-    public static new readonly SqliteULongTypeMapping Default = new(SqliteTypeMappingSource.IntegerTypeName);
+    public static new SqliteULongTypeMapping Default { get; } = new(SqliteTypeMappingSource.IntegerTypeName);
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
