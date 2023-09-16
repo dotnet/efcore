018ad06bb4d7d6ed4f8667b1e941d4f7b5e406e8
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-16 11:28:36 +0100

    Make expression methods of list comparer private since they are not now needed for the compiled model (#31758)

--- src/EFCore/ChangeTracking/NullableValueTypeListComparer.cs
+++ src/EFCore/ChangeTracking/NullableValueTypeListComparer.cs
@@ -38,14 +38,7 @@ public sealed class NullableValueTypeListComparer<TElement> : ValueComparer<IEnu
     /// </summary>
     public ValueComparer ElementComparer { get; }
 
-    /// <summary>
-    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-    ///     any release. You should only use it directly in your code with extreme caution and knowing that
-    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-    /// </summary>
-    [EntityFrameworkInternal]
-    public static bool Compare(IEnumerable<TElement?>? a, IEnumerable<TElement?>? b, ValueComparer<TElement?> elementComparer)
+    private static bool Compare(IEnumerable<TElement?>? a, IEnumerable<TElement?>? b, ValueComparer<TElement?> elementComparer)
     {
         if (ReferenceEquals(a, b))
         {
@@ -102,14 +95,7 @@ public sealed class NullableValueTypeListComparer<TElement> : ValueComparer<IEnu
                 typeof(IList<>).MakeGenericType(elementComparer.Type.MakeNullable()).ShortDisplayName()));
     }
 
-    /// <summary>
-    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-    ///     any release. You should only use it directly in your code with extreme caution and knowing that
-    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-    /// </summary>
-    [EntityFrameworkInternal]
-    public static int GetHashCode(IEnumerable<TElement?> source, ValueComparer<TElement?> elementComparer)
+    private static int GetHashCode(IEnumerable<TElement?> source, ValueComparer<TElement?> elementComparer)
     {
         var hash = new HashCode();
 
@@ -121,14 +107,7 @@ public sealed class NullableValueTypeListComparer<TElement> : ValueComparer<IEnu
         return hash.ToHashCode();
     }
 
-    /// <summary>
-    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-    ///     any release. You should only use it directly in your code with extreme caution and knowing that
-    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-    /// </summary>
-    [EntityFrameworkInternal]
-    public static IList<TElement?> Snapshot(IEnumerable<TElement?> source, ValueComparer<TElement?> elementComparer)
+    private static IList<TElement?> Snapshot(IEnumerable<TElement?> source, ValueComparer<TElement?> elementComparer)
     {
         if (source is not IList<TElement?> sourceList)
         {
