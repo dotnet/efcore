8781dbe2df8e745a4b39f17fdfbfdf067e14f29d
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-18 19:27:36 +0100

    Don't throw for collection skip navigation without a setter
    Fixes #31740

--- src/EFCore/Metadata/Internal/PropertyBaseExtensions.cs
+++ src/EFCore/Metadata/Internal/PropertyBaseExtensions.cs
@@ -121,7 +121,8 @@ public static class PropertyBaseExtensions
         var setterProperty = propertyInfo?.FindSetterProperty();
         var getterProperty = propertyInfo?.FindGetterProperty();
 
-        var isCollectionNav = (propertyBase as IReadOnlyNavigation)?.IsCollection == true;
+        var isCollectionNav = propertyBase is ISkipNavigation
+            || (propertyBase as IReadOnlyNavigation)?.IsCollection == true;
         var hasField = fieldInfo != null;
         var hasSetter = setterProperty != null;
         var hasGetter = getterProperty != null;
