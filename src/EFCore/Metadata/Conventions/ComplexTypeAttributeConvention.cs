365cc057da5d7849e61fef66ccf8df0f817e2f56
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-20 10:21:02 +0100

    Discover ComplexTypeAttribute on nested complex types (#31808)

--- src/EFCore/Metadata/Conventions/ComplexTypeAttributeConvention.cs
+++ src/EFCore/Metadata/Conventions/ComplexTypeAttributeConvention.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
@@ -11,7 +12,8 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
 /// <remarks>
 ///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
 /// </remarks>
-public class ComplexTypeAttributeConvention : TypeAttributeConventionBase<ComplexTypeAttribute>
+public class ComplexTypeAttributeConvention : TypeAttributeConventionBase<ComplexTypeAttribute>,
+    IComplexPropertyAddedConvention
 {
     /// <summary>
     ///     Creates a new instance of <see cref="ComplexTypeAttributeConvention" />.
@@ -20,6 +22,28 @@ public class ComplexTypeAttributeConvention : TypeAttributeConventionBase<Comple
     public ComplexTypeAttributeConvention(ProviderConventionSetBuilderDependencies dependencies)
         : base(dependencies)
     {
+    }
+
+    /// <summary>
+    ///     Called after a complex property is added to a type-like object.
+    /// </summary>
+    /// <param name="propertyBuilder">The builder for the complex property.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    public override void ProcessComplexPropertyAdded(
+        IConventionComplexPropertyBuilder propertyBuilder,
+        IConventionContext<IConventionComplexPropertyBuilder> context)
+    {
+        var complexType = propertyBuilder.Metadata.ComplexType;
+        var memberTypes = complexType.GetRuntimeProperties().Values.Select(e => e.PropertyType)
+            .Concat(complexType.GetRuntimeFields().Values.Select(e => e.FieldType));
+
+        foreach (var memberType in memberTypes)
+        {
+            if (Attribute.IsDefined(memberType, typeof(ComplexTypeAttribute), inherit: true))
+            {
+                complexType.Builder.Metadata.Model.Builder.ComplexType(memberType);
+            }
+        }
     }
 
     /// <summary>
