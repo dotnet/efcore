51ae3837990797bc12a5abc57c6f94560c2c3f46
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-20 10:45:44 +0100

    Update based on review feedback

--- src/EFCore/Metadata/Internal/PropertyBaseExtensions.cs
+++ src/EFCore/Metadata/Internal/PropertyBaseExtensions.cs
@@ -121,8 +121,7 @@ public static class PropertyBaseExtensions
         var setterProperty = propertyInfo?.FindSetterProperty();
         var getterProperty = propertyInfo?.FindGetterProperty();
 
-        var isCollectionNav = propertyBase is ISkipNavigation
-            || (propertyBase as IReadOnlyNavigation)?.IsCollection == true;
+        var isCollectionNav = (propertyBase as IReadOnlyNavigationBase)?.IsCollection == true;
         var hasField = fieldInfo != null;
         var hasSetter = setterProperty != null;
         var hasGetter = getterProperty != null;
