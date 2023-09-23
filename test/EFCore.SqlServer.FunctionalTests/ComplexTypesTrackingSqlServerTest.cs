202d9006e927d96921ca9713888500033c9bc849
Arthur Vickers <ajcvickers@hotmail.com>
2023-09-23 11:09:57 +0100

    Tests for proxies with complex types (#31833)

--- test/EFCore.SqlServer.FunctionalTests/ComplexTypesTrackingSqlServerTest.cs
+++ test/EFCore.SqlServer.FunctionalTests/ComplexTypesTrackingSqlServerTest.cs
@@ -3,9 +3,119 @@
 
 namespace Microsoft.EntityFrameworkCore;
 
-public class ComplexTypesTrackingSqlServerTest : ComplexTypesTrackingTestBase<ComplexTypesTrackingSqlServerTest.SqlServerFixture>
+public class ComplexTypesTrackingSqlServerTest(
+        ComplexTypesTrackingSqlServerTest.SqlServerFixture fixture,
+        ITestOutputHelper testOutputHelper)
+    : ComplexTypesTrackingSqlServerTestBase<ComplexTypesTrackingSqlServerTest.SqlServerFixture>(fixture, testOutputHelper)
 {
-    public ComplexTypesTrackingSqlServerTest(SqlServerFixture fixture, ITestOutputHelper testOutputHelper)
+    public class SqlServerFixture : SqlServerFixtureBase
+    {
+        protected override string StoreName
+            => nameof(ComplexTypesTrackingSqlServerTest);
+    }
+}
+
+public class ComplexTypesTrackingProxiesSqlServerTest(
+        ComplexTypesTrackingProxiesSqlServerTest.SqlServerFixture fixture,
+        ITestOutputHelper testOutputHelper)
+    : ComplexTypesTrackingSqlServerTestBase<ComplexTypesTrackingProxiesSqlServerTest.SqlServerFixture>(fixture, testOutputHelper)
+{
+    // Fields can't be proxied
+    public override Task Can_track_entity_with_complex_objects_with_fields(EntityState state, bool async)
+        => Task.CompletedTask;
+
+    // Fields can't be proxied
+    public override void Can_mark_complex_type_properties_modified_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_read_original_values_for_properties_of_complex_types_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_write_original_values_for_properties_of_complex_types_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override Task Can_track_entity_with_complex_structs_with_fields(EntityState state, bool async)
+        => Task.CompletedTask;
+
+    // Fields can't be proxied
+    public override void Can_mark_complex_readonly_struct_properties_modified_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_read_original_values_for_properties_of_structs_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_write_original_values_for_properties_of_structs_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override Task Can_track_entity_with_complex_readonly_structs_with_fields(EntityState state, bool async)
+        => Task.CompletedTask;
+
+    // Fields can't be proxied
+    public override void Can_mark_complex_readonly_readonly_struct_properties_modified_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_read_original_values_for_properties_of_readonly_structs_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_write_original_values_for_properties_of_readonly_structs_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override Task Can_track_entity_with_complex_record_objects_with_fields(EntityState state, bool async)
+        => Task.CompletedTask;
+
+    // Fields can't be proxied
+    public override void Can_mark_complex_record_type_properties_modified_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_read_original_values_for_properties_of_record_complex_types_with_fields(bool trackFromQuery)
+    {
+    }
+
+    // Fields can't be proxied
+    public override void Can_write_original_values_for_properties_of_record_complex_types_with_fields(bool trackFromQuery)
+    {
+    }
+
+    public class SqlServerFixture : SqlServerFixtureBase
+    {
+        protected override string StoreName
+            => nameof(ComplexTypesTrackingProxiesSqlServerTest);
+
+        public override bool UseProxies
+            => true;
+
+        public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+            => base.AddOptions(builder.UseLazyLoadingProxies().UseChangeTrackingProxies());
+
+        protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
+            => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
+    }
+}
+
+public abstract class ComplexTypesTrackingSqlServerTestBase<TFixture> : ComplexTypesTrackingTestBase<TFixture>
+    where TFixture : ComplexTypesTrackingSqlServerTestBase<TFixture>.SqlServerFixtureBase, new()
+{
+    protected ComplexTypesTrackingSqlServerTestBase(TFixture fixture, ITestOutputHelper testOutputHelper)
         : base(fixture)
     {
         fixture.TestSqlLoggerFactory.Clear();
@@ -15,7 +125,7 @@ public class ComplexTypesTrackingSqlServerTest : ComplexTypesTrackingTestBase<Co
     protected override void UseTransaction(DatabaseFacade facade, IDbContextTransaction transaction)
         => facade.UseTransaction(transaction.GetDbTransaction());
 
-    public class SqlServerFixture : FixtureBase
+    public abstract class SqlServerFixtureBase : FixtureBase
     {
         protected override ITestStoreFactory TestStoreFactory
             => SqlServerTestStoreFactory.Instance;
