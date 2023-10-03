86d87ac38c2d5546ce5fc641b23572e9b5958c32
Arthur Vickers <ajcvickers@hotmail.com>
2023-10-03 12:07:27 +0100

    Make model ID API pubternal

--- src/EFCore/Metadata/IReadOnlyModel.cs
+++ src/EFCore/Metadata/IReadOnlyModel.cs
@@ -199,8 +199,12 @@ namespace Microsoft.EntityFrameworkCore.Metadata
         }
 
         /// <summary>
-        ///     A unique identifier for the <see cref="Model"/> instance and the <see cref="IRuntimeModel"/> created from it.
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
+        [EntityFrameworkInternal]
         public Guid ModelId { get; }
 
         /// <summary>
