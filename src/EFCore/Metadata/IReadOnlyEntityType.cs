a0ba1774938b48e1edb81137f71c0b959ad7b019
Arthur Vickers <ajcvickers@hotmail.com>
2023-10-04 15:52:27 +0100

    Make GetOrCreateMaterializer pubternal

--- src/EFCore/Metadata/IReadOnlyEntityType.cs
+++ src/EFCore/Metadata/IReadOnlyEntityType.cs
@@ -811,6 +811,7 @@ namespace Microsoft.EntityFrameworkCore.Metadata
         ///     any release. You should only use it directly in your code with extreme caution and knowing that
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
+        [EntityFrameworkInternal]
         Func<MaterializationContext, object> GetOrCreateMaterializer(IEntityMaterializerSource source);
 
         /// <summary>
