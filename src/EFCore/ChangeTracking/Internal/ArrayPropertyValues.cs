113214d7bd7d049550daab07f1167c3c331e19fa
wtgodbe <wigodbe@microsoft.com>
2023-10-18 16:36:23 -0700

    Revert "[release/6.0] Stop storing model references in the EntityMaterializerSource (#31943)"
    This reverts commit 7fb644dd78844266ebdc917b22f5fa36fa53c72e.

--- src/EFCore/ChangeTracking/Internal/ArrayPropertyValues.cs
+++ src/EFCore/ChangeTracking/Internal/ArrayPropertyValues.cs
@@ -42,7 +42,7 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public override object ToObject()
-            => EntityType.GetOrCreateMaterializer(MaterializerSource)(
+            => MaterializerSource.GetMaterializer(EntityType)(
                 new MaterializationContext(
                     new ValueBuffer(_values),
                     InternalEntry.StateManager.Context));
