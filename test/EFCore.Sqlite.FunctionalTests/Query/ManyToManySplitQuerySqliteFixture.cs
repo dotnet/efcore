08287b46fa0233c4fcac894636658ab47ff4836d
Arthur Vickers <ajcvickers@hotmail.com>
2023-11-22 18:37:26 +0000

    Handle shadow navigations when loading with split queries
    Fixes #32225

---
+++ test/EFCore.Sqlite.FunctionalTests/Query/ManyToManySplitQuerySqliteFixture.cs
@@ -0,0 +1,13 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.Query;
+
+public class ManyToManySplitQuerySqliteFixture : ManyToManyQuerySqliteFixture
+{
+    protected override string StoreName
+        => "ManyToManySplitQuery";
+
+    public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+        => base.AddOptions(builder.UseSqlite(b => b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+}
