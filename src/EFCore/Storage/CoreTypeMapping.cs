8e430fb05f9739e8bc3bd3acab413ce6f0f85683
Arthur Vickers <ajcvickers@hotmail.com>
2023-11-29 10:45:23 +0000

    Create correct JSON reader/writer for double nested value conversions (#32444)

--- src/EFCore/Storage/CoreTypeMapping.cs
+++ src/EFCore/Storage/CoreTypeMapping.cs
@@ -4,6 +4,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.EntityFrameworkCore.Storage.Json;
 
 namespace Microsoft.EntityFrameworkCore.Storage;
@@ -130,15 +131,30 @@ public abstract class CoreTypeMapping
                 ProviderValueComparer,
                 ValueGeneratorFactory,
                 elementMapping ?? ElementTypeMapping,
-                jsonValueReaderWriter
-                ?? (converter == null || JsonValueReaderWriter == null
-                    ? JsonValueReaderWriter
-                    : RuntimeFeature.IsDynamicCodeSupported
-                        ? (JsonValueReaderWriter)Activator.CreateInstance(
-                            typeof(JsonConvertedValueReaderWriter<,>).MakeGenericType(
-                                converter.ModelClrType, JsonValueReaderWriter.ValueType),
-                            JsonValueReaderWriter, converter)!
-                        : throw new InvalidOperationException(CoreStrings.NativeAotNoCompiledModel)));
+                jsonValueReaderWriter ?? CreateReaderWriter(converter, JsonValueReaderWriter));
+
+            static JsonValueReaderWriter? CreateReaderWriter(ValueConverter? converter, JsonValueReaderWriter? readerWriter)
+            {
+                if (converter == null || readerWriter == null)
+                {
+                    return readerWriter;
+                }
+
+                if (!RuntimeFeature.IsDynamicCodeSupported)
+                {
+                    throw new InvalidOperationException(CoreStrings.NativeAotNoCompiledModel);
+                }
+
+                if (readerWriter is IJsonConvertedValueReaderWriter convertedValueReaderWriter)
+                {
+                    readerWriter = convertedValueReaderWriter.InnerReaderWriter;
+                }
+
+                return (JsonValueReaderWriter)Activator.CreateInstance(
+                    typeof(JsonConvertedValueReaderWriter<,>).MakeGenericType(
+                        converter.ModelClrType, readerWriter.ValueType),
+                    readerWriter, converter)!;
+            }
         }
     }
 
