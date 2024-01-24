714249797389ffb28f65192515424942466e8589
Arthur Vickers <ajcvickers@hotmail.com>
2024-01-24 09:53:22 +0000

    Update Mono.TextTemplating to new preview package (#32895)
    Part of #32385

--- src/EFCore.Design/Scaffolding/Internal/TextTemplatingModelGenerator.cs
+++ src/EFCore.Design/Scaffolding/Internal/TextTemplatingModelGenerator.cs
@@ -107,7 +107,7 @@ public class TextTemplatingModelGenerator : TemplatedModelGenerator
         {
             host.TemplateFile = contextTemplate;
 
-            generatedCode = Engine.ProcessTemplate(File.ReadAllText(contextTemplate), host);
+            generatedCode = Engine.ProcessTemplateAsync(File.ReadAllText(contextTemplate), host).GetAwaiter().GetResult();;
             CheckEncoding(host.OutputEncoding);
             HandleErrors(host);
         }
@@ -165,7 +165,8 @@ public class TextTemplatingModelGenerator : TemplatedModelGenerator
 
                     if (compiledEntityTypeTemplate is null)
                     {
-                        compiledEntityTypeTemplate = Engine.CompileTemplate(File.ReadAllText(entityTypeTemplate), host);
+                        compiledEntityTypeTemplate = Engine.CompileTemplateAsync(File.ReadAllText(entityTypeTemplate), host, default)
+                            .GetAwaiter().GetResult();;
                         entityTypeExtension = host.Extension;
                         CheckEncoding(host.OutputEncoding);
                     }
@@ -208,7 +209,8 @@ public class TextTemplatingModelGenerator : TemplatedModelGenerator
 
                     if (compiledConfigurationTemplate is null)
                     {
-                        compiledConfigurationTemplate = Engine.CompileTemplate(File.ReadAllText(configurationTemplate), host);
+                        compiledConfigurationTemplate = Engine.CompileTemplateAsync(File.ReadAllText(configurationTemplate), host, default)
+                            .GetAwaiter().GetResult();;
                         configurationExtension = host.Extension;
                         CheckEncoding(host.OutputEncoding);
                     }
