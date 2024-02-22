717f15b4a142d565cd381288915eaeee92264949
Andriy Svyryd <Andriy.Svyryd@microsoft.com>
2024-02-22 10:46:24 -0800

    Update SDK to 9.0.0-preview1

--- test/Microsoft.Data.Sqlite.Tests/SqliteConnectionTest.cs
+++ test/Microsoft.Data.Sqlite.Tests/SqliteConnectionTest.cs
@@ -1205,11 +1205,11 @@ public class SqliteConnectionTest
     [InlineData("")]
     [InlineData(" ")]
     [InlineData("Unknown")]
-    public void GetSchema_throws_when_unknown_collection(string collectionName)
+    public void GetSchema_throws_when_unknown_collection(string? collectionName)
     {
         using var connection = new SqliteConnection("Data Source=:memory:");
 
-        var ex = Assert.Throws<ArgumentException>(() => connection.GetSchema(collectionName));
+        var ex = Assert.Throws<ArgumentException>(() => connection.GetSchema(collectionName!));
 
         Assert.Equal(Resources.UnknownCollection(collectionName), ex.Message);
     }
