7188d142f1a3df855e644b87029d8a45a8597a1e
Andriy Svyryd <Andriy.Svyryd@microsoft.com>
2024-05-14 16:21:22 -0700

    Correctly initialize design-time relational model for externally built models
    Fixes #33684

--- src/EFCore.Relational/Metadata/Internal/RelationalModel.cs
+++ src/EFCore.Relational/Metadata/Internal/RelationalModel.cs
@@ -1,10 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.Json;
-using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Microsoft.EntityFrameworkCore.Metadata.Internal;
 
