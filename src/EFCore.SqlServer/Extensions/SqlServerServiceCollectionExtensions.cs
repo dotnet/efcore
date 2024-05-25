ba47747dd7a7f25af9db4e298bddc6072af8838a
Weihan Li <weihanli@outlook.com>
2024-05-25 16:27:40 +0800

    Update SqlServerServiceCollectionExtensions.cs
    add AddSqlServer with IServiceProvider override

--- src/EFCore.SqlServer/Extensions/SqlServerServiceCollectionExtensions.cs
+++ src/EFCore.SqlServer/Extensions/SqlServerServiceCollectionExtensions.cs
@@ -59,6 +59,55 @@ public static class SqlServerServiceCollectionExtensions
         this IServiceCollection serviceCollection,
         string? connectionString,
         Action<SqlServerDbContextOptionsBuilder>? sqlServerOptionsAction = null,
+        Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction = null)
+        where TContext : DbContext
+        => serviceCollection.AddDbContext<TContext>(
+            (provider, options) =>
+            {
+                optionsAction?.Invoke(provider, options);
+                options.UseSqlServer(connectionString, sqlServerOptionsAction);
+            });
+    
+    /// <summary>
+    ///     Registers the given Entity Framework <see cref="DbContext" /> as a service in the <see cref="IServiceCollection" />
+    ///     and configures it to connect to a SQL Server database.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         This method is a shortcut for configuring a <see cref="DbContext" /> to use SQL Server. It does not support all options.
+    ///         Use <see cref="O:EntityFrameworkServiceCollectionExtensions.AddDbContext" /> and related methods for full control of
+    ///         this process.
+    ///     </para>
+    ///     <para>
+    ///         Use this method when using dependency injection in your application, such as with ASP.NET Core.
+    ///         For applications that don't use dependency injection, consider creating <see cref="DbContext" />
+    ///         instances directly with its constructor. The <see cref="DbContext.OnConfiguring" /> method can then be
+    ///         overridden to configure the SQL Server provider and connection string.
+    ///     </para>
+    ///     <para>
+    ///         To configure the <see cref="DbContextOptions{TContext}" /> for the context, either override the
+    ///         <see cref="DbContext.OnConfiguring" /> method in your derived context, or supply
+    ///         an optional action to configure the <see cref="DbContextOptions" /> for the context.
+    ///     </para>
+    ///     <para>
+    ///         See <see href="https://aka.ms/efcore-docs-di">Using DbContext with dependency injection</see> for more information and examples.
+    ///     </para>
+    ///     <para>
+    ///         See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+    ///         <see href="https://aka.ms/efcore-docs-sqlserver">Accessing SQL Server and Azure SQL databases with EF Core</see>
+    ///         for more information and examples.
+    ///     </para>
+    /// </remarks>
+    /// <typeparam name="TContext">The type of context to be registered.</typeparam>
+    /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+    /// <param name="connectionString">The connection string of the database to connect to.</param>
+    /// <param name="sqlServerOptionsAction">An optional action to allow additional SQL Server specific configuration.</param>
+    /// <param name="optionsAction">An optional action to configure the <see cref="DbContextOptions" /> for the context.</param>
+    /// <returns>The same service collection so that multiple calls can be chained.</returns>
+    public static IServiceCollection AddSqlServer<TContext>(
+        this IServiceCollection serviceCollection,
+        string? connectionString,
+        Action<SqlServerDbContextOptionsBuilder>? sqlServerOptionsAction = null,
         Action<DbContextOptionsBuilder>? optionsAction = null)
         where TContext : DbContext
         => serviceCollection.AddDbContext<TContext>(
