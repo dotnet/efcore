ca0dbac2605fc990a576d3efab20f1ef86e6c707
Andrea Canciani <ranma42@gmail.com>
2024-07-03 10:10:55 +0200

    Drop `ELSE NULL` from `CASE` expressions
    This makes the expression a little simpler and (especially) normalizes the ELSE
    sub-expression.

--- src/EFCore.Relational/Query/SqlNullabilityProcessor.cs
+++ src/EFCore.Relational/Query/SqlNullabilityProcessor.cs
@@ -573,6 +573,11 @@ public class SqlNullabilityProcessor
             return elseResult ?? _sqlExpressionFactory.Constant(null, caseExpression.Type, caseExpression.TypeMapping);
         }
 
+        if (IsNull(elseResult))
+        {
+            elseResult = null;
+        }
+
         return caseExpression.Update(operand, whenClauses, elseResult);
     }
 
