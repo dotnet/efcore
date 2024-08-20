7020187c047326aa946f98d1283ed484dbd53e48
koenigst <koenigst@users.noreply.github.com>
2024-08-20 15:47:42 +0200

    Associate the DatabaseRoot with the scoped options instance and not the singleton options. - Remove the DatabaseRoot from the InMemoryOptionsExtension service provider equality condition. - Remove the DatabaseRoot from the InMemorySingletonOptions. - Resolve the IInMemoryStore in a scoped service (IInMemoryStoreProvider) instead of a singleton service (IInMemoryStoreCache). - Fall back to the DatabaseRoot stored in a singleton service (IInMemoryDatabaseRootCache) if the options does not specify one.
    Fixes #34476

--- src/EFCore.InMemory/Storage/Internal/InMemoryStoreProvider.cs
+++ src/EFCore.InMemory/Storage/Internal/InMemoryStoreProvider.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Concurrent;
 using Microsoft.EntityFrameworkCore.InMemory.Infrastructure.Internal;
 
 namespace Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
@@ -12,10 +11,12 @@ namespace Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
 ///     any release. You should only use it directly in your code with extreme caution and knowing that
 ///     doing so can result in application failures when updating to a new Entity Framework Core release.
 /// </summary>
-public class InMemoryStoreCache : IInMemoryStoreCache
+public class InMemoryStoreProvider : IInMemoryStoreProvider
 {
+    private readonly IDbContextOptions _options;
+    private readonly IInMemoryDatabaseRootCache _databaseRootCache;
     private readonly IInMemoryTableFactory _tableFactory;
-    private readonly ConcurrentDictionary<string, IInMemoryStore> _namedStores;
+    private IInMemoryStore? _store;
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -23,24 +24,11 @@ public class InMemoryStoreCache : IInMemoryStoreCache
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
-    public InMemoryStoreCache(
-        IInMemoryTableFactory tableFactory,
-        IInMemorySingletonOptions? options)
+    public InMemoryStoreProvider(IDbContextOptions options, IInMemoryDatabaseRootCache databaseRootCache, IInMemoryTableFactory tableFactory)
     {
+        _options = options;
+        _databaseRootCache = databaseRootCache;
         _tableFactory = tableFactory;
-
-        if (options?.DatabaseRoot != null)
-        {
-            LazyInitializer.EnsureInitialized(
-                ref options.DatabaseRoot.Instance,
-                () => new ConcurrentDictionary<string, IInMemoryStore>());
-
-            _namedStores = (ConcurrentDictionary<string, IInMemoryStore>)options.DatabaseRoot.Instance;
-        }
-        else
-        {
-            _namedStores = new ConcurrentDictionary<string, IInMemoryStore>();
-        }
     }
 
     /// <summary>
@@ -49,6 +37,19 @@ public class InMemoryStoreCache : IInMemoryStoreCache
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
-    public virtual IInMemoryStore GetStore(string name)
-        => _namedStores.GetOrAdd(name, _ => new InMemoryStore(_tableFactory));
+    public IInMemoryStore Store
+        => LazyInitializer.EnsureInitialized(ref _store, InitializeStore);
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected virtual IInMemoryStore InitializeStore()
+    {
+        var extension = _options.Extensions.OfType<InMemoryOptionsExtension>().First();
+        var root = extension.DatabaseRoot ?? _databaseRootCache.SharedRoot;
+        return root.GetStore(extension.StoreName, _tableFactory);
+    }
 }
