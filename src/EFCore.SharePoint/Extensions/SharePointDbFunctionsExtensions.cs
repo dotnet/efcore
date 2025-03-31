4b072c10518c73cc5f56da3350516c51ac70d821
Yasin Asadnezhad <y.asadnezhad@smarterbusiness.at>
2025-03-31 16:52:11 +0200

    Created classes.

--- src/EFCore.SharePoint/Extensions/SharePointDbFunctionsExtensions.cs
+++ src/EFCore.SharePoint/Extensions/SharePointDbFunctionsExtensions.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.EntityFrameworkCore.SharePoint.Extensions
+
+namespace Microsoft.EntityFrameworkCore;
+
+internal class SharePointDbFunctionsExtensions
 {
-    internal class SharePointDbFunctionsExtensions
-    {
-    }
 }
