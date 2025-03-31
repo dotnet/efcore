dede6f25963b348c1836735045ef190470c88bbb
Yasin Asadnezhad <y.asadnezhad@smarterbusiness.at>
2025-03-31 15:54:53 +0200

    Created some classes.

---
+++ src/EFCore.SharePoint/Diagnostics/ConflictingValueGenerationStrategiesEventData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Diagnostics;
+
+namespace Microsoft.EntityFrameworkCore.SharePoint.Diagnostics;
+
+ 
+/// <summary>
+///     A <see cref="DiagnosticSource" /> event payload class for events that have
+///     a property.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-diagnostics">Logging, events, and diagnostics</see>, and
+///     for more information and examples.
+/// </remarks>
+public class ConflictingValueGenerationStrategiesEventData : EventData
+{
+    /// <summary>
+    ///     Constructs the event payload.
+    /// </summary>
+    /// <param name="eventDefinition">The event definition.</param>
+    /// <param name="messageGenerator">A delegate that generates a log message for this event.</param>
+    /// <param name="sharePointValueGenerationStrategy">The SQL Server value generation strategy.</param>
+    /// <param name="otherValueGenerationStrategy">The other value generation strategy.</param>
+    /// <param name="property">The property.</param>
+    public ConflictingValueGenerationStrategiesEventData(
+        EventDefinitionBase eventDefinition,
+        Func<EventDefinitionBase, EventData, string> messageGenerator,
+        SqlServerValueGenerationStrategy sharePointValueGenerationStrategy,
+        string otherValueGenerationStrategy,
+        IReadOnlyProperty property)
+        : base(eventDefinition, messageGenerator)
+    {
+        SqlServerValueGenerationStrategy = sharePointValueGenerationStrategy;
+        OtherValueGenerationStrategy = otherValueGenerationStrategy;
+        Property = property;
+    }
+
+    /// <summary>
+    ///     The SQL Server value generation strategy.
+    /// </summary>
+    public virtual SqlServerValueGenerationStrategy SqlServerValueGenerationStrategy { get; }
+
+    /// <summary>
+    ///     The other value generation strategy.
+    /// </summary>
+    public virtual string OtherValueGenerationStrategy { get; }
+
+    /// <summary>
+    ///     The property.
+    /// </summary>
+    public virtual IReadOnlyProperty Property { get; }
+}
---
+++ src/EFCore.SharePoint/Extensions/SharePointDbFunctionsExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.EntityFrameworkCore.SharePoint.Extensions
+{
+    internal class SharePointDbFunctionsExtensions
+    {
+    }
+}
