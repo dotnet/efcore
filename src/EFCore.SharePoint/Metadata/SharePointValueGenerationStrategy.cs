922a525852b803314ac44650dd9d849a14758ad9
Yasin Asadnezhad <y.asadnezhad@smarterbusiness.at>
2025-04-09 17:06:14 +0200

    Created some classes.

--- src/EFCore.SharePoint/Metadata/SharePointValueGenerationStrategy.cs
+++ src/EFCore.SharePoint/Metadata/SharePointValueGenerationStrategy.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 
-namespace Microsoft.EntityFrameworkCore.Infrastructure;
+namespace Microsoft.EntityFrameworkCore.Metadata;
 
-internal class SharePointDbContextOptionsBuilder
+internal class SharePointValueGenerationStrategy
 {
 }
