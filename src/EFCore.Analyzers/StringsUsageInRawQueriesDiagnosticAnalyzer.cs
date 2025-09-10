9a448705f479dce6fa9e7e3b212a7a6d0c5c50e7
Jiri Cincura ↹ <jiri@cincura.net>
2025-09-10 08:37:44 +0200

    Analyzer for string concatenation in raw SQL methods. (#36698)

--- src/EFCore.Analyzers/StringsUsageInRawQueriesDiagnosticAnalyzer.cs
+++ src/EFCore.Analyzers/StringsUsageInRawQueriesDiagnosticAnalyzer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -10,9 +11,9 @@ using Microsoft.CodeAnalysis.Operations;
 namespace Microsoft.EntityFrameworkCore;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
-public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : DiagnosticAnalyzer
+public sealed class StringsUsageInRawQueriesDiagnosticAnalyzer : DiagnosticAnalyzer
 {
-    private static readonly DiagnosticDescriptor Descriptor
+    private static readonly DiagnosticDescriptor InterpolatedStringDescriptor
         = new(
             EFDiagnostics.InterpolatedStringUsageInRawQueries,
             title: AnalyzerStrings.InterpolatedStringUsageInRawQueriesAnalyzerTitle,
@@ -21,8 +22,17 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
+    private static readonly DiagnosticDescriptor StringConcatenationDescriptor
+        = new(
+            EFDiagnostics.StringConcatenationUsageInRawQueries,
+            title: AnalyzerStrings.StringConcatenationUsageInRawQueriesAnalyzerTitle,
+            messageFormat: AnalyzerStrings.StringConcatenationUsageInRawQueriesMessageFormat,
+            category: "Security",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-        => [Descriptor];
+        => [InterpolatedStringDescriptor, StringConcatenationDescriptor];
 
     public override void Initialize(AnalysisContext context)
     {
@@ -37,19 +47,19 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
         var invocation = (IInvocationOperation)context.Operation;
         var targetMethod = invocation.TargetMethod;
 
-        var report = targetMethod.Name switch
+        var diagnosticDescriptor = targetMethod.Name switch
         {
             "FromSqlRaw" => AnalyzeFromSqlRawInvocation(invocation),
             "ExecuteSqlRaw" or "ExecuteSqlRawAsync" => AnalyzeExecuteSqlRawInvocation(invocation),
             "SqlQueryRaw" => AnalyzeSqlQueryRawInvocation(invocation),
-            _ => false
+            _ => null
         };
 
-        if (report)
+        if (diagnosticDescriptor is not null)
         {
             context.ReportDiagnostic(
                 Diagnostic.Create(
-                    Descriptor,
+                    diagnosticDescriptor,
                     GetTargetLocation(invocation.Syntax),
                     targetMethod.Name,
                     GetReplacementMethodName(targetMethod.Name)));
@@ -93,7 +103,7 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
             _ => oldName
         };
 
-    private static bool AnalyzeFromSqlRawInvocation(IInvocationOperation invocation)
+    private static DiagnosticDescriptor? AnalyzeFromSqlRawInvocation(IInvocationOperation invocation)
     {
         var targetMethod = invocation.TargetMethod;
         Debug.Assert(targetMethod.Name == "FromSqlRaw");
@@ -103,19 +113,13 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
 
         Debug.Assert(correctFromSqlRaw is not null, "Unable to find original `FromSqlRaw` method");
 
-        // Verify that the method is the one we analyze and its second argument, which corresponds to `string sql`, is an interpolated string
-        if (correctFromSqlRaw is null
-            || !targetMethod.ConstructedFrom.Equals(correctFromSqlRaw, SymbolEqualityComparer.Default)
-            || invocation.Arguments[1].Value is not IInterpolatedStringOperation interpolatedString)
-        {
-            return false;
-        }
-
-        // Report warning if interpolated string is not a constant and all its interpolations are not constants
-        return AnalyzeInterpolatedString(interpolatedString);
+        // Verify that the method is the one we analyze and its second argument, which corresponds to `string sql`, is...
+        return correctFromSqlRaw is not null && targetMethod.ConstructedFrom.Equals(correctFromSqlRaw, SymbolEqualityComparer.Default)
+            ? AnalyzeInvocation(invocation)
+            : null;
     }
 
-    private static bool AnalyzeExecuteSqlRawInvocation(IInvocationOperation invocation)
+    private static DiagnosticDescriptor? AnalyzeExecuteSqlRawInvocation(IInvocationOperation invocation)
     {
         var targetMethod = invocation.TargetMethod;
         Debug.Assert(targetMethod.Name is "ExecuteSqlRaw" or "ExecuteSqlRawAsync");
@@ -130,7 +134,7 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
 
             if (!correctMethods.Contains(targetMethod.ConstructedFrom, SymbolEqualityComparer.Default))
             {
-                return false;
+                return null;
             }
         }
         else
@@ -141,23 +145,17 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
 
             if (!correctMethods.Contains(targetMethod.ConstructedFrom, SymbolEqualityComparer.Default))
             {
-                return false;
+                return null;
             }
         }
 
         // At this point assume that the method is correct since both `ExecuteSqlRaw` and `ExecuteSqlRawAsync` have multiple overloads.
         // Checking for every possible one is too much work for almost no gain.
         // So check whether the second argument, that corresponds to `string sql` parameter, is an interpolated string...
-        if (invocation.Arguments[1].Value is not IInterpolatedStringOperation interpolatedString)
-        {
-            return false;
-        }
-
-        // ...and report warning if interpolated string is not a constant and all its interpolations are not constants
-        return AnalyzeInterpolatedString(interpolatedString);
+        return AnalyzeInvocation(invocation);
     }
 
-    private static bool AnalyzeSqlQueryRawInvocation(IInvocationOperation invocation)
+    private static DiagnosticDescriptor? AnalyzeSqlQueryRawInvocation(IInvocationOperation invocation)
     {
         var targetMethod = invocation.TargetMethod;
         Debug.Assert(targetMethod.Name == "SqlQueryRaw");
@@ -168,16 +166,29 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
 
         Debug.Assert(correctSqlQueryRaw is not null, "Unable to find original `SqlQueryRaw` method");
 
-        // Verify that the method is the one we analyze and its second argument, which corresponds to `string sql`, is an interpolated string
-        if (correctSqlQueryRaw is null
-            || !targetMethod.ConstructedFrom.Equals(correctSqlQueryRaw, SymbolEqualityComparer.Default)
-            || invocation.Arguments[1].Value is not IInterpolatedStringOperation interpolatedString)
+        // Verify that the method is the one we analyze and its second argument, which corresponds to `string sql`, is...
+        return correctSqlQueryRaw is not null && targetMethod.ConstructedFrom.Equals(correctSqlQueryRaw, SymbolEqualityComparer.Default)
+            ? AnalyzeInvocation(invocation)
+            : null;
+    }
+
+    private static DiagnosticDescriptor? AnalyzeInvocation(IInvocationOperation invocation)
+    {
+        // ...an interpolated string
+        if (invocation.Arguments[1].Value is IInterpolatedStringOperation interpolatedString
+            && AnalyzeInterpolatedString(interpolatedString))
         {
-            return false;
+            return InterpolatedStringDescriptor;
         }
 
-        // Report warning if interpolated string is not a constant and all its interpolations are not constants
-        return AnalyzeInterpolatedString(interpolatedString);
+        // ...a string concatenation
+        if (TryGetStringConcatenation(invocation.Arguments[1].Value, out var concatenation)
+            && AnalyzeConcatenation(concatenation))
+        {
+            return StringConcatenationDescriptor;
+        }
+
+        return null;
     }
 
     private static bool AnalyzeInterpolatedString(IInterpolatedStringOperation interpolatedString)
@@ -202,6 +213,36 @@ public sealed class InterpolatedStringUsageInRawQueriesDiagnosticAnalyzer : Diag
         }
 
         return false;
+    }
+
+    private static bool TryGetStringConcatenation(IOperation operation, [NotNullWhen(true)] out IBinaryOperation? concatenation)
+    {
+        if (operation is IBinaryOperation
+            {
+                OperatorKind: BinaryOperatorKind.Add,
+                Type.SpecialType: SpecialType.System_String,
+            } binaryOperation)
+        {
+            concatenation = binaryOperation;
+            return true;
+        }
+
+        concatenation = default;
+        return false;
+    }
+
+    private static bool AnalyzeConcatenation(IBinaryOperation operation)
+    {
+        var left = operation.LeftOperand;
+        var right = operation.RightOperand;
+
+        if ((left is IBinaryOperation leftBinary && AnalyzeConcatenation(leftBinary))
+            || (right is IBinaryOperation rightBinary && AnalyzeConcatenation(rightBinary)))
+        {
+            return true;
+        }
+
+        return !left.ConstantValue.HasValue || !right.ConstantValue.HasValue;
     }
 
     private static IMethodSymbol? FromSqlRawMethod(Compilation compilation)
---
+++ test/EFCore.Analyzers.Tests/StringInterpolationnRawQueriesAnalyzerTests.cs
@@ -0,0 +1,225 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore;
+
+using Microsoft.CodeAnalysis.Testing;
+
+using Verify = CSharpCodeFixVerifier<StringsUsageInRawQueriesDiagnosticAnalyzer, InterpolatedStringUsageInRawQueriesCodeFixProvider>;
+
+public class StringInterpolationnRawQueriesAnalyzerTests
+{
+    public static readonly TheoryData<string> DoNotReportData =
+    [
+        "db.Users.FromSqlRaw(",
+        "db.Database.ExecuteSqlRaw(",
+        "db.Database.ExecuteSqlRawAsync(",
+        "db.Database.SqlQueryRaw<int>(",
+        "RelationalQueryableExtensions.FromSqlRaw(db.Users, ",
+    ];
+
+    public static readonly TheoryData<string> ShouldReportData =
+    [
+        "db.Users.{|#0:FromSqlRaw|}(",
+        "db.Database.{|#0:ExecuteSqlRaw|}(",
+        "db.Database.{|#0:ExecuteSqlRawAsync|}(",
+        "db.Database.{|#0:SqlQueryRaw|}<int>(",
+        "RelationalQueryableExtensions.{|#0:FromSqlRaw|}(db.Users, ",
+    ];
+
+    public static readonly TheoryData<string, string> ShouldReportDataWithFix = new()
+    {
+        { "db.Users.{|#0:FromSqlRaw|}(", "db.Users.FromSql(" },
+        { "db.Database.{|#0:ExecuteSqlRaw|}(", "db.Database.ExecuteSql(" },
+        { "db.Database.{|#0:ExecuteSqlRawAsync|}(", "db.Database.ExecuteSqlAsync(" },
+        { "db.Database.{|#0:SqlQueryRaw|}<int>(", "db.Database.SqlQuery<int>(" },
+        { "RelationalQueryableExtensions.{|#0:FromSqlRaw|}(db.Users, ", "RelationalQueryableExtensions.FromSql(db.Users, " },
+    };
+
+    public static readonly TheoryData<string> CorrectCallsData =
+    [
+        "db.Users.FromSql(",
+        "db.Users.FromSqlInterpolated(",
+        "db.Database.ExecuteSql(",
+        "db.Database.ExecuteSqlInterpolated(",
+        "db.Database.ExecuteSqlAsync(",
+        "db.Database.ExecuteSqlInterpolatedAsync(",
+        "db.Database.SqlQuery<int>(",
+    ];
+
+    private const string MyDbContext =
+        """
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class MyDbContext : DbContext
+{
+    public DbSet<User> Users { get; init; }
+}
+""";
+
+    [Theory]
+    [MemberData(nameof(CorrectCallsData))]
+    public Task Correct_interpolation_do_not_report(string call)
+        => Verify.VerifyAnalyzerAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        {{call}}$"FooBar WHERE Id = {id}");
+    }
+}
+""");
+
+    [Theory]
+    [MemberData(nameof(DoNotReportData))]
+    public Task Constant_string_do_not_report(string call)
+        => Verify.VerifyAnalyzerAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db)
+    {
+        {{call}}"FooBar WHERE Id = 1");
+    }
+}
+""");
+
+    [Theory]
+    [MemberData(nameof(DoNotReportData))]
+    public Task Constant_string_with_parameters_do_not_report(string call)
+        => Verify.VerifyAnalyzerAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        {{call}}"FooBar WHERE Id = {0}", id);
+    }
+}
+""");
+
+    [Theory]
+    [MemberData(nameof(ShouldReportDataWithFix))]
+    public Task Interpolated_string_should_report(string call, string fixedCall)
+        => Verify.VerifyCodeFixAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        {{call}}$"FooBar WHERE Id = {id}");
+    }
+}
+""",
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        {{fixedCall}}$"FooBar WHERE Id = {id}");
+    }
+}
+""",
+            DiagnosticResult.CompilerWarning(EFDiagnostics.InterpolatedStringUsageInRawQueries).WithLocation(0));
+
+    [Theory]
+    [MemberData(nameof(ShouldReportData))]
+    public async Task Interpolated_string_with_other_parameters_should_report(string call)
+    {
+        var source = $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        {{call}}$"FooBar WHERE Id = {id}", id);
+    }
+}
+""";
+        await Verify.VerifyCodeFixAsync(source, source,
+            DiagnosticResult.CompilerWarning(EFDiagnostics.InterpolatedStringUsageInRawQueries).WithLocation(0));
+    }
+
+    [Theory]
+    [MemberData(nameof(DoNotReportData))]
+    public Task Constant_interpolated_string_do_not_report(string call)
+        => Verify.VerifyAnalyzerAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db)
+    {
+        const string id = "1";
+        {{call}}$"FooBar WHERE Id = {id}");
+    }
+}
+""");
+
+    [Theory]
+    [MemberData(nameof(DoNotReportData))]
+    public Task Pseudo_constant_interpolated_string_do_not_report(string call)
+        => Verify.VerifyAnalyzerAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db)
+    {
+        {{call}}$"FooBar WHERE Id = {1}");
+    }
+}
+""");
+
+    [Fact]
+    public Task Fix_all()
+        => Verify.VerifyCodeFixAsync(
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        db.Users.{|#0:FromSqlRaw|}($"FooBar WHERE Id = {id}");
+        db.Users.{|#1:FromSqlRaw|}($"FooBar WHERE Id = {id}");
+    }
+}
+""",
+            $$"""
+{{MyDbContext}}
+
+class C
+{
+    void M(MyDbContext db, int id)
+    {
+        db.Users.FromSql($"FooBar WHERE Id = {id}");
+        db.Users.FromSql($"FooBar WHERE Id = {id}");
+    }
+}
+""",
+            DiagnosticResult.CompilerWarning(EFDiagnostics.InterpolatedStringUsageInRawQueries).WithLocation(0),
+            DiagnosticResult.CompilerWarning(EFDiagnostics.InterpolatedStringUsageInRawQueries).WithLocation(1));
+}
