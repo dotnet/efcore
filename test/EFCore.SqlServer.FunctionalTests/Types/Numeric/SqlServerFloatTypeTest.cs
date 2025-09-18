64da066bcf751948b4e24e32d938fa26604160a4
Shay Rojansky <roji@roji.org>
2025-09-18 11:19:41 +0200

    Improve type tests

---
+++ test/EFCore.SqlServer.FunctionalTests/Types/Numeric/SqlServerFloatTypeTest.cs
@@ -0,0 +1,215 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.Types.Numeric;
+
+public class SqlServerFloatTypeTest(SqlServerFloatTypeTest.FloatTypeFixture fixture, ITestOutputHelper testOutputHelper)
+    : RelationalTypeTestBase<float, SqlServerFloatTypeTest.FloatTypeFixture>(fixture, testOutputHelper)
+{
+    public override async Task Equality_in_query_with_parameter()
+    {
+        await base.Equality_in_query_with_parameter();
+
+        AssertSql(
+            """
+@Fixture_Value='30.5'
+
+SELECT TOP(2) [t].[Id], [t].[OtherValue], [t].[Value]
+FROM [TypeEntity] AS [t]
+WHERE [t].[Value] = @Fixture_Value
+""");
+    }
+
+    public override async Task Equality_in_query_with_constant()
+    {
+        await base.Equality_in_query_with_constant();
+
+        AssertSql(
+            """
+SELECT TOP(2) [t].[Id], [t].[OtherValue], [t].[Value]
+FROM [TypeEntity] AS [t]
+WHERE [t].[Value] = CAST(30.5 AS real)
+""");
+    }
+
+    public override async Task SaveChanges()
+    {
+        await base.SaveChanges();
+
+        AssertSql(
+            """
+@p1='1'
+@p0='30'
+
+SET IMPLICIT_TRANSACTIONS OFF;
+SET NOCOUNT ON;
+UPDATE [TypeEntity] SET [Value] = @p0
+OUTPUT 1
+WHERE [Id] = @p1;
+""");
+    }
+
+    #region JSON
+
+    public override async Task Query_property_within_json()
+    {
+        await base.Query_property_within_json();
+
+        if (Fixture.UsingJsonType)
+        {
+            AssertSql(
+                """
+@Fixture_Value='30.5'
+
+SELECT TOP(2) [j].[Id], [j].[OtherValue], [j].[Value], [j].[JsonContainer]
+FROM [JsonTypeEntity] AS [j]
+WHERE JSON_VALUE([j].[JsonContainer], '$.Value' RETURNING real) = @Fixture_Value
+""");
+        }
+        else
+        {
+            AssertSql(
+                """
+@Fixture_Value='30.5'
+
+SELECT TOP(2) [j].[Id], [j].[OtherValue], [j].[Value], [j].[JsonContainer]
+FROM [JsonTypeEntity] AS [j]
+WHERE CAST(JSON_VALUE([j].[JsonContainer], '$.Value') AS real) = @Fixture_Value
+""");
+        }
+    }
+
+    public override async Task SaveChanges_within_json()
+    {
+        await base.SaveChanges_within_json();
+
+        AssertSql(
+            """
+@p0='{"OtherValue":30,"Value":30}' (Nullable = false) (Size = 28)
+@p1='1'
+
+SET IMPLICIT_TRANSACTIONS OFF;
+SET NOCOUNT ON;
+UPDATE [JsonTypeEntity] SET [JsonContainer] = @p0
+OUTPUT 1
+WHERE [Id] = @p1;
+""");
+    }
+
+    public override async Task ExecuteUpdate_within_json_to_parameter()
+    {
+        await base.ExecuteUpdate_within_json_to_parameter();
+
+        if (Fixture.UsingJsonType)
+        {
+            AssertSql(
+                """
+@Fixture_OtherValue='30'
+
+UPDATE [j]
+SET [JsonContainer].modify('$.Value', @Fixture_OtherValue)
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+        else
+        {
+            AssertSql(
+                """
+@Fixture_OtherValue='30'
+
+UPDATE [j]
+SET [j].[JsonContainer] = JSON_MODIFY([j].[JsonContainer], '$.Value', @Fixture_OtherValue)
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+    }
+
+    public override async Task ExecuteUpdate_within_json_to_constant()
+    {
+        await base.ExecuteUpdate_within_json_to_constant();
+
+        if (Fixture.UsingJsonType)
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [JsonContainer].modify('$.Value', CAST(30 AS real))
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+        else
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [j].[JsonContainer] = JSON_MODIFY([j].[JsonContainer], '$.Value', CAST(30 AS real))
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+    }
+
+    public override async Task ExecuteUpdate_within_json_to_another_json_property()
+    {
+        await base.ExecuteUpdate_within_json_to_another_json_property();
+
+        if (Fixture.UsingJsonType)
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [JsonContainer].modify('$.Value', JSON_VALUE([j].[JsonContainer], '$.OtherValue' RETURNING real))
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+        else
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [j].[JsonContainer] = JSON_MODIFY([j].[JsonContainer], '$.Value', CAST(JSON_VALUE([j].[JsonContainer], '$.OtherValue') AS real))
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+    }
+
+    [SqlServerCondition(SqlServerCondition.SupportsFunctions2022)]
+    public override async Task ExecuteUpdate_within_json_to_nonjson_column()
+    {
+        // TODO: Currently failing on Helix only, see #36746
+        if (Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT") is not null)
+        {
+            return;
+        }
+
+        await base.ExecuteUpdate_within_json_to_nonjson_column();
+
+        if (Fixture.UsingJsonType)
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [JsonContainer].modify('$.Value', [j].[OtherValue])
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+        else
+        {
+            AssertSql(
+                """
+UPDATE [j]
+SET [j].[JsonContainer] = JSON_MODIFY([j].[JsonContainer], '$.Value', [j].[OtherValue])
+FROM [JsonTypeEntity] AS [j]
+""");
+        }
+    }
+
+    #endregion JSON
+
+    public class FloatTypeFixture : SqlServerTypeFixture<float>
+    {
+        public override float Value { get; } = 30.5f;
+        public override float OtherValue { get; } = 30f;
+
+        protected override ITestStoreFactory TestStoreFactory => SqlServerTestStoreFactory.Instance;
+    }
+}
