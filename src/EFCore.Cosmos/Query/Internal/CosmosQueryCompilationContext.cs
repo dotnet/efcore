017ffc73719b4f1304dea9296ef6ac516cb820a1
JoasE <32096708+JoasE@users.noreply.github.com>
2025-11-01 12:19:08 +0100

    Cleanup

--- src/EFCore.Cosmos/Query/Internal/CosmosQueryCompilationContext.cs
+++ src/EFCore.Cosmos/Query/Internal/CosmosQueryCompilationContext.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.EntityFrameworkCore.Cosmos.Storage;
-
 namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal;
 
 /// <summary>
@@ -11,18 +9,9 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Query.Internal;
 ///     any release. You should only use it directly in your code with extreme caution and knowing that
 ///     doing so can result in application failures when updating to a new Entity Framework Core release.
 /// </summary>
-public class CosmosQueryCompilationContext : QueryCompilationContext
+public class CosmosQueryCompilationContext(QueryCompilationContextDependencies dependencies, bool async)
+    : QueryCompilationContext(dependencies, async)
 {
-    /// <summary>
-    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-    ///     any release. You should only use it directly in your code with extreme caution and knowing that
-    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-    /// </summary>
-    public CosmosQueryCompilationContext(QueryCompilationContextDependencies dependencies, bool async) : base(dependencies, async)
-    {
-    }
-
     /// <summary>
     ///     The root entity type being queried.
     /// </summary>
--- src/EFCore.Cosmos/Query/Internal/CosmosQueryCompilationContextFactory.cs
+++ src/EFCore.Cosmos/Query/Internal/CosmosQueryCompilationContextFactory.cs
@@ -18,15 +18,10 @@ public class CosmosQueryCompilationContextFactory : IQueryCompilationContextFact
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
     public CosmosQueryCompilationContextFactory(QueryCompilationContextDependencies dependencies)
-    {
-        Dependencies = dependencies;
-    }
+        => Dependencies = dependencies;
 
     /// <summary>
-    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-    ///     any release. You should only use it directly in your code with extreme caution and knowing that
-    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    ///     Dependencies for this service.
     /// </summary>
     protected virtual QueryCompilationContextDependencies Dependencies { get; }
 
