edd875f4b5902f2df1ccb01c7e2b4649566cf098
Ladislav Bitto <ladislav.bitto@microsoft.com>
2025-11-20 12:40:10 -0800

    updating comments

--- src/EFCore/Extensions/EntityFrameworkQueryableExtensions.cs
+++ src/EFCore/Extensions/EntityFrameworkQueryableExtensions.cs
@@ -2710,7 +2710,8 @@ public static class EntityFrameworkQueryableExtensions
                 Expression.Call(
                     instance: null,
                     method: IgnoreNamedQueryFiltersMethodInfo.MakeGenericMethod(typeof(TEntity)),
-                    // converting the collection to an array if it isn't already one to ensure consistent caching
+                    // converting the collection to an array if it isn't already one to ensure consistent caching. Fixes #37112.
+                    // #37212 may be a possible future solution providing broader capabilities around parameterizing collections.
                     arguments: [source.Expression, Expression.Constant(filterKeys is string[] ? filterKeys : filterKeys.ToArray())]))
             : source;
 
