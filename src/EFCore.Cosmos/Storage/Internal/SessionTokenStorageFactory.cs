c4eeae1497d548149568e254cab09f75b362c286
JoasE <32096708+JoasE@users.noreply.github.com>
2025-11-23 15:53:53 +0100

    Move back to private fields for _defaultContainerName and _mode inSessionTokenStorageFactory

--- src/EFCore.Cosmos/Storage/Internal/SessionTokenStorageFactory.cs
+++ src/EFCore.Cosmos/Storage/Internal/SessionTokenStorageFactory.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.EntityFrameworkCore.Cosmos.Infrastructure;
 using Microsoft.EntityFrameworkCore.Cosmos.Infrastructure.Internal;
 using Microsoft.EntityFrameworkCore.Cosmos.Metadata.Internal;
 
@@ -14,6 +15,8 @@ namespace Microsoft.EntityFrameworkCore.Cosmos.Storage.Internal;
 /// </summary>
 public class SessionTokenStorageFactory : ISessionTokenStorageFactory
 {
+    private SessionTokenManagementMode? _mode;
+    private string? _defaultContainerName;
     private HashSet<string>? _containerNames;
 
     /// <summary>
@@ -23,22 +26,14 @@ public class SessionTokenStorageFactory : ISessionTokenStorageFactory
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
     public ISessionTokenStorage Create(DbContext dbContext)
-    {
-        var options = dbContext.GetService<IDbContextOptions>().FindExtension<CosmosOptionsExtension>();
-        Debug.Assert(options != null, "CosmosOptionsExtension is not found");
+        => new SessionTokenStorage(
+            _defaultContainerName ??= (string)dbContext.Model.GetAnnotation(CosmosAnnotationNames.ContainerName).Value!,
+            _containerNames ??= [.. GetContainerNames(dbContext.Model)],
+            _mode ??= dbContext.GetService<IDbContextOptions>().FindExtension<CosmosOptionsExtension>()!.SessionTokenManagementMode);
 
-        var defaultContainerName = (string)dbContext.Model.GetAnnotation(CosmosAnnotationNames.ContainerName).Value!;
-        _containerNames ??= new HashSet<string>(GetContainerNames(dbContext.Model, defaultContainerName));
-
-        return new SessionTokenStorage(
-            defaultContainerName,
-            _containerNames,
-            options.SessionTokenManagementMode);
-    }
-
-    private static IEnumerable<string> GetContainerNames(IModel model, string defaultContainerName)
-        => new[] { defaultContainerName }.Concat(model.GetEntityTypes()
+    private static IEnumerable<string> GetContainerNames(IModel model)
+        => model.GetEntityTypes()
             .Where(et => et.FindPrimaryKey() != null)
             .Select(et => et.GetContainer())
-            .Where(container => container != null))!;
+            .Where(container => container != null)!;
 }
