0bbc02494b7cacda0d9084560ddb09e6c2f1e06e
Shay Rojansky <roji@roji.org>
2025-11-27 10:27:29 +0100

    Apply suggestion from @roji

--- src/EFCore.Relational/Query/StructuralTypeProjectionExpression.cs
+++ src/EFCore.Relational/Query/StructuralTypeProjectionExpression.cs
@@ -22,7 +22,6 @@ public class StructuralTypeProjectionExpression : Expression
 
     private static readonly bool UseOldBehavior37205 =
         AppContext.TryGetSwitch("Microsoft.EntityFrameworkCore.Issue37205", out var enabled) && enabled;
-
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
     ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
