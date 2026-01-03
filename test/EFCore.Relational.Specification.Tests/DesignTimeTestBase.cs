72356482fd2ead1b9e574102b85b529161ae266f
Tanzim Hossain Romel <romel.rcs@gmail.com>
2026-01-03 11:53:14 +0600

    Add runtime migration tests to DesignTimeTestBase
    Add tests for scaffolding, compiling, registering, and applying
    migrations at runtime. These tests run for all providers that
    implement DesignTimeTestBase (SQLite and SQL Server).

--- test/EFCore.Relational.Specification.Tests/DesignTimeTestBase.cs
+++ test/EFCore.Relational.Specification.Tests/DesignTimeTestBase.cs
@@ -49,6 +49,144 @@ public abstract class DesignTimeTestBase<TFixture>(TFixture fixture) : IClassFix
         Assert.NotNull(migrationsScaffolder);
     }
 
+    [ConditionalFact]
+    public void Can_scaffold_migration()
+    {
+        using var context = Fixture.CreateContext();
+        using var services = CreateDesignTimeServices(context);
+        using var scope = services.CreateScope();
+
+        var scaffolder = scope.ServiceProvider.GetRequiredService<IMigrationsScaffolder>();
+
+        var migration = scaffolder.ScaffoldMigration(
+            "TestMigration",
+            rootNamespace: "TestNamespace",
+            subNamespace: null,
+            language: "C#",
+            dryRun: true);
+
+        Assert.NotNull(migration);
+        Assert.Contains("TestMigration", migration.MigrationId);
+        Assert.NotEmpty(migration.MigrationCode);
+        Assert.NotEmpty(migration.MetadataCode);
+        Assert.NotEmpty(migration.SnapshotCode);
+    }
+
+    [ConditionalFact]
+    public void Can_compile_migration()
+    {
+        using var context = Fixture.CreateContext();
+        using var services = CreateDesignTimeServices(context);
+        using var scope = services.CreateScope();
+
+        var scaffolder = scope.ServiceProvider.GetRequiredService<IMigrationsScaffolder>();
+        var compiler = scope.ServiceProvider.GetRequiredService<IMigrationCompiler>();
+
+        var migration = scaffolder.ScaffoldMigration(
+            "CompiledMigration",
+            rootNamespace: "TestNamespace",
+            subNamespace: null,
+            language: "C#",
+            dryRun: true);
+
+        var compiledAssembly = compiler.CompileMigration(migration, context.GetType());
+
+        Assert.NotNull(compiledAssembly);
+
+        var migrationType = compiledAssembly.GetTypes()
+            .FirstOrDefault(t => typeof(Migration).IsAssignableFrom(t) && !t.IsAbstract);
+        Assert.NotNull(migrationType);
+    }
+
+    [ConditionalFact]
+    public void Can_register_and_apply_compiled_migration()
+    {
+        using var context = Fixture.CreateContext();
+        using var services = CreateDesignTimeServices(context);
+        using var scope = services.CreateScope();
+
+        var scaffolder = scope.ServiceProvider.GetRequiredService<IMigrationsScaffolder>();
+        var compiler = scope.ServiceProvider.GetRequiredService<IMigrationCompiler>();
+        var migrationsAssembly = scope.ServiceProvider.GetRequiredService<IMigrationsAssembly>();
+        var migrator = scope.ServiceProvider.GetRequiredService<IMigrator>();
+
+        var migration = scaffolder.ScaffoldMigration(
+            "AppliedMigration",
+            rootNamespace: "TestNamespace",
+            subNamespace: null,
+            language: "C#",
+            dryRun: true);
+
+        var compiledAssembly = compiler.CompileMigration(migration, context.GetType());
+        migrationsAssembly.AddMigrations(compiledAssembly);
+
+        Assert.Contains(migration.MigrationId, migrationsAssembly.Migrations.Keys);
+
+        migrator.Migrate(migration.MigrationId);
+
+        var appliedMigrations = context.Database.GetAppliedMigrations().ToList();
+        Assert.Contains(migration.MigrationId, appliedMigrations);
+    }
+
+    [ConditionalFact]
+    public void Compiled_migration_generates_valid_sql()
+    {
+        using var context = Fixture.CreateContext();
+        using var services = CreateDesignTimeServices(context);
+        using var scope = services.CreateScope();
+
+        var scaffolder = scope.ServiceProvider.GetRequiredService<IMigrationsScaffolder>();
+        var compiler = scope.ServiceProvider.GetRequiredService<IMigrationCompiler>();
+        var migrationsAssembly = scope.ServiceProvider.GetRequiredService<IMigrationsAssembly>();
+        var sqlGenerator = scope.ServiceProvider.GetRequiredService<IMigrationsSqlGenerator>();
+
+        var migration = scaffolder.ScaffoldMigration(
+            "SqlGenMigration",
+            rootNamespace: "TestNamespace",
+            subNamespace: null,
+            language: "C#",
+            dryRun: true);
+
+        var compiledAssembly = compiler.CompileMigration(migration, context.GetType());
+        migrationsAssembly.AddMigrations(compiledAssembly);
+
+        var migrationTypeInfo = migrationsAssembly.Migrations[migration.MigrationId];
+        var migrationInstance = migrationsAssembly.CreateMigration(
+            migrationTypeInfo,
+            context.Database.ProviderName);
+
+        var commands = sqlGenerator.Generate(
+            migrationInstance.UpOperations,
+            context.Model).ToList();
+
+        Assert.NotEmpty(commands);
+    }
+
+    [ConditionalFact]
+    public void HasPendingModelChanges_returns_true_for_new_model()
+    {
+        using var context = Fixture.CreateContext();
+        using var services = CreateDesignTimeServices(context);
+        using var scope = services.CreateScope();
+
+        var migrator = scope.ServiceProvider.GetRequiredService<IMigrator>();
+
+        Assert.True(migrator.HasPendingModelChanges());
+    }
+
+    protected ServiceProvider CreateDesignTimeServices(DbContext context)
+    {
+        var serviceCollection = new ServiceCollection()
+            .AddEntityFrameworkDesignTimeServices()
+            .AddDbContextDesignTimeServices(context);
+        ((IDesignTimeServices)Activator.CreateInstance(
+                ProviderAssembly.GetType(
+                    ProviderAssembly.GetCustomAttribute<DesignTimeProviderServicesAttribute>().TypeName,
+                    throwOnError: true))!)
+            .ConfigureDesignTimeServices(serviceCollection);
+        return serviceCollection.BuildServiceProvider(validateScopes: true);
+    }
+
     public abstract class DesignTimeFixtureBase : SharedStoreFixtureBase<PoolableDbContext>
     {
         protected override string StoreName
