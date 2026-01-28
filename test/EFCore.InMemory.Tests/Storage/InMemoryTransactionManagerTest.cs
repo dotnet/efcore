087b1994d414f00512361406c678c7f0e90b83a6
Shady Nagy <info@shadynagy.com>
2026-01-29 02:28:48 +0400

    Update InMemoryTransactionManagerTest.cs fix using

--- test/EFCore.InMemory.Tests/Storage/InMemoryTransactionManagerTest.cs
+++ test/EFCore.InMemory.Tests/Storage/InMemoryTransactionManagerTest.cs
@@ -1,12 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Data;
 using System.Transactions;
 using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
 using Microsoft.EntityFrameworkCore.InMemory.Diagnostics.Internal;
 using Microsoft.EntityFrameworkCore.InMemory.Internal;
 using Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
+using IsolationLevel = System.Data.IsolationLevel;
 
 // ReSharper disable InconsistentNaming
 namespace Microsoft.EntityFrameworkCore.Storage;
