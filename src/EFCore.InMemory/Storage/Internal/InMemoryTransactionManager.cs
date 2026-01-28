f96dfa3e6ae43e7ca12263d90dfaf7937ec0711e
Shady Nagy <info@shadynagy.com>
2026-01-29 02:27:34 +0400

    Update InMemoryTransactionManager.cs for using

--- src/EFCore.InMemory/Storage/Internal/InMemoryTransactionManager.cs
+++ src/EFCore.InMemory/Storage/Internal/InMemoryTransactionManager.cs
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Data;
 using System.Transactions;
 using Microsoft.EntityFrameworkCore.InMemory.Internal;
+using IsolationLevel = System.Data.IsolationLevel;
 
 namespace Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
 
