67beccb8d660470f37632820947abe8ffcd15241
Andriy Svyryd <Andriy.Svyryd@microsoft.com>
2026-03-04 14:49:14 -0800

    Revert "Fix complex property JSON column not marked nullable in TPH hierarchy (#37781)"
    This reverts commit 6a7fa6163f38cd2c55984a03c6019c3f37486048.

--- test/EFCore.SqlServer.FunctionalTests/Scaffolding/Baselines/ComplexTypes/DbContextModelBuilder.cs
+++ test/EFCore.SqlServer.FunctionalTests/Scaffolding/Baselines/ComplexTypes/DbContextModelBuilder.cs
@@ -406,10 +406,7 @@ namespace TestNamespace
             var flagsEnum2Column = new Column("FlagsEnum2", "int", principalBaseTable);
             principalBaseTable.Columns.Add("FlagsEnum2", flagsEnum2Column);
             flagsEnum2Column.Accessors = ColumnAccessorsFactory.CreateGeneric<int>(flagsEnum2Column);
-            var manyOwnedColumn = new JsonColumn("ManyOwned", "nvarchar(max)", principalBaseTable)
-            {
-                IsNullable = true
-            };
+            var manyOwnedColumn = new JsonColumn("ManyOwned", "nvarchar(max)", principalBaseTable);
             principalBaseTable.Columns.Add("ManyOwned", manyOwnedColumn);
             manyOwnedColumn.Accessors = ColumnAccessorsFactory.CreateGeneric<JsonTypePlaceholder>(manyOwnedColumn);
             var owned_NumberColumn = new Column("Owned_Number", "int", principalBaseTable);
