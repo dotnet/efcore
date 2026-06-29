8695695e1607d1f67e6ddc82e25aab1184853bda
Wassim K <wassim.khalil9@gmail.com>
2026-06-29 14:19:39 +1000

    Reuse a correlated single-result subquery across projected members (#7776)
    A projection that reads two or more members of one correlated First/Single/
    Last/ElementAt subquery now lifts to a single SelectMany during navigation
    expansion, so the members come from a single join instead of a duplicated
    subquery per member.

--- test/EFCore.SqlServer.FunctionalTests/Query/NorthwindSelectQuerySqlServerTest.cs
+++ test/EFCore.SqlServer.FunctionalTests/Query/NorthwindSelectQuerySqlServerTest.cs
@@ -2911,6 +2911,61 @@ ORDER BY [c0].[CustomerID]
 """);
     }
 
+    public override async Task Multiple_members_of_correlated_single_result_subquery_lift_to_single_join(bool async, string method)
+    {
+        await base.Multiple_members_of_correlated_single_result_subquery_lift_to_single_join(async, method);
+
+        AssertSql(
+            method switch
+            {
+                nameof(Queryable.First) or
+                nameof(Queryable.FirstOrDefault) or
+                nameof(Queryable.Single) or
+                nameof(Queryable.SingleOrDefault) => """
+SELECT [o].[OrderID], [c1].[City], [c1].[Country], [c1].[ContactName]
+FROM [Orders] AS [o]
+LEFT JOIN (
+    SELECT [c0].[City], [c0].[ContactName], [c0].[Country], [c0].[CustomerID0]
+    FROM (
+        SELECT [c].[City], [c].[ContactName], [c].[Country], [c].[CustomerID] AS [CustomerID0], ROW_NUMBER() OVER(PARTITION BY [c].[CustomerID] ORDER BY [c].[CustomerID]) AS [row]
+        FROM [Customers] AS [c]
+    ) AS [c0]
+    WHERE [c0].[row] <= 1
+) AS [c1] ON [o].[CustomerID] = [c1].[CustomerID0]
+WHERE [o].[CustomerID] IS NOT NULL
+""",
+                nameof(Queryable.Last) or
+                nameof(Queryable.LastOrDefault) => """
+SELECT [o].[OrderID], [c1].[City], [c1].[Country], [c1].[ContactName]
+FROM [Orders] AS [o]
+LEFT JOIN (
+    SELECT [c0].[City], [c0].[ContactName], [c0].[Country], [c0].[CustomerID0]
+    FROM (
+        SELECT [c].[City], [c].[ContactName], [c].[Country], [c].[CustomerID] AS [CustomerID0], ROW_NUMBER() OVER(PARTITION BY [c].[CustomerID] ORDER BY [c].[CustomerID] DESC) AS [row]
+        FROM [Customers] AS [c]
+    ) AS [c0]
+    WHERE [c0].[row] <= 1
+) AS [c1] ON [o].[CustomerID] = [c1].[CustomerID0]
+WHERE [o].[CustomerID] IS NOT NULL
+""",
+                nameof(Queryable.ElementAt) or
+                nameof(Queryable.ElementAtOrDefault) => """
+SELECT [o].[OrderID], [c1].[City], [c1].[Country], [c1].[ContactName]
+FROM [Orders] AS [o]
+LEFT JOIN (
+    SELECT [c0].[City], [c0].[ContactName], [c0].[Country], [c0].[CustomerID0]
+    FROM (
+        SELECT [c].[City], [c].[ContactName], [c].[Country], [c].[CustomerID] AS [CustomerID0], ROW_NUMBER() OVER(PARTITION BY [c].[CustomerID] ORDER BY [c].[CustomerID]) AS [row]
+        FROM [Customers] AS [c]
+    ) AS [c0]
+    WHERE 0 < [c0].[row] AND [c0].[row] <= 1
+) AS [c1] ON [o].[CustomerID] = [c1].[CustomerID0]
+WHERE [o].[CustomerID] IS NOT NULL
+""",
+                _ => throw new InvalidOperationException(method)
+            });
+    }
+
     private void AssertSql(params string[] expected)
         => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
 
