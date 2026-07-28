ff15f8624fa11330a130fbc9acbabf026a58bb5c
Will Godbe <willgodbe@Wills-MacBook-Pro.local>
2026-07-28 15:57:52 -0700

    Revert "Fix union model validator tests for new union shape"
    This reverts commit 26b60585f53ba192eb79cc5152f1702975cf467b.

--- test/EFCore.Tests/Infrastructure/ModelValidatorTest.cs
+++ test/EFCore.Tests/Infrastructure/ModelValidatorTest.cs
@@ -1219,17 +1219,11 @@ public partial class ModelValidatorTest : ModelValidatorTestBase
     {
         var modelBuilder = CreateConventionModelBuilder();
 
-        var exception = Record.Exception(() => modelBuilder.Entity(typeof(UnionEntity)));
-        if (exception is null)
-        {
-            VerifyError(
-                CoreStrings.UnionTypeNotSupported(nameof(UnionEntity)),
-                modelBuilder);
-        }
-        else
-        {
-            Assert.Equal(CoreStrings.InvalidEntityType(typeof(UnionEntity)), Assert.IsType<ArgumentException>(exception).Message);
-        }
+        modelBuilder.Entity(typeof(UnionEntity));
+
+        VerifyError(
+            CoreStrings.UnionTypeNotSupported(nameof(UnionEntity)),
+            modelBuilder);
     }
 
     [Fact]
@@ -1239,22 +1233,10 @@ public partial class ModelValidatorTest : ModelValidatorTestBase
 
         modelBuilder.Entity<WithUnionComplexProperty>().ComplexProperty(e => e.Union);
 
-        var expectedUnionError = CoreStrings.UnionTypeNotSupported(
-            nameof(WithUnionComplexProperty) + "." + nameof(WithUnionComplexProperty.Union) + "#" + nameof(UnionEntity));
-        var message = Assert.Throws<InvalidOperationException>(() => Validate(modelBuilder)).Message;
-
-        if (message != expectedUnionError)
-        {
-            var typeName = nameof(WithUnionComplexProperty) + "." + nameof(WithUnionComplexProperty.Union) + "#" + nameof(UnionEntity);
-            var expectedConstructorError = CoreStrings.ConstructorNotFound(
-                typeName,
-                Environment.NewLine
-                + "    "
-                + CoreStrings.ConstructorBindingFailed("value", typeName + "(int value)")
-                + Environment.NewLine);
-
-            Assert.Equal(expectedConstructorError, message);
-        }
+        VerifyError(
+            CoreStrings.UnionTypeNotSupported(
+                nameof(WithUnionComplexProperty) + "." + nameof(WithUnionComplexProperty.Union) + "#" + nameof(UnionEntity)),
+            modelBuilder);
     }
 
     protected union UnionEntity(int);
