957cb20e6295ff3b6215c005b5e3359ac2b38240
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2026-09-10 02:07:40 +0530

    Fix projecting JSON complex collection together with collection navigation (#38932)
    Return index-based ProjectionBindingExpression for JSON complex collections when in index-based binding mode in RelationalProjectionBindingExpressionVisitor
    
    Fixes #38928

--- test/EFCore.Sqlite.FunctionalTests/Query/ComplexTypeQuerySqliteTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/Query/ComplexTypeQuerySqliteTest.cs
@@ -1454,6 +1454,35 @@ INNER JOIN "HookMetadata" AS "h0" ON "h"."Id" = "h0"."HookId"
 """);
     }
 
+    public override async Task Project_json_complex_collection_together_with_collection_navigation()
+    {
+        await base.Project_json_complex_collection_together_with_collection_navigation();
+
+        AssertSql(
+            """
+            SELECT "p"."Id", "p"."Items"
+            FROM "Parent" AS "p"
+            ORDER BY "p"."Id"
+            LIMIT 2
+            """,
+            //
+            """
+            SELECT "s"."Id", "p3"."Id"
+            FROM (
+                SELECT "p"."Id"
+                FROM "Parent" AS "p"
+                ORDER BY "p"."Id"
+                LIMIT 1
+            ) AS "p3"
+            INNER JOIN (
+                SELECT "l1"."Id", "p2"."ParentsId"
+                FROM "ParentLinks" AS "p2"
+                INNER JOIN "Link" AS "l1" ON "p2"."LinksId" = "l1"."Id"
+            ) AS "s" ON "p3"."Id" = "s"."ParentsId"
+            ORDER BY "p3"."Id"
+            """);
+    }
+
     #endregion Non-shared test resources
 
     [Fact]
